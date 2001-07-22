3355bf4fc82a1386d310ef4156db577d88f83ff1
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-07-22 18:48:57 +0000

    oops, it needs the 3.1
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ast-optimizer-branch@44244 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "version.h"
 
-const char *const version_string = "ast-optimizer-branch (experimental)";
+const char *const version_string = "3.1 (ast-optimizer-branch)";
