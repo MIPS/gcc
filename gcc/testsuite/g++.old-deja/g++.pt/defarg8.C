d0a08a24919e6da0ccebb35d8f757a7fb9a4d263
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-10-07 13:39:13 +0000

    Adjust XFAIL location
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cp-parser-branch@46059 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.pt/defarg8.C
+++ gcc/testsuite/g++.old-deja/g++.pt/defarg8.C
@@ -8,7 +8,10 @@ template <class T, class U> class foo2;
 
 struct bar {
   template <class T, class U>
-  bar(int i = foo1<T>::baz, // ok
-      int j = int(foo2<T, U>::baz), // ok
-      int k = foo2<T, U>::baz) {} // gets bogus error - before > - XFAIL *-*-*
+  bar(int i = foo1<T>::baz); // ok
+  template <class T, class U>
+  int baz (int j = int(foo2<T, U>::baz)); // ok
+  // this might not be an error -- need definitive ruling
+  template <class T, class U>
+  int boz (int k = foo2<T, U>::baz); // gets bogus error - - XFAIL *-*-*
 };
