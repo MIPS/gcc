f5713eb81369ef98e7475c4e5f89c40a3170e547
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-01-10 21:57:47 +0000

    Fix merger lossage.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cfg-branch@48756 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gcse.c
+++ gcc/gcse.c
@@ -3926,10 +3926,7 @@ try_replace_reg (from, to, insn)
     }
 
   /* If we've failed to do replacement, have a single SET, and don't already
-     have a note, add a REG_EQUAL note to not lose information.
-
-     The copy is required, as simplify_replace_rtx does not copy the
-     parts that has not been touched.  */
+     have a note, add a REG_EQUAL note to not lose information.  */
   if (!success && note == 0 && set != 0)
     note = set_unique_reg_note (insn, REG_EQUAL, copy_rtx (src));
 
