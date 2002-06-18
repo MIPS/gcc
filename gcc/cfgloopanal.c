98208caf860029caa1ff30ed4785c28ee5864774
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-06-18 16:00:39 +0000

    * cfgloopanal.c (simple_condition_p, simple_increment, 	simple_loop_exit_p, simple_loop_p, variable_initial_value): 	Modified to use bitmaps of invariant registers. 	(invariant_in_blocks_p): Removed. 	(unmark_altered, blocks_invariant_registers, unmark_altered_insn, 	blocks_single_set_registers, invariant_rtx_wrto_regs_p, 	invariant_rtx_wrto_regs_p_helper): New static 	functions.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cfg-branch@54745 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfgloopanal.c
+++ gcc/cfgloopanal.c
@@ -10,6 +10,7 @@
 #include "loop.h"
 #include "expr.h"
 
+struct unmark_altered_insn_data;
 typedef int (*loop_function)	 PARAMS ((rtx insn, void *data));
 static int cfg_for_each_insn_in_loop PARAMS ((struct loop *, loop_function, void *));
 static int mark_def_varies	 PARAMS ((rtx insn, void *data));
@@ -18,22 +19,26 @@ static void note_mem_store	 PARAMS ((rtx, rtx, void *));
 static int discover_invariant	 PARAMS ((rtx insn, void *data));
 static int not_invariant_rtx	 PARAMS ((rtx *rtxp, void *data));
 static bool mark_maybe_invariant_set PARAMS ((rtx, rtx, rtx, struct loop_invariants *));
-static bool invariant_in_blocks_p PARAMS ((rtx, basic_block *, int));
+static void unmark_altered	 PARAMS ((rtx, rtx, regset));
+static void blocks_invariant_registers PARAMS ((basic_block *, int, regset));
+static void unmark_altered_insn	 PARAMS ((rtx, rtx, struct unmark_altered_insn_data *));
+static void blocks_single_set_registers PARAMS ((basic_block *, int, rtx *));
+static int invariant_rtx_wrto_regs_p_helper PARAMS ((rtx *, regset));
+static bool invariant_rtx_wrto_regs_p PARAMS ((rtx, regset));
 static rtx test_for_iteration PARAMS ((struct loop_desc *desc,
 				       unsigned HOST_WIDE_INT));
 static bool constant_iterations PARAMS ((struct loop_desc *,
 					 unsigned HOST_WIDE_INT *,
 					 bool *));
 static bool simple_loop_exit_p PARAMS ((struct loops *, struct loop *,
-					edge, basic_block *,
+					edge, regset, rtx *,
 					struct loop_desc *));
-static rtx variable_initial_value PARAMS ((rtx, basic_block, rtx, rtx *));
+static rtx variable_initial_value PARAMS ((rtx, regset, rtx, rtx *));
 static rtx variable_initial_values PARAMS ((edge, rtx));
-static bool simple_condition_p PARAMS ((struct loop *, basic_block *,
-					rtx, struct loop_desc *));
+static bool simple_condition_p PARAMS ((struct loop *, rtx,
+					regset, struct loop_desc *));
 static basic_block simple_increment PARAMS ((struct loops *, struct loop *,
-					     basic_block *,
-					     struct loop_desc *));
+					     rtx *, struct loop_desc *));
 
 static int
 cfg_for_each_insn_in_loop (loop, fun, data)
@@ -579,34 +584,167 @@ num_loop_insns (loop)
   return ninsns;
 }
 
-/* Checks whether EXPR is invariant in basic blocks BBS.  */
-static bool
-invariant_in_blocks_p (expr, bbs, nbbs)
-    rtx expr;
-    basic_block *bbs;
-    int nbbs;
+/* Unmarks modified registers; helper to blocks_invariant_registers.  */
+static void
+unmark_altered (what, by, regs)
+     rtx what;
+     rtx by ATTRIBUTE_UNUSED;
+     regset regs;
 {
+  if (GET_CODE (what) == SUBREG)
+    what = SUBREG_REG (what);
+  if (!REG_P (what))
+    return;
+  CLEAR_REGNO_REG_SET (regs, REGNO (what));
+}
+
+/* Marks registers that are invariant inside blocks BBS.  */
+static void
+blocks_invariant_registers (bbs, nbbs, regs)
+     basic_block *bbs;
+     int nbbs;
+     regset regs;
+{
+  rtx insn;
   int i;
 
+  for (i = 0; i < max_reg_num (); i++)
+    SET_REGNO_REG_SET (regs, i);
   for (i = 0; i < nbbs; i++)
-    if (modified_between_p (expr, bbs[i]->head, NEXT_INSN (bbs[i]->end)))
-      return false;
+    for (insn = bbs[i]->head;
+	 insn != NEXT_INSN (bbs[i]->end);
+	 insn = NEXT_INSN (insn))
+      if (INSN_P (insn))
+	note_stores (PATTERN (insn),
+		     (void (*) PARAMS ((rtx, rtx, void *))) unmark_altered,
+		     regs);
+}
 
-  return true;
+/* Unmarks modified registers; helper to blocks_single_set_registers.  */
+struct unmark_altered_insn_data
+{
+  rtx *regs;
+  rtx insn;
+};
+
+static void
+unmark_altered_insn (what, by, data)
+     rtx what;
+     rtx by ATTRIBUTE_UNUSED;
+     struct unmark_altered_insn_data *data;
+{
+  int rn;
+
+  if (GET_CODE (what) == SUBREG)
+    what = SUBREG_REG (what);
+  if (!REG_P (what))
+    return;
+  rn = REGNO (what);
+  if (data->regs[rn] == data->insn)
+    return;
+  data->regs[rn] = NULL;
+}
+
+/* Marks registers that have just single simple set in BBS; the relevant
+   insn is returned in REGS.  */
+static void
+blocks_single_set_registers (bbs, nbbs, regs)
+     basic_block *bbs;
+     int nbbs;
+     rtx *regs;
+{
+  rtx insn;
+  int i;
+  struct unmark_altered_insn_data data;
+
+  for (i = 0; i < max_reg_num (); i++)
+    regs[i] = NULL;
+
+  for (i = 0; i < nbbs; i++)
+    for (insn = bbs[i]->head;
+	 insn != NEXT_INSN (bbs[i]->end);
+	 insn = NEXT_INSN (insn))
+      {
+	rtx set = single_set (insn);
+	if (!set)
+	  continue;
+	if (!REG_P (SET_DEST (set)))
+	  continue;
+	regs[REGNO (SET_DEST (set))] = insn;
+      }
+
+  data.regs = regs;
+  for (i = 0; i < nbbs; i++)
+    for (insn = bbs[i]->head;
+	 insn != NEXT_INSN (bbs[i]->end);
+	 insn = NEXT_INSN (insn))
+      {
+        if (!INSN_P (insn))
+	  continue;
+	data.insn = insn;
+	note_stores (PATTERN (insn),
+	    (void (*) PARAMS ((rtx, rtx, void *))) unmark_altered_insn,
+	    &data);
+      }
+}
+
+/* Helper for invariant_rtx_wrto_regs_p.  */
+static int
+invariant_rtx_wrto_regs_p_helper (expr, invariant_regs)
+     rtx *expr;
+     regset invariant_regs;
+{
+  switch (GET_CODE (*expr))
+    {
+    case CC0:
+    case PC:
+    case UNSPEC_VOLATILE:
+      return 1;
+
+    case CONST_INT:
+    case CONST_DOUBLE:
+    case CONST:
+    case SYMBOL_REF:
+    case LABEL_REF:
+      return 0;
+
+    case ASM_OPERANDS:
+      return MEM_VOLATILE_P (*expr);
+
+    case MEM:
+      /* If the memory is not constant, assume it is modified.  If it is
+	 constant, we still have to check the address.  */
+      return !RTX_UNCHANGING_P (*expr);
+
+    case REG:
+      return !REGNO_REG_SET_P (invariant_regs, REGNO (*expr));
+
+    default:
+      return 0;
+    }
+}
+
+/* Checks that EXPR is invariant provided that INVARIANT_REGS are invariant. */
+static bool
+invariant_rtx_wrto_regs_p (expr, invariant_regs)
+     rtx expr;
+     regset invariant_regs;
+{
+  return !for_each_rtx (&expr, (rtx_function) invariant_rtx_wrto_regs_p_helper,
+			invariant_regs);
 }
 
 /* Checks whether CONDITION is a simple comparison in that one of operands
    is register and the other one is invariant in the LOOP. Fills var, lim
    and cond fields in DESC.  */
 static bool
-simple_condition_p (loop, body, condition, desc)
-     struct loop *loop;
-     basic_block *body;
+simple_condition_p (loop, condition, invariant_regs, desc)
+     struct loop *loop ATTRIBUTE_UNUSED;
      rtx condition;
+     regset invariant_regs;
      struct loop_desc *desc;
 {
   rtx op0, op1;
-  edge e = loop_preheader_edge (loop);
 
   /* Check condition.  */
   switch (GET_CODE (condition))
@@ -636,15 +774,13 @@ simple_condition_p (loop, body, condition, desc)
   op1 = XEXP (condition, 1);
   
   /* One of operands must be invariant.  */
-  if (invariant_in_blocks_p (op0, body, loop->num_nodes))
+  if (invariant_rtx_wrto_regs_p (op0, invariant_regs))
     {
       /* And the other one must be a register.  */
       if (!REG_P (op1))
 	return false;
       desc->var = op1;
       desc->lim = op0;
-
-      desc->lim_alts = variable_initial_values (e, op0);
 
       desc->cond = swap_condition (GET_CODE (condition));
       if (desc->cond == UNKNOWN)
@@ -653,15 +789,13 @@ simple_condition_p (loop, body, condition, desc)
     }
 
   /* Check the other operand. */
-  if (!invariant_in_blocks_p (op1, body, loop->num_nodes))
+  if (!invariant_rtx_wrto_regs_p (op1, invariant_regs))
     return false;
   if (!REG_P (op0))
     return false;
 
   desc->var = op0;
   desc->lim = op1;
-
-  desc->lim_alts = variable_initial_values (e, op1);
 
   desc->cond = GET_CODE (condition);
 
@@ -672,36 +806,20 @@ simple_condition_p (loop, body, condition, desc)
    iteration.  Fills in DESC->stride and returns block in that DESC->var is
    modified.  */
 static basic_block
-simple_increment (loops, loop, body, desc)
+simple_increment (loops, loop, simple_increment_regs, desc)
      struct loops *loops;
      struct loop *loop;
-     basic_block *body;
+     rtx *simple_increment_regs;
      struct loop_desc *desc;
 {
-  rtx mod_insn, insn, set, set_src, set_add;
-  basic_block mod_bb = NULL;
-  int i;
+  rtx mod_insn, set, set_src, set_add;
+  basic_block mod_bb;
 
   /* Find insn that modifies var.  */
-  mod_insn = NULL;
-  for (i = 0; i < loop->num_nodes; i++)
-    if (modified_between_p
-	(desc->var, body[i]->head, NEXT_INSN (body[i]->end)))
-      {
-	for (insn = NEXT_INSN (body[i]->head);
-	     insn != NEXT_INSN (body[i]->end); insn = NEXT_INSN (insn))
-	  if (modified_between_p
-	      (desc->var, PREV_INSN (insn), NEXT_INSN (insn)))
-	    {
-	      if (mod_insn)
-		return NULL;
-	      else
-		mod_insn = insn;
-	    }
-	mod_bb = body[i];
-      }
+  mod_insn = simple_increment_regs[REGNO (desc->var)];
   if (!mod_insn)
     return NULL;
+  mod_bb = BLOCK_FOR_INSN (mod_insn);
 
   /* Check that it is executed exactly once each iteration.  */
   if (!just_once_each_iteration_p (loops, loop, mod_bb))
@@ -710,9 +828,9 @@ simple_increment (loops, loop, body, desc)
   /* mod_insn must be a simple increment/decrement.  */
   set = single_set (mod_insn);
   if (!set)
-    return NULL;
+    abort ();
   if (!rtx_equal_p (SET_DEST (set), desc->var))
-    return NULL;
+    abort ();
 
   set_src = SET_SRC (set);
   if (GET_CODE (set_src) != PLUS)
@@ -730,13 +848,13 @@ simple_increment (loops, loop, body, desc)
   return mod_bb;
 }
 
-/* Tries to find initial value of VAR in INSN.  This value must be valid in
-   END_BB too.  If SET_INSN is not NULL, insn in that var is set is placed
-   here.  */
+/* Tries to find initial value of VAR in INSN.  This value must be invariant
+   wrto INVARIANT_REGS.  If SET_INSN is not NULL, insn in that var is set is
+   placed here.  */
 static rtx
-variable_initial_value (insn, end_bb, var, set_insn)
+variable_initial_value (insn, invariant_regs, var, set_insn)
      rtx insn;
-     basic_block end_bb;
+     regset invariant_regs;
      rtx var;
      rtx *set_insn;
 {
@@ -748,14 +866,19 @@ variable_initial_value (insn, end_bb, var, set_insn)
   while (1)
     {
       for (; insn != bb->head; insn = PREV_INSN (insn))
-	if (modified_between_p (var, PREV_INSN (insn), NEXT_INSN (insn)))
-	  break;
+	{
+	  if (modified_between_p (var, PREV_INSN (insn), NEXT_INSN (insn)))
+	    break;
+	  if (INSN_P (insn))
+	    note_stores (PATTERN (insn),
+		(void (*) PARAMS ((rtx, rtx, void *))) unmark_altered,
+		invariant_regs);
+	}
 
       if (insn != bb->head)
 	{
 	  /* We found place where var is set.  */
 	  rtx set_dest;
-	  basic_block b;
 	  rtx val;
 	  rtx note;
           
@@ -771,16 +894,14 @@ variable_initial_value (insn, end_bb, var, set_insn)
 	    val = XEXP (note, 0);
 	  else
 	    val = SET_SRC (set);
-	  if (modified_between_p (val, insn, NEXT_INSN (bb->end)))
+	  if (!invariant_rtx_wrto_regs_p (val, invariant_regs))
 	    return NULL;
-	  for (b = end_bb; b != bb; b = b->pred->src)
-	    if (modified_between_p (val, b->head, NEXT_INSN (b->end)))
-	      return NULL;
 
 	  if (set_insn)
 	    *set_insn = insn;
 	  return val;
 	}
+
 
       if (bb->pred->pred_next || bb->pred->src == ENTRY_BLOCK_PTR)
 	return NULL;
@@ -799,6 +920,13 @@ variable_initial_values (e, var)
      rtx var;
 {
   rtx set_insn, list;
+  regset invariant_regs;
+  regset_head invariant_regs_head;
+  int i;
+
+  invariant_regs = INITIALIZE_REG_SET (invariant_regs_head);
+  for (i = 0; i < max_reg_num (); i++)
+    SET_REGNO_REG_SET (invariant_regs, i);
 
   list = alloc_EXPR_LIST (0, copy_rtx (var), NULL);
 
@@ -807,9 +935,10 @@ variable_initial_values (e, var)
 
   set_insn = e->src->end;
   while (REG_P (var)
-	 && (var = variable_initial_value (set_insn, e->src, var, &set_insn)))
+	 && (var = variable_initial_value (set_insn, invariant_regs, var, &set_insn)))
     list = alloc_EXPR_LIST (0, copy_rtx (var), list);
 
+  FREE_REG_SET (invariant_regs);
   return list;
 }
 
@@ -1037,14 +1166,16 @@ test_for_iteration (desc, iter)
 
 
 /* Tests whether exit at EXIT_EDGE from LOOP is simple.  Returns simple loop
-   description joined to it in in DESC.  */
+   description joined to it in in DESC.  INVARIANT_REGS and SINGLE_SET_REGS
+   are results of blocks_{invariant,single_set}_regs over BODY.  */
 static bool
-simple_loop_exit_p (loops, loop, exit_edge, body, desc)
+simple_loop_exit_p (loops, loop, exit_edge, invariant_regs, single_set_regs, desc)
      struct loops *loops;
      struct loop *loop;
      edge exit_edge;
-     basic_block *body;
      struct loop_desc *desc;
+     regset invariant_regs;
+     rtx *single_set_regs;
 {
   basic_block mod_bb, exit_bb;
   int fallthru_out;
@@ -1078,21 +1209,22 @@ simple_loop_exit_p (loops, loop, exit_edge, body, desc)
   if (!(condition = get_condition (exit_bb->end, NULL)))
     return false;
 
-  if (!simple_condition_p (loop, body, condition, desc))
+  if (!simple_condition_p (loop, condition, invariant_regs, desc))
     return false;
 
   /*  Var must be simply incremented or decremented in exactly one insn that
      is executed just once every iteration.  */
-  if (!(mod_bb = simple_increment (loops, loop, body, desc)))
+  if (!(mod_bb = simple_increment (loops, loop, single_set_regs, desc)))
     return false;
 
   /* OK, it is simple loop.  Now just fill in remaining info.  */
   desc->postincr = !dominated_by_p (loops->cfg.dom, exit_bb, mod_bb);
   desc->neg = !fallthru_out;
 
-  /* Find initial value of var.  */
+  /* Find initial value of var and alternative values for lim.  */
   e = loop_preheader_edge (loop);
   desc->var_alts = variable_initial_values (e, desc->var);
+  desc->lim_alts = variable_initial_values (e, desc->lim);
 
   /* Number of iterations. */
   if (!count_loop_iterations (desc, NULL, NULL))
@@ -1115,14 +1247,24 @@ simple_loop_p (loops, loop, desc)
   edge e;
   struct loop_desc act;
   bool any = false;
+  regset invariant_regs;
+  regset_head invariant_regs_head;
+  rtx *single_set_regs;
   
   body = get_loop_body (loop);
+
+  invariant_regs = INITIALIZE_REG_SET (invariant_regs_head);
+  single_set_regs = xmalloc (max_reg_num () * sizeof (rtx));
+
+  blocks_invariant_registers (body, loop->num_nodes, invariant_regs);
+  blocks_single_set_registers (body, loop->num_nodes, single_set_regs);
 
   for (i = 0; i < loop->num_nodes; i++)
     {
       for (e = body[i]->succ; e; e = e->succ_next)
 	if (!flow_bb_inside_loop_p (loop, e->dest)
-	    && simple_loop_exit_p (loops, loop, e, body, &act))
+	    && simple_loop_exit_p (loops, loop, e,
+		   invariant_regs, single_set_regs, &act))
 	  {
 	    /* Prefer constant iterations; the less the better.  */
 	    if (!any)
@@ -1169,6 +1311,8 @@ simple_loop_p (loops, loop, desc)
     }
 
   free (body);
+  FREE_REG_SET (invariant_regs);
+  free (single_set_regs);
   return any;
 }
 
