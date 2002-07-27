05f0dee80577caf0c2778883ccf4ec6a15367ea8
cgf <cgf@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-27 20:02:34 +0000

    Merge from 3.2.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cygwin-mingw-gcc-3_2-branch@55800 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.2.12.66 $
+--                            $Revision: 1.2.12.65.2.1 $
 --                                                                          --
 --          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
 --                                                                          --
@@ -38,11 +38,7 @@
 
 package Gnatvsn is
 
-<<<<<<< gnatvsn.ads
-   Gnat_Version_String : constant String := "3.1.1 20020727 (prerelease)";
-=======
    Gnat_Version_String : constant String := "3.2 20020727 (prerelease)";
->>>>>>> 1.2.12.66
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
