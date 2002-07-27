39312d234a710dc8d0586a3e9a8c2409c9d041bb
cgf <cgf@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-07-27 16:58:10 +0000

    Merge from 3.1.1.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cygwin-mingw-gcc-3_1-branch@55797 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.1.1 20020727 (prerelease)";
+const char *const ffe_version_string = "3.2 (prerelease) 2002-07-26";
