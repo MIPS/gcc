d0f1f4abeae19430c02b17ef239bc73ccb5d483a
sayle <sayle@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-13 13:40:45 +0000

    * stmt.c (struct nexting): Remove unused alt_end_label field. 	(expand_start_loop): Delete initialization of alt_end_label. 	(expand_start_null_loop): Likewise. 	(expand_exit_loop_if_false): Delete updating of alt_end_label.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_4-basic-improvements-branch@57106 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/stmt.c
+++ gcc/stmt.c
@@ -166,9 +166,6 @@ struct nesting GTY(())
 	  rtx start_label;
 	  /* Label at the end of the whole construct.  */
 	  rtx end_label;
-	  /* Label before a jump that branches to the end of the whole
-	     construct.  This is where destructors go if any.  */
-	  rtx alt_end_label;
 	  /* Label for `continue' statement to jump to;
 	     this is in front of the stepper of the loop.  */
 	  rtx continue_label;
@@ -2420,7 +2417,6 @@ expand_start_loop (exit_flag)
   thisloop->depth = ++nesting_depth;
   thisloop->data.loop.start_label = gen_label_rtx ();
   thisloop->data.loop.end_label = gen_label_rtx ();
-  thisloop->data.loop.alt_end_label = 0;
   thisloop->data.loop.continue_label = thisloop->data.loop.start_label;
   thisloop->exit_label = exit_flag ? thisloop->data.loop.end_label : 0;
   loop_stack = thisloop;
@@ -2462,7 +2458,6 @@ expand_start_null_loop ()
   thisloop->depth = ++nesting_depth;
   thisloop->data.loop.start_label = emit_note (NULL, NOTE_INSN_DELETED);
   thisloop->data.loop.end_label = gen_label_rtx ();
-  thisloop->data.loop.alt_end_label = NULL_RTX;
   thisloop->data.loop.continue_label = thisloop->data.loop.end_label;
   thisloop->exit_label = thisloop->data.loop.end_label;
   loop_stack = thisloop;
@@ -2698,7 +2693,6 @@ expand_exit_loop_if_false (whichloop, cond)
      tree cond;
 {
   rtx label = gen_label_rtx ();
-  rtx last_insn;
   clear_last_expr ();
 
   if (whichloop == 0)
@@ -2710,9 +2704,6 @@ expand_exit_loop_if_false (whichloop, cond)
      necessary, they go before the unconditional branch.  */
 
   do_jump (cond, NULL_RTX, label);
-  last_insn = get_last_insn ();
-  if (GET_CODE (last_insn) == CODE_LABEL)
-    whichloop->data.loop.alt_end_label = last_insn;
   expand_goto_internal (NULL_TREE, whichloop->data.loop.end_label,
 			NULL_RTX);
   emit_label (label);
