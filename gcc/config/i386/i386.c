74ad58f5fe4583c40e6d97dca47322b4f1119207
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-01 01:12:28 +0000

    Avoid inlining of constant blocks with large size.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/rtlopt-branch@58700 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -10280,7 +10280,6 @@ ix86_expand_movstr (dst, src, count_exp, align_exp)
   unsigned HOST_WIDE_INT count = 0;
   rtx insns;
 
-  start_sequence ();
 
   if (GET_CODE (align_exp) == CONST_INT)
     align = INTVAL (align_exp);
@@ -10290,7 +10289,11 @@ ix86_expand_movstr (dst, src, count_exp, align_exp)
     align = 64;
 
   if (GET_CODE (count_exp) == CONST_INT)
-    count = INTVAL (count_exp);
+    {
+      count = INTVAL (count_exp);
+      if (!TARGET_INLINE_ALL_STRINGOPS && count > 64)
+	return 0;
+    }
 
   /* Figure out proper mode for counter.  For 32bits it is always SImode,
      for 64bits use SImode when possible, otherwise DImode.
@@ -10300,6 +10303,8 @@ ix86_expand_movstr (dst, src, count_exp, align_exp)
     counter_mode = SImode;
   else
     counter_mode = DImode;
+
+  start_sequence ();
 
   if (counter_mode != SImode && counter_mode != DImode)
     abort ();
@@ -10514,7 +10519,11 @@ ix86_expand_clrstr (src, count_exp, align_exp)
     align = 32;
 
   if (GET_CODE (count_exp) == CONST_INT)
-    count = INTVAL (count_exp);
+    {
+      count = INTVAL (count_exp);
+      if (!TARGET_INLINE_ALL_STRINGOPS && count > 64)
+	return 0;
+    }
   /* Figure out proper mode for counter.  For 32bits it is always SImode,
      for 64bits use SImode when possible, otherwise DImode.
      Set count to number of bytes copied when known at compile time.  */
