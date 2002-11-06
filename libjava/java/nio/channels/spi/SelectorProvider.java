0fe54282b930f7e26088d50260da7dd318c6350b
(no author) <(no author)@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-06 00:58:49 +0000

    This commit was manufactured by cvs2svn to create branch 'mips-3_4-rewrite-branch'.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/mips-3_4-rewrite-branch@58848 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libjava/java/nio/channels/spi/SelectorProvider.java
@@ -0,0 +1,82 @@
+/* SelectorProvider.java -- 
+   Copyright (C) 2002 Free Software Foundation, Inc.
+
+This file is part of GNU Classpath.
+
+GNU Classpath is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GNU Classpath is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Classpath; see the file COPYING.  If not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA.
+
+Linking this library statically or dynamically with other modules is
+making a combined work based on this library.  Thus, the terms and
+conditions of the GNU General Public License cover the whole
+combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent
+modules, and to copy and distribute the resulting executable under
+terms of your choice, provided that you also meet, for each linked
+independent module, the terms and conditions of the license of that
+module.  An independent module is a module which is not derived from
+or based on this library.  If you modify this library, you may extend
+this exception to your version of the library, but you are not
+obligated to do so.  If you do not wish to do so, delete this
+exception statement from your version. */
+
+package java.nio.channels.spi;
+
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.Pipe;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @since 1.4
+ */
+public abstract class SelectorProvider
+{
+  static SelectorProvider pr;
+    
+  /**
+   * Creates the <code>SelectorProvider<code> object
+   *
+   * @exception SecurityException If a security manager has been installed and
+   * it denies RuntimePermission("selectorProvider")
+   */
+  protected SelectorProvider()
+  {
+    SecurityManager sm = System.getSecurityManager ();
+    if (sm != null)
+      sm.checkPermission (new RuntimePermission ("selectorProvider"));
+  }
+    
+  public abstract  DatagramChannel openDatagramChannel();
+
+  public abstract  Pipe openPipe();
+  
+  public abstract  AbstractSelector openSelector();
+  
+  public abstract  ServerSocketChannel openServerSocketChannel();
+  
+  public abstract  SocketChannel openSocketChannel();
+   
+  /**
+   * Returns the global <code>SelectorProvider</code> object
+   */
+  public static SelectorProvider provider()
+  {
+    return pr;
+  }
+}
