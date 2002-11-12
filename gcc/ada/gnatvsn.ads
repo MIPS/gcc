59b5bfafdc8a797bda6fea4e97f88291889bc117
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-11-12 16:25:43 +0000

    cvs update -jgcc-3_2-branch:200211{09,12}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_2-rhl8-branch@59053 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.2.12.65.2.107 $
+--                            $Revision: 1.2.12.65.2.110 $
 --                                                                          --
 --          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
 --                                                                          --
@@ -38,7 +38,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.2 20021108 (3.2-12)";
+   Gnat_Version_String : constant String := "3.2 20021111 (3.2-12)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2 20021108 (Red Hat Linux 8.0 3.2-12)";
+const char *const ffe_version_string = "3.2 20021111 (Red Hat Linux 8.0 3.2-12)";
