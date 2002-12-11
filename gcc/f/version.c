ab2f6952b0059a72b116e3d2ab82502d4b1ac5a6
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-12-11 12:35:55 +0000

    cvs update -jgcc-3_2-branch:200212{08,11}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_2-rhl8-branch@60033 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2.1 20021207 (Red Hat Linux 8.0 3.2.1-2)";
+const char *const ffe_version_string = "3.2.1 20021210 (Red Hat Linux 8.0 3.2.1-3)";
