94d37750dff8e07f1d1a18b9121c38794445aaf8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-01-28 14:14:46 +0000

    cvs update -jgcc-3_2-branch:200301{17,28}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_2-rhl8-branch@61968 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2.1 20030116 (Red Hat Linux 8.0 3.2.1-4)";
+const char *const ffe_version_string = "3.2.1 20030127 (Red Hat Linux 8.0 3.2.1-5)";
