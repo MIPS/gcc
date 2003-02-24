88873a2b59c54f5f77ea251a1947748362885609
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-02-24 00:16:17 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cygwin-mingw-gcc-3_2-branch@63342 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libU77/Version.c
+++ libf2c/libU77/Version.c
@@ -1,6 +1,6 @@
 static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
 
-char __G77_LIBU77_VERSION__[] = "3.2 20030223 (prerelease)";
+char __G77_LIBU77_VERSION__[] = "3.2 20030224 (prerelease)";
 
 #include <stdio.h>
 
