809774effd0a4925e4e10116b70052653617f046
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-02-25 02:27:32 +0000

    bump version
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_2-rhl8-branch@63395 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/f/version.c
+++ gcc/f/version.c
@@ -1,4 +1,4 @@
 #include "ansidecl.h"
 #include "f/version.h"
 
-const char *const ffe_version_string = "3.2.2 20030222 (Red Hat Linux 8.0 3.2.2-3)";
+const char *const ffe_version_string = "3.2.2 20030222 (Red Hat Linux 3.2.2-4)";
