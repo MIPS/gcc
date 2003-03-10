3509286c96665f787e6e2ff2d6ba5dfa38357c72
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-10 14:44:45 +0000

    * i386.c (x86_machine_dependent_reorg):  Replace rep instead of padding. 	* i386.md (UNSPEC_REP): New constant. 	(return_internal_long): New pattern.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@64081 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -14949,9 +14949,10 @@ x86_machine_dependent_reorg (first)
     basic_block bb = e->src;
     rtx ret = bb->end;
     rtx prev;
-    bool insert = false;
+    bool replace = false;
 
-    if (!returnjump_p (ret) || !maybe_hot_bb_p (bb))
+    if (GET_CODE (ret) != JUMP_INSN || GET_CODE (PATTERN (ret)) != RETURN
+	|| !maybe_hot_bb_p (bb))
       continue;
     for (prev = PREV_INSN (ret); prev; prev = PREV_INSN (prev))
       if (active_insn_p (prev) || GET_CODE (prev) == CODE_LABEL)
@@ -14962,21 +14963,24 @@ x86_machine_dependent_reorg (first)
 	for (e = bb->pred; e; e = e->pred_next)
 	  if (EDGE_FREQUENCY (e) && e->src->index >= 0
 	      && !(e->flags & EDGE_FALLTHRU))
-	    insert = 1;
+	    replace = true;
       }
-    if (!insert)
+    if (!replace)
       {
 	prev = prev_active_insn (ret);
 	if (prev && GET_CODE (prev) == JUMP_INSN
 	    && any_condjump_p (prev))
-	  insert = 1;
+	  replace = true;
 	/* Empty functions get branch misspredict even when the jump destination
 	   is not visible to us.  */
 	if (!prev && cfun->function_frequency > FUNCTION_FREQUENCY_UNLIKELY_EXECUTED)
-	  insert = 1;
+	  replace = true;
       }
-    if (insert)
-      emit_insn_before (gen_nop (), ret);
+    if (replace)
+      {
+        emit_insn_before (gen_return_internal_long (), ret);
+	delete_insn (ret);
+      }
   }
 }
 
