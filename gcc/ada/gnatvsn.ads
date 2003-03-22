36f8e6d8aa49a28f7301cc0f60c679e35680be91
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-03-22 00:16:33 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cygwin-mingw-gcc-3_1-branch@64694 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---                            $Revision: 1.2.12.2.2.278 $
+--                            $Revision: 1.2.12.2.2.279 $
 --                                                                          --
 --          Copyright (C) 1992-2002 Free Software Foundation, Inc.          --
 --                                                                          --
@@ -39,9 +39,9 @@
 package Gnatvsn is
 
 <<<<<<< gnatvsn.ads
-   Gnat_Version_String : constant String := "3.1.1 20030321 (prerelease)";
+   Gnat_Version_String : constant String := "3.1.1 20030322 (prerelease)";
 =======
-   Gnat_Version_String : constant String := "3.2 20030321 (prerelease)";
+   Gnat_Version_String : constant String := "3.2 20030322 (prerelease)";
 >>>>>>> 1.2.12.66
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
