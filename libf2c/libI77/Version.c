098e103e3c178509b5f2e21178da84e47fddfd64
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-04-15 00:16:18 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cygwin-mingw-gcc-3_2-branch@65607 138bc75d-0d04-0410-961f-82ee72b054a4

--- libf2c/libI77/Version.c
+++ libf2c/libI77/Version.c
@@ -3,7 +3,7 @@ static char junk[] = "\n@(#) LIBI77 VERSION pjw,dmg-mods 20001205\n";
 /*
 */
 
-char __G77_LIBI77_VERSION__[] = "3.2 20030414 (prerelease)";
+char __G77_LIBI77_VERSION__[] = "3.2 20030415 (prerelease)";
 
 /*
 2.01	$ format added
