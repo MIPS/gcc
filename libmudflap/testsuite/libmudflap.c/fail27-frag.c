27a1e0bf61d1be75c851f2ea61f6ca3bb02859d9
fche <fche@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-02 18:34:32 +0000

    2003-05-02  Frank Ch. Eigler  <fche@redhat.com>
    	* testsuite/libmudflap.c/fail27-frag.c: Add more volatile flags.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@66394 138bc75d-0d04-0410-961f-82ee72b054a4

--- libmudflap/testsuite/libmudflap.c/fail27-frag.c
+++ libmudflap/testsuite/libmudflap.c/fail27-frag.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char *foo (unsigned i)
+char volatile *foo (unsigned i)
 {
-  char buffer[10];
-  char *k = i ? & buffer[i] : NULL; /* defeat addr-of-local-returned warning */
+  char volatile buffer[10];
+  char volatile *k = i ? & buffer[i] : NULL; /* defeat addr-of-local-returned warning */
   return k;
 }
 
 int main ()
 {
-char *f = foo (5);
+char volatile *f = foo (5);
 f[0] = 'b';
 
 return 0;
