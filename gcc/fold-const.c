9c33a16cf1e331ba672bee5444a677c243680349
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-05-10 19:18:46 +0000

    * fold-const.c (fold):  Fix fp compare optimization.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@66669 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -6018,7 +6018,7 @@ fold (expr)
 	  tree targ1 = strip_float_extensions (arg1);
 	  tree newtype = TREE_TYPE (targ0);
 
-	  if (TYPE_PRECISION (TREE_TYPE (targ1)) > TYPE_PRECISION (type))
+	  if (TYPE_PRECISION (TREE_TYPE (targ1)) > TYPE_PRECISION (newtype))
 	    newtype = TREE_TYPE (targ1);
 
 	  /* Fold (double)float1 CMP (double)float2 into float1 CMP float2.  */
