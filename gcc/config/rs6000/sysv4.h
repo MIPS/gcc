db2d8e12c937b15a9849308558bfd3cfd94b79ad
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-06-06 08:47:42 +0000

    Fix.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@67534 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/sysv4.h
+++ gcc/config/rs6000/sysv4.h
@@ -681,7 +681,7 @@ do {									\
     {									\
       fprintf (FILE, "%s", LCOMM_ASM_OP);				\
       assemble_name ((FILE), (NAME));					\
-      fprintf ((FILE), ",%u,%u\n", (SIZE), (ALIGN) / BITS_PER_UNIT);	\
+      fprintf ((FILE), ","HOST_WIDE_INT_PRINT_UNSIGNED",%u\n", (SIZE), (ALIGN) / BITS_PER_UNIT);	\
     }									\
   ASM_OUTPUT_TYPE_DIRECTIVE (FILE, NAME, "object");			\
 } while (0)
