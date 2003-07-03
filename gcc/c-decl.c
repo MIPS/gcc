a144b46674ea46a64fe237155c33a854407831e8
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-03 09:07:57 +0000

    * c-decl.c (store_parm_decls): Strip away NOP_EXPRs when looking         for hidden use variables.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@68873 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-decl.c
+++ gcc/c-decl.c
@@ -6142,9 +6142,14 @@ store_parm_decls ()
 	   t = TREE_CHAIN (t))
 	{
 	  /* We will have a nonlocal use of this variable.  */
-	  if (TREE_CODE (TREE_OPERAND (TREE_VALUE (t), 0)) == VAR_DECL
-	      || TREE_CODE (TREE_OPERAND (TREE_VALUE (t), 0)) == PARM_DECL)
-	    DECL_NONLOCAL (TREE_OPERAND (TREE_VALUE (t), 0)) = 1;
+	  tree var = TREE_OPERAND (TREE_VALUE (t), 0);
+
+	  /* Strip away any NOP_EXPRs.  STRIP_NOPS is not sufficient
+	     here since the nodes may have different modes.  */
+	  while (TREE_CODE (var) == NOP_EXPR)
+	    var = TREE_OPERAND (var, 0);
+
+	  DECL_NONLOCAL (var) = 1;
 	  SAVE_EXPR_CONTEXT (TREE_VALUE (t)) = context;
 	}
     }
