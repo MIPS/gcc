d04402e98c16abfc04193dd9d47e54c71debb185
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-07-28 15:48:09 +0000

    * configure.in (--enable-checking): Add fold category. 	(ENABLE_FOLD_CHECKING): Define if requested. 	* configure: Rebuilt. 	* config.in: Rebuilt. 	* doc/install.texi: Document it. 	* fold-const.c: Include md5.h. 	[ENABLE_FOLD_CHECKING] (fold): Define to fold_1. 	[ENABLE_FOLD_CHECKING] (fold, fold_checksum_tree, fold_check_failed, 	print_fold_checksum): New functions.
    	* fold-const.c (fold): Never modify argument passed to fold, instead
    	change a copy and return it.
    	* convert.c (convert_to_integer): Likewise.
    testsuite/
    	* gcc.c-torture/compile/20030725-1.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_3-rhl-branch@69888 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/convert.c
+++ gcc/convert.c
@@ -156,6 +156,7 @@ convert_to_integer (type, expr)
 
       if (TREE_CODE_CLASS (ex_form) == '<')
 	{
+	  expr = copy_node (expr);
 	  TREE_TYPE (expr) = type;
 	  return expr;
 	}
@@ -164,6 +165,7 @@ convert_to_integer (type, expr)
 	       || ex_form == TRUTH_OR_EXPR || ex_form == TRUTH_ORIF_EXPR
 	       || ex_form == TRUTH_XOR_EXPR)
 	{
+	  expr = copy_node (expr);
 	  TREE_OPERAND (expr, 0) = convert (type, TREE_OPERAND (expr, 0));
 	  TREE_OPERAND (expr, 1) = convert (type, TREE_OPERAND (expr, 1));
 	  TREE_TYPE (expr) = type;
@@ -172,6 +174,7 @@ convert_to_integer (type, expr)
 
       else if (ex_form == TRUTH_NOT_EXPR)
 	{
+	  expr = copy_node (expr);
 	  TREE_OPERAND (expr, 0) = convert (type, TREE_OPERAND (expr, 0));
 	  TREE_TYPE (expr) = type;
 	  return expr;
