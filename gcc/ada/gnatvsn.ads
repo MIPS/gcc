27c3d39e6bb6cd194f75ed582fd982b9d031a5c8
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-01 20:39:48 +0000

    cvs update -Pd -jgcc-3_2-branch:20030{423,801}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_2-rhl8-branch@70065 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ada/gnatvsn.ads
+++ gcc/ada/gnatvsn.ads
@@ -37,7 +37,7 @@
 
 package Gnatvsn is
 
-   Gnat_Version_String : constant String := "3.2.3 20030422 (3.2.3-3)";
+   Gnat_Version_String : constant String := "3.2.3 20030502 (3.2.3-13)";
    --  Version output when GNAT (compiler), or its related tools, including
    --  GNATBIND, GNATCHOP, GNATFIND, GNATLINK, GNATMAKE, GNATXREF, are run
    --  (with appropriate verbose option switch set).
