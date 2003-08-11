d69abdaa8b2fb4f5564182aafd4c636b78b7d426
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-11 10:41:24 +0000

    Fix comment
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@70321 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/intrinsics/abort.c
+++ libgfortran/intrinsics/abort.c
@@ -1,4 +1,4 @@
-/* Implementation of the CPU_TIME intrinsic.
+/* Implementation of the ABORT intrinsic.
    Copyright (C) 2003 Free Software Foundation, Inc.
 
 This file is part of the GNU Fortran 95 runtime library (libgfortran).
