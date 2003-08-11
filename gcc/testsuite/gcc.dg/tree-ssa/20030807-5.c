5f9a46f38d458126f39332fbf4b0a4f275d24062
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-11 16:20:16 +0000

    * gcc.dg/tree-ssa/20030807-5.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@70329 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20030807-5.c
@@ -0,0 +1,36 @@
+/* { dg-do compile */
+/* { dg-options "-O1 -fdump-tree-ssa" } */
+                                                                                
+struct rtx_def;
+typedef struct rtx_def *rtx;
+
+
+struct rtx_def
+{
+
+  int code;
+  unsigned int unchanging:1;
+
+};
+static rtx current_sym_addr;
+
+static int
+foo ()
+{
+  if (current_sym_addr->code == 42
+      && (({
+	       rtx _rtx = current_sym_addr;
+	       if (((_rtx)->code) != 42)
+	         abort ();
+	       _rtx;}
+	   )->unchanging))
+    ;
+}
+
+/* There should be precisely one load of ->code.  If there is
+   more than, then the dominator optimizations failed.  */
+/* { dg-final { scan-tree-dump-times "->code" 1 "ssa"} } */
+
+/* There should be precisely one IF statement.  If there is
+   more than, then the dominator optimizations failed.  */
+/* { dg-final { scan-tree-dump-times "if " 1 "ssa"} } */
