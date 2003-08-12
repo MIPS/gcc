a712351d4702398dc2c2463e6ef35a71736199ad
law <law@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-12 15:57:58 +0000

    * gcc.dg/tree-ssa/20030807-6.c: New test.         * gcc.dg/tree-ssa/20030807-7.c: New test.         * gcc.dg/tree-ssa/20030807-9.c: New test.         * gcc.dg/tree-ssa/20030807-11.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@70378 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/20030807-9.c
@@ -0,0 +1,12 @@
+/* { dg-do compile */
+/* { dg-options "-O1 -fdump-tree-ssa" } */
+
+static void
+bar ()
+{
+  const char *label2 = (*"*.L_sfnames_b" == '*') + "*.L_sfnames_b";
+  oof (label2);
+}
+
+/* There should be no IF conditionals.  */
+/* { dg-final { scan-tree-dump-times "if " 0 "ssa"} } */
