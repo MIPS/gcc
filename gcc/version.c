08c8b61e811af30dfa3a6d94121b6fc945fb2897
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-15 23:50:00 +0000

    cvs update -Pd -jgcc-3_3-branch:200308{12,15}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_3-rhl-branch@70496 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -6,7 +6,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.3.1 20030811 (Red Hat Linux 3.3-18)";
+const char version_string[] = "3.3.1 20030814 (Red Hat Linux 3.3.1-2)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
