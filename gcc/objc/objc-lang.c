db83625a2d3d6b3d880f523490316e734c5d8133
bothner <bothner@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-08-16 01:31:15 +0000

    * objc/lang-specs.h: Handle -server flag. 	* objc/objc-lang.c (LANG_HOOKS_INIT_ONCE):  New definition.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/compile-server-branch@70501 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/objc-lang.c
+++ gcc/objc/objc-lang.c
@@ -35,6 +35,8 @@ enum c_language_kind c_language = clk_objc;
 
 #undef LANG_HOOKS_NAME
 #define LANG_HOOKS_NAME "GNU Objective-C"
+#undef LANG_HOOKS_INIT_ONCE
+#define LANG_HOOKS_INIT_ONCE init_c_objc_common_once
 #undef LANG_HOOKS_INIT
 #define LANG_HOOKS_INIT objc_init
 #undef LANG_HOOKS_FINISH
