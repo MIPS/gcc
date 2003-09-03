fba6dd593d654898729e1b4ea827329d3b0a7237
zlaski <zlaski@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-09-03 00:14:27 +0000

    Don't compare a pointer with an integer. :-(
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/objc-improvements-branch@71013 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/objc-act.c
+++ gcc/objc/objc-act.c
@@ -6378,7 +6378,7 @@ lookup_method_static (interface, ident, is_class)
 	  if (CLASS_PROTOCOL_LIST (category))
 	    {
 	      if ((meth = (lookup_method_in_protocol_list
-			   (CLASS_PROTOCOL_LIST (category), ident, is_class != NULL_TREE))))
+			   (CLASS_PROTOCOL_LIST (category), ident, is_class))))
 		return meth;
 	    }
 	}
@@ -6387,7 +6387,7 @@ lookup_method_static (interface, ident, is_class)
       if (CLASS_PROTOCOL_LIST (inter))
 	{
 	  if ((meth = (lookup_method_in_protocol_list
-		       (CLASS_PROTOCOL_LIST (inter), ident, is_class != NULL_TREE))))
+		       (CLASS_PROTOCOL_LIST (inter), ident, is_class))))
 	    return meth;
 	}
 
