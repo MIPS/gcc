0097acd1b97ed90c15310db80d54d395ac86af04
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2003-12-12 12:53:10 +0000

    2003-12-12  Huang Chun <chunhuang73@hotmail.com>
    	* gfortran.fortran-torture/execute/intrinsic_len.f90: Fix.
    	* gfortran.fortran-torture/execute/intrinsic_index.f90: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@74566 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_len.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_len.f90
@@ -2,10 +2,16 @@
 program test
   character(len=10) a
   character(len=8) w
+  type person
+    character(len=10) name
+    integer age
+  end type person
+  type(person) Tom
   integer n
   a = w (n)
 
   if ((a .ne. "01234567") .or. (n .ne. 8)) call abort
+  if (len(Tom%name) .ne. 10) call abort
 end
 
 function w(i)
