55007545db19b5bbb987f88af3102844637bf872
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-09 22:25:44 +0000

    2003-05-13  Richard Henderson  <rth@redhat.com>
    	* class.c (layout_class_method): Set DECL_EXTERNAL.
    	* decl.c (java_mark_decl_local, java_mark_class_local): New.
    	* java-tree.h (java_mark_class_local): Declare.
    	* jcf-parse.c (parse_class_file): Use it.
    	* parse.y (java_expand_classes): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_3-rhl-branch@75606 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/decl.c
+++ gcc/java/decl.c
@@ -1894,4 +1894,35 @@ void java_optimize_inline (fndecl)
     }
 }
 
+/* We pessimistically marked all methods and fields external until we
+   knew what set of classes we were planning to compile.  Now mark those
+   associated with CLASS to be generated locally as not external.  */
+
+static void
+java_mark_decl_local (decl)
+     tree decl;
+{
+  DECL_EXTERNAL (decl) = 0;
+
+  /* If we've already constructed DECL_RTL, give encode_section_info
+     a second chance, now that we've changed the flags.  */
+  if (DECL_RTL_SET_P (decl))
+    make_decl_rtl (decl, NULL);
+}
+
+void
+java_mark_class_local (class)
+     tree class;
+{
+  tree t;
+
+  for (t = TYPE_FIELDS (class); t ; t = TREE_CHAIN (t))
+    if (FIELD_STATIC (t))
+      java_mark_decl_local (t);
+
+  for (t = TYPE_METHODS (class); t ; t = TREE_CHAIN (t))
+    if (!METHOD_ABSTRACT (t) && (!METHOD_NATIVE (t) || flag_jni))
+      java_mark_decl_local (t);
+}
+
 #include "gt-java-decl.h"
