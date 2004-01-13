f6b0d91f5dc86d50c9f0a7a67ee4f0ae67e7f603
toon <toon@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-13 17:34:35 +0000

    2004-01-13  Toon Moene  <toon@moene.indiv.nluug.nl>
    	PR fortran/12912
    	* lang-specs.h: Enable compilation of files ending
    	in .f, .for and .FOR.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@75807 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/lang-specs.h
+++ gcc/fortran/lang-specs.h
@@ -10,5 +10,9 @@ This file is licensed under the GPL.  */
 {".f90", "@f95", 0},
 {".f95", "@f95", 0},
 {"@f95", "%{!E:f951 %i %<ffixed-format %(cc1_options) %{J*} %{I*}\
-         %{!fsyntax-only:%(invoke_as)}}", 0}
-,
+         %{!fsyntax-only:%(invoke_as)}}", 0},
+{".f",   "@f77", 0},
+{".for", "@f77", 0},
+{".FOR", "@f77", 0},
+{"@f77", "%{!E:f951 %i %{!ffree-form:-ffixed-form} %(cc1_options) %{J*} %{I*}\
+         %{!fsyntax-only:%(invoke_as)}}", 0},
