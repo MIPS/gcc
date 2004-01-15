5f92ba659198a7871026cc3155f11c04d76a415e
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-15 14:39:36 +0000

    * cfghooks.c (predicted_by_p): Add missing return.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@75920 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfghooks.c
+++ gcc/cfghooks.c
@@ -473,7 +473,7 @@ predicted_by_p (basic_block bb, enum br_predictor predictor)
   if (!cfg_hooks->predict_edge)
     internal_error ("%s does not support predicted_by_p.", cfg_hooks->name);
 
-  cfg_hooks->predicted_by_p (bb, predictor);
+  return cfg_hooks->predicted_by_p (bb, predictor);
 }
 
 /* Merges basic block B into basic block A.  */
