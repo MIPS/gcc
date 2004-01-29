987bedad65fb8e34e465a2c87a333d1b7bc7d664
hpenner <hpenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-29 07:28:47 +0000

    * gcc/config/rs6000/rs6000.c (rs6000_stack_info)        Calculate always vrsave_mask if TARGET_ALTIVEC.        (rs6000_emit_prologue): Emit code for vrsave        only if TARGET_ALTIVEC_VRSAVE.        (rs6000_emit_epilogue): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@76849 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -10054,15 +10054,16 @@ rs6000_stack_info ()
   else
     info_ptr->spe_gp_size = 0;
 
-  if (TARGET_ALTIVEC_ABI)
-    info_ptr->vrsave_mask = compute_vrsave_mask ();
+  if (TARGET_ALTIVEC_ABI && TARGET_ALTIVEC_VRSAVE)
+    {
+      info_ptr->vrsave_mask = compute_vrsave_mask ();
+      info_ptr->vrsave_size  = info_ptr->vrsave_mask ? 4 : 0;
+    }
   else
-    info_ptr->vrsave_mask = 0;
-
-  if (TARGET_ALTIVEC_VRSAVE && info_ptr->vrsave_mask)
-    info_ptr->vrsave_size  = 4;
-  else
-    info_ptr->vrsave_size  = 0;
+    {
+      info_ptr->vrsave_mask = 0;
+      info_ptr->vrsave_size = 0;
+    }
 
   /* Calculate the offsets.  */
   switch (DEFAULT_ABI)
@@ -11134,8 +11135,7 @@ rs6000_emit_prologue ()
      used in this function, and do the corresponding magic in the
      epilogue.  */
 
-  if (TARGET_ALTIVEC && TARGET_ALTIVEC_VRSAVE
-      && info->vrsave_mask != 0)
+  if (TARGET_ALTIVEC && info->vrsave_mask != 0)
     {
       rtx reg, mem, vrsave;
       int offset;
@@ -11597,8 +11597,7 @@ rs6000_emit_epilogue (sibcall)
     }
 
   /* Restore VRSAVE if needed.  */
-  if (TARGET_ALTIVEC_ABI && TARGET_ALTIVEC_VRSAVE 
-      && info->vrsave_mask != 0)
+  if (TARGET_ALTIVEC_ABI && info->vrsave_mask != 0)
     {
       rtx addr, mem, reg;
 
