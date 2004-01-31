4e18cd5dfae64b1f30d1513d1541152af0d33df0
canqun <canqun@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-01-31 10:44:31 +0000

    * gfortran.fortran-torture/execute/intrinsic_mmloc_4.f90: 	Delete print statements.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@77028 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_mmloc_4.f90
+++ gcc/testsuite/gfortran.fortran-torture/execute/intrinsic_mmloc_4.f90
@@ -6,10 +6,8 @@ program intrinsic_mmloc_4
   integer, dimension(2) :: b
 
   allocate (d(0))
-  print *, maxloc (d, 1)
   if (maxloc (d, 1) .ne. 0) call abort()
   allocate (a(1, 0))
   b = minloc (a)
-  print *, b
   if (any (b .ne. 0)) call abort()
 end program
