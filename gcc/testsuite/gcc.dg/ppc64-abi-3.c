8fb70f857b1525c8acd0e8358bf9012fd4c537ca
hpenner <hpenner@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-13 07:11:04 +0000

    * gcc.dg/ppc64-abi-3.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@77751 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/ppc64-abi-3.c
+++ gcc/testsuite/gcc.dg/ppc64-abi-3.c
@@ -4,25 +4,17 @@
    for the PowerPC64 ABI.  */
 
 typedef int __attribute__((mode(V4SI))) v4si;
-typedef int __attribute__((mode(V2SI))) v2si;
 
 v4si 
 f(v4si v)
-{ /* { dg-error "altivec instructions are disabled" } */
-    return v;
-}
-
-v2si 
-g(v2si v)
 {
     return v;
-}
+} /* { dg-error "altivec instructions are disabled" } */
+
 
 int 
 main()
 {
     v4si v;
-    v2si w;
     v = f (v); /* { dg-error "altivec instructions are disabled" } */
-    w = g (w);
 }
