9a19d4125f5ebdd47c3594ece01bd587ef835ed1
amodra <amodra@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-15 12:54:56 +0000

    Apply patch from mainline. 	2003-09-04  Jan Hubicka  <jh@suse.cz> 	* toplev.c (wrapup_global_declarations): Fix final pass in 	unit-at-atime mode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@77844 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -2061,7 +2061,7 @@ wrapup_global_declarations (vec, len)
 	      if (flag_unit_at_a_time
 		  && cgraph_varpool_node (decl)->finalized)
 		needed = 0;
-	      else if (flag_unit_at_a_time
+	      else if (flag_unit_at_a_time && !cgraph_global_info_ready
 		       && (TREE_USED (decl)
 			   || TREE_USED (DECL_ASSEMBLER_NAME (decl))))
 		/* needed */;
