28a6c8913ad324beceaa90dff9683f74d52d4272
aj <aj@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-18 07:36:20 +0000

    * toplev.c: Include <alloc-pool.h> for dump_alloc_pool_statistics 	prototype.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/hammer-3_3-branch@78013 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/toplev.c
+++ gcc/toplev.c
@@ -74,6 +74,7 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "cfgloop.h"
 #include "vpt.h"
 #include "cgraph.h"
+#include "alloc-pool.h"
 
 #if defined (DWARF2_UNWIND_INFO) || defined (DWARF2_DEBUGGING_INFO)
 #include "dwarf2out.h"
