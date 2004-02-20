53c5b7ce6dd570d0e4be5b3f93c085b7793b83d8
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-02-20 21:01:06 +0000

    * cfgcleanup.c (try_optimize_cfg): Do not merge blocks separated by 	tablejump.
    	* tree-cfg.c (cleanup_control_expr_graph): Update profile.
    
    	* tree-cfg.c (disband_implicit_edges): Set fallthru edges correctly.
    
    	* cfg.c (dump_flow_info): Work on trees too.
    
    	* tree-pretty-print.c (dump_generic_bb_stuff):  Do not touch bb
    	annotations when not allocated.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-profiling-branch@78186 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-cfg.c
+++ gcc/tree-cfg.c
@@ -1673,6 +1673,8 @@ cleanup_control_expr_graph (basic_block bb, block_stmt_iterator bsi)
 	  next = e->succ_next;
 	  if (e != taken_edge)
 	    {
+	      taken_edge->probability += e->probability;
+	      taken_edge->count += e->count;
 	      ssa_remove_edge (e);
 	      retval = true;
 	    }
@@ -2291,6 +2293,7 @@ disband_implicit_edges (void)
 		COND_EXPR_ELSE (stmt) = build_empty_stmt ();
 	      else
 		abort ();
+	      e->flags |= EDGE_FALLTHRU;
 	    }
 
 	  continue;
@@ -2341,6 +2344,7 @@ disband_implicit_edges (void)
       bsi_insert_after (&last,
 			build1 (GOTO_EXPR, void_type_node, label),
 			BSI_NEW_STMT);
+      e->flags &= ~EDGE_FALLTHRU;
     }
 
   factored_computed_goto = NULL;
