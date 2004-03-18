d97ec96ad51232f1ac0e50108026419494186b11
(no author) <(no author)@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-03-18 11:45:13 +0000

    This commit was manufactured by cvs2svn to create branch 'tree-profiling-branch'.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-profiling-branch@79618 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/i386-sse-8.c
@@ -0,0 +1,13 @@
+/* PR target/14313 */
+/* Origin: <Pawe Sikora <pluto@ds14.agh.edu.pl> */
+
+/* { dg-do compile } */
+/* { dg-options "-march=pentium3" { target i?86-*-* x86_64-*-* } } */
+
+int main() 
+{ 
+  typedef long long int v __attribute__ ((vector_size (16))); 
+  v a, b; 
+  a = b; 
+  return 0; 
+}
