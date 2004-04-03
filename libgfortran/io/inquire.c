e4a026897a74b163cadea7d53329212e8797da67
pbrook <pbrook@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-03 14:14:23 +0000

    PR 14831 	* io/inquire.c (inquire_via_unit): Changed return string for 	BLANK=NULL. Use correct variable for ACTION. testsuite 	* gfortran.fortran-torture/execute/inquire_1.f90: New yest.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@80377 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/io/inquire.c
+++ libgfortran/io/inquire.c
@@ -137,7 +137,7 @@ inquire_via_unit (unit_t * u)
 	switch (u->flags.blank)
 	  {
 	  case BLANK_NULL:
-	    p = "BLANK";
+          p = "NULL";
 	    break;
 	  case BLANK_ZERO:
 	    p = "ZERO";
@@ -161,7 +161,7 @@ inquire_via_unit (unit_t * u)
       cf_strcpy (ioparm.blank, ioparm.blank_len, p);
     }
 
-  if (ioparm.access != NULL)
+  if (ioparm.action != NULL)
     {
       if (u == NULL)
 	p = undefined;
@@ -181,7 +181,7 @@ inquire_via_unit (unit_t * u)
 	    internal_error ("inquire_via_unit(): Bad action");
 	  }
 
-      cf_strcpy (ioparm.access, ioparm.access_len, p);
+      cf_strcpy (ioparm.action, ioparm.action_len, p);
     }
 
   if (ioparm.read != NULL)
