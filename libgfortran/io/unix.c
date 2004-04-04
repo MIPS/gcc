4064e66042885540e601176a63a076229e2b5542
bdavis <bdavis@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-04 00:17:19 +0000

    PR gfortran/14837         * gfortran.fortran-torture/execute/inquire_2.f90: New test.         * io/unix.c (find_file0): Use fd field of struct
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@80389 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/io/unix.c
+++ libgfortran/io/unix.c
@@ -1117,7 +1117,7 @@ find_file0 (unit_t * u, struct stat *st1)
   if (u == NULL)
     return NULL;
 
-  if (fstat (((unix_stream *) u)->fd, &st2) >= 0 &&
+  if (fstat (((unix_stream *) u->s)->fd, &st2) >= 0 &&
       st1->st_dev == st2.st_dev && st1->st_ino == st2.st_ino)
     return u;
 
