fec83635a6aea136aa5dd097742b9324e10332b5
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-04-22 17:49:21 +0000

    * gcc.dg/i386-sse-8.c: Don't use -march=pentium3 on x86_64. 	* gcc.dg/i386-local.c: Expect %edi, not %eax register on x86_64. 	* gcc.dg/20020103-1.c: Add -fno-asynchronous-unwind-tables to 	options.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_4-rhl-branch@81038 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/i386-sse-8.c
+++ gcc/testsuite/gcc.dg/i386-sse-8.c
@@ -2,7 +2,7 @@
 /* Origin: <Pawe Sikora <pluto@ds14.agh.edu.pl> */
 
 /* { dg-do compile } */
-/* { dg-options "-march=pentium3" { target i?86-*-* x86_64-*-* } } */
+/* { dg-options "-march=pentium3" { target i?86-*-* } } */
 
 int main() 
 { 
