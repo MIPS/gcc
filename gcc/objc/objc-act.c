9993273e05ff50a263f4f638b9289a0878829a49
pinskia <pinskia@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-03 20:14:13 +0000

    2004-05-03  Andrew Pinski  <pinskia@physics.uc.edu>
            * objc/objc-act.c (build_objc_string_object):
            Add the fields to the purpose of the list for
            the constructor.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-ssa-20020619-branch@81452 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/objc/objc-act.c
+++ gcc/objc/objc-act.c
@@ -1381,6 +1381,7 @@ build_objc_string_object (tree string)
 {
   tree initlist, constructor, constant_string_class;
   int length;
+  tree fields;
 
   string = fix_string_type (string);
 
@@ -1410,6 +1411,7 @@ build_objc_string_object (tree string)
 	}
       add_class_reference (constant_string_id);
     }
+  fields = TYPE_FIELDS (constant_string_type);
 
   /* & ((NXConstantString) { NULL, string, length })  */
 
@@ -1425,18 +1427,23 @@ build_objc_string_object (tree string)
 	  return error_mark_node;
 	}
       initlist = build_tree_list
-	(NULL_TREE,
+	(fields,
 	 copy_node (build_unary_op (ADDR_EXPR, string_class_decl, 0)));
     }
   else
     {
-      initlist = build_tree_list (NULL_TREE, build_int_2 (0, 0));
+      initlist = build_tree_list (fields, build_int_2 (0, 0));
     }
 
+  fields = TREE_CHAIN (fields);
+  
   initlist
-    = tree_cons (NULL_TREE, copy_node (build_unary_op (ADDR_EXPR, string, 1)),
+    = tree_cons (fields, copy_node (build_unary_op (ADDR_EXPR, string, 1)),
 		 initlist);
-  initlist = tree_cons (NULL_TREE, build_int_2 (length, 0), initlist);
+                 
+  fields = TREE_CHAIN (fields);
+  
+  initlist = tree_cons (fields, build_int_2 (length, 0), initlist);
   constructor = objc_build_constructor (constant_string_type,
 					nreverse (initlist));
 
