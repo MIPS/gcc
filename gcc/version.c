f0f0c7f076139fb8553668d7838b0cb01396f71e
gccadmin <gccadmin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-28 00:16:19 +0000

    Daily bump.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lno-branch@82346 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -5,7 +5,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.5-tree-ssa-lno 20040527 (merged 20040426)";
+const char version_string[] = "3.5-tree-ssa-lno 20040528 (merged 20040426)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
