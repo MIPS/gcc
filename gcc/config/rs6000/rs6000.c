4f66d241422ab7d780f1632a4bb4f6a312156321
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-05-29 15:25:52 +0000

    2004-05-29  Dale Johannesen  <dalej@apple.com>
    	* config/rs6000/rs6000.c (rs6000_override_options):  Enable
    	hot/cold partitioning with -fast -profile-use.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@82423 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -834,10 +834,8 @@ rs6000_override_options (const char *default_cpu)
       align_jumps = 16;
       set_fast_math_flags (1);
       flag_reorder_blocks = 1;
-#if 0
       if (flag_branch_probabilities && !flag_exceptions)
 	flag_reorder_blocks_and_partition = 1;
-#endif
       if (!flag_pic)
         set_target_switch ("dynamic-no-pic");
         
