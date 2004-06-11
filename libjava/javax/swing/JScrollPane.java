9f767a52e5d069ff97a99f0c38a0b6d3d0647e98
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-11 06:49:32 +0000

    2004-06-11  Michael Koch  <konqueror@gmx.de>
    	* javax/swing/JScrollPane.java
    	(serialVersionUID): New field.
    	(columnHeader): Made protected.
    	(rowHeader): Likewise.
    	(lowerLeft): Likewise.
    	(lowerRight): Likewise.
    	(upperLeft): Likewise.
    	(upperRight): Likewise.
    	(horizontalScrollBar): Likewise.
    	(horizontalScrollBarPolicy): Likewise.
    	(verticalScrollBar): Likewise.
    	(verticalScrollBarPolicy): Likewise.
    	(viewport): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/java-gui-branch@82966 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/javax/swing/JScrollPane.java
+++ libjava/javax/swing/JScrollPane.java
@@ -46,8 +46,8 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Rectangle;
 
-import javax.swing.border.Border;
 import javax.accessibility.Accessible;
+import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.plaf.ScrollPaneUI;
@@ -78,20 +78,23 @@ public class JScrollPane
   extends JComponent 
   implements Accessible, ScrollPaneConstants
 {
-  JViewport columnHeader;
-  JViewport rowHeader;
+  private static final long serialVersionUID = 5203525440012340014L;
+  
+  protected JViewport columnHeader;
+  protected JViewport rowHeader;
 
-  Component lowerLeft;
-  Component lowerRight;
-  Component upperLeft;
-  Component upperRight;
+  protected Component lowerLeft;
+  protected Component lowerRight;
+  protected Component upperLeft;
+  protected Component upperRight;
 
-  JScrollBar horizontalScrollBar;
-  int horizontalScrollBarPolicy;
-  JScrollBar verticalScrollBar;
-  int verticalScrollBarPolicy;
+  protected JScrollBar horizontalScrollBar;
+  protected int horizontalScrollBarPolicy;
+  protected JScrollBar verticalScrollBar;
+  protected int verticalScrollBarPolicy;
 
-  JViewport viewport;
+  protected JViewport viewport;
+  
   Border viewportBorder;
   boolean wheelScrollingEnabled;
   ChangeListener scrollListener;  
