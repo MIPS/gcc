3e7154b760fafd62588df95087925df98ae028af
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-06-18 15:36:23 +0000

    2004-06-18  Michael Koch  <konqueror@gmx.de>
    	* javax/swing/ToolTipManager.java
    	(stillInsideTimerAction): Fixed constructor arguments.
    	(outsideTimerAction): Likewise.
    	(insideTimerAction): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/java-gui-branch@83353 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/javax/swing/ToolTipManager.java
+++ libjava/javax/swing/ToolTipManager.java
@@ -37,7 +37,6 @@ exception statement from your version. */
 
 package javax.swing;
 
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -73,7 +72,7 @@ public class ToolTipManager extends MouseAdapter implements MouseMotionListener 
 		 * Constructor stillInsideTimerAction
 		 * @param manager TODO
 		 */
-		protected stillInsideTimerAction(ToolTipManager manager) {
+		protected stillInsideTimerAction() {
 			// TODO
 		} // stillInsideTimerAction()
 
@@ -111,7 +110,7 @@ public class ToolTipManager extends MouseAdapter implements MouseMotionListener 
 		 * Constructor outsideTimerAction
 		 * @param manager TODO
 		 */
-		protected outsideTimerAction(ToolTipManager manager) {
+		protected outsideTimerAction() {
 			// TODO
 		} // outsideTimerAction()
 
@@ -149,7 +148,7 @@ public class ToolTipManager extends MouseAdapter implements MouseMotionListener 
 		 * Constructor insideTimerAction
 		 * @param manager TODO
 		 */
-		protected insideTimerAction(ToolTipManager manager) {
+		protected insideTimerAction() {
 			// TODO
 		} // insideTimerAction()
 
