e5120cbed0d9d9ab6ab22b58862b605534f136d6
zlaski <zlaski@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-13 21:55:34 +0000

    3739325 3.5: Objective-C sections should be marked with the new no_dead_strip section attribute Fix brought over from mainline.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@85965 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/darwin.h
+++ gcc/config/darwin.h
@@ -804,7 +804,7 @@ SECTION_FUNCTION (objc_selector_refs_section,	\
 		  ".objc_message_refs", 1)	\
 SECTION_FUNCTION (objc_selector_fixup_section,	\
 		  in_objc_selector_fixup,	\
-		  ".section __OBJC, __sel_fixup", 1)	\
+		  ".section __OBJC, __sel_fixup, regular, no_dead_strip", 1)	\
 SECTION_FUNCTION (objc_symbols_section,		\
 		  in_objc_symbols,		\
 		  ".objc_symbols", 1)	\
@@ -819,7 +819,7 @@ SECTION_FUNCTION (objc_string_object_section,	\
 		  ".objc_string_object", 1)	\
 SECTION_FUNCTION (objc_constant_string_object_section,	\
 		  in_objc_constant_string_object,	\
-		  ".section __OBJC, __cstring_object", 1)	\
+		  ".section __OBJC, __cstring_object, regular, no_dead_strip", 1)	\
 /* APPLE LOCAL begin constant cfstrings */	\
 /* Unlike constant NSStrings, constant CFStrings do not live */\
 /* in the __OBJC segment since they may also occur in pure C */\
@@ -831,7 +831,7 @@ SECTION_FUNCTION (cfstring_constant_object_section,	\
 /* Fix-and-Continue image marker.  */		\
 SECTION_FUNCTION (objc_image_info_section,	\
                   in_objc_image_info,		\
-                  ".section __OBJC, __image_info", 1)	\
+                  ".section __OBJC, __image_info, regular, no_dead_strip", 1)	\
 SECTION_FUNCTION (objc_class_names_section,	\
 		in_objc_class_names,		\
 		".objc_class_names", 1)	\
