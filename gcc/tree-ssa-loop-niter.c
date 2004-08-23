24c666d7065b734c1758b8bba99794dd4817b3c9
rakdver <rakdver@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-08-23 07:40:18 +0000

    PR tree-optimization/17016 	* tree-ssa-loop-niter.c (number_of_iterations_cond, 	number_of_iterations_exit): Use POINTER_TYPE_P instead 	of testing for POINTER_TYPE.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lno-branch@86410 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-loop-niter.c
+++ gcc/tree-ssa-loop-niter.c
@@ -151,7 +151,7 @@ number_of_iterations_cond (tree type, tree base0, tree step0,
 	return;
     }
 
-  if (TREE_CODE (type) == POINTER_TYPE)
+  if (POINTER_TYPE_P (type))
     {
       /* We assume pointer arithmetics never overflows.  */
       mmin = mmax = NULL_TREE;
@@ -619,7 +619,7 @@ number_of_iterations_exit (struct loop *loop, edge exit,
   type = TREE_TYPE (op0);
 
   if (TREE_CODE (type) != INTEGER_TYPE
-    && TREE_CODE (type) != POINTER_TYPE)
+    && POINTER_TYPE_P (type))
     return false;
      
   if (!simple_iv (loop, stmt, op0, &base0, &step0))
