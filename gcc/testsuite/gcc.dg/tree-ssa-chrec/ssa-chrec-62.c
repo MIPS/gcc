35758fbf49bbb01826224874894ead7a9a5b669a
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-02 00:12:16 +0000

    * ssa-chrec-62.c: Update.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lno-branch@86945 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/tree-ssa-chrec/ssa-chrec-62.c
+++ gcc/testsuite/gcc.dg/tree-ssa-chrec/ssa-chrec-62.c
@@ -1,7 +1,9 @@
 /* { dg-do compile } */ 
-/* { dg-options "-O1 -floop-test -fdump-tree-lptest-details " } */
+/* { dg-options "-O1 -ftree-loop-linear -floop-test -fdump-tree-lptest-details -fdump-tree-loop-linear-details" } */
 
 double u[1782225];
+double v[1782225];
+double t[1782225];
 
 void foo (int N, double *res)
 {
@@ -11,9 +13,13 @@ void foo (int N, double *res)
   for (i = 0; i < N; i++)
     {
       for (j = 0; j < N; j++)
-	sum = sum + u[i + 1335 * j];
+	{
+	  sum = sum + u[i + 1335 * j];
+	  sum = sum + v[i + 1335 * j];
+	  sum = sum + t[i + 1335 * j];
+	}
       u[1336 * i] *= 2;
     }
-  *res = sum;
+  *res = sum + N;
 }
 
