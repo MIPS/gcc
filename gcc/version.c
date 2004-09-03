81b6c1a880c12d7ff76173e8272f38b23555f5d6
jonz <jonz@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-03 11:34:33 +0000

    Bump version to 1779.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@87032 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -15,7 +15,7 @@
    - There are other scripts that search for first word of the string
      to get version number string. Do not use new line.
 */
-const char version_string[] = "3.5.0 20040822 (Apple Computer, Inc. build 1778)";
+const char version_string[] = "3.5.0 20040822 (Apple Computer, Inc. build 1779)";
 /* APPLE LOCAL end Apple version */
 
 /* This is the location of the online document giving instructions for
