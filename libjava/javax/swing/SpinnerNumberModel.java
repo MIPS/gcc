a3e489700e8732adf03a6f447c94a3fb9c4efdbd
mkoch <mkoch@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-13 09:37:22 +0000

    2004-09-13  Michael Koch  <konqueror@gmx.de>
    	* javax/swing/SpinnerNumberModel.java
    	(getMinimum): New method.
    	(setMinimum): Likewise.
    	(getMaximum): Likewise.
    	(setMaximum): Likewise.
    	(getStepSize): Likewise.
    	(setStepSize): Likewise.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/java-gui-branch@87433 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/javax/swing/SpinnerNumberModel.java
+++ libjava/javax/swing/SpinnerNumberModel.java
@@ -247,4 +247,49 @@ public class SpinnerNumberModel extends AbstractSpinnerModel
   {
     return value;
   }
+
+  public Comparable getMinimum()
+  {
+    return minimum;
+  }
+
+  public void setMinimum(Comparable newMinimum)
+  {
+    if (minimum != newMinimum)
+      {
+	minimum = newMinimum;
+	fireStateChanged();
+      }
+  }
+
+  public Comparable getMaximum()
+  {
+    return maximum;
+  }
+
+  public void setMaximum(Comparable newMaximum)
+  {
+    if (maximum != newMaximum)
+      {
+	maximum = newMaximum;
+	fireStateChanged();
+      }
+  }
+
+  public Number getStepSize()
+  {
+    return stepSize;
+  }
+
+  public void setStepSize(Number newStepSize)
+  {
+    if (newStepSize == null)
+      throw new IllegalArgumentException();
+
+    if (stepSize != newStepSize)
+      {
+	stepSize = newStepSize;
+	fireStateChanged();
+      }
+  }
 }
