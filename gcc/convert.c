fe286c8b827392c78a257159ab2d1cba1bfb1cac
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-17 16:56:00 +0000

    Bug #: 3803125. Reviewed by Dale.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@87652 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/convert.c
+++ gcc/convert.c
@@ -28,8 +28,6 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #include "coretypes.h"
 #include "tm.h"
 #include "tree.h"
-/* APPLE LOCAL AltiVec */
-#include "c-tree.h"
 #include "flags.h"
 #include "convert.h"
 #include "toplev.h"
@@ -762,8 +760,7 @@ convert_to_vector (tree type, tree expr)
       /* APPLE LOCAL begin AltiVec */
       if (TREE_CODE (type) == VECTOR_TYPE  
 	  && TREE_CODE (TREE_TYPE (expr)) == VECTOR_TYPE
-	  && TREE_CODE (expr) == CONSTRUCTOR && TREE_CONSTANT (expr) 
-	  && ! vector_types_convertible_p (TREE_TYPE (expr), type))
+	  && TREE_CODE (expr) == CONSTRUCTOR && TREE_CONSTANT (expr))
 	  /* converting a constant vector to new vector type with Motorola Syntax. */
 	  return build_constructor (type, CONSTRUCTOR_ELTS (expr));
       /* APPLE LOCAL end AltiVec */
