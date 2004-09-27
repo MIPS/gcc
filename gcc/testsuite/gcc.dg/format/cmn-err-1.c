ceffaf242e25a778df70b44f0997942dfece12c1
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-27 16:22:50 +0000

    Revert: 	2004-09-24  Mark Mitchell <mark@codesourcery.com> 	* gcc/config/sol2-c.c (cmn_err_flag_specs): Add "#" 	(cmn_err_char_table): Likewise. 	(solaris_format_types): Likewise. 	* gcc/testsuite/gcc.dg/format/cmn-err-1.c: Add "#" tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@88182 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/format/cmn-err-1.c
+++ gcc/testsuite/gcc.dg/format/cmn-err-1.c
@@ -17,7 +17,6 @@ int main()
   int i = 1;
   long l = 2;
   llong ll = 3;
-  float f = 4.0;
 
   cmn_err_func (0, "%s", string);
   cmn_err_func (0, "%d %D %o %O %x %X %u", i, i, i, i, i, i, i);
@@ -26,7 +25,6 @@ int main()
 		ll, ll, ll, ll, ll, ll, ll);
   cmn_err_func (0, "%b %s", i, "\01Foo", string);
   cmn_err_func (0, "%p", string);
-  cmn_err_func (0, "%#x", i);
 
   cmn_err_func (0, "%i", i);		/* { dg-error "unknown|too many" } */
   cmn_err_func (0, "%d", l);		/* { dg-error "int format, (long int|different type) arg" } */
@@ -35,6 +33,5 @@ int main()
   cmn_err_func (0, "%b", i, i);		/* { dg-error "is not a pointer" } */
   cmn_err_func (0, "%b", string, i);	/* { dg-error "int format, pointer arg|is not a pointer" } */
   cmn_err_func (0, "%p", 3);            /* { dg-error "is not a pointer" } */
-  cmn_err_func (0, "%#x", f);           /* { dg-error "double arg" } */
   return 0;
 }
