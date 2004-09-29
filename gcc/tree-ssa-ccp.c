74ae8e2e20bd33636f6317d892dcea2f0629b1d8
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-29 13:00:10 +0000

    2004-09-29  Daniel Berlin  <dberlin@dberlin.org>
    	* tree-ssa-ccp.c (substitute_and_fold): Don't try to access
    	operand 1 of non-MODIFY_EXPR stmts.
    	* version.c: Note that this is the structure aliasing branch
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/structure-aliasing-branch@88287 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-ccp.c
+++ gcc/tree-ssa-ccp.c
@@ -577,8 +577,9 @@ substitute_and_fold (void)
 	      /* If we folded a builtin function, we'll likely
 		 need to rename VDEFs.  */
 	      if (replaced_address || changed 
-		  || handled_component_p (TREE_OPERAND (stmt, 0))
-		  || handled_component_p (TREE_OPERAND (stmt, 1)))
+	          || (TREE_CODE (stmt) == MODIFY_EXPR 
+		      && (handled_component_p (TREE_OPERAND (stmt, 0))
+			  || handled_component_p (TREE_OPERAND (stmt, 1)))))
 		{
 		  mark_new_vars_to_rename (stmt, vars_to_rename);
 		  if (maybe_clean_eh_stmt (stmt))
