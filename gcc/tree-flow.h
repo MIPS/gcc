71c3bf6b892f24f6965ddab366bd8562f26f1647
amacleod <amacleod@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-06 12:46:04 +0000

    2004-10-06  Andrew MacLeod  <amacleod@redhat.com>
    	PR tree-optimization/16447
    	* tree-cfg.c (bsi_commit_one_edge_insert): Rename from
    	bsi_commit_edge_inserts_1, and make funtion external.  Return new block.
    	(bsi_commit_edge_inserts): Use renamed bsi_commit_one_edge_insert.
    	* tree-optimize.c (pass_cleanup_cfg_post_optimizing): Enable listing.
    	* tree-flow.h (bsi_commit_one_edge_insert): Extern decl.
    	* tree-outof-ssa.c (rewrite_trees): Don't commit edges here.
    	(same_stmt_list_p): New.  Return TRUE if edge is to be forwarded.
    	(identical_copies_p): New.  Return true is two copies are the same.
    	(identical_stmt_lists_p): New.  Return true if stmt lists are the same.
    	(analyze_edges_for_bb): New.  Determine how best to insert edge stmts
    	for a basic block.
    	(perform_edge_inserts): New.  Determine what to do with all stmts that
    	have been inserted on edges.
    	(remove_ssa_form):  Analyze and commit edges from here.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/tree-cleanup-branch@88603 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-flow.h
+++ gcc/tree-flow.h
@@ -489,6 +489,7 @@ extern void tree_optimize_tail_calls (bool, enum tree_dump_index);
 extern edge tree_block_forwards_to (basic_block bb);
 extern void bsi_insert_on_edge (edge, tree);
 extern basic_block bsi_insert_on_edge_immediate (edge, tree);
+extern void bsi_commit_one_edge_insert (edge e, basic_block *);
 extern void bsi_commit_edge_inserts (int *);
 extern void notice_special_calls (tree);
 extern void clear_special_calls (void);
