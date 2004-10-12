d3967a5dda71b77bdc1539bb1e60f80141c154c0
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-10-12 07:23:00 +0000

    Revert: 	* gcc/config/i386/sol2-10.h (LIBGCC_SPEC): Add support for -nolibgcc.
    	* gcc/config.gcc: Do not build crt1.o, crti.o, crtn.o, gcrt1.o.
    
    	* gcc/fixinc/inclhack.def (solaris_stdio_tag): Add bypass.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@88917 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/sol2-10.h
+++ gcc/config/i386/sol2-10.h
@@ -86,15 +86,6 @@ Boston, MA 02111-1307, USA.  */
 #define TARGET_LD_EMULATION ""
 #endif
 
-/* On, Solaris 2.10 some of the system utilities are built with GCC.
-   Sun does not want these utilities should not be linked with libgcc
-   because a future installation of GCC in /usr/local might then
-   result in the base system utilities picking up a new version of
-   libgcc.  */
-#undef LIBGCC_SPEC
-#define LIBGCC_SPEC \
-  "%{!nolibgcc:-lgcc}"
-
 #undef LINK_ARCH_SPEC
 #define LINK_ARCH_SPEC TARGET_LD_EMULATION \
 		       "%{m64:" LINK_ARCH64_SPEC "}%{!m64:" LINK_ARCH32_SPEC "}"
