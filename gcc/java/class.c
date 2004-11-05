e9f80ad929d8010c744c88aa970e965d00c67c11
tromey <tromey@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-05 01:49:15 +0000

    * class.c (build_static_field_ref): Reverted previous patch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcj-abi-2-dev-branch@90102 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/java/class.c
+++ gcc/java/class.c
@@ -1054,7 +1054,8 @@ build_static_field_ref (tree fdecl)
      However, currently sometimes gcj is too eager and will end up
      returning the field itself, leading to an incorrect external
      reference being generated.  */
-  if ((is_compiled && ! flag_indirect_dispatch)
+  if ((is_compiled 
+       && (! flag_indirect_dispatch || current_class == fclass))
       || (FIELD_FINAL (fdecl) && DECL_INITIAL (fdecl) != NULL_TREE
 	  && (JSTRING_TYPE_P (TREE_TYPE (fdecl))
 	      || JNUMERIC_TYPE_P (TREE_TYPE (fdecl)))
