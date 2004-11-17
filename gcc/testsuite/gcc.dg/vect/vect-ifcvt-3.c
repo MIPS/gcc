7c881e4ff079f8246767aa95c9dda3abbaa60b6d
dpatel <dpatel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-17 02:38:56 +0000

    New test cases.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@90786 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/vect/vect-ifcvt-3.c
@@ -0,0 +1,39 @@
+/* { dg-do run } */
+/* { dg-options "-O2 -ftree-vectorize -fdump-tree-vect-stats -maltivec" { target powerpc*-*-* } } */
+
+#include <stdarg.h>
+#include <signal.h>
+
+#define N 16
+#define MAX 42
+
+extern void abort(void); 
+
+int main ()
+{  
+  int A[N] = {36,39,42,45,43,32,21,12,23,34,45,56,67,78,89,11};
+  int B[N] = {36,39,42,45,43,32,21,12,23,34,45,56,67,78,89,11};
+  int i, j;
+
+  for (i = 0; i < 16; i++)
+    A[i] = ( A[i] > MAX ? MAX : 0); 
+
+  /* check results:  */
+  for (i = 0; i < N; i++)
+    if (A[i] > MAX)
+      abort ();
+
+  for (i = 0; i < 16; i++)
+    B[i] = ( B[i] > MAX ? MAX : 0); 
+
+  /* check results:  */
+  for (i = 0; i < N; i++)
+    if (B[i] > MAX)
+      abort ();
+
+  return 0;
+}
+
+
+
+/* { dg-final { scan-tree-dump-times "vectorized 2 loops" 1 "vect" { xfail i?86-*-* x86_64-*-* } } } */
