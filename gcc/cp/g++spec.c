96f46044bee8e8c72c1a5cfe3b7064eb9a085112
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-11-17 17:59:39 +0000

    * gcc/cp/g++spec.c (lang_specific_driver): Add a -R option if 	-lstdc++ is added. 	* gcc/cp/Make-lang.in (g++spec.o): Define LIBDIR. 	* gcc/config/i386/t-sol2-10 (MULTILIB_DIRNAMES, 	MULTILIB_OSDIRNAMES): Use amd64 instead of 64. 	* gcc/config/i386/i386.c (optimization_options): Use 	SUBTARGET_OPTIMIZATION_OPTIONS. 	* gcc/config/i386/sol2-10.h (SUBTARGET_OPTIMIZATION_OPTIONS): 	Define. 	(TARGET_SUBTARGET_DEFAULT): Don't include 	MASK_OMIT_LEAF_FRAME_POINTER.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@90812 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -58,6 +58,9 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
   /* If nonzero, the user gave us the `-v' flag.  */
   int saw_verbose_flag = 0;
 
+  /* If nonzero, the user gave us the '-m64' flag.  */
+  int saw_m64_flag = 0;
+
   /* This is a tristate:
      -1 means we should not link in libstdc++
      0  means we should link in libstdc++ if it is needed
@@ -189,6 +192,10 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
 	    shared_libgcc = 0;
 	  else if (DEFAULT_WORD_SWITCH_TAKES_ARG (&argv[i][1]))
 	    i++;
+	  else if (strcmp (argv[i], "-m64") == 0)
+	    saw_m64_flag = 1;
+	  else if (strcmp (argv[i], "-m32") == 0)
+	    saw_m64_flag = 0;
 	  else
 	    /* Pass other options through.  */
 	    continue;
@@ -245,7 +252,7 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
 #endif
 
   /* Make sure to have room for the trailing NULL argument.  */
-  num_args = argc + added + need_math + shared_libgcc + (library > 0) + 1;
+  num_args = argc + added + need_math + shared_libgcc + 2 * (library > 0) + 1;
   arglist = xmalloc (num_args * sizeof (char *));
 
   i = 0;
@@ -303,6 +310,9 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
   /* Add `-lstdc++' if we haven't already done so.  */
   if (library > 0)
     {
+      arglist[j++] = saw_m64_flag
+	? "-R" LIBDIR "/amd64"
+	: "-R" LIBDIR;
       arglist[j++] = saw_profile_flag ? LIBSTDCXX_PROFILE : LIBSTDCXX;
       added_libraries++;
     }
