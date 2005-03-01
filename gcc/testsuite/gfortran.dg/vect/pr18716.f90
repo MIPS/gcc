5e97eb566275eb566ed8903d37c08a2c15894bad
dorit <dorit@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-01 20:04:11 +0000

    * gfortran.dg/vect: New.         * gfortran.dg/vect/pr18716.f90: New.         * gfortran.dg/vect/vect-1.f90: New.         * gfortran.dg/vect/vect.exp: New.
            * lib/target-supports.exp (check_vmx_hw_available): Add -maltivec
            option for darwin.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/autovect-branch@95753 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/vect/pr18716.f90
@@ -0,0 +1,15 @@
+! { dg-do compile }
+! { dg-require-effective-target vect_float }
+! PR tree-opt/18716
+subroutine test(n, v)
+  implicit none
+  integer :: n, i
+  real(4) :: v(n)
+
+  do i = 1, n
+    v(i) = 0
+  end do
+
+  return
+end
+! { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } }
