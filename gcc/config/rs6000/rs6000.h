099eb8803430be9518f57f6edea15e378449be0e
dalej <dalej@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-06 03:22:26 +0000

    2005-03-04  Dale Johannesen  <dalej@apple.com>
    	Radar 4025985 (partial)
    	* config/rs6000/rs6000.c:  Restore 64-bit ABI from mainline.
    	* config/rs6000/rs6000.h:  Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@95963 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -1732,6 +1732,11 @@ typedef struct rs6000_args
   int stdarg;			/* Whether function is a stdarg function.  */
   int call_cookie;		/* Do special things for this call */
   int sysv_gregno;		/* next available GP register */
+  /* APPLE LOCAL begin mainline */
+  int intoffset;		/* running offset in struct (darwin64) */
+  int use_stack;		/* any part of struct on stack (darwin64) */
+  int named;			/* false for varargs params */
+  /* APPLE LOCAL end mainline  */
 } CUMULATIVE_ARGS;
 
 /* Initialize a variable CUM of type CUMULATIVE_ARGS
