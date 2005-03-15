9edaa30bc516fed4ef9245e9948c377f84866d90
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-15 00:54:19 +0000

    * gcc.dg/dfp/loop.c: Correct dg-options. 	(main): Don't produce stray test output.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@96460 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/loop.c
+++ gcc/testsuite/gcc.dg/dfp/loop.c
@@ -1,20 +1,19 @@
 /* { dg-do run } */
-/* { dg-options "std=gnu99" } */
+/* { dg-options "-std=gnu99" } */
 
 #include <stdio.h>
 extern void abort (void);
 
 int main()
 {
-  _Decimal32 d;
   int i;
+  _Decimal32 d;
 
   for (d = 1.1, i=0; d <= 1.5; d+= 0.1, i++)
-    printf("d = %f\n", d);
+    ;
 
   if (i != 5)
     abort();
   
   return (0);
-
 }
