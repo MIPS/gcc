7d5b8d480d5846cca803606a9222687f71fcd293
pbrook <pbrook@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-03-29 03:00:11 +0000

    2005-03-29  Paul Brook  <paul@codesourcery.com>
    	* config/arm/arm-protos.h (arm_dbx_register_number): Add prototype.
    	* config/arm/arm.c (arm_dbx_register_number): New function.
    	* config/arm/arm.h (IS_FPA_REGNUM, DBX_REGISTER_NUMBER): Define.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-arm-branch@97149 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/arm/arm-protos.h
+++ gcc/config/arm/arm-protos.h
@@ -39,6 +39,7 @@ extern HOST_WIDE_INT arm_compute_initial_elimination_offset (unsigned int,
 extern HOST_WIDE_INT thumb_compute_initial_elimination_offset (unsigned int,
 							       unsigned int);
 extern void arm_output_fn_unwind (FILE *, bool);
+extern unsigned int arm_dbx_register_number (unsigned int);
 
 #ifdef TREE_CODE
 extern int arm_return_in_memory (tree);
