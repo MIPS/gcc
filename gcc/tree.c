bc4845b188fee1d467023e74d9b943c4f4e9552e
zlaski <zlaski@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-04-02 21:37:36 +0000

    [gcc/ChangeLog.apple-ppc] 2005-03-30  Ziemowit Laski  <zlaski@apple.com>
            Radar 4072153
            * tree.c (build2_stat): Never mark tcc_expression-class nodes
            as constant, unless AltiVec PIM support has been selected
            and we have a compound expression with constant elements.
    
    [gcc/testsuite/ChangeLog.apple-ppc]
    2005-03-30  Ziemowit Laski  <zlaski@apple.com>
    
            Radar 4072153
            * gcc.apple/comma-expr-1.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-ppc-branch@97463 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -2566,8 +2566,10 @@ build2_stat (enum tree_code code, tree tt, tree arg0, tree arg1 MEM_STAT_DECL)
   /* Expressions without side effects may be constant if their
      arguments are as well.  */
   constant = (TREE_CODE_CLASS (code) == tcc_comparison
-	      /* APPLE LOCAL Altivec */
-	      || TREE_CODE_CLASS (code) == tcc_expression
+	      /* APPLE LOCAL begin Altivec */
+	      || (targetm.cast_expr_as_vector_init
+		  && code == COMPOUND_EXPR)
+	      /* APPLE LOCAL end AltiVec */
 	      || TREE_CODE_CLASS (code) == tcc_binary);
   read_only = 1;
   side_effects = TREE_SIDE_EFFECTS (t);
