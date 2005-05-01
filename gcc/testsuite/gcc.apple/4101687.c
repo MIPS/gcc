26b9fad1e8945a66a6d83dcd451a80961725479f
jonz <jonz@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-01 07:32:30 +0000

    Radar 4101687         * gcc.apple/4101687.c: clean up
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@99057 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.apple/4101687.c
+++ gcc/testsuite/gcc.apple/4101687.c
@@ -2,37 +2,25 @@
 /* { dg-do run } */
 /* { dg-options "-Os" } */
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-char *
-MD5End(void *ctx, char *buf)
+void
+fill(char *buf)
 {
     int i;
-    unsigned char digest[16];
-    static const char hex[]="0123456789abcdef";
 
-    if (!buf)
-        buf = malloc(2*16 + 1);
-    if (!buf)
-        return 0;
-    memset(digest, 0x31, 16);
     for (i = 0; i < 16; i++) {
-        buf[i+i] = hex[digest[i] >> 4];
-        buf[i+i+1] = hex[digest[i] & 0x0f];
+        buf[i+i] = 2;
+        buf[i+i+1] = 3;
     }
     buf[i+i] = '\0';
-    return buf;
 }
-int main(int argc, char *argv[])
-{
- char buf[33];
 
- if (!MD5End(((void *)0), buf)) {
-   fprintf((&__sF[2]), "error calculating md5\n");
-   return 1;
- }
-  if (buf[16] == '\0')
-    abort ();
- return 0;
+int main()
+{
+    char buf[33];
+
+    fill(buf);
+    if (buf[16] == '\0')
+      abort ();
+    return 0;
 }
