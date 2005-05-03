8f69559b22e566d2847fcef9b32349b2145aa10c
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-03 22:57:27 +0000

    cvs update -Pd -jgcc-4_0-branch:2005{"0428 12:00","0503 21:00"}
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-4_0-rhl-branch@99191 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -5,7 +5,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "4.0.0 20050423 (Red Hat 4.0.0-2)";
+const char version_string[] = "4.0.0 20050503 (Red Hat 4.0.0-3)";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
