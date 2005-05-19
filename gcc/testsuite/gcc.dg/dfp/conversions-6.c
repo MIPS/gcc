54d925b4490c02ba7cc7ca890ea56f925386c23e
jgrimm <jgrimm@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-19 20:35:28 +0000

    More testcases to excercise conversion paths.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@99983 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/dfp/conversions-6.c
@@ -0,0 +1,77 @@
+/* { dg-do run } */
+/* { dg-options "-std=gnu99 -O3" } */
+
+/* Test various conversions involving decimal floating types. */
+#include <stdio.h>
+extern void abort (void);
+
+_Decimal32 d32;
+_Decimal64 d64;
+_Decimal128 d128;
+
+int main()
+{
+  /* Conversions between decimal floats.  Similar to conversions-4.c
+     but most of the checks below should be optimized away, exercising
+     different code-paths in GCC. */
+   
+  d64 = 1.5df;
+  if (d64 != 1.5dd)
+    abort();
+
+  d128 = 1.5df;
+  if (d128 != 1.5dl)
+    abort();
+
+  /* Conversions from _Decimal64. */
+  d32 = -1.5dd;
+  if (d32 != -1.5df)
+    abort();
+  
+  d128 = -1.5dd;
+  if (d128 != -1.5dl)
+    abort();
+
+  /* Conversions from _Decimal128. */
+  d32 = 2.15dl;
+  if (d32 != 2.15df)
+    abort();
+
+  d64 = 2.15dl;
+  if (d64 != 2.15dd)
+    abort();
+
+  /* Test demotion to non-representable decimal floating type. */
+  /* Assumes a default rounding mode of 'near'.  The rules are a 
+     bit odd in that if off by one digit, it rounds to the maximum
+     value, but otherwise to HUGE_VAL. */
+  d32 = 9.9999991E96DD;
+  if (d32 != __DEC32_MAX__)
+    abort();
+
+  d32 = 9.99999912E96DD;
+  if (d32 != __builtin_infd32())
+    abort();
+
+  /* One digit more than _Decimal32 can handle. */
+  d32 = 9.9999991E96DD;
+  if (d32 != __DEC32_MAX__)
+    abort();
+
+  /* Two digits more than _Decimal32 can handle. */
+  d32 = 9.99999912E96DD;
+  if (d32 != __builtin_infd32())
+    abort();
+
+  /* One digit more that _Decimal64 can handle. */
+  d64 = 9.9999999999999991E384DL;
+  if (d64 != __DEC64_MAX__)
+    abort();
+
+  /* Two digits more than _Decimal64 can handle. */
+  d64 = 9.99999999999999912E384DL;
+  if (d64 != __builtin_infd64())
+    abort();
+
+  return 0;
+}
--- gcc/testsuite/gcc.dg/dfp/conversions-7.c
+++ gcc/testsuite/gcc.dg/dfp/conversions-7.c
@@ -5,98 +5,93 @@
 #include <stdio.h>
 extern void abort (void);
 
-volatile _Decimal32 d32;
-volatile _Decimal64 d64;
-volatile _Decimal128 d128;
-volatile float sf;
-volatile double df;
-volatile long double tf;
+_Decimal32 d32;
+_Decimal64 d64;
+_Decimal128 d128;
+float sf;
+double df;
+long double tf;
 
 int main()
 {
   /* Conversions between decimal floats. */
 
   /* Conversions from _Decimal32. */
-  d32 = 2.0df;
-  sf = d32;
+  sf = 2.0df;
   if (sf != 2.0)
     abort();
 
-  df = d32;
+  df = 2.0df;
   if (df != 2.0)
     abort();
 
-  tf = d32;
+  tf = 2.0df;
   if (tf != 2.0)
     abort();
 
   /* Conversions from _Decimal64. */
-  d64 = -7.0dd;
-  sf = d64;
+  sf = -7.0dd;
   if (sf != -7.0)
     abort();
   
-  df = d64;
+  df = -7.0dd;
   if (df != -7.0)
     abort();
 
-  tf = d64;
+  tf = -7.0dd;
   if (tf != -7.0)
     abort();
 
   /* Conversions from _Decimal128. */
-  d128 = 30.0dl;
-  sf = d128;
+  sf = 30.0dl;
   if (sf != 30.0)
     abort();
 
-  df = d128;
+  df = 3.0dl;
   if (df != 3.0)
     abort();
 
-  df = d128;
+  df = 3.0dl;
   if (df != 3.0)
     abort();
 
   /* Conversions to regular floating point. */
-  sf = 30.0f;
-  d32 = sf;
+  d32 = 30.0f;
   if (d32 != 30.0df)
     abort();
 
-  d64 = sf;
+  d64 = 30.0f;
   if (d64 != 30.0dd)
     abort();
 
-  df = -2.0;
-  d32 = df;
-  if (d32 != -2.0df)
-    abort();
-
-  d64 = df;
-  if (d64 != -2.0dd)
-    abort();
-
-  d128 = df;
-  if (d128 != -2.0dl)
-    abort();
-  
-  d128 = sf;
+  d128 = 30.0f;
   if (d128 != 30.0dl)
     abort();
 
-  tf = -22.0l;
-  d32 = tf;
+  d32 = -2.0;
+  if (d32 != -2.0df)
+    abort();
+
+  d64 = -2.0;
+  if (d64 != -2.0dd)
+    abort();
+
+  d128 = -2.0;
+  if (d128 != -2.0dl)
+    abort();
+  
+  d32 = -22.0l;
   if (d32 != -22.0df)
     abort();
 
-  d64 = tf;
+  d64 = -22.0l;
   if (d64 != -22.0dd)
     abort();
 
-  d128 = tf;
+  d128 = -22.0l;
   if (d128 != -22.0dl)
     abort();
+
 
   /* Test demotion to non-representable decimal floating type. */
   /* Assumes a default rounding mode of 'near'.  The rules are a 
