46aedf6f621fb4c84ef28f5fe878154df3131d70
jgrimm <jgrimm@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-20 22:11:02 +0000

    Add tests for constant conversions involving integers.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@100030 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/dfp/conversions-8.c
@@ -0,0 +1,67 @@
+/* { dg-do run } */
+/* { dg-options "-std=gnu99" } */
+
+/* Test various conversions involving decimal floating types. */
+extern void abort (void);
+
+_Decimal32 d32;
+_Decimal64 d64;
+_Decimal128 d128;
+unsigned int usi;
+unsigned long long udi;
+
+int main()
+{
+  /* Constant conversions to unsigned integers. */
+  usi = 1.50df;
+
+  if (usi != 1)
+    abort();
+
+  udi = 1.50df;
+  if (udi != 1)
+    abort();
+
+  usi = 1.55dd;
+  if (usi != 1)
+    abort();
+
+  udi = 1.55dd;
+  if (udi != 1)
+    abort();
+
+  usi = 1555.55dl;
+  if (usi != 1555)
+    abort();
+
+  udi = 1555.55dl;
+  if (udi != 1555)
+    abort();
+
+  /* Conversions from unsigned integer constants.*/
+  d32 = 1555;
+  if (d32 != 1555.0df)
+    abort();
+
+  d32 = 1555LL;
+  if (d32 != 1555.0df)
+    abort();
+
+  d64 = 1555;
+  if (d64 != 1555.0dd)
+    abort();
+
+  d64 = 1555LL;
+  if (d64 != 1555.0dd)
+    abort();
+
+  d128 = 1555;
+  if (d128 != 1555.0dl)
+    abort();
+
+  d128 = 1555LL;
+  if (d128 != 1555.0dl)
+    abort();
+
+  return 0;
+}
---
+++ gcc/testsuite/gcc.dg/dfp/conversions-9.c
@@ -0,0 +1,68 @@
+/* { dg-do run } */
+/* { dg-options "-std=gnu99" } */
+
+/* Test various conversions involving decimal floating types. */
+
+extern void abort (void);
+
+_Decimal32 d32;
+_Decimal64 d64;
+_Decimal128 d128;
+signed int si;
+signed long long di;
+
+int main()
+{
+
+  /* Constant conversions to signed integers. */
+  si = -1.50df;
+  if (si != -1)
+    abort();
+
+  di = -1.50df;
+  if (di != -1)
+    abort();
+
+  si = -1.55dd;
+  if (si != -1)
+    abort();
+
+  di = -1.55dd;
+  if (di != -1)
+    abort();
+
+  si = -1555.55dl;
+  if (si != -1555)
+    abort();
+
+  di = -1555.55dl;
+  if (di != -1555)
+    abort();
+
+  /* Conversions from signed integer constants. */
+  d32 = -1555;
+  if (d32 != -1555.0df)
+    abort();
+
+  d32 = -1555ll;
+  if (d32 != -1555.0df)
+    abort();
+
+  d64 = -1555;
+  if (d64 != -1555.0dd)
+    abort();
+
+  d64 = -1555ll;
+  if (d64 != -1555.0dd)
+    abort();
+
+  d128 = -1555;
+  if (d128 != -1555.0dl)
+    abort();
+
+  d128 = -1555ll;
+  if (d128 != -1555.0dl)
+    abort();
+
+  return 0;
+}
