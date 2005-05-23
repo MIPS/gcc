fe9b2333ac95c338d29d0fe676facad2bde002a4
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-23 10:29:03 +0000

    * gfortran.dg/vect/vect-4.f90: Only one unaligned access.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/autovect-branch@100079 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/vect/vect-4.f90
+++ gcc/testsuite/gfortran.dg/vect/vect-4.f90
@@ -8,4 +8,4 @@ END
 
 ! { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail vect_no_align } } } 
 ! { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail vect_no_align } } }
-! { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 2 "vect" { xfail vect_no_align } } }
+! { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" { xfail vect_no_align } } }
