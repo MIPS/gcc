b9dae8c54ca6b3f85237db5f309223ae6e36edf1
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-05-26 17:53:53 +0000

    PR rtl-optimization/21239 	* combine.c (combine_simplify_rtx) <case VEC_SELECT>: Fix a typo.
    	* gcc.dg/i386-sse-11.c: New test.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-3_4-rhl-branch@100214 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/i386-sse-11.c
+++ gcc/testsuite/gcc.dg/i386-sse-11.c
@@ -18,57 +18,20 @@ foo (unsigned int x, double *y, const double *z)
     }
 }
 
-void
-bar (unsigned int x, float *y, const float *z)
-{
-  __m128 tmp;
-  unsigned int i;
-  for (i = 0; i < x; ++i)
-    {
-      tmp = (__m128) { *z, 0, 0, 0 };
-      *y = __builtin_ia32_vec_ext_v4sf (tmp, 0);
-      ++z; ++y;
-    }
-  for (i = 0; i < x; ++i)
-    {
-      tmp = (__m128) { 0, *z, 0, 0 };
-      *y = __builtin_ia32_vec_ext_v4sf (tmp, 1);
-      ++z; ++y;
-    }
-  for (i = 0; i < x; ++i)
-    {
-      tmp = (__m128) { 0, 0, *z, 0 };
-      *y = __builtin_ia32_vec_ext_v4sf (tmp, 2);
-      ++z; ++y;
-    }
-  for (i = 0; i < x; ++i)
-    {
-      tmp = (__m128) { 0, 0, 0, *z };
-      *y = __builtin_ia32_vec_ext_v4sf (tmp, 3);
-      ++z; ++y;
-    }
-}
-
 void __attribute__((noinline))
 run_tests (void)
 {
   unsigned int i;
   double a[16], b[16];
-  float c[16], d[16];
   for (i = 0; i < 16; ++i)
     {
       a[i] = 1;
       b[i] = 2;
-      c[i] = 3;
-      d[i] = 4;
     }
   foo (16, a, b);
-  bar (4, c, d);
   for (i = 0; i < 16; ++i)
     {
       if (a[i] != 2)
-	abort ();
-      if (c[i] != 4)
 	abort ();
     }
 }
