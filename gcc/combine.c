67089bbdabf822a7a5ddfbabdb4f14aef2283f93
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-20 18:04:35 +0000

    Radar 4149154. Also in FSF mainline.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@101213 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/combine.c
+++ gcc/combine.c
@@ -5252,6 +5252,13 @@ simplify_set (rtx x)
 	  SUBST (SET_SRC (x), gen_rtx_COMPARE (compare_mode, op0, op1));
 	  src = SET_SRC (x);
 	}
+      /* APPLE LOCAL begin radar 4149154 */
+      else if (GET_MODE (op0) == compare_mode && op1 == const0_rtx)
+	{
+	  SUBST(SET_SRC (x), op0);
+	  src = SET_SRC (x);
+        }
+      /* APPLE LOCAL end radar 4149154 */
       else
 	{
 	  /* Otherwise, update the COMPARE if needed.  */
