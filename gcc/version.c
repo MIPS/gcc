8a8033836225b5271e8d653d92df29b601dbaf8c
jonz <jonz@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-28 05:14:37 +0000

    Bump version to gcc-5207.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@101371 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/version.c
+++ gcc/version.c
@@ -16,7 +16,7 @@
      to get version number string. Do not use new line.
 */
 
-const char version_string[] = "4.0.0 (Apple Computer, Inc. build 5206)";
+const char version_string[] = "4.0.0 (Apple Computer, Inc. build 5207)";
 /* APPLE LOCAL end Apple version */
 
 /* This is the location of the online document giving instructions for
