56008e992a387fd0204c07141e5a9c4ac8ead538
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-06-29 15:07:39 +0000

    .: 	* loop-unswitch.c (unswitch_single_loop): Disable if condition is 	in a different bb to jump. testsuite: 	* gcc.c-torture/compile/20050629-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-arm-branch@101420 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.c-torture/compile/20050629-1.c
@@ -0,0 +1,14 @@
+void f(int type){
+    int tmp;
+	
+    for(tmp = 0; tmp < 2; tmp++){
+        switch(type) {
+        case 0: 
+		s1(); break;
+        case 1: 
+		s2(); break;
+	case 2:
+		s3(); break;
+	}
+    }
+}
