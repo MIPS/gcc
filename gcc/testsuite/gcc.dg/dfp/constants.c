121c1dff15a211936f913f889219aaf8769435a5
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-04 05:24:08 +0000

    * gcc.dg/dfp/constants.c: Reorganise. Add references to N1107. 	* gcc.dg/dfp/ctypes.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@101566 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/constants.c
+++ gcc/testsuite/gcc.dg/dfp/constants.c
@@ -1,30 +1,18 @@
 /* { dg-do compile } */
 /* { dg-options "-std=gnu99" } */
 
-/* Various decimal floating point constants.  */
-_Decimal32 a32 = 1.1df;
-_Decimal32 A32 = 10.1DF;
-_Decimal64 a64 = 2.2dd;
-_Decimal64 A64 = -20.2DD;
-_Decimal128 a128 = -3.3dl;
-_Decimal128 A128 = 30.3DL;
+/* Section 6 of WG14 N1107: constants.  */
 
-_Decimal32 b32 = -11e-1df;
-_Decimal32 B32 = .101e2DF;
-_Decimal64 b64 = 2.2e-0dd;
-_Decimal64 B64 = .00000202e2dd;
-_Decimal128 b128 = 000.3e0dl;
-_Decimal128 B128 = 3000300030003e0DL;
+_Decimal32 arr1[] =  { 1.1df, 10.1DF, -11e-1df, .101e2DF };
+_Decimal64 arr2[] =  { 2.2dd, -20.2DD, -3.3dl, 30.3DL };
+_Decimal128 arr3[] = { 000.3e0dl, 3000300030003e0DL };
 
 /* N1107 6.4.4.2: The df, dd, dl, DF, DD and DL suffixes shall not be
    used in a hexadecimal-floating-constant.  */
 
-_Decimal32 x32 = 0x1.0p1df;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-_Decimal32 X32 = 0x1.0p1DF;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-_Decimal64 x64 = 0x2.0p-2dd; /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-_Decimal64 X64 = 0x2.0p2DD;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-_Decimal128 x128 = 0x3.0p3dl;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-_Decimal128 X128 = 0x3.0p3DL;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
-
-
-
+_Decimal32 bad1 = 0x1.0p1df;   /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
+_Decimal32 bad2 = 0x1.0p1DF;   /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
+_Decimal64 bad3 = 0x2.0p-2dd;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
+_Decimal64 bad4 = 0x2.0p2DD;   /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
+_Decimal128 bad5 = 0x3.0p3dl;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
+_Decimal128 bad6 = 0x3.0p3DL;  /* { dg-error "invalid suffix" "hexadecimal floating constant" } */
--- gcc/testsuite/gcc.dg/dfp/ctypes.c
+++ gcc/testsuite/gcc.dg/dfp/ctypes.c
@@ -1,6 +1,14 @@
 /* { dg-do compile } */
 /* { dg-options "-std=gnu99" } */
 
+/* Tests for sections 3 and 4 of WG14 N1107.  */
+
+/* Section 3: Test for the existence of the types.  */
+_Decimal32 sd1;
+_Decimal32 dd2;
+_Decimal128 td3;
+
+/* Section 4: Test sizes for these types.  */
 int ssize[sizeof (_Decimal32) == 4 ? 1 : -1];
 int dsize[sizeof (_Decimal64) == 8 ? 1 : -1];
 int tsize[sizeof (_Decimal128) == 16 ? 1 : -1];
@@ -8,7 +16,3 @@ int tsize[sizeof (_Decimal128) == 16 ? 1 : -1];
 int salign = __alignof (_Decimal32);
 int dalign = __alignof (_Decimal64);
 int talign = __alignof (_Decimal128);
-
-_Decimal32 sd1;
-_Decimal32 dd2;
-_Decimal128 td3;
