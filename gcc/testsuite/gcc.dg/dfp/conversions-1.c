89ba1ed66f0184febf2c602cfb87293e44be28f5
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-07-04 06:31:52 +0000

    Whitespace tweaks.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@101568 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/conversions-1.c
+++ gcc/testsuite/gcc.dg/dfp/conversions-1.c
@@ -20,7 +20,6 @@ extern void abort (void);
 
 int main()
 {
-
   ASSERT_CONST_TYPE (1.3 + 2.1df, _Decimal32); /* { dg-bogus "assignment from incompatible pointer type" } */
   ASSERT_CONST_TYPE (1.3df + 2.1f, _Decimal32); /* { dg-bogus "assignment from incompat\ible pointer type" } */
   ASSERT_CONST_TYPE (1.55f - 55.0dd, _Decimal64); /* { dg-bogus "assignment from incompat\ible pointer type" } */
@@ -28,5 +27,6 @@ int main()
   ASSERT_CONST_TYPE (.88dl / .1DF, _Decimal128); /* { dg-bogus "assignment from incompat\ible pointer type" } */
   ASSERT_CONST_TYPE (.114df - 1.6dd, _Decimal64); /* { dg-bogus "assignment from incompat\ible pointer type" } */
   ASSERT_CONST_TYPE (1.3 - .11f + .55df, _Decimal32); /* { dg-bogus "assignment from incompat\ible pointer type" } */
+
   return 0;
 }
