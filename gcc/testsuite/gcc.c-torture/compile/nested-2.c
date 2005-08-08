8e8d6c70c340782b0c1d177df3f491acceb97e43
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-08-08 19:02:59 +0000

    Radar 4135448 (PR 21984) from FSF mainline.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@102877 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.c-torture/compile/nested-2.c
+++ gcc/testsuite/gcc.c-torture/compile/nested-2.c
@@ -1,4 +1,5 @@
-/* PR 21105 */
+/* APPLE LOCAL begin mainline 2005-08-08 */
+/* PR 21894 */
 
 void
 CheckFile ()
@@ -14,3 +15,4 @@ CheckFile ()
   if (a == tagname)
     validate ();
 }
+/* APPLE LOCAL end mainline 2005-08-08 */
