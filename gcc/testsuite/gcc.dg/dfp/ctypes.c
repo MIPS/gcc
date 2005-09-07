54b74dd35ccaa84cdd6ca8db67f13010a8c2b9e7
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-07 22:03:06 +0000

    2005-09-08  Yao Qi  <qiyaoltc@cn.ibm.com>
    	* ctypes.c: Test size of arrays of decimal float types.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104014 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/ctypes.c
+++ gcc/testsuite/gcc.dg/dfp/ctypes.c
@@ -5,8 +5,14 @@
 
 /* Section 3: Test for the existence of the types.  */
 _Decimal32 sd1;
-_Decimal32 dd2;
+_Decimal64 dd2;
 _Decimal128 td3;
+
+#define ARRAY_SIZE      7
+
+static _Decimal32 d32[ARRAY_SIZE];
+static _Decimal64 d64[ARRAY_SIZE];
+static _Decimal128 d128[ARRAY_SIZE];
 
 /* Section 4: Test sizes for these types.  */
 int ssize[sizeof (_Decimal32) == 4 ? 1 : -1];
@@ -16,3 +22,21 @@ int tsize[sizeof (_Decimal128) == 16 ? 1 : -1];
 int salign = __alignof (_Decimal32);
 int dalign = __alignof (_Decimal64);
 int talign = __alignof (_Decimal128);
+
+/* operator of sizeof applied on an array of DFP types is n times
+   the size of a single variable of this type.   */
+
+int d32_array_size [sizeof(d32) == ARRAY_SIZE * sizeof(sd1) ? 1 : -1];
+int d64_array_size [sizeof(d64) == ARRAY_SIZE * sizeof(dd2) ? 1 : -1];
+int d128_array_size [sizeof(d128) == ARRAY_SIZE * sizeof(td3)? 1 : -1];
+
+void f()
+{
+  _Decimal32 d32[ARRAY_SIZE];
+  _Decimal64 d64[ARRAY_SIZE];
+  _Decimal128 d128[ARRAY_SIZE];
+
+  int d32_array_size [sizeof(d32) == ARRAY_SIZE * sizeof(_Decimal32) ? 1 : -1];
+  int d64_array_size [sizeof(d64) == ARRAY_SIZE * sizeof(_Decimal64) ? 1 : -1];
+  int d128_array_size [sizeof(d128) == ARRAY_SIZE * sizeof(_Decimal128)? 1 : -1];
+}
