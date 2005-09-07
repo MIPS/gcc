ca164d237a7277ae8885ea2fc4cbef4f14a995cd
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-07 23:10:59 +0000

    2005-09-08  Yao Qi  <qiyaoltc@cn.ibm.com>
    	* nan-1.c: Add more cases.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104017 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/nan-1.c
+++ gcc/testsuite/gcc.dg/dfp/nan-1.c
@@ -43,6 +43,36 @@ int main()
   if (0.0df/0.0dl == __builtin_nand32(""))
     abort();
 
+  /* 0.0 * INF => NaN.  */
+  if (!__builtin_isnand32 (0.0df * __builtin_infd32()))
+    abort ();
+
+  if (!__builtin_isnand64 (0.0dd * __builtin_infd64()))
+    abort ();
+
+  if (!__builtin_isnand128 (0.0dd * __builtin_infd128()))
+    abort ();
+
+  /* INF - INF => NaN.  */
+  if (!__builtin_isnand32 (__builtin_infd32() - __builtin_infd32()))
+    abort ();
+
+  if (!__builtin_isnand64 (__builtin_infd64() - __builtin_infd64()))
+    abort ();
+
+  if (!__builtin_isnand128 (__builtin_infd128() - __builtin_infd128()))
+    abort ();
+
+  /* INF/INF => NaN.  */
+  if (!__builtin_isnand32 (__builtin_infd32()/__builtin_infd32()) )
+    abort ();
+
+  if (!__builtin_isnand64 (__builtin_infd64()/__builtin_infd64()) )
+    abort ();
+
+  if (!__builtin_isnand128 (__builtin_infd128()/__builtin_infd128()) )
+    abort ();
+  
   /* 0.0/0.0 => NaN, but NaN != NaN.  */
   if ((0.0dd/0.0df) == (0.0dd/0.0df))
     abort();
