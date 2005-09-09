3f61b9cb683c4e18f36ae9f28d011b4bd48452f1
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-09 06:41:52 +0000

    * dfp.c: Whitespace fix.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104079 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dfp.c
+++ gcc/dfp.c
@@ -17,6 +17,7 @@
    along with GCC; see the file COPYING.  If not, write to the Free
    Software Foundation, 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA.  */
+
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
