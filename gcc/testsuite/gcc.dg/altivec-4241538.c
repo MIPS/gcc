5b0512ec8044f5db10cdc2875e2805887169c0d0
dpatel <dpatel@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-16 21:52:23 +0000

    * gcc.dg/altivec-4241538.c: Fix.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@104356 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/altivec-4241538.c
+++ gcc/testsuite/gcc.dg/altivec-4241538.c
@@ -6,8 +6,8 @@
 int main( int argc, char **argv )
 {
     float array[ 4 ];
-    vector float some_vector = vec_ld( array );
+    vector float some_vector = vec_ld( array ); /* { dg-error "invalid argument|incompatible types in initialization" } */
+
     return 0;
 }
-
 
