b7e734457bd2d5750f1d8448a8f213b84bcd9f74
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-19 07:02:08 +0000

    * gcc.dg/dfp/arg-passing-4.c: New test. 	* gcc.dg/dfp/operatorspec-1.c: Likewise. 	* gcc.dg/dfp/operatorspec-3.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104415 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/dfp/operatorspec-3.c
@@ -0,0 +1,44 @@
+/* { dg-do run } */
+/* { dg-options "-std=gnu99" } */
+
+extern void abort (void);
+
+#define AUTO_INCREASE_DECREASE(TYPE,SUFFIX) \
+do \
+{ \
+  _Decimal##TYPE in_de_d##TYPE = 0.0##SUFFIX; \
+  if (in_de_d##TYPE++) abort (); \
+  if (--in_de_d##TYPE) abort (); \
+  if (++in_de_d##TYPE == 0.0##SUFFIX) abort (); \
+  if (in_de_d##TYPE-- == 0.0##SUFFIX) abort (); \
+} while(0)
+
+#define UNARY_OPERATOR(TYPE,SUFFIX) \
+do \
+{ \
+ _Decimal##TYPE unary_d##TYPE = 1.0##SUFFIX; \
+ _Decimal##TYPE* unary_dp##TYPE; \
+ unary_dp##TYPE = &(unary_d##TYPE); /*  & operator.  */ \
+ unary_d##TYPE = *(unary_dp##TYPE); /*  * operator.  */ \
+ unary_d##TYPE = -unary_d##TYPE; /*  - operator.  */ \
+ if (unary_d##TYPE != -1.0##SUFFIX) abort (); \
+ unary_d##TYPE = +unary_d##TYPE; /*  + operator.  */ \
+ if (unary_d##TYPE != -1.0##SUFFIX) abort (); \
+ if (!unary_d##TYPE) abort (); /*  ! operator.  */ \
+} while(0)
+
+int
+main ()
+{
+  /*  C99 6.5.3.1 Prefix increment and decrement operators. */
+  AUTO_INCREASE_DECREASE(32, DF);
+  AUTO_INCREASE_DECREASE(64, DD);
+  AUTO_INCREASE_DECREASE(128, DL);
+
+  /*  C99 6.5.3 Unary operators.  */
+  UNARY_OPERATOR(32, DF);
+  UNARY_OPERATOR(64, DD);
+  UNARY_OPERATOR(128, DL);
+
+  return 0;
+}
