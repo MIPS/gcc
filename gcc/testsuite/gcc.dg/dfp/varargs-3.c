ec62a286a877bd8d78faa8a67f1f10257dd3049f
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-20 01:46:55 +0000

    * gcc.dg/dfp/varargs-3.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104440 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/dfp/varargs-3.c
@@ -0,0 +1,42 @@
+/* { dg-do run } */
+/* { dg-options "-std=gnu99" } */
+
+#include <stdarg.h>
+
+extern void abort ();
+
+struct S1
+{
+  struct
+  {
+    _Decimal128 e;
+  } b[0];
+};
+
+/* Test handling vararg parameters whose size is 0.  */
+
+int check_var(int z,...)
+{
+  double d;
+  struct S1 s1;
+  long long result;
+  va_list ap;
+  va_start (ap, z);
+  d = va_arg (ap, double);
+  s1 = va_arg (ap, struct S1);
+  result = va_arg (ap, long long);
+  va_end (ap);
+  return (result == 2LL);
+
+}
+
+int main ()
+{
+  struct S1 s1;
+  struct S1 a1[5];
+
+  if (check_var(5, 1.0, s1, 2LL, a1[2], a1[2]) == 0)
+    abort ();
+
+  return 0;
+}
