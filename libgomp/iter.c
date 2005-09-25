fe9b708b5a08bee5c51e292826c2d9d326a17281
rth <rth@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-25 05:13:56 +0000

    * iter.c (gomp_iter_static_next): Round up when computing number         of iterations.  Don't bother distributing a remainder equally.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-20050608-branch@104617 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/iter.c
+++ libgomp/iter.c
@@ -62,76 +62,75 @@ gomp_iter_static_next (long *pstart, long *pend)
      trip through the outer loop.  */
   if (ws->chunk_size == 0)
     {
-      unsigned long n, q, r, i;
-      long s0, e0, s, e;
+      unsigned long n, q, i;
+      unsigned long s0, e0;
+      long s, e;
 
       if (thr->ts.static_trip > 0)
 	return 1;
 
       /* Compute the total number of iterations.  */
-      n = (ws->end - ws->next) / ws->incr;
+      s = ws->incr + (ws->incr > 0 ? -1 : 1);
+      n = (ws->end - ws->next + s) / ws->incr;
       i = thr->ts.team_id;
-      q = n / nthreads;
-      r = n % nthreads;
 
       /* Compute the "zero-based" start and end points.  That is, as
-         if the loop began at zero and incremented by one.  We begin
-         with the straight quotient and adjust that to evenly 
-         distribute the remainder amongst the lowest numbered threads.  */
+         if the loop began at zero and incremented by one.  */
+      q = n / nthreads;
+      q += (q * nthreads != n);
       s0 = q * i;
-      if (r)
-        s0 += i > r ? r : i;
       e0 = s0 + q;
-      if (r)
-        e0 += i < r;
-
-      /* Transform these to the actual start and end numbers.  */
-      s = s0 * ws->incr + ws->next;
-      e = e0 * ws->incr + ws->next;
+      if (e0 > n)
+        e0 = n;
 
       /* Notice when no iterations allocated for this thread.  */
-      if (s == e)
+      if (s0 >= e0)
 	{
 	  thr->ts.static_trip = 1;
 	  return 1;
 	}
 
+      /* Transform these to the actual start and end numbers.  */
+      s = (long)s0 * ws->incr + ws->next;
+      e = (long)e0 * ws->incr + ws->next;
+
       *pstart = s;
       *pend = e;
-      thr->ts.static_trip = (e == ws->end ? -1 : 1);
+      thr->ts.static_trip = (e0 == n ? -1 : 1);
       return 0;
     }
   else
     {
-      unsigned long n, s0, n0, i, c;
+      unsigned long n, s0, e0, i, c;
       long s, e;
 
       /* Otherwise, each thread gets exactly chunk_size iterations
 	 (if available) each time through the loop.  */
 
-      n = (ws->end - ws->next) / ws->incr;
+      s = ws->incr + (ws->incr > 0 ? -1 : 1);
+      n = (ws->end - ws->next + s) / ws->incr;
       i = thr->ts.team_id;
       c = ws->chunk_size;
 
       /* Initial guess is a C sized chunk positioned nthreads iterations
 	 in, offset by our thread number.  */
       s0 = (thr->ts.static_trip * nthreads + i) * c;
-      n0 = c;
+      e0 = s0 + c;
 
       /* Detect overflow.  */
       if (s0 >= n)
 	return 1;
-      if (n - s0 < n0)
-	n0 = n - s0;
+      if (e0 > n)
+	e0 = n;
 
       /* Transform these to the actual start and end numbers.  */
-      s = s0 * ws->incr + ws->next;
-      e = n0 * ws->incr + s;
+      s = (long)s0 * ws->incr + ws->next;
+      e = (long)e0 * ws->incr + ws->next;
 
       *pstart = s;
       *pend = e;
 
-      if (e == ws->end)
+      if (e0 == n)
 	thr->ts.static_trip = -1;
       else
 	thr->ts.static_trip++;
