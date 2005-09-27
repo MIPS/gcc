aefeab6568840cdd65f1cec7fecc26c07aeb165f
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-27 16:43:27 +0000

    * testsuite/libgomp.dg/omp-parallel-for.c: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-20050608-branch@104700 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ libgomp/testsuite/libgomp.dg/omp-parallel-for.c
@@ -0,0 +1,19 @@
+extern void abort (void);
+
+main()
+{
+  int i, a;
+
+  a = 30;
+
+#pragma omp parallel for firstprivate (a) lastprivate (a) num_threads (2)
+  for (i = 0; i < 10; i++)
+    a = a + i;
+
+  /* The thread that owns the last iteration will have computed
+     30 + 5 + 6 + 7 + 8 + 9 = 65.  */
+  if (a != 65)
+    abort ();
+
+  return 0;
+}
