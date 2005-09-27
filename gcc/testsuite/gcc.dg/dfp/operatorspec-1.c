c31858d9c27ee4c232cc381bfd52238ca41146cb
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-27 06:15:22 +0000

    * gcc.dg/dfp/operatorspec-1.c (DECIMAL_COMPOUND_ASSIGNMENT): 	Reformat comments in macro defintion. 	* gcc.dg/dfp/operatorspec-3.c (UNARY_OPERATOR): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104690 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/operatorspec-1.c
+++ gcc/testsuite/gcc.dg/dfp/operatorspec-1.c
@@ -3,75 +3,76 @@
 
 extern void abort (void);
 
-#define OPERATE(OPRD1,OPRT,OPRD2,RLT) \
-do \
-{ \
-  if (( (OPRD1) OPRT (OPRD2) )!= RLT) \
-    abort (); \
+#define OPERATE(OPRD1,OPRT,OPRD2,RLT)         \
+do                                    \
+{                                     \
+  if (( (OPRD1) OPRT (OPRD2) )!= RLT)         \
+    abort ();                                 \
 }while(0)
 
-#define DECIMAL_LOGICAL_OR(OPRD) \
-do \
-{ \
-  OPRD = 1.0; \
-  OPERATE(1,||,OPRD,1); \
-  OPERATE(0,||,OPRD,1); \
-  OPERATE(OPRD,||,1,1); \
-  OPERATE(OPRD,||,0,1); \
-  OPRD = 0.0; \
-  OPERATE(1,||,OPRD,1); \
-  OPERATE(0,||,OPRD,0); \
-  OPERATE(OPRD,||,1,1); \
-  OPERATE(OPRD,||,0,0); \
+#define DECIMAL_LOGICAL_OR(OPRD)      \
+do                                    \
+{                                     \
+  OPRD = 1.0;                                 \
+  OPERATE(1,||,OPRD,1);               \
+  OPERATE(0,||,OPRD,1);               \
+  OPERATE(OPRD,||,1,1);               \
+  OPERATE(OPRD,||,0,1);               \
+  OPRD = 0.0;                                 \
+  OPERATE(1,||,OPRD,1);               \
+  OPERATE(0,||,OPRD,0);               \
+  OPERATE(OPRD,||,1,1);               \
+  OPERATE(OPRD,||,0,0);               \
 }while(0)
 
-#define DECIMAL_LOGICAL_AND(OPRD) \
-do \
-{ \
-  OPRD = 1.0; \
-  OPERATE(1,&&,OPRD,1); \
-  OPERATE(0,&&,OPRD,0); \
-  OPERATE(OPRD,&&,1,1); \
-  OPERATE(OPRD,&&,0,0); \
-  OPRD = 0.0; \
-  OPERATE(1,&&,OPRD,0); \
-  OPERATE(0,&&,OPRD,0); \
-  OPERATE(OPRD,&&,1,0); \
-  OPERATE(OPRD,&&,0,0); \
+#define DECIMAL_LOGICAL_AND(OPRD)     \
+do                                    \
+{                                     \
+  OPRD = 1.0;                                 \
+  OPERATE(1,&&,OPRD,1);               \
+  OPERATE(0,&&,OPRD,0);               \
+  OPERATE(OPRD,&&,1,1);               \
+  OPERATE(OPRD,&&,0,0);               \
+  OPRD = 0.0;                                 \
+  OPERATE(1,&&,OPRD,0);               \
+  OPERATE(0,&&,OPRD,0);               \
+  OPERATE(OPRD,&&,1,0);               \
+  OPERATE(OPRD,&&,0,0);               \
 }while(0)
 
-
-#define DECIMAL_COMPOUND_ASSIGNMENT(TYPE, OPRD) \
-do \
-{\
-  _Decimal##TYPE _d##TYPE = OPRD; /* Compound assignment of addition.  */ \
-  OPERATE(OPRD,+=,1,(_d##TYPE+1)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,0,(_d##TYPE)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,-1,(_d##TYPE-1)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,1.25,(_d##TYPE+1.25)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,1.2df,(_d##TYPE+1.2df)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,2.8dd,(_d##TYPE+2.8dd)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,+=,4.7dl,(_d##TYPE+4.7dl)); \
-  _d##TYPE = OPRD; /* Compound assignment of subtraction.  */ \
-  OPERATE(OPRD,-=,1,(_d##TYPE-1)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,0,(_d##TYPE)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,-1,(_d##TYPE+1)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,1.25,(_d##TYPE-1.25)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,1.2df,(_d##TYPE-1.2df)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,2.8dd,(_d##TYPE-2.8dd)); \
-  _d##TYPE = OPRD; \
-  OPERATE(OPRD,-=,4.7dl,(_d##TYPE-4.7dl)); \
+#define DECIMAL_COMPOUND_ASSIGNMENT(TYPE, OPRD)       \
+do                                                    \
+{                                                     \
+  _Decimal##TYPE _d##TYPE = OPRD;                     \
+  /* Compound assignment of addition.  */             \
+  OPERATE(OPRD,+=,1,(_d##TYPE+1));                    \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,0,(_d##TYPE));                      \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,-1,(_d##TYPE-1));                   \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,1.25,(_d##TYPE+1.25));              \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,1.2df,(_d##TYPE+1.2df));            \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,2.8dd,(_d##TYPE+2.8dd));            \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,+=,4.7dl,(_d##TYPE+4.7dl));            \
+  _d##TYPE = OPRD;                                    \
+  /* Compound assignment of subtraction.  */          \
+  OPERATE(OPRD,-=,1,(_d##TYPE-1));                    \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,0,(_d##TYPE));                      \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,-1,(_d##TYPE+1));                   \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,1.25,(_d##TYPE-1.25));              \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,1.2df,(_d##TYPE-1.2df));            \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,2.8dd,(_d##TYPE-2.8dd));            \
+  _d##TYPE = OPRD;                                    \
+  OPERATE(OPRD,-=,4.7dl,(_d##TYPE-4.7dl));            \
 }while(0)
 
 int main()
--- gcc/testsuite/gcc.dg/dfp/operatorspec-3.c
+++ gcc/testsuite/gcc.dg/dfp/operatorspec-3.c
@@ -3,29 +3,33 @@
 
 extern void abort (void);
 
-#define AUTO_INCREASE_DECREASE(TYPE,SUFFIX) \
-do \
-{ \
-  _Decimal##TYPE in_de_d##TYPE = 0.0##SUFFIX; \
-  if (in_de_d##TYPE++) abort (); \
-  if (--in_de_d##TYPE) abort (); \
+#define AUTO_INCREASE_DECREASE(TYPE,SUFFIX)   \
+do                                            \
+{                                             \
+  _Decimal##TYPE in_de_d##TYPE = 0.0##SUFFIX;         \
+  if (in_de_d##TYPE++) abort ();              \
+  if (--in_de_d##TYPE) abort ();              \
   if (++in_de_d##TYPE == 0.0##SUFFIX) abort (); \
   if (in_de_d##TYPE-- == 0.0##SUFFIX) abort (); \
-} while(0)
+}while(0)
 
-#define UNARY_OPERATOR(TYPE,SUFFIX) \
-do \
-{ \
- _Decimal##TYPE unary_d##TYPE = 1.0##SUFFIX; \
- _Decimal##TYPE* unary_dp##TYPE; \
- unary_dp##TYPE = &(unary_d##TYPE); /*  & operator.  */ \
- unary_d##TYPE = *(unary_dp##TYPE); /*  * operator.  */ \
- unary_d##TYPE = -unary_d##TYPE; /*  - operator.  */ \
- if (unary_d##TYPE != -1.0##SUFFIX) abort (); \
- unary_d##TYPE = +unary_d##TYPE; /*  + operator.  */ \
- if (unary_d##TYPE != -1.0##SUFFIX) abort (); \
- if (!unary_d##TYPE) abort (); /*  ! operator.  */ \
-} while(0)
+#define UNARY_OPERATOR(TYPE,SUFFIX)           \
+do                                            \
+{                                             \
+ _Decimal##TYPE unary_d##TYPE = 1.0##SUFFIX;  \
+ _Decimal##TYPE* unary_dp##TYPE;              \
+ /*  & operator.  */                          \
+ unary_dp##TYPE = &(unary_d##TYPE);           \
+ /*  * operator.  */                          \
+ unary_d##TYPE = *(unary_dp##TYPE);           \
+ /*  - operator.  */                          \
+ unary_d##TYPE = -unary_d##TYPE;              \
+ if (unary_d##TYPE != -1.0##SUFFIX) abort ();         \
+ /*  + operator.  */                          \
+ unary_d##TYPE = +unary_d##TYPE;              \
+ if (unary_d##TYPE != -1.0##SUFFIX) abort ();         \
+ if (!unary_d##TYPE) abort (); /*! operator.  */ \
+}while(0)
 
 int
 main ()
