9a56aca98db722371adcdcd376f6294d87c333a9
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-09-27 06:54:30 +0000

    * gcc.dg/dfp/composite-type.c: Extend the tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@104693 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/composite-type.c
+++ gcc/testsuite/gcc.dg/dfp/composite-type.c
@@ -3,29 +3,44 @@
 
 /* C99 Section 6.2.7 Composite Type.  */
 
-_Decimal32 g1();
-_Decimal32 g2(_Decimal32 *);
+#define DECIMAL_COMPOSITE_DECL(TYPE) \
+  _Decimal##TYPE g1_##TYPE(); \
+  _Decimal##TYPE g2_##TYPE(); \
+  _Decimal##TYPE (*h1_##TYPE)[2]; \
+  _Decimal##TYPE (*h2_##TYPE)[3]; \
+  _Decimal##TYPE (*h3_##TYPE)[4]; \
+  _Decimal##TYPE f1_##TYPE(_Decimal##TYPE(*)()); \
+  _Decimal##TYPE f1_##TYPE(_Decimal##TYPE(*)(_Decimal##TYPE*)); \
+  _Decimal##TYPE f1_##TYPE (_Decimal##TYPE(*g)(_Decimal##TYPE*)) \
+   { \
+     _Decimal##TYPE d##TYPE; \
+     d##TYPE = ((_Decimal##TYPE (*) (_Decimal##TYPE*)) g)(&d##TYPE); \
+     d##TYPE = ((_Decimal##TYPE (*) ()) g); \
+     return d##TYPE; \
+   } \
+   _Decimal##TYPE f2_##TYPE(_Decimal##TYPE(*)[]); \
+   _Decimal##TYPE f2_##TYPE(_Decimal##TYPE(*)[3]);
 
-_Decimal32 (*h1)[2];
-_Decimal32 (*h2)[3];
-_Decimal32 (*h3)[4];
-
-_Decimal32 f1(_Decimal32(*)());
-_Decimal32 f1(_Decimal32(*)(_Decimal32*));
-
-_Decimal32 f2(_Decimal32(*)[]);
-_Decimal32 f2(_Decimal32(*)[3]);
+#define DECIMAL_COMPOSITE_TEST(TYPE) \
+do \
+{ \
+ _Decimal##TYPE d##TYPE; \
+ d##TYPE = f1_##TYPE(g1_##TYPE); \
+ d##TYPE = f1_##TYPE(g2_##TYPE); \
+ d##TYPE = f2_##TYPE(h1_##TYPE); \
+ d##TYPE = f2_##TYPE(h2_##TYPE); \
+ d##TYPE = f2_##TYPE(h3_##TYPE); \
+} while(0)
+ 
+DECIMAL_COMPOSITE_DECL(32);  /* { dg-error "incompatible types in assignment" } */
+DECIMAL_COMPOSITE_DECL(64);  /* { dg-error "incompatible types in assignment" } */
+DECIMAL_COMPOSITE_DECL(128); /* { dg-error "incompatible types in assignment" } */
 
 int main()
 {
-  _Decimal32 i;
- 
-  i = f1(g1);
-  i = f1(g2);
-  
-  i = f2(h1); /* { dg-warning "incompatible pointer type" } */
-  i = f2(h2);
-  i = f2(h3); /* { dg-warning "incompatible pointer type" } */
+  DECIMAL_COMPOSITE_TEST(32);  /* { dg-warning "incompatible pointer type" } */
+  DECIMAL_COMPOSITE_TEST(64);  /* { dg-warning "incompatible pointer type" } */
+  DECIMAL_COMPOSITE_TEST(128); /* { dg-warning "incompatible pointer type" } */
 
   return 0;
 }
