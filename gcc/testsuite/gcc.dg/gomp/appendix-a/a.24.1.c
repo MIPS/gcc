feb45bc59a783d18ca6906b34ecf1e27f6fb8251
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-18 17:56:15 +0000

    * gcc.dg/gomp/appendix-a/a.24.1.c: Fix expected messages, 	for real now.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-20050608-branch@105569 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/gomp/appendix-a/a.24.1.c
+++ gcc/testsuite/gcc.dg/gomp/appendix-a/a.24.1.c
@@ -1,14 +1,14 @@
 /* { dg-do compile } */
 
 extern int omp_get_num_threads (void);
-int x, y, y1, z[1000];
+int x, y, t, z[1000];
 #pragma omp threadprivate(x)
 void
 a24 (int a)
 {
   const int c = 1;
   int i = 0;
-  int j = 0;
+  int l = 0;
 #pragma omp parallel default(none) private(a) shared(z)
   {
     int j = omp_get_num_threads ();
@@ -21,17 +21,14 @@ a24 (int a)
     /* { dg-error "'i' not specified" "" { target *-*-* } 20 } */
     /* { dg-error "enclosing parallel" "" { target *-*-* } 12 } */
     /* { dg-error "'y' not specified" "" { target *-*-* } 20 }  */
-    /* { dg-error "enclosing parallel" "" { target *-*-* } 12 } */
 #pragma omp for firstprivate(y)
     for (i = 0; i < 10; i++)
       {
 	z[i] = y;		/* O.K. - i is the loop iteration variable */
 				/*      - y is listed in firstprivate clause */
       }
-    z[j] = y1;
-    /* { dg-error "'j' not specified" "" { target *-*-* } 31 } */
-    /* { dg-error "enclosing parallel" "" { target *-*-* } 12 } */
-    /* { dg-error "'y1' not specified" "" { target *-*-* } 31 }  */
-    /* { dg-error "enclosing parallel" "" { target *-*-* } 12 } */
+    z[l] = t;
+    /* { dg-error "'l' not specified" "" { target *-*-* } 30 } */
+    /* { dg-error "'t' not specified" "" { target *-*-* } 30 }  */
   }
 }
