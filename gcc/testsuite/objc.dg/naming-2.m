9b527f5bdb7884a5caa78d8d23aed0a1be7fb4e9
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-10-20 15:17:01 +0000

    Added apple local marker.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@105677 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/objc.dg/naming-2.m
+++ gcc/testsuite/objc.dg/naming-2.m
@@ -4,9 +4,11 @@
 
 @interface Foo
 @end
+/* APPLE LOCAL begin radar 4281748 */
 float Foo;  /* { dg-error "parse error|syntax error|expected|redeclaration" } */
+/* APPLE LOCAL end radar 4281748 */
 
 double Bar;
 @interface Bar
 @end  /* { dg-error "redeclared as different kind of symbol" } */
-/* { dg-error "previous declaration of" "" { target *-*-* } 9 } */
+/* { dg-error "previous declaration of" "" { target *-*-* } 11 } */
