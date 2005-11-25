331fd47c8376b94f893aa7a42294ae377840e071
loki <loki@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-11-25 13:30:08 +0000

    2005-11-25  Gabor Loki  <loki@gcc.gnu.org>
            * fact-common.h : Fix FSF header.
            * fact-common.c : Ditto.
            * tree-ssa-lfact.c : Ditto.
            * tree-seqabstr.c : Ditto.
            * rtl-factoring.c : Ditto.
            (rest_of_rtl_seqabstr): Add cleanup with crossjump after sequence
            abstraction.
    							
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cfo-branch@107505 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/rtl-factoring.c
+++ gcc/rtl-factoring.c
@@ -1,18 +1,18 @@
 /* RTL factoring (code hoisting/sinking and sequence abstraction).
    Copyright (C) 2004 Free Software Foundation, Inc.
- 
+
 This file is part of GCC.
- 
-GCC is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
- 
-GCC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
- 
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
 You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING.  If not, write to the Free
 Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
@@ -1853,7 +1853,9 @@ rest_of_rtl_seqabstr (void)
   /* Abstract out common insn sequences. */
   rtl_seqabstr ();
 
-  cleanup_cfg (CLEANUP_EXPENSIVE | CLEANUP_UPDATE_LIFE);
+  cleanup_cfg (CLEANUP_EXPENSIVE |
+               CLEANUP_UPDATE_LIFE |
+               (flag_crossjumping ? CLEANUP_CROSSJUMP : 0));
 }
 
 struct tree_opt_pass pass_rtl_seqabstr = {
