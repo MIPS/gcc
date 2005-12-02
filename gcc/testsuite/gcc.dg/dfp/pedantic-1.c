5af5421c1f0dda851210fe4604a1ae794ada8a61
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-02 22:54:56 +0000

    * gcc.dg/dfp/pedantic-1.c: Delete; subsumed by another test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@107954 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/pedantic-1.c
+++
@@ -1,6 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-std=gnu99 -pedantic" } */
-
-_Decimal32 x;	/* { dg-warning "GCC extension|ISO C" } */
-_Decimal64 y;	/* { dg-warning "GCC extension|ISO C" } */
-_Decimal128 z;	/* { dg-warning "GCC extension|ISO C" } */
