b644b201245476e266dee774454dddf27d894a52
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-03 00:53:07 +0000

    * decContext.h: Temporarily remove guards to include stdint.h.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@107976 138bc75d-0d04-0410-961f-82ee72b054a4

--- libdecnumber/decContext.h
+++ libdecnumber/decContext.h
@@ -40,9 +40,7 @@
 #define DECCFULLNAME "Decimal Context Descriptor"	/* Verbose name */
 #define DECCAUTHOR   "Mike Cowlishaw"	/* Who to blame */
 
-#ifdef HAVE_STDINT_H
 #include <stdint.h>		/* C99 standard integers */
-#endif
 #include <signal.h>		/* for traps */
 
 
