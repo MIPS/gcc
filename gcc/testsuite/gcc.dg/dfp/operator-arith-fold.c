7a9595792edd1229ae714efcddb31f77ee1c9260
janis <janis@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-03 01:03:16 +0000

    * gcc.dg/dfp/operator-arith-fold.c: Update, for real this time.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@107985 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/operator-arith-fold.c
+++ gcc/testsuite/gcc.dg/dfp/operator-arith-fold.c
@@ -1,24 +1,41 @@
-/* { dg-do run } */
-/* { dg-options "-std=gnu99" } */
+/* { dg-do compile } */
+/* { dg-options "-std=gnu99 -O" } */
 
-/* A few simple checks on arithmetic operations. */
-#include <stdio.h>
-#include <stdlib.h>
+extern void link_error (void);
 
-int main()
+int
+main ()
 {
+  _Decimal32 d32_1, d32_2;
+
   /* Compare like-typed positive constants. */
   if (2.99df + 5.1df != 8.09df)
-    abort();
+    link_error ();
 
   if (5.77df - 2.22dd != 3.55df)
-    abort();
+    link_error ();
 
   if (2.0dl * 3.7dd * -2 != -14.8df)
-    abort();
+    link_error ();
 
   if (.18df / -.2df + 1 != 1.e-1dd)
-    abort();
+    link_error ();
+
+  d32_1 = 3.0df;
+  d32_2 = 1.0df;
+
+  if (!__builtin_constant_p (d32_1 + 0.2df))
+    link_error ();
+
+  if (!__builtin_constant_p (1.0df / 3.0df))
+    link_error ();
+
+  if (!__builtin_constant_p (d32_2 / d32_1))
+    link_error ();
+
+  d32_2 = 2.0df;
+  if (!__builtin_constant_p (d32_2 / d32_1))
+    link_error ();
 
   return 0;
 }
