6158f2b5cfecf030af73ab555a742429db321514
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-06 00:49:04 +0000

    Create ongoing development branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple@108090 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ trunk/gcc/testsuite/gcc.c-torture/compile/920415-1.c
@@ -0,0 +1,24 @@
+/* APPLE LOCAL begin testsuite nested functions */
+/* { dg-options "-fnested-functions" } */
+/* APPLE LOCAL end testsuite nested functions */
+
+/* APPLE LOCAL testsuite added stdlib.h */
+#include <stdlib.h>
+
+#ifndef NO_LABEL_VALUES
+f ()
+{
+  __label__ l;
+  void *x()
+    {
+      return &&l;
+    }
+  goto *x ();
+  abort ();
+  return;
+ l:
+  exit (0);
+}
+#else
+int x;
+#endif
