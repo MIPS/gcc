39c443970b914f1afdcdb92cdcf3b265dbd2310f
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-08 08:47:13 +0000

    * gcc.dg/gomp/atomic-3.c: Conditionalize scan test on targets that 	implement __sync_fetch_and_add. 	* gcc.dg/gomp/atomic-9.c: Likewise. 	* g++.dg/gomp/atomic-3.C: Likewise. 	* g++.dg/gomp/atomic-9.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-20050608-branch@108218 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/gomp/atomic-9.C
+++ gcc/testsuite/g++.dg/gomp/atomic-9.C
@@ -9,5 +9,5 @@ void f1(void)
     *bar() += 1;
 }
 
-/* { dg-final { scan-tree-dump-times "__sync_fetch_and_add" 1 "gimple" } } */
+/* { dg-final { scan-tree-dump-times "__sync_fetch_and_add" 1 "gimple" { target i?86-*-* x86_64-*-* ia64-*-* powerpc*-*-* alpha*-*-* } } } */
 /* { dg-final { cleanup-tree-dump "gimple" } } */
