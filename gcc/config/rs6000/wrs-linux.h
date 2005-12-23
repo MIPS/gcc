f573b69012385efe99a54c93f5d86aa8ba11c8df
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2005-12-23 03:10:02 +0000

    * gcc/config.gcc (powerpc-wrs-linux-gnu): Use rs6000/wrs-linux.h 	* gcc/config/arm/t-wrs-linux (EXTRA_MULTILIB_PARTS): Add 	crtbeginT.o. 	* gcc/config/mips/t-wrs-linux, gcc/config/mips/wrs-linux.h: Add 	hard-float multilibs. 	* gcc/config/rs6000/wrs-linux.h: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-3_4_3-linux-branch@109008 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/rs6000/wrs-linux.h
+++ gcc/config/rs6000/wrs-linux.h
@@ -19,22 +19,7 @@ along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
-/* We do not need to provide an explicit big-endian multilib.  */
-#undef MULTILIB_DEFAULTS
-#define MULTILIB_DEFAULTS \
-  { "meb" }
-
-/* The GLIBC headers are in /usr/include, relative to the sysroot; the
-   uClibc headers are in /uclibc/usr/include.  */
-#undef SYSROOT_HEADERS_SUFFIX_SPEC
-#define SYSROOT_HEADERS_SUFFIX_SPEC		\
-  "%{muclibc:/uclibc}" 
-
 /* The various C libraries each have their own subdirectory.  */
 #undef SYSROOT_SUFFIX_SPEC
 #define SYSROOT_SUFFIX_SPEC			\
-  "%{muclibc:%{mel:/uclibc/mel ;		\
-               :/uclibc} ;			\
-     mel:%{mhard-float:/mel/hard-float ;	\
-           :/mel} ;				\
-     mhard-float:/hard-float}"
+  "%{msoft-float:/soft-float}"
