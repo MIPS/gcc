4786d7e31e37382d10c9fa1c051e8c3a482a679e
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-11 23:36:54 +0000

    * gcc.dg/dfp/inf-1.c: Avoid external call to libc finited32 ().
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@109610 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/dfp/inf-1.c
+++ gcc/testsuite/gcc.dg/dfp/inf-1.c
@@ -10,58 +10,55 @@ extern void abort (void);
 int main()
 {
   /* Assumes rounding mode. */
-  if (9999999.E90DF + 1.E90df != __builtin_infd32())
-    abort();
+  if (9999999.E90DF + 1.E90df != __builtin_infd32 ())
+    abort ();
 
-  if (!__builtin_isinfd32(9999999.E90DF + 1.E90df))
-    abort();
+  if (!__builtin_isinfd32 (9999999.E90DF + 1.E90df))
+    abort ();
 
   if (9.999999999999999E384dd + .000000000000001e384dd 
-      != __builtin_infd32())
-    abort();
+      != __builtin_infd32 ())
+    abort ();
 
-  if (-9999999.E90DF - 1.E90df != -__builtin_infd32())
-    abort();
+  if (-9999999.E90DF - 1.E90df != -__builtin_infd32 ())
+    abort ();
 
-  if (!__builtin_isinfd32(9.999999999999999E384dd + .000000000000001e384dd))
-    abort();
+  if (!__builtin_isinfd32 (9.999999999999999E384dd + .000000000000001e384dd))
+    abort ();
 
   if (7.999999999999999999999999999999999E6144dl + 3.0E6144dl
-      != __builtin_infd32())
-    abort();
+      != __builtin_infd32 ())
+    abort ();
 
-  if (__builtin_infd32() * __builtin_infd32() != __builtin_infd32())
-    abort();
+  if (__builtin_infd32 () * __builtin_infd32 () != __builtin_infd32 ())
+    abort ();
 
-  if (__builtin_infd32() * 2 != __builtin_infd32())
-    abort();
+  if (__builtin_infd32 () * 2 != __builtin_infd32 ())
+    abort ();
 
-  if (__builtin_infd64() * -5 != -__builtin_infd32())
-    abort();
+  if (__builtin_infd64 () * -5 != -__builtin_infd32 ())
+    abort ();
 
-  if (!__builtin_isinfd128(__builtin_infd32() / 4))
-    abort();
+  if (!__builtin_isinfd128 (__builtin_infd32 () / 4))
+    abort ();
 
-  if (__builtin_infd64() != __builtin_infd128())
-    abort();
+  if (__builtin_infd64 () != __builtin_infd128 ())
+    abort ();
 
-  if (!__builtin_isinfd64(__builtin_infd128()))
-    abort();
+  if (!__builtin_isinfd64 (__builtin_infd128 ()))
+    abort ();
 
-  if (__builtin_finited32(9999999.E90DF + 1.E90df))
-    abort();
+  if (__builtin_finited64 (__builtin_infd32 () * 4))
+    abort ();
 
-  if (__builtin_finited64(__builtin_infd32() * 4))
-    abort();
+  if (!__builtin_finited128 (9.999999E90DL - 1.E90dd))
+    abort ();
 
-  if (!__builtin_finited128(9.999999E90DL - 1.E90dd))
-    abort();
+  if (__builtin_finited128 (__builtin_infd32 () *__builtin_infd128 ()))
+    abort ();
 
-  if (__builtin_finited128(__builtin_infd32() *__builtin_infd128()))
-    abort();
-
-  if (__builtin_finited32(__builtin_nand32("")))
-    abort();
+  if (__builtin_finited32 (__builtin_nand32 ("")))
+    abort ();
     
   return 0;
 }
