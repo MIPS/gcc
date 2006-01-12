fa705733565b1a72f230520abc063820efd94c95
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-01-12 06:18:09 +0000

    * config/i386/t-linux (TARGET_LIBGCC2_CFLAGS): Remove. 	* config/i386/t-linux64 (TARGET_LIBGCC2_CFLAGS): Likewise. 	* config/i386/i386.c (ALIGN_MODE_128): Include TDmode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/dfp-branch@109637 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -580,7 +580,7 @@ extern int x86_prefetch_sse;
 
 /* Decide whether a variable of mode MODE should be 128 bit aligned.  */
 #define ALIGN_MODE_128(MODE) \
- ((MODE) == XFmode || SSE_REG_MODE_P (MODE))
+ ((MODE) == XFmode || (MODE) == TDmode || SSE_REG_MODE_P (MODE))
 
 /* The published ABIs say that doubles should be aligned on word
    boundaries, so lower the alignment for structure fields unless
