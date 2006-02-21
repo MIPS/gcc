f04d5513b67be687780512b29012a30e82d463fd
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-02-21 22:14:23 +0000

    Index: ChangeLog 2006-02-21  Geoffrey Keating  <geoffk@apple.com>
    	Radar 4439051
    	* config/i386/darwin.h (DWARF2_FRAME_REG_OUT): New.
    
    Index: testsuite/ChangeLog
    2006-02-21  Geoffrey Keating  <geoffk@apple.com>
    
    	Radar 4439051
    	* g++.dg/eh/spbp.C: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@111346 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/darwin.h
+++ gcc/config/i386/darwin.h
@@ -256,14 +256,22 @@ extern int flag_cw_asm_blocks;
 #define CW_ASM_REGISTER_NAME(STR, BUF) i386_cw_asm_register_name (STR, BUF)
 /* APPLE LOCAL end CW asm blocks */
 
-/* APPLE LOCAL begin mainline 2006-02-10 4439051 */
+/* APPLE LOCAL begin mainline 2006-02-21 4439051 */
 /* Darwin uses the standard DWARF register numbers but the default
    register numbers for STABS.  Fortunately for 64-bit code the
    default and the standard are the same.  */
 #undef DBX_REGISTER_NUMBER
-#define DBX_REGISTER_NUMBER(n) (TARGET_64BIT			\
-				? dbx64_register_map[n]		\
-				: write_symbols == DWARF2_DEBUG	\
-				? svr4_dbx_register_map[n]	\
-				: dbx_register_map[n])
-/* APPLE LOCAL end mainline 2006-02-10 4439051 */
+#define DBX_REGISTER_NUMBER(n) 					\
+  (TARGET_64BIT ? dbx64_register_map[n]				\
+   : write_symbols == DWARF2_DEBUG ? svr4_dbx_register_map[n]	\
+   : dbx_register_map[n])
+
+/* Unfortunately, the 32-bit EH information also doesn't use the standard
+   DWARF register numbers.  */
+#define DWARF2_FRAME_REG_OUT(n, for_eh)					\
+  (! (for_eh) || write_symbols != DWARF2_DEBUG || TARGET_64BIT ? (n)	\
+   : (n) == 5 ? 4							\
+   : (n) == 4 ? 5							\
+   : (n) >= 11 && (n) <= 18 ? (n) + 1					\
+   : (n))
+/* APPLE LOCAL end mainline 2006-02-21 4439051 */
