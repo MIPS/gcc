464a88a9b395cb1731aca3249caf1c786ab26ac3
fjahanian <fjahanian@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-02 16:16:05 +0000

    Radar 4451818. OKed by Mike.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple@111640 138bc75d-0d04-0410-961f-82ee72b054a4

--- trunk/gcc/cp/call.c
+++ trunk/gcc/cp/call.c
@@ -686,9 +686,14 @@ standard_conversion (tree to, tree from, tree expr, bool c_cast_p,
 	       && !TYPE_PTRMEM_P (from)
 	       && TREE_CODE (TREE_TYPE (from)) != FUNCTION_TYPE)
 	{
+	  /* APPLE LOCAL begin radar 4451818 */
+	  tree nfrom = TREE_TYPE (from);
+	  if (c_dialect_objc ())
+	    nfrom = objc_non_volatilized_type (nfrom);
 	  from = build_pointer_type
 	    (cp_build_qualified_type (void_type_node, 
-				      cp_type_quals (TREE_TYPE (from))));
+			              cp_type_quals (nfrom)));
+	  /* APPLE LOCAL end radar 4451818 */
 	  conv = build_conv (ck_ptr, from, conv);
 	}
       else if (TYPE_PTRMEM_P (from))
@@ -1250,6 +1255,11 @@ implicit_conversion (tree to, tree from, tree expr, bool c_cast_p,
   if (from == error_mark_node || to == error_mark_node
       || expr == error_mark_node)
     return NULL;
+
+  /* APPLE LOCAL begin radar 4451818 */
+  if (c_dialect_objc ())
+    from = objc_non_volatilized_type (from);
+  /* APPLE LOCAL end radar 4451818 */
 
   if (TREE_CODE (to) == REFERENCE_TYPE)
     conv = reference_binding (to, from, expr, flags);
