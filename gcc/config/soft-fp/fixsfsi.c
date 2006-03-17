8fa8d18565a08a9edaec68dd747bf5b733c61184
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-17 22:01:26 +0000

    * gcc/config/soft-fp: Update license notices. 	* gcc/config/soft-fp/README: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-ppc4xx-branch@112187 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/soft-fp/fixsfsi.c
+++ gcc/config/soft-fp/fixsfsi.c
@@ -1,14 +1,14 @@
 /* Software floating-point emulation.
    Convert a to 32bit signed integer
    Copyright (C) 1997,1999,2006 Free Software Foundation, Inc.
-   This file is part of GCC.
+   This file is not part of GCC.  This file is part of the GNU C Library.
    Contributed by Richard Henderson (rth@cygnus.com) and
 		  Jakub Jelinek (jj@ultra.linux.cz).
 
-   GCC is free software; you can redistribute it and/or modify it under
-   the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2, or (at your option) any later
-   version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2,
+   or (at your option) any later version.
 
    In addition to the permissions in the GNU General Public License, the
    Free Software Foundation gives you unlimited permission to link the
@@ -19,15 +19,15 @@
    the file, and distribution when not linked into a combine
    executable.)
 
-   GCC is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-   for more details.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with GCC; see the file COPYING.  If not, write to the Free
-   Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA.  */
+   along with the GNU C Library; see the file COPYING.  If not, write to
+   the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
 
 #include "soft-fp.h"
 #include "single.h"
--- gcc/config/soft-fp/op-8.h
+++ gcc/config/soft-fp/op-8.h
@@ -1,15 +1,15 @@
 /* Software floating-point emulation.
    Basic eight-word fraction declaration and manipulation.
    Copyright (C) 1997,1998,1999,2006 Free Software Foundation, Inc.
-   This file is part of GCC.
+   This file is not part of GCC.  This file is part of the GNU C Library.
    Contributed by Richard Henderson (rth@cygnus.com),
 		  Jakub Jelinek (jj@ultra.linux.cz) and
 		  Peter Maydell (pmaydell@chiark.greenend.org.uk).
 
-   GCC is free software; you can redistribute it and/or modify it under
-   the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2, or (at your option) any later
-   version.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2,
+   or (at your option) any later version.
 
    In addition to the permissions in the GNU General Public License, the
    Free Software Foundation gives you unlimited permission to link the
@@ -20,15 +20,15 @@
    the file, and distribution when not linked into a combine
    executable.)
 
-   GCC is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-   for more details.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with GCC; see the file COPYING.  If not, write to the Free
-   Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA.  */
+   along with the GNU C Library; see the file COPYING.  If not, write to
+   the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.  */
 
 /* We need just a few things from here for op-4, if we ever need some
    other macros, they can be added. */
