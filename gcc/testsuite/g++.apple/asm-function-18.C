becc50f5e2fb3b37ab1b3a7a05bfff8b376c89f0
mrs <mrs@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-28 20:42:49 +0000

    * asm-array-ref.C, asm-block-1.C: Move from g++.dg to g++.apple. 	* asm-block-2.C, asm-block-3.C, asm-block-4.C, asm-block-5.C: Likwise. 	* asm-block-6.C, asm-block-7.C, asm-block-8.C, asm-block-9.C: Likwise. 	* asm-block-10.C, asm-block-11.C, asm-block-12.C: Likewise. 	* asm-block-13.C, asm-block-14.C, asm-block-15.C: Likewise. 	* asm-block-16.C, asm-block-17.C, asm-block-18.C: Likewise. 	* asm-block-19.C, asm-block-20.C, asm-block-21.C: Likewise. 	* asm-block-22.C, asm-block-23.C, asm-block-24.C: Likewise. 	* asm-block-25.C, asm-block-26.C, asm-block-27.C: Likewise. 	* asm-block-28.C, asm-block-29.C, asm-block-30.C: Likewise. 	* asm-block-31.C, asm-block-32.C, asm-block-33.C: Likewise. 	* asm-block-34.C, asm-block-35.C, asm-block-36.C: Likewise. 	* asm-block-37.C, asm-block-38.C, asm-block-39.C: Likewise. 	* asm-block-40.C, asm-block-41.C, asm-block-42.C: Likewise. 	* asm-block-43.C, asm-block-44.C, asm-block-45.C: Likewise. 	* asm-block-46.C, asm-block-47.C: Likewise. 	* asm-block-48.C, asm-block-dot-align.C, asm-function-1.C: Likewise. 	* asm-function-2.C, asm-function-3.C, asm-function-4.C: Likewise. 	* asm-function-5.C, asm-function-6.C, asm-function-7.C: Likewise. 	* asm-function-8.C, asm-function-9.C, asm-function-10.C: Likewise. 	* asm-function-11.C, asm-function-12.C, asm-function-13.C: Likewise. 	* asm-function-14.C, asm-function-15.C, asm-function-16.C: Likewise. 	* asm-function-17.C, asm-function-18.C, asm-function-19.C: Likewise. 	* asm-function-20.C, asm-nested-struct-1.C: Likewise. 	* asm-static-entry-1.C: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@112473 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.apple/asm-function-18.C
@@ -0,0 +1,14 @@
+/* APPLE LOCAL begin CW asm blocks */
+/* { dg-do assemble { target powerpc*-*-* } } */
+/* { dg-options "-fasm-blocks" } */
+/* Radar 4258924 */
+
+struct AsmAlignCodeSample {
+  void AsmAlignCode();
+  static void NextLabelFunction();
+};
+
+asm void AsmAlignCodeSample::AsmAlignCode() {
+  b NextLabelFunction
+  .align 4
+}
