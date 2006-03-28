63ea4f398d1f34378cc52a9dac21af447c3c04d3
geoffk <geoffk@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-28 00:32:07 +0000

    Index: gcc/ChangeLog 2006-03-24  Geoffrey Keating  <geoffk@apple.com>
    	Radar 4485597
    	* dwarf2out.c (is_c_family): Understand new DWARF3 language types.
    	(is_cxx): Likewise.
    	(gen_compile_unit_die): Use new language types for Objective-C and
    	Objective-C++.
    	(dwarf2out_decl): Use is_cxx rather than testing explicitly against
    	DW_LANG_C_plus_plus.
    
    2006-03-01  Jakub Jelinek  <jakub@redhat.com>
    
    	* dwarf2.h (DW_TAG_condition, DW_TAG_shared_type): New constants
    	from DWARF 3.
    	(DW_AT_description, DW_AT_binary_scale, DW_AT_decimal_scale,
    	DW_AT_small, DW_AT_decimal_sign, DW_AT_digit_count,
    	DW_AT_picture_string, DW_AT_mutable, DW_AT_threads_scaled,
    	DW_AT_explicit, DW_AT_object_pointer, DW_AT_endianity,
    	DW_AT_elemental, DW_AT_pure, DW_AT_recursive): New.
    	(DW_OP_form_tls_address, DW_OP_call_frame_cfa, DW_OP_bit_piece): New.
    	(DW_ATE_packed_decimal, DW_ATE_numeric_string, DW_ATE_edited,
    	DW_ATE_signed_fixed, DW_ATE_unsigned_fixed): New.
    	(DW_DS_unsigned, DW_DS_leading_overpunch, DW_DS_trailing_overpunch,
    	DW_DS_leading_separate, DW_DS_trailing_separate): New.
    	(DW_END_default, DW_END_big, DW_END_little): New.
    	(DW_END_lo_user, DW_END_hi_user): Define.
    	(DW_LNE_lo_user, DW_LNE_hi_user): Define.
    	(DW_CFA_val_offset, DW_CFA_val_offset_sf, DW_CFA_val_expression): New.
    	(DW_LANG_PLI, DW_LANG_ObjC, DW_LANG_ObjC_plus_plus, DW_LANG_UPC,
    	DW_LANG_D): New.
    
    Index: gcc/testsuite/ChangeLog
    2006-03-24  Geoffrey Keating  <geoffk@apple.com>
    
    	Radar 4485597
    	* objc.dg/dwarf-2.m: New.
    	* obj-c++.dg/dwarf-2.mm: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/apple-local-200502-branch@112434 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -5340,8 +5340,11 @@ is_c_family (void)
 {
   unsigned int lang = get_AT_unsigned (comp_unit_die, DW_AT_language);
 
-  return (lang == DW_LANG_C || lang == DW_LANG_C89
-	  || lang == DW_LANG_C_plus_plus);
+/* APPLE LOCAL begin mainline 2006-03-24 4485597 */
+  return (lang == DW_LANG_C || lang == DW_LANG_C89 || lang == DW_LANG_ObjC
+	  || lang == DW_LANG_C99
+	  || lang == DW_LANG_C_plus_plus || lang == DW_LANG_ObjC_plus_plus);
+/* APPLE LOCAL end mainline 2006-03-24 4485597 */
 }
 
 /* Return TRUE if the language is C++.  */
@@ -5349,8 +5352,11 @@ is_c_family (void)
 static inline bool
 is_cxx (void)
 {
-  return (get_AT_unsigned (comp_unit_die, DW_AT_language)
-	  == DW_LANG_C_plus_plus);
+/* APPLE LOCAL begin mainline 2006-03-24 4485597 */
+  unsigned int lang = get_AT_unsigned (comp_unit_die, DW_AT_language);
+  
+  return lang == DW_LANG_C_plus_plus || lang == DW_LANG_ObjC_plus_plus;
+/* APPLE LOCAL end mainline 2006-03-24 4485597 */
 }
 
 /* Return TRUE if the language is Fortran.  */
@@ -12067,6 +12073,12 @@ gen_compile_unit_die (const char *filename)
     language = DW_LANG_Pascal83;
   else if (strcmp (language_string, "GNU Java") == 0)
     language = DW_LANG_Java;
+/* APPLE LOCAL begin mainline 2006-03-24 4485597 */
+  else if (strcmp (language_string, "GNU Objective-C") == 0)
+    language = DW_LANG_ObjC;
+  else if (strcmp (language_string, "GNU Objective-C++") == 0)
+    language = DW_LANG_ObjC_plus_plus;
+/* APPLE LOCAL end mainline 2006-03-24 4485597 */
   else
     language = DW_LANG_C89;
 
@@ -13212,8 +13224,9 @@ dwarf2out_decl (tree decl)
 	{
 	  /* OK, we need to generate one for `bool' so GDB knows what type
 	     comparisons have.  */
-	  if ((get_AT_unsigned (comp_unit_die, DW_AT_language)
-	       == DW_LANG_C_plus_plus)
+/* APPLE LOCAL begin mainline 2006-03-24 4485597 */
+	  if (is_cxx ()
+/* APPLE LOCAL end mainline 2006-03-24 4485597 */
 	      && TREE_CODE (TREE_TYPE (decl)) == BOOLEAN_TYPE
 	      && ! DECL_IGNORED_P (decl))
 	    modified_type_die (TREE_TYPE (decl), 0, 0, NULL);
