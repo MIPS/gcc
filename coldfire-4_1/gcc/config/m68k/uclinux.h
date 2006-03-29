e670a63355564619d49d0674e164fc297c9dd0f5
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-03-29 16:02:36 +0000

    * gcc/config.gcc (m68k-*-uclinux*): Reorder includes to match 	m68k-*-linux.  Include linux.h and m68k/linux.h.  Include linux.opt 	and define UCLIBC_DEFAULT. 	* gcc/config/m68k/uclinux.h (STARTFILE_SPEC): Delete. 	(ENDFILE_SPEC): Delete. 	(HAS_INIT_SECTION): Delete. 	(TARGET_OS_CPP_BUILTINS): Use LINUX_TARGET_OS_CPP_BUILTINS. 	(CPLUSPLUS_CPP_SPEC): Delete. 	(TARGET_ASM_FILE_START_APP_OFF): Delete. 	(MULTILIB_DEFAULTS): Undefine before redefining. 	(SYSROOT_SUFFIX_SPEC): Likewise. 	(SYSROOT_HEADERS_SUFFIX_SPEC): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl@112498 138bc75d-0d04-0410-961f-82ee72b054a4

--- coldfire-4_1/gcc/config/m68k/uclinux.h
+++ coldfire-4_1/gcc/config/m68k/uclinux.h
@@ -21,12 +21,6 @@ along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301, USA.  */
 
-#undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt1.o%s crti.o%s crtbegin.o%s"
-
-#undef  ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
-
 /* Override the default LIB_SPEC from gcc.c.  We don't currently support
    profiling, or libg.a.  */
 #undef LIB_SPEC
@@ -34,38 +28,23 @@ Boston, MA 02110-1301, USA.  */
 %{mid-shared-library:-R libc.gdb%s -elf2flt -shared-lib-id 0} -lc \
 "
 
-/* we have init/fini section. */
-#define HAS_INIT_SECTION
-
-/* Bring in standard linux defines */
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()		\
   do						\
     {						\
-	builtin_define ("__uClinux__");		\
-	builtin_define_std ("linux");		\
-	builtin_define_std ("unix");		\
-	builtin_define ("__gnu_linux__");	\
-	builtin_assert ("system=linux");	\
-	builtin_assert ("system=unix");		\
-	builtin_assert ("system=posix");	\
-	if (TARGET_ID_SHARED_LIBRARY)		\
-	  builtin_define ("__ID_SHARED_LIBRARY__"); \
+      LINUX_TARGET_OS_CPP_BUILTINS ();		\
+      builtin_define ("__uClinux__");		\
+      if (TARGET_ID_SHARED_LIBRARY)		\
+	builtin_define ("__ID_SHARED_LIBRARY__"); \
     }						\
   while (0)
 
-/* The GNU C++ standard library requires that these macros be defined.  */
-#undef CPLUSPLUS_CPP_SPEC
-#define CPLUSPLUS_CPP_SPEC "-D_GNU_SOURCE %(cpp)"
-
-/* uclibc uses comments to hide section attributes.  So setting
-   ASM_FILE_START_APP_OFF breaks it.  */
-#undef TARGET_ASM_FILE_START_APP_OFF
-
 /* Arbitrarily pick one multilib to be the default.  The true default
    is enforced by the with_cpu setting in config.gcc.  */
+#undef MULTILIB_DEFAULTS
 #define MULTILIB_DEFAULTS { "mcpu=5206" }
 
+#undef SYSROOT_SUFFIX_SPEC
 #define SYSROOT_SUFFIX_SPEC	\
   "%{mcpu=5206: /m5206 ;	\
      mcpu=5206e: /m5206e ;	\
@@ -75,6 +54,7 @@ Boston, MA 02110-1301, USA.  */
      mcpu=532x: /m532x ;	\
      mcpu=5407: /m5407}"
 
+#undef SYSROOT_HEADERS_SUFFIX_SPEC
 #define SYSROOT_HEADERS_SUFFIX_SPEC "/m5206"
 
 /* The uclinux binary format relies on relocations against a segment being
