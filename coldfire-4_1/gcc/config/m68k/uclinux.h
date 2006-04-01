5043dee7521a2930722969ec675c7673821fcca7
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-01 14:44:16 +0000

    * gcc/config/m68k/uclinux.h (STARTFILE_SPEC): Reinstate defintion. 	(END_SPEC): Likewise. 	(LINK_SPEC): Make -elf2flt the default.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl@112606 138bc75d-0d04-0410-961f-82ee72b054a4

--- coldfire-4_1/gcc/config/m68k/uclinux.h
+++ coldfire-4_1/gcc/config/m68k/uclinux.h
@@ -21,12 +21,22 @@ along with GCC; see the file COPYING.  If not, write to
 the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301, USA.  */
 
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt1.o%s crti.o%s crtbegin.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
 /* Override the default LIB_SPEC from gcc.c.  We don't currently support
    profiling, or libg.a.  */
 #undef LIB_SPEC
 #define LIB_SPEC "\
 %{mid-shared-library:-R libc.gdb%s -elf2flt -shared-lib-id 0} -lc \
 "
+
+/* Default to using -elf2flt with no options.  */
+#undef LINK_SPEC
+#define LINK_SPEC "%{!elf2flt*:-elf2flt}"
 
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()		\
