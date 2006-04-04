d1d835fd88e61898e4f88dadf686a48cfe57cde3
jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-04 13:37:32 +0000

    * gcc/config/soft-fp: Update license notices.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-ppc4xx-branch@112663 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/soft-fp/floatundidf.c
+++ gcc/config/soft-fp/floatundidf.c
@@ -1,32 +1,32 @@
 /* Software floating-point emulation.
    Convert a 64bit unsigned integer to IEEE double
    Copyright (C) 1997,1999, 2006 Free Software Foundation, Inc.
-   This file is not part of GCC.  This file is part of the GNU C Library.
+   This file is part of the GNU C Library.
    Contributed by Richard Henderson (rth@cygnus.com) and
 		  Jakub Jelinek (jj@ultra.linux.cz).
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2,
-   or (at your option) any later version.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-   In addition to the permissions in the GNU General Public License, the
-   Free Software Foundation gives you unlimited permission to link the
-   compiled version of this file into combinations with other programs,
-   and to distribute those combinations without any restriction coming
-   from the use of this file.  (The General Public License restrictions
-   do apply in other respects; for example, they cover modification of
-   the file, and distribution when not linked into a combine
-   executable.)
+   In addition to the permissions in the GNU Lesser General Public
+   License, the Free Software Foundation gives you unlimited
+   permission to link the compiled version of this file into
+   combinations with other programs, and to distribute those
+   combinations without any restriction coming from the use of this
+   file.  (The Lesser General Public License restrictions do apply in
+   other respects; for example, they cover modification of the file,
+   and distribution when not linked into a combine executable.)
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with the GNU C Library; see the file COPYING.  If not, write to
-   the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
    MA 02110-1301, USA.  */
 
 #include "soft-fp.h"
