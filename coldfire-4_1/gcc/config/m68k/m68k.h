01b2c263c9d264082970a3835960648ca7e642fa
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-04 23:07:20 +0000

    * gcc/config/m68k/m68k.h (PREFERRED_STACK_BOUNDARY): Define to 32 	for Coldfire targets.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl@112682 138bc75d-0d04-0410-961f-82ee72b054a4

--- coldfire-4_1/gcc/config/m68k/m68k.h
+++ coldfire-4_1/gcc/config/m68k/m68k.h
@@ -254,6 +254,8 @@ Boston, MA 02110-1301, USA.  */
 #define STACK_BOUNDARY 16
 #define FUNCTION_BOUNDARY 16
 #define EMPTY_FIELD_BOUNDARY 16
+/* Coldfire strongly prefers a 32-bit aligned stack.  */
+#define PREFERRED_STACK_BOUNDARY (m68k_arch_coldfire ? 32 : 16)
 
 /* No data type wants to be aligned rounder than this.
    Most published ABIs say that ints should be aligned on 16 bit
