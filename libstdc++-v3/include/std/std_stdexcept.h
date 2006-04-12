c58ef92f995b369bcc058ac00264268c414aacaf
paolo <paolo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-04-12 16:12:00 +0000

    2006-04-12  Paolo Carlini  <pcarlini@suse.de>
    	* include/std/std_stdexcept.h (class __exception_string): Add.
    	(class logic_error, class runtime_error): Use it.
    	* src/stdexcept.cc (_exception_string::_Rep::_M_destroy,
    	__exception_string::__exception_string(const string&),
    	__exception_string::operator=(const __exception_string&)): Define.
    	* config/abi/pre/gnu.ver: Update exports.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/libstdcxx_so_7-branch@112888 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/include/std/std_stdexcept.h
+++ libstdc++-v3/include/std/std_stdexcept.h
@@ -1,6 +1,7 @@
 // Standard exception classes  -*- C++ -*-
 
-// Copyright (C) 2001, 2002, 2005 Free Software Foundation, Inc.
+// Copyright (C) 2001, 2002, 2003, 2004, 2005, 2006
+// Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -42,8 +43,65 @@
 
 #include <exception>
 #include <string>
+#include <bits/atomicity.h>
 
 _GLIBCXX_BEGIN_NAMESPACE(std)
+
+  /** Reference-counted "mini-string" for internal use in the std exception
+   *  classes, needed due to the no-throw requirements for copy-constructor
+   *  and assignment operator, not fulfilled by sso_string.  */
+  class __exception_string
+  {
+    struct _Rep
+    {
+      size_t	    _M_size;
+      _Atomic_word  _M_refcount;
+
+      void
+      _M_destroy() throw();
+
+      void
+      _M_dispose() throw()
+      {
+	if (__gnu_cxx::__exchange_and_add(&_M_refcount, -1) <= 0)
+	  _M_destroy();
+      }
+
+      char*
+      _M_refdata() throw()
+      { return reinterpret_cast<char*>(this + 1); }
+
+      char*
+      _M_refcopy() throw()
+      {
+	__gnu_cxx::__atomic_add(&_M_refcount, 1);
+	return _M_refdata();
+      }
+    };
+
+    char* _M_data;
+
+    _Rep*
+    _M_rep() const throw()
+    { return &((reinterpret_cast<_Rep*>(_M_data))[-1]); }
+
+  public:
+    __exception_string(const __exception_string& __ex_str)
+    : _M_data(__ex_str._M_rep()->_M_refcopy()) { }
+
+    __exception_string(const string& __str);
+
+    ~__exception_string()
+    { _M_rep()->_M_dispose(); }
+
+    __exception_string&
+    operator=(const __exception_string& __ex_str);
+
+    const char*
+    c_str() const
+    { return _M_data; }
+  };
+
 
   /** Logic errors represent problems in the internal logic of a program;
    *  in theory, these are preventable, and even detectable before the
@@ -52,7 +110,7 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
    */
   class logic_error : public exception 
   {
-    string _M_msg;
+    __exception_string _M_msg;
 
   public:
     /** Takes a character string describing the error.  */
@@ -106,7 +164,7 @@ _GLIBCXX_BEGIN_NAMESPACE(std)
    */
   class runtime_error : public exception 
   {
-    string _M_msg;
+    __exception_string _M_msg;
 
   public:
     /** Takes a character string describing the error.  */
