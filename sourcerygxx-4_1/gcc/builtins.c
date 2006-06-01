3609a909fd26230647f8074920398bdf6f01e882
shinwell <shinwell@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-01 17:10:17 +0000

    Patch for issue #693.
    	* gcc/builtins.c (expand_builtin_return_addr): Always use
    	hard_frame_pointer_rtx and prevent frame pointer elimination
    	if INITIAL_FRAME_ADDRESS_RTX isn't set.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl@114314 138bc75d-0d04-0410-961f-82ee72b054a4

--- sourcerygxx-4_1/gcc/builtins.c
+++ sourcerygxx-4_1/gcc/builtins.c
@@ -496,20 +496,14 @@ expand_builtin_return_addr (enum built_in_function fndecl_code, int count)
 #else
   rtx tem;
 
-  /* For a zero count, we don't care what frame address we return, so frame
-     pointer elimination is OK, and using the soft frame pointer is OK.
-     For a non-zero count, we require a stable offset from the current frame
-     pointer to the previous one, so we must use the hard frame pointer, and
-     we must disable frame pointer elimination.  */
-  if (count == 0)
-    tem = frame_pointer_rtx;
-  else 
-    {
-      tem = hard_frame_pointer_rtx;
+  /* We require a stable offset from the current frame pointer to the
+     previous one, so we must use the hard frame pointer, and we must
+     disable frame pointer elimination.  */
 
-      /* Tell reload not to eliminate the frame pointer.  */
-      current_function_accesses_prior_frames = 1;
-    }
+  tem = hard_frame_pointer_rtx;
+
+  /* Tell reload not to eliminate the frame pointer.  */
+  current_function_accesses_prior_frames = 1;
 #endif
 
   /* Some machines need special handling before we can access
