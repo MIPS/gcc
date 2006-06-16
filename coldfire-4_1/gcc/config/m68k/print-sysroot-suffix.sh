48492652581d6e22ca87257aeca3eeea74b50f10
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-06-16 12:38:17 +0000

    gcc/ 	* config/m68k/print-sysroot-suffix.sh: Rewrite to be more flexible. 	Take the sysroot, MULTILIB_MATCHES and MULTILIB_OPTIONS as arguments. 	* config/m68k/t-mlib-matches (sysroot-suffix.h): Update accordingly.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl@114719 138bc75d-0d04-0410-961f-82ee72b054a4

--- coldfire-4_1/gcc/config/m68k/print-sysroot-suffix.sh
+++ coldfire-4_1/gcc/config/m68k/print-sysroot-suffix.sh
@@ -17,26 +17,66 @@
 # the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 # Boston MA 02110-1301, USA.
 
-# Go through the multilib matches in $matches.  For each match of the
-# form mcpu?<core>=<option>, check whether the sysroot in $sysroot
-# has a subdirectory called m<core>.  Use that subdirectory for
-# <option> if so.
+# This script takes the following arguments:
+#
+#    - the target sysroot
+#    - the value of $(MULTILIB_MATCHES)
+#    - the value of $(MULTILIB_OPTIONS)
+#
+# It uses these arguments to construct a definition of SYSROOT_SUFFIX_SPEC,
+# which it prints to the standard output.  For each multilib directory FOO,
+# the script checks whether $sysroot has a subdirectory FOO, and if so will
+# use /FOO for all compatible command-line options.  It will not add a
+# suffix for /FOO's options otherwise.  These suffixes are concatenated,
+# with one subspec for each space-separated entry in $(MULTILIB_OPTIONS).
 set -e
-spec=
-for match
+sysroot=$1
+matches=$2
+options=$3
+
+# For each multilib option OPT, add to $substs a sed command of the
+# form "-e 's/OPT/OPT/'".
+substs=""
+for option in `echo "$options" | tr '/' ' '`
 do
-  case $match in
-    mcpu[?]*=*)
-      option=`echo "$match" | sed -e 's/.*=//' -e 's/?/=/g'`
-      suffix=`echo "$match" | sed -e 's/=.*//' -e 's/cpu?//g'`
-      if test -d "$sysroot/$suffix"; then
-        test -z "$spec" || spec="$spec;"
-        spec="$spec$option:/$suffix"
-      fi
-      ;;
-  esac
+  substs="$substs -e 's/$option/$option/g'"
+done
+
+# For each ALIAS=CANONICAL entry in $MULTILIB_MATCHES, look for sed
+# arguments in $substs of the form "-e 's/CANONICAL/.../'".  Replace
+# such entries with "-e 's/CANONICAL/ALIAS|.../'".  Both the ALIAS and
+# CANONICAL parts of $MULTILIB_MATCHES use '?' to stand for '='.
+#
+# After this loop, a command of the form "echo FOO | eval sed $substs"
+# will replace a canonical option FOO with a %{...}-style spec pattern.
+for match in $matches
+do
+  canonical=`echo "$match" | sed -e 's/=.*//' -e 's/?/=/g'`
+  alias=`echo "$match" | sed -e 's/.*=//' -e 's/?/=/g'`
+  substs=`echo "$substs" | sed -e "s,s/$canonical/,&$alias|,"`
+done
+
+# Build up the final SYSROOT_SUFFIX_SPEC in $spec.
+spec=
+for combo in $options
+do
+  # See which option alternatives in $combo have their own sysroot
+  # directory.  Create a subspec of the form "%{PAT1:/DIR1;...;PATn:DIRn}"
+  # from each such option OPTi, where DIRi is the directory associated
+  # with OPTi and PATi is the result of passing OPTi through $substs.
+  subspec=
+  for option in `echo "$combo" | tr '/' ' '`
+  do
+    dir=`echo "$option" | sed 's/cpu=//'`
+    if test -d "$sysroot/$dir"; then
+      test -z "$subspec" || subspec="$subspec;"
+      subspec="$subspec"`echo "$option" | eval sed $substs`":/$dir"
+    fi
+  done
+  # Concatenate all the subspecs.
+  test -z "$subspec" || spec="$spec%{$subspec}"
 done
 if test -n "$spec"; then
   echo "#undef SYSROOT_SUFFIX_SPEC"
-  echo "#define SYSROOT_SUFFIX_SPEC \"%{$spec}\""
+  echo "#define SYSROOT_SUFFIX_SPEC \"$spec\""
 fi
