4716c141fd51f01cc2787aae95bfa69f30af60f9
amacleod <amacleod@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-09-29 14:17:28 +0000

    use simpler hash function from conflict.c
    2006-09-29  Andrew MacLeod  <amacleod@redhat.com>
    
    	* tree-ssa-ilve.c (mix): Remove.
    	(coalesce_pair_map_hash): Use simpler hash function.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/out-of-ssa-the-sequel@117305 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-live.c
+++ gcc/tree-ssa-live.c
@@ -783,20 +783,8 @@ calculate_live_ranges (var_map map)
   return live;
 }
 
-/* Borrowed from hashtab.c iterative_hash implementation.  */
-#define mix(a,b,c) \
-{ \
-  a -= b; a -= c; a ^= (c>>13); \
-  b -= c; b -= a; b ^= (a<< 8); \
-  c -= a; c -= b; c ^= ((b&0xffffffff)>>13); \
-  a -= b; a -= c; a ^= ((c&0xffffffff)>>12); \
-  b -= c; b -= a; b = (b ^ (a<<16)) & 0xffffffff; \
-  c -= a; c -= b; c = (c ^ (b>> 5)) & 0xffffffff; \
-  a -= b; a -= c; a = (a ^ (c>> 3)) & 0xffffffff; \
-  b -= c; b -= a; b = (b ^ (a<<10)) & 0xffffffff; \
-  c -= a; c -= b; c = (c ^ (b>>15)) & 0xffffffff; \
-}
 
+#define COALESCE_HASH_FN(R1, R2) ((R2) * ((R2) - 1) / 2 + (R1))
 
 /* Hash function for coalesce list.  Calculate hash for PAIR.   */
 
@@ -806,9 +794,7 @@ coalesce_pair_map_hash (const void *pair)
   hashval_t a = (hashval_t)(((coalesce_pair_p)pair)->first_element);
   hashval_t b = (hashval_t)(((coalesce_pair_p)pair)->second_element);
 
-  hashval_t val = 0x9e3779b9;
-  mix (a, b, val);
-  return val;
+  return COALESCE_HASH_FN (a,b);
 }
 
 
