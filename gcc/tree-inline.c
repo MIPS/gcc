ff7ceb4df2d743e3b3d459837531c40d984b7be5
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-09 16:40:31 +0000

    tree-inline.c (copy_bb): Check for GIMPLE_MODIFY_STMT, and adjust        accordingly.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gimple-tuples-branch@117579 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-inline.c
+++ gcc/tree-inline.c
@@ -709,9 +709,9 @@ copy_bb (copy_body_data *id, basic_block bb, int frequency_scale, int count_scal
 
 	  /* With return slot optimization we can end up with
 	     non-gimple (foo *)&this->m, fix that here.  */
-	  if (TREE_CODE (stmt) == MODIFY_EXPR
-	      && TREE_CODE (TREE_OPERAND (stmt, 1)) == NOP_EXPR
-	      && !is_gimple_val (TREE_OPERAND (TREE_OPERAND (stmt, 1), 0)))
+	  if (TREE_CODE (stmt) == GIMPLE_MODIFY_STMT
+	      && TREE_CODE (GIMPLE_STMT_OPERAND (stmt, 1)) == NOP_EXPR
+	      && !is_gimple_val (TREE_OPERAND (GIMPLE_STMT_OPERAND (stmt, 1), 0)))
 	    gimplify_stmt (&stmt);
 
           bsi_insert_after (&copy_bsi, stmt, BSI_NEW_STMT);
