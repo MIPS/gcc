252727f91a0b752fdc39e5f7b3e14db2e158c5cb
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-10-19 04:05:34 +0000

    * doc/invoke.texi: Add cpu_type power6x 	(RS/6000 and PowerPC Options): Add -mmfpgpr. 	* recog.c (store_data_bypass_p): Add support to allow IN_INSN to 	be a PARALLEL containing sets.	Return false when out_pat is not 	a PARALLEL insn. 	* config/rs6000/aix52.h (ASM_CPU_SPEC): Add power6x. 	* config.gcc: Add cpu_type power6x. 	* configure.ac: Add test for mf{t,f}gpr instructions. 	(HAVE_AS_MFPGPR): New. 	* config.in: Regenerate. 	* configure: Regenerate. 	* config/rs6000/linux64.h (PROCESSOR_DEFAULT): Default to POWER6. 	(PROCESSOR_DEFAULT64): Likewise. 	* config/rs6000/rs6000.md (define_attr "type"): Add insert_dword, 	shift,trap,var_shift_rotate,cntlz,exts, var_delayed_compare, mffgpr 	and mftgpr attributes. 	(define_attr "cpu"): Add power6. Add power6x. 	Change instruction sequences to use new attributes. 	(floatsidf2,fix_truncdfsi2): use TARGET_MFPGPR. 	(fix_truncdfsi2_internal_mfpgpr): New. 	(floatsidf_ppc64_mfpgpr): New. 	(floatsidf_ppc64): Added !TARGET_MFPGPR condition. 	(movdf_hardfloat64_mfpgpr,movdi_internal64_mfpgpr): New. 	(movdf_hardfloat64): Added !TARGET_MFPGPR condition. 	(movdi_internal64): Added !TARGET_MFPGPR and related conditions. 	(fix_truncdfsi2): Use gpc_reg_operand constraint. 	* config/rs6000/{6xx.md,power4.md,8540.md,603.md,mpc.md, 	7xx.md,rios2.md,7450.md,440.md,rios1.md,rs64.md,power5.md,40x.md}: 	Add descriptions for insert_dword, shift,trap,var_shift_rotate, 	cntlz,exts and var_delayed_compare. 	* config/rs6000/rs6000-c.c (rs6000_cpu_cpp_builtins): Define 	_ARCH_PWR6X, if features enabled. 	* config/rs6000/rs6000.opt (mmfpgpr): New. 	* config/rs6000/rs6000.c (rs6000_align_branch_targets): New variable. 	(cached_can_issue_more): New variable. 	(processor_costs): Add power6_cost. 	(rs6000_sched_init): New function. 	(is_dispatch_slot_restricted): Deleted. 	(set_to_load_agen): New function. 	(is_load_insn,is_store_insn): New functions. 	(adjacent_mem_locations): New function. 	(insn_must_be_first_in_group): New function. 	(insn_must_be_last_in_group): New function. 	(rs6000_sched_reorder): New function. 	(rs6000_sched_reorder2): New function. 	(TARGET_SCHED_INIT,TARGET_SCHED_REORDER, 	TARGET_SCHED_REORDER2): Define. 	(processor_target_table): Use PROCESSOR_POWER6 for power6. 	Add power6x. Add MASK_MFPGPR for power6x. 	(POWERPC_MASKS): Add MASK_MFPGPR. 	(rs6000_override_options): Set rs6000_always_hint to false 	for power6.  Set rs6000_align_branch_targets. Replace 	rs6000_sched_groups check with rs6000_align_branch_targets. 	Use PROCESSOR_POWER6. 	(last_scheduled_insn): New variable. 	(load_store_pendulum): New variable. 	(rs6000_variable_issue): Set last_scheduled_insn and 	cached_can_issue_more. 	(rs6000_adjust_cost): Add power6 cost adjustments. 	(rs6000_adjust_priority): Replace is_dispatch_slot_restricted 	with insn_must_be_first_in_group. Add power6 priority adjustments. 	(rs6000_issue_rate): Add CPU_POWER6. Add CPU_POWER6X. 	(insn_terminates_group_p): Use insn_must_be_{first,last}_in_group. 	* config/rs6000/rs6000.h (processor_type): Add PROCESSOR_POWER6. 	(TARGET_MFPGPR): New. 	(SECONDARY_MEMORY_NEEDED): Use TARGET_MFPGPR. 	(ASM_CPU_SPEC): Add power6x. Pass -mpower5 when cpu=power5. 	Pass -mpower5 when cpu=power5+.  Pass -mpower6 when cpu=power6. 	(SECONDARY_MEMORY_NEEDED): Added mode!=DFmode and mode!=DImode 	conditions. 	* config/rs6000/power6.md: New file.
    	PR rtl-optimization/26026
    	Backport from mainline
    	2006-04-19  Alan Modra	<amodra@bigpond.net.au>
    	* fold-const.c (fold_binary): Optimize div and mod where the divisor
    	is a known power of two shifted left a variable amount.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@117877 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_1-branch/gcc/config/rs6000/rs6000.h
+++ gcc-4_1-branch/gcc/config/rs6000/rs6000.h
@@ -72,9 +72,10 @@
 %{mcpu=power2: -mpwrx} \
 %{mcpu=power3: -mppc64} \
 %{mcpu=power4: -mpower4} \
-%{mcpu=power5: -mpower4} \
-%{mcpu=power5+: -mpower4} \
-%{mcpu=power6: -mpower4 -maltivec} \
+%{mcpu=power5: -mpower5} \
+%{mcpu=power5+: -mpower5} \
+%{mcpu=power6: -mpower6} \
+%{mcpu=power6x: -mpower6} \
 %{mcpu=powerpc: -mppc} \
 %{mcpu=rios: -mpwr} \
 %{mcpu=rios1: -mpwr} \
@@ -162,6 +163,14 @@
 #define TARGET_FPRND 0
 #endif
 
+/* Define TARGET_MFPGPR if the target assembler does not support the
+   mffpr and mftgpr instructions. */
+
+#ifndef HAVE_AS_MFPGPR
+#undef  TARGET_MFPGPR
+#define TARGET_MFPGPR 0
+#endif
+
 #ifndef TARGET_SECURE_PLT
 #define TARGET_SECURE_PLT 0
 #endif
@@ -212,7 +221,8 @@ enum processor_type
    PROCESSOR_PPC7450,
    PROCESSOR_PPC8540,
    PROCESSOR_POWER4,
-   PROCESSOR_POWER5
+   PROCESSOR_POWER5,
+   PROCESSOR_POWER6
 };
 
 extern enum processor_type rs6000_cpu;
@@ -1195,12 +1205,18 @@ enum reg_class
   secondary_reload_class (CLASS, MODE, IN)
 
 /* If we are copying between FP or AltiVec registers and anything
-   else, we need a memory location.  */
+   else, we need a memory location.  The exception is when we are
+   targeting ppc64 and the move to/from fpr to gpr instructions
+   are available.*/
 
-#define SECONDARY_MEMORY_NEEDED(CLASS1,CLASS2,MODE) 		\
- ((CLASS1) != (CLASS2) && ((CLASS1) == FLOAT_REGS		\
-			   || (CLASS2) == FLOAT_REGS		\
-			   || (CLASS1) == ALTIVEC_REGS		\
+#define SECONDARY_MEMORY_NEEDED(CLASS1,CLASS2,MODE)			\
+ ((CLASS1) != (CLASS2) && (((CLASS1) == FLOAT_REGS			\
+                            && (!TARGET_MFPGPR || !TARGET_POWERPC64	\
+				|| ((MODE != DFmode) && (MODE != DImode)))) \
+			   || ((CLASS2) == FLOAT_REGS			\
+                               && (!TARGET_MFPGPR || !TARGET_POWERPC64	\
+				|| ((MODE != DFmode) && (MODE != DImode)))) \
+			   || (CLASS1) == ALTIVEC_REGS			\
 			   || (CLASS2) == ALTIVEC_REGS))
 
 /* Return the maximum number of consecutive registers
