843a6ddb359853812f23530fc402818487635d6b
megacz <megacz@138bc75d-0d04-0410-961f-82ee72b054a4>
2006-12-05 10:48:00 +0000

    2006-12-04  Adam Megacz <megacz@cs.berkeley.edu>
            * include/win32.h (_Jv_platform_process): change to Win32Process
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcj@119537 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcj-eclipse-merge-branch/libjava/include/win32.h
+++ gcj-eclipse-merge-branch/libjava/include/win32.h
@@ -77,7 +77,7 @@ extern jstring _Jv_Win32NewString (LPCTSTR pcsz);
 #define _Jv_platform_solib_suffix ".dll"
 
 // Name of the Process implementation.
-#define _Jv_platform_process ::java::lang::PosixProcess
+#define _Jv_platform_process ::java::lang::Win32Process
 
 // Separator for file name components.
 #define _Jv_platform_file_separator ((jchar) '\\')
