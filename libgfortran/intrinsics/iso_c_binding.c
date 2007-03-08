4652f413e61336a03d9fc8c7fdf5e9fc85d707e7
fxcoudert <fxcoudert@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-03-08 21:51:48 +0000

    * libgfortran/intrincsics/iso_c_binding.h: Add copyright notice. 	* libgfortran/intrincsics/iso_c_binding.c: Add copyright notice. 	* libgfortran/intrincsics/iso_c_generated.c: Add copyright notice.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/fortran-experiments@122718 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgfortran/intrinsics/iso_c_binding.c
+++ libgfortran/intrinsics/iso_c_binding.c
@@ -1,3 +1,34 @@
+/* Implementation of the ISO_C_BINDING library helper functions.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+   Contributed by Christopher Rickett.
+
+This file is part of the GNU Fortran 95 runtime library (libgfortran).
+
+Libgfortran is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file into combinations with other programs,
+and to distribute those combinations without any restriction coming
+from the use of this file.  (The General Public License restrictions
+do apply in other respects; for example, they cover modification of
+the file, and distribution when not linked into a combine
+executable.)
+
+Libgfortran is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with libgfortran; see the file COPYING.  If not,
+write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
+
 /**
  * Implement the functions and subroutines provided by the intrinsic
  * iso_c_binding module.
@@ -5,8 +36,8 @@
  */
 
 #include <stdlib.h>
-#include "libgfortran.h"
 
+#include "libgfortran.h"
 #include "iso_c_binding.h"
 
 
--- libgfortran/intrinsics/iso_c_binding.h
+++ libgfortran/intrinsics/iso_c_binding.h
@@ -1,5 +1,37 @@
-#ifndef __iso_c_binding_h_
-#define __iso_c_binding_h_
+/* Copyright (C) 2007 Free Software Foundation, Inc.
+   Contributed by Christopher Rickett.
+
+This file is part of the GNU Fortran 95 runtime library (libgfortran).
+
+Libgfortran is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file into combinations with other programs,
+and to distribute those combinations without any restriction coming
+from the use of this file.  (The General Public License restrictions
+do apply in other respects; for example, they cover modification of
+the file, and distribution when not linked into a combine
+executable.)
+
+Libgfortran is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Libgfortran; see the file COPYING.  If not, write to
+the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
+
+/* Declarations for ISO_C_BINDING library helper functions.  */
+
+#ifndef GFOR_ISO_C_BINDING_H
+#define GFOR_ISO_C_BINDING_H
 
 #include "libgfortran.h"
 
