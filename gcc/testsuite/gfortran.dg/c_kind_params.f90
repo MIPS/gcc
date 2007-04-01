c021ab61aa01acf2a2b713ac3f1c991ed0f43189
burnus <burnus@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-04-01 11:53:25 +0000

    2007-04-01  Tobias Burnus  <burnus@net-b.de>
           * iso-c-binding.def: Add PR reference.
           * gfortran.dg/c_kind_params.f90: Make test runable.
           * gfortran.dg/c_kinds.c: Ditto.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/fortran-experiments@123399 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gfortran.dg/c_kind_params.f90
+++ gcc/testsuite/gfortran.dg/c_kind_params.f90
@@ -3,14 +3,17 @@
 ! { dg-options "-w -std=c99" }
 ! the -w option is needed to make f951 not report a warning for 
 ! the -std=c99 option that the C file needs.
+!
+! Note: int_fast*_t currently not supported, cf. PR 448.
 module c_kind_params
   use, intrinsic :: iso_c_binding
+  implicit none
 
 contains
   subroutine param_test(my_short, my_int, my_long, my_long_long, &
-       my_int8_t, my_int_least8_t, my_int_fast8_t, my_int16_t, &
-       my_int_least16_t, my_int_fast16_t, my_int32_t, my_int_least32_t, &
-       my_int_fast32_t, my_int64_t, my_int_least64_t, my_int_fast64_t, &
+       my_int8_t, my_int_least8_t, my_int16_t, &
+       my_int_least16_t, my_int32_t, my_int_least32_t, &
+       my_int64_t, my_int_least64_t, &
        my_intmax_t, my_intptr_t, my_float, my_double, my_long_double, &
        my_char, my_bool) bind(c)
     integer(c_short), value :: my_short
@@ -19,16 +22,16 @@ contains
     integer(c_long_long), value :: my_long_long
     integer(c_int8_t), value :: my_int8_t
     integer(c_int_least8_t), value :: my_int_least8_t
-    integer(c_int_fast8_t), value :: my_int_fast8_t
+!   integer(c_int_fast8_t), value :: my_int_fast8_t
     integer(c_int16_t), value :: my_int16_t
     integer(c_int_least16_t), value :: my_int_least16_t
-    integer(c_int_fast16_t), value :: my_int_fast16_t
+!   integer(c_int_fast16_t), value :: my_int_fast16_t
     integer(c_int32_t), value :: my_int32_t
     integer(c_int_least32_t), value :: my_int_least32_t
-    integer(c_int_fast32_t), value :: my_int_fast32_t
+!   integer(c_int_fast32_t), value :: my_int_fast32_t
     integer(c_int64_t), value :: my_int64_t
     integer(c_int_least64_t), value :: my_int_least64_t
-    integer(c_int_fast64_t), value :: my_int_fast64_t
+!   integer(c_int_fast64_t), value :: my_int_fast64_t
     integer(c_intmax_t), value :: my_intmax_t
     integer(c_intptr_t), value :: my_intptr_t
     real(c_float), value :: my_float
@@ -37,37 +40,37 @@ contains
     character(c_char), value :: my_char
     logical(c_bool), value :: my_bool
 
-    print *, 'my_short is:              ', my_short
-    print *, 'my_int is:                ', my_int
-    print *, 'my_long is:               ', my_long
-    print *, 'my_long_long is:          ', my_long_long
-    print *
-    print *, 'my_int8_t is:             ', my_int8_t
-    print *, 'my_int_least8_t is:       ', my_int_least8_t
-    print *, 'my_int_fast8_t is:        ', my_int_fast8_t
-    print *
-    print *, 'my_int16_t is:            ', my_int16_t
-    print *, 'my_int_least16_t is:      ', my_int_least16_t
-    print *, 'my_int_fast16_t is:       ', my_int_fast16_t
-    print *
-    print *, 'my_int32_t is:            ', my_int32_t
-    print *, 'my_int_least32_t is:      ', my_int_least32_t
-    print *, 'my_int_fast32_t is:       ', my_int_fast32_t
-    print *
-    print *, 'my_int64_t is:            ', my_int64_t
-    print *, 'my_int_least64_t is:      ', my_int_least64_t
-    print *, 'my_int_fast64_t is:       ', my_int_fast64_t
-    print *
-    print *, 'my_intmax_t is:           ', my_intmax_t
-    print *, 'my_intptr_t is:           ', my_intptr_t
-    print *
-    print *, 'my_float is:              ', my_float
-    print *, 'my_double is:             ', my_double
-    print *, 'my_long_double is:        ', my_long_double
-    print *
-    print *, 'my_char is:               ', my_char
-    print *, 'my_bool is:               ', my_bool
+    if(my_short     /= 1_c_short)     call abort()
+    if(my_int       /= 2_c_int)       call abort()
+    if(my_long      /= 3_c_long)      call abort()
+    if(my_long_long /= 4_c_long_long) call abort()
 
+    if(my_int8_t      /= 1_c_int8_t)        call abort()
+    if(my_int_least8_t  /= 2_c_int_least8_t ) call abort()
+    print *, 'c_int_fast8_t is:        ', c_int_fast8_t
+
+    if(my_int16_t     /= 1_c_int16_t)       call abort()
+    if(my_int_least16_t /= 2_c_int_least16_t) call abort()
+    print *, 'c_int_fast16_t is:       ', c_int_fast16_t
+
+    if(my_int32_t     /= 1_c_int32_t)       call abort()
+    if(my_int_least32_t /= 2_c_int_least32_t) call abort()
+    print *, 'c_int_fast32_t is:       ', c_int_fast32_t
+
+    if(my_int64_t     /= 1_c_int64_t)       call abort()
+    if(my_int_least64_t /= 2_c_int_least64_t) call abort()
+    print *, 'c_int_fast64_t is:       ', c_int_fast64_t
+
+    if(my_intmax_t /= 1_c_intmax_t) call abort()
+    if(my_intptr_t /= 4_c_intptr_t) call abort()
+
+    if(my_float       /= 1.0_c_float) call abort()
+    if(my_double      /= 2.0_c_double) call abort()
+    if(my_long_double /= 3.0_c_long_double) call abort()
+
+    if(my_char        /= c_char_'y') call abort()
+    if(my_bool      .neqv. .true._c_bool) call abort()
   end subroutine param_test
     
 end module c_kind_params
+! { dg-final { cleanup-modules "c_kind_params" } }
