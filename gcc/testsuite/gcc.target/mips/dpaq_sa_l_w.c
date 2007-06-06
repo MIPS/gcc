a7be656a367e17862c7ff4b4824aba28e96d38df
chaoyingfu <chaoyingfu@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-06 18:56:51 +0000

    * gcc.target/mips/dpaq_sa_l_w.c: Update types because of the change of 	fixed-point type sizes. 	* gcc.target/mips/dpsq_sa_l_w.c: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/fixed-point@125501 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.target/mips/dpaq_sa_l_w.c
+++ gcc/testsuite/gcc.target/mips/dpaq_sa_l_w.c
@@ -2,22 +2,22 @@
 /* { dg-mips-options "-O2 -mips32r2 -mdsp" } */
 /* { dg-final { scan-assembler-times "\tdpaq_sa.l.w\t\\\$ac" 3 } } */
 
-_Sat long _Fract
-f1 (_Sat _Fract x, _Sat _Fract y, _Sat long _Fract z)
+_Sat long long _Fract
+f1 (_Sat long _Fract x, _Sat long _Fract y, _Sat long long _Fract z)
 {
-  return (_Sat long _Fract) x * y + z;
+  return (_Sat long long _Fract) x * y + z;
 }
 
-_Sat long _Fract
-f2 (_Sat _Fract x, _Sat _Fract y, _Sat long _Fract z)
+_Sat long long _Fract
+f2 (_Sat long _Fract x, _Sat long _Fract y, _Sat long long _Fract z)
 {
-  return z + (_Sat long _Fract) y * x;
+  return z + (_Sat long long _Fract) y * x;
 }
 
-_Sat long _Fract
-f3 (_Sat _Fract x, _Sat _Fract y, _Sat long _Fract z)
+_Sat long long _Fract
+f3 (_Sat long _Fract x, _Sat long _Fract y, _Sat long long _Fract z)
 {
-  _Sat long _Fract t = (_Sat long _Fract) x * y;
+  _Sat long long _Fract t = (_Sat long long _Fract) x * y;
   int temp = 5;
   if (temp == 5)
     z = t + z; /* Need to put z at the end.  GCC does not swap operands to
--- gcc/testsuite/gcc.target/mips/dpsq_sa_l_w.c
+++ gcc/testsuite/gcc.target/mips/dpsq_sa_l_w.c
@@ -2,16 +2,16 @@
 /* { dg-mips-options "-O2 -mips32r2 -mdsp" } */
 /* { dg-final { scan-assembler-times "\tdpsq_sa.l.w\t\\\$ac" 2 } } */
 
-_Sat long _Fract
-f1 (_Sat _Fract x, _Sat _Fract y, _Sat long _Fract z)
+_Sat long long _Fract
+f1 (_Sat long _Fract x, _Sat long _Fract y, _Sat long long _Fract z)
 {
-  return z - (_Sat long _Fract) x * y;
+  return z - (_Sat long long _Fract) x * y;
 }
 
-_Sat long _Fract
-f2 (_Sat _Fract x, _Sat _Fract y, _Sat long _Fract z)
+_Sat long long _Fract
+f2 (_Sat long _Fract x, _Sat long _Fract y, _Sat long long _Fract z)
 {
-  _Sat long _Fract t = (_Sat long _Fract) x * y;
+  _Sat long long _Fract t = (_Sat long long _Fract) x * y;
   int temp = 5;
   if (temp == 5)
     z -= t;
