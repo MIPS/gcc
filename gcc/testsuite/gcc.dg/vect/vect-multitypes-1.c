0f816189b0dd32992f69e7d625e3fa35940cf003
irar <irar@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-06-08 06:31:39 +0000

    Backport from mainline:
    	2007-05-03  Dorit Nuzman  <dorit@il.ibm.com>
    
    	PR tree-optimization/31699
    	* tree-vect-analyze.c (vect_update_misalignment_for_peel): Remove wrong
    	code.
    	(vect_enhance_data_refs_alignment): Compute peel amount using
    	TYPE_VECTOR_SUBPARTS instead of vf.
    	* tree-vect-transform.c (vect_gen_niters_for_prolog_loop): Likewise.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/autovect-branch@125560 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/vect/vect-multitypes-1.c
+++ gcc/testsuite/gcc.dg/vect/vect-multitypes-1.c
@@ -14,7 +14,7 @@ int ib[N] = {0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,
 
 /* Current peeling-for-alignment scheme will consider the 'sa[i+7]'
    access for peeling, and therefore will examine the option of
-   using a peeling factor = VF-7%VF. This will result in a peeling factor 1,
+   using a peeling factor = 8-7%8. This will result in a peeling factor 1,
    which will also align the access to 'ia[i+3]', and the loop could be 
    vectorized on all targets that support unaligned loads.
  */
@@ -43,17 +43,15 @@ int main1 (int n)
 
 /* Current peeling-for-alignment scheme will consider the 'ia[i+3]'
    access for peeling, and therefore will examine the option of
-   using a peeling factor = VF-3%VF. This will result in a peeling factor
-   5 if VF=8, or 1 if VF=4,2. In either case, this will also align the access 
-   to 'sa[i+3]', and the loop could be vectorized on targets that support 
-   unaligned loads.  */
+   using a peeling factor = (4-3)%4. This will result in a peeling factor 1.
+   This will not align the access 'sa[i+3]', and the loop could be vectorized.  */
 
 int main2 (int n)
 {
   int i;
 
   /* Multiple types with different sizes, used in independent
-     copmutations. Vectorizable.  */
+     copmutations.  */
   for (i = 0; i < n; i++)
     {
       ia[i+3] = ib[i];
@@ -80,8 +78,11 @@ int main (void)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { xfail vect_no_align } } } */
-/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 2 "vect" { xfail vect_no_align } } } */
-/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 4 "vect" { xfail vect_no_align } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { xfail vect_no_align } } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 2 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { xfail vect_no_align } } } */
+/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 4 "vect" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 2 "vect" { xfail vect_no_align } } } */
 /* { dg-final { cleanup-tree-dump "vect" } } */
 
