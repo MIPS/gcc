c6d62a6cfae78b3ea8955890cae1c9f48beda4a6
doko <doko@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-07-27 22:49:25 +0000

    import classpath from the trunk (tag libgcj-import-20070727)
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/CLASSPATH@127004 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/classpath/include/config-int.h
+++ libjava/classpath/include/config-int.h
@@ -1,8 +1,8 @@
 #ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
 #define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "classpath 0.95"
-/* generated using gnu compiler gcc (GCC) 4.1.2 20070403 (Red Hat 4.1.2-8) */
+#define _GENERATED_STDINT_H "classpath 0.96-pre"
+/* generated using gnu compiler gcc (GCC) 4.1.3 20070718 (prerelease) (Debian 4.1.2-14) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
