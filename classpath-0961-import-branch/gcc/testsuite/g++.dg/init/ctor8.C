c89d4ecd5c8a5f309ac91d23dfbfc95a1cfd2d63
doko <doko@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-10-21 12:14:24 +0000

    new branch gcj/classpath-0961-import-branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcj@129534 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ classpath-0961-import-branch/gcc/testsuite/g++.dg/init/ctor8.C
@@ -0,0 +1,9 @@
+// PR c++/29039
+
+typedef struct S { // { dg-error "reference" }
+  int &r; 
+}; // { dg-warning "'typedef' was ignored" }
+
+S f () {
+  return S (); // { dg-error "synthesized" }
+}
---
+++ classpath-0961-import-branch/gcc/testsuite/gcc.target/i386/sse-14.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O0 -msse4.1 -msse5" } */
+
+/* Test that the intrinsics compile without optimization.  All of them are
+   defined as inline functions in {,x,e,p,t,s,a}mmintrin.h that reference
+   the proper builtin functions.  Defining away "static" and "__inline"
+   results in all of them being compiled as proper functions.  */
+
+#define static
+#define __inline
+
+#include <bmmintrin.h>
+#include <smmintrin.h>
