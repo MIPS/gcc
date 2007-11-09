3e7aecd5ed6b242634d571f119071f5d2a747903
froydnj <froydnj@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-11-09 14:00:35 +0000

    * dwarf2out.c (lto_var_ref): Remove DW_AT_declaration if we are 	forcing an initialized global variable.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lto@130044 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -15137,6 +15137,11 @@ lto_var_ref (tree var,
   gcc_assert (TREE_CODE (var) == VAR_DECL);
   /* Generate the DIE for VAR.  */
   die = force_decl_die (var);
+  /* If we are forcing an initialized global variable, then remove
+     DW_AT_declaration so the LTO reader understands that we need to
+     read in its initializer.  */
+  if (DECL_CONTEXT (var) == NULL_TREE && DECL_INITIAL (var))
+    remove_AT (die, DW_AT_declaration);
   /* Construct the reference.  */
   lto_init_ref (ref, die, DECL_CONTEXT (var));
 }
