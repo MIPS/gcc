20ff23d18b17e056ae8aabb56ff10bb32fed65a8
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-03-04 11:49:32 +0000

    * gimple.c (walk_gimple_op): Update comment.         (walk_gimple_stmt): Same.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gimple-tuples-branch@132857 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gimple.c
+++ gcc/gimple.c
@@ -1177,8 +1177,18 @@ walk_gimple_asm (gimple stmt, walk_tree_fn callback_op,
 /* Helper function of WALK_GIMPLE_STMT.  Walk every tree operand in
    STMT.  CALLBACK_OP and WI are as in WALK_GIMPLE_STMT.
 
-   If the callback returns non-NULL for any operand, the remaining
-   operands are not scanned.  */
+   CALLBACK_OP is called on each operand of STMT via walk_tree.
+   Additional parameters to walk_tree must be stored in WI.  For each operand
+   OP, walk_tree is called as:
+  
+	walk_tree (&OP, CALLBACK_OP, WI, WI->PSET)
+
+   If CALLBACK_OP returns non-NULL for an operand, the remaining operands are
+   not scanned.
+        
+   The return value is that returned by the last call to walk_tree, or
+   NULL_TREE if no CALLBACK_OP is specified.  */
+
 inline tree
 walk_gimple_op (gimple stmt, walk_tree_fn callback_op,
 		struct walk_stmt_info *wi)
@@ -1362,23 +1372,18 @@ walk_gimple_op (gimple stmt, walk_tree_fn callback_op,
 }
 
 
-/* Walk GIMPLE statement STMT (optionally using traversal state stored
-   in WI).  If WI is NULL, no state is kept during traversal.
-   The callback CALLBACK_STMT is called.  If CALLBACK_STMT returns
-   true, it means that the callback function has handled all the
-   operands of STMT and it is not necessary to walk STMT's operands.
+/* Walk the current statement in GSI (optionally using traversal state stored
+   in WI).  If WI is NULL, no state is kept during traversal.  The callback
+   CALLBACK_STMT is called.  If CALLBACK_STMT returns true, it means that the
+   callback function has handled all the operands of the statement and it is
+   not necessary to walk its operands.
 
    If CALLBACK_STMT is NULL or it returns false, CALLBACK_OP is called
-   on each operand of STMT via walk_tree.  Additional parameters to
-   walk_tree must be stored in WI.  For each operand OP, walk_tree is
-   called as:
+   on each operand of the statement via walk_gimple_op.  If walk_gimple_op
+   returns non-NULL for any operand, the remaining operands are not
+   scanned.
 
-   	walk_tree (&OP, CALLBACK_OP, WI, WI->PSET)
-
-   If CALLBACK_OP returns non-NULL for an operand, the remaining
-   operands are not scanned.
-
-   The return value is that returned by the last call to walk_tree, or
+   The return value is that returned by the last call to walk_gimple_op, or
    NULL_TREE if no CALLBACK_OP is specified.  */
 
 tree
