21b608d5fef3dd382726605c004b372d47f6161e
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-26 14:31:05 +0000

    Merged with trunk at revision 135927.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ix86@135929 138bc75d-0d04-0410-961f-82ee72b054a4

--- avx/gcc/ada/trans.c
+++ avx/gcc/ada/trans.c
@@ -221,14 +221,14 @@ gigi (Node_Id gnat_root, int max_gnat_node, int number_name,
       struct Elist_Header *elists_ptr, struct Elmt_Item *elmts_ptr,
       struct String_Entry *strings_ptr, Char_Code *string_chars_ptr,
       struct List_Header *list_headers_ptr, Nat number_file,
-      struct File_Info_Type *file_info_ptr ATTRIBUTE_UNUSED,
+      struct File_Info_Type *file_info_ptr,
       Entity_Id standard_integer, Entity_Id standard_long_long_float,
       Entity_Id standard_exception_type, Int gigi_operating_mode)
 {
   tree gnu_standard_long_long_float;
   tree gnu_standard_exception_type;
   struct elab_info *info;
-  int i ATTRIBUTE_UNUSED;
+  int i;
 
   max_gnat_nodes = max_gnat_node;
   number_names = number_name;
@@ -920,7 +920,7 @@ Attribute_to_gnu (Node_Id gnat_node, tree *gnu_result_type_p, int attribute)
 	    TREE_CONSTANT (gnu_expr) = 1;
 
 	  if (TREE_CODE (gnu_expr) == ADDR_EXPR)
-	    TREE_STATIC (gnu_expr) = TREE_CONSTANT (gnu_expr) = 1;
+	    TREE_NO_TRAMPOLINE (gnu_expr) = TREE_CONSTANT (gnu_expr) = 1;
 	}
 
       /* For other address attributes applied to a nested function,
