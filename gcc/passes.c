2cd43c1accce881fc1d3d3b232ec6c2465ded5ce
steven <steven@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-06-10 20:50:09 +0000

    * passes.c (init_optimization_passes): Re-enable pass_uncprop. 	* tree-ssa-uncprop (associate_equivalences_with_edges): Convert 	to tuples. 	(uncprop_into_successor_phis): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gimple-tuples-branch@136640 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -716,10 +716,7 @@ init_optimization_passes (void)
       NEXT_PASS (pass_phiopt);
       NEXT_PASS (pass_tail_calls);
       NEXT_PASS (pass_rename_ssa_copies);
-/* FIXME tuples.  */
-#if 0
       NEXT_PASS (pass_uncprop);
-#endif
       NEXT_PASS (pass_del_ssa);
       NEXT_PASS (pass_nrv);
       NEXT_PASS (pass_mark_used_blocks);
