f1fe26c3008ab83831fedd2b9cc29155b2f0ead5
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-08-12 06:21:42 +0000

    * rtlanal.c (side_effects_p): Not in VAR_LOCATION.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/var-tracking-assignments-4_3-branch@138985 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/rtlanal.c
+++ gcc/rtlanal.c
@@ -1,6 +1,6 @@
 /* Analyze RTL for GNU compiler.
    Copyright (C) 1987, 1988, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
-   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 Free Software
+   1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 Free Software
    Foundation, Inc.
 
 This file is part of GCC.
@@ -2116,6 +2116,7 @@ side_effects_p (const_rtx x)
     case SCRATCH:
     case ADDR_VEC:
     case ADDR_DIFF_VEC:
+    case VAR_LOCATION:
       return 0;
 
     case CLOBBER:
