edae8d3929fcb1349596d26dce83ad3e41328a2c
gandalf <gandalf@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-05 19:16:45 +0000

    Import libgcj-snapshot-20090102.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/CLASSPATH@143086 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/classpath/include/config-int.h
+++ libjava/classpath/include/config-int.h
@@ -2,7 +2,7 @@
 #define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "classpath 0.98-pre"
-/* generated using gnu compiler gcc (Debian 4.3.2-1) 4.3.2 */
+/* generated using gnu compiler gcc (Gentoo 4.3.3 p1.1) 4.3.3 20081229 (prerelease) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
