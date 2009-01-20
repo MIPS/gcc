893908535e46f46d18c460cea8ffa96d9a17c8b2
lcwu <lcwu@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-20 21:20:57 +0000

    Fix an issue in cp_parser_init_declarator. When calling grok_field, besides prefix_attributes, we should also pass in the attributes that were just parsed and returned from cp_parser_attributes_opt.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/thread-annotations@143528 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.c
+++ gcc/cp/parser.c
@@ -12921,7 +12921,7 @@ cp_parser_init_declarator (cp_parser* parser,
       decl = grokfield (declarator, decl_specifiers,
 			initializer, !is_non_constant_init,
 			/*asmspec=*/NULL_TREE,
-			prefix_attributes);
+			chainon (prefix_attributes, attributes));
       if (decl && TREE_CODE (decl) == FUNCTION_DECL)
 	cp_parser_save_default_args (parser, decl);
     }
