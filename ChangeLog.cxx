a4d4a4491f47ca297fa50819ff7bc96731dc5c31
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-01-30 15:40:16 +0000

    * configure.ac: Don't add c++ to missing_languages.  Don't use 	-fkeep-inline-functions. 	* configure: Rebuild.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-in-cxx@143791 138bc75d-0d04-0410-961f-82ee72b054a4

--- ChangeLog.cxx
+++ ChangeLog.cxx
@@ -1,3 +1,9 @@
+2009-01-30  Ian Lance Taylor  <iant@google.com>
+
+	* configure.ac: Don't add c++ to missing_languages.  Don't use
+	-fkeep-inline-functions.
+	* configure: Rebuild.
+
 2008-06-18  Ian Lance Taylor  <iant@google.com>
 
 	* configure.ac: Build C++ in stage 1.
