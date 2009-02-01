08f3a4e2c3bf584a4b18c21cd9bf17e84c27b59a
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-01 11:22:08 +0000

    2009-02-01  Richard Guenther  <rguenther@suse.de>
    	* tree-ssa-coalesce.c (create_outofssa_var_map): Adjust
    	virtual operands sanity check.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/alias-improvements@143853 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-coalesce.c
+++ gcc/tree-ssa-coalesce.c
@@ -1114,11 +1114,10 @@ create_outofssa_var_map (coalesce_list_p cl, bitmap used_in_copy)
 	  FOR_EACH_SSA_TREE_OPERAND (var, stmt, iter, (SSA_OP_DEF|SSA_OP_USE))
 	    bitmap_set_bit (used_in_real_ops, DECL_UID (SSA_NAME_VAR (var)));
 
-	  /* Validate that virtual ops don't get used in funny ways.
-	     ???  This verifying is now quite useless.  Checking that
-	     the gimple VOP is not used in a real operand would be enough.  */
-	  bitmap_set_bit (used_in_virtual_ops, 
-			  DECL_UID (gimple_vop (cfun)));
+	  /* Validate that virtual ops don't get used in funny ways.  */
+	  if (gimple_vuse (stmt))
+	    bitmap_set_bit (used_in_virtual_ops, 
+			    DECL_UID (SSA_NAME_VAR (gimple_vuse (stmt))));
 #endif /* ENABLE_CHECKING */
 	}
     }
