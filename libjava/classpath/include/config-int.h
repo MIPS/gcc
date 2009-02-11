6544239dbd42b079a3cd3d8e30b8a47659616ae2
gandalf <gandalf@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-11 20:52:44 +0000

    Synchronise with Classpath 0.98 release.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/CLASSPATH@144110 138bc75d-0d04-0410-961f-82ee72b054a4

--- libjava/classpath/include/config-int.h
+++ libjava/classpath/include/config-int.h
@@ -1,8 +1,8 @@
 #ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
 #define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "classpath 0.98-pre"
-/* generated using gnu compiler gcc (Gentoo 4.3.3 p1.1) 4.3.3 20081229 (prerelease) */
+#define _GENERATED_STDINT_H "classpath 0.98"
+/* generated using gnu compiler gcc (Gentoo 4.3.3 p1.0, pie-10.1.5) 4.3.3 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
