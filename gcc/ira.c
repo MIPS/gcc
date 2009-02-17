4d798c1cb7e78a6efcd63ad51ade3b4e7234eec8
tneumann <tneumann@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-17 20:46:25 +0000

    * ira.c (setup_prohibited_mode_move_regs): Add missing casts to 	enum machine_mode
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-in-cxx@144245 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ira.c
+++ gcc/ira.c
@@ -1317,9 +1317,9 @@ setup_prohibited_mode_move_regs (void)
 	  if (! HARD_REGNO_MODE_OK (j, (enum machine_mode) i))
 	    continue;
 	  SET_REGNO (test_reg1, j);
-	  PUT_MODE (test_reg1, i);
+	  PUT_MODE (test_reg1, (enum machine_mode) i);
 	  SET_REGNO (test_reg2, j);
-	  PUT_MODE (test_reg2, i);
+	  PUT_MODE (test_reg2, (enum machine_mode) i);
 	  INSN_CODE (move_insn) = -1;
 	  recog_memoized (move_insn);
 	  if (INSN_CODE (move_insn) < 0)
