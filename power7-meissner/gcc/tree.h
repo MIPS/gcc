6aeaf041cb9d94db3c90957b561d595f337d80d9
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-02-24 17:07:43 +0000

    Sync up to 144411
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@144413 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-meissner/gcc/tree.h
+++ power7-meissner/gcc/tree.h
@@ -1,6 +1,6 @@
 /* Front-end tree definitions for GNU compiler.
    Copyright (C) 1989, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
-   2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
+   2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
    Free Software Foundation, Inc.
 
 This file is part of GCC.
@@ -5022,6 +5022,7 @@ extern bool gimple_alloca_call_p (const_gimple);
 extern bool alloca_call_p (const_tree);
 extern bool must_pass_in_stack_var_size (enum machine_mode, const_tree);
 extern bool must_pass_in_stack_var_size_or_pad (enum machine_mode, const_tree);
+extern tree block_ultimate_origin (const_tree);
 
 /* In attribs.c.  */
 
