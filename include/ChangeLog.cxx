045a2961c73b89d14d64c2de6b4a1af18ae4567d
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-06 23:34:29 +0000

    * ansidecl.h: Don't define inline as a macro for C++.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-in-cxx@144684 138bc75d-0d04-0410-961f-82ee72b054a4

--- include/ChangeLog.cxx
+++ include/ChangeLog.cxx
@@ -1,3 +1,7 @@
+2009-03-06  Ian Lance Taylor  <iant@google.com>
+
+	* ansidecl.h: Don't define inline as a macro for C++.
+
 2009-02-10  Ian Lance Taylor  <iant@google.com>
 
 	* ansidecl.h (ATTRIBUTE_UNUSED_LABEL): Define for g++ 4.4.
