4efa16613f5cfa6fc128571e339eb81389d01aaa
aoliva <aoliva@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-05-19 00:39:00 +0000

    * tree-ssa-live.c (remove_unused_scope_block_p): Don't remove unused declarations when not optimizing.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/var-tracking-assignments-4_4-branch@147689 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-live.c
+++ gcc/tree-ssa-live.c
@@ -517,6 +517,13 @@ remove_unused_scope_block_p (tree scope)
 		&& ann->used)
 	unused = false;
 
+      /* Removing declarations, when we're not optimizing, is going to
+	 affect the order in which variables are expanded, causing
+	 codegen differences we ought to avoid.  We can't even remove
+	 the blocks, for the same reason.  */
+      else if (!optimize)
+	unused = false;
+
       /* When we are not doing full debug info, we however can keep around
 	 only the used variables for cfgexpand's memory packing saving quite
 	 a lot of memory.  
