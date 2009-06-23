f0fe1eaf059dff71521ec12e0c2beda6dccd2292
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-23 22:27:55 +0000

    Delete old tree
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@148878 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-tmp/gcc/testsuite/gcc.dg/ucnid-11.c
+++
@@ -1,5 +0,0 @@
-/* { dg-do run } */
-/* { dg-xfail-if "" { powerpc-ibm-aix* *-*-solaris2.* } { "*" } { "" } } */
-/* { dg-options "-std=c99 -fextended-identifiers -fdata-sections" } */
-
-#include "ucnid-3.c"
--- power7-tmp/gcc/testsuite/gcc.dg/ucnid-12.c
+++
@@ -1,5 +0,0 @@
-/* { dg-do run } */
-/* { dg-xfail-if "" { powerpc-ibm-aix* *-*-solaris2.* } { "*" } { "" } } */
-/* { dg-options "-std=c99 -fextended-identifiers -ffunction-sections" } */
-
-#include "ucnid-4.c"
