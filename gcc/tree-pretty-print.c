34aeae4d8abcc08822bb330d25088d3524e271d3
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-06 04:45:54 +0000

    * tree-pretty-print.c (debug_tree_chain): Handle cycles.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lto@149272 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-pretty-print.c
+++ gcc/tree-pretty-print.c
@@ -99,13 +99,24 @@ debug_generic_stmt (tree t)
 void
 debug_tree_chain (tree t)
 {
+  struct pointer_set_t *seen = pointer_set_create ();
+
   while (t)
-  {
-    print_generic_expr (stderr, t, TDF_VOPS|TDF_MEMSYMS|TDF_UID);
-    fprintf(stderr, " ");
-    t = TREE_CHAIN (t);
-  }
+    {
+      print_generic_expr (stderr, t, TDF_VOPS|TDF_MEMSYMS|TDF_UID);
+      fprintf (stderr, " ");
+      t = TREE_CHAIN (t);
+      if (pointer_set_insert (seen, t))
+	{
+	  fprintf (stderr, "... [cycled back to ");
+	  print_generic_expr (stderr, t, TDF_VOPS|TDF_MEMSYMS|TDF_UID);
+	  fprintf (stderr, "]");
+	  break;
+	}
+    }
   fprintf (stderr, "\n");
+
+  pointer_set_destroy (seen);
 }
 
 /* Prints declaration DECL to the FILE with details specified by FLAGS.  */
