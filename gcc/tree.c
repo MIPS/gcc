36b7353621fbdade51bc65f6371ad5bddbe880ca
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-08-10 17:56:49 +0000

    * tree.c (build2_stat): Allow POINTER_PLUS_EXPR to match sizetype         of cfun instead of current sizetype.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/milepost-integration@150629 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.c
+++ gcc/tree.c
@@ -3323,8 +3323,14 @@ build2_stat (enum tree_code code, tree tt, tree arg0, tree arg1 MEM_STAT_DECL)
   if (code == POINTER_PLUS_EXPR && arg0 && arg1 && tt)
     gcc_assert (POINTER_TYPE_P (tt) && POINTER_TYPE_P (TREE_TYPE (arg0))
 		&& INTEGRAL_TYPE_P (TREE_TYPE (arg1))
-		&& useless_type_conversion_p (targetm.sizetype,
-					      TREE_TYPE (arg1)));
+		&& (useless_type_conversion_p (targetm.sizetype,
+					       TREE_TYPE (arg1))
+		    /* When analyzing a loop for vectorization on another
+		       target, we might temporarily process pointer trees
+		       that don't agree with targetm.  */
+		    || (useless_type_conversion_p
+			 (targetm_array[cfun->target_arch]->sizetype,
+			  TREE_TYPE (arg1)))));
 
   t = make_node_stat (code PASS_MEM_STAT);
   TREE_TYPE (t) = tt;
