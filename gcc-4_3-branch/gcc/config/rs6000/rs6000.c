ada90d22df3e10a87972a06a0c5b900b497eabca
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-23 15:40:48 +0000

    Backport change from Jakub to set TREE_READONLY instead of TREE_CONSTANT
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@153502 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_3-branch/gcc/config/rs6000/rs6000.c
+++ gcc-4_3-branch/gcc/config/rs6000/rs6000.c
@@ -8438,7 +8438,7 @@ def_builtin (int mask, const char *name, tree type, int code)
 
 	  /* const function, function only depends on the inputs.  */
 	case RS6000_BTC_CONST:
-	  TREE_CONSTANT (t) = 1;
+	  TREE_READONLY (t) = 1;
 	  TREE_NOTHROW (t) = 1;
 	  break;
 
@@ -8462,7 +8462,7 @@ def_builtin (int mask, const char *name, tree type, int code)
 	      DECL_IS_NOVOPS (t) = 1;
 	    }
 	  else
-	    TREE_CONSTANT (t) = 1;
+	    TREE_READONLY (t) = 1;
 	  break;
 	}
     }
