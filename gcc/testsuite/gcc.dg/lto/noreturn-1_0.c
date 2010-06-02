ea9c755ad5471a8f850ae59838901d9d6e90fc71
hubicka <hubicka@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-02 10:12:58 +0000

    Merge from mainline.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pretty-ipa@160145 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/lto/noreturn-1_0.c
+++ gcc/testsuite/gcc.dg/lto/noreturn-1_0.c
@@ -1,17 +1,6 @@
-void exit (int);
-__attribute__ ((noreturn))
-int
-call_me (void)
-{
-  exit (0);
-}
-void exit (int);
-__attribute__ ((noreturn))
-int
-call_me (void)
-{
-  exit (0);
-}
+/* { dg-lto-do run } */
+/* { dg-lto-options {{-O2 -fwhopr} } } */
+
 void exit (int);
 __attribute__ ((noreturn))
 int
--- gcc/testsuite/gcc.dg/lto/noreturn-1_1.c
+++ gcc/testsuite/gcc.dg/lto/noreturn-1_1.c
@@ -1,24 +1,3 @@
-/* { dg-lto-do run } */
-/* { dg-lto-options {{-O2 -fwhopr} } } */
-
-int call_me (void);
-int
-main(void)
-{
- return call_me ();
-}
-/* { dg-lto-do run } */
-/* { dg-lto-options {{-O2 -fwhopr} } } */
-
-int call_me (void);
-int
-main(void)
-{
- return call_me ();
-}
-/* { dg-lto-do run } */
-/* { dg-lto-options {{-O2 -fwhopr} } } */
-
 int call_me (void);
 int
 main(void)
