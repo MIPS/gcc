71b81e9a5972de2b3dbce791a3f38c1f713da11f
amylaar <amylaar@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-21 19:30:44 +0000

    * tree.c, tree.h: Back out more cross-target autoparallelization /         vectorization code to facilitate the merge of the basic multi-target         infrastructure.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/multi-target-20100613-branch@161110 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.h
+++ gcc/tree.h
@@ -1049,12 +1049,6 @@ extern void omp_clause_range_check_failed (const_tree, const char *, int,
   (TREE_CODE (TYPE) == COMPLEX_TYPE	\
    && TREE_CODE (TREE_TYPE (TYPE)) == REAL_TYPE)
 
-/* Nonzero if TYPE represents a integer floating-point type.  */
-
-#define VECTOR_INT_TYPE_P(TYPE)	\
-  (TREE_CODE (TYPE) == VECTOR_TYPE	\
-   && TREE_CODE (TREE_TYPE (TYPE)) == INTEGER_TYPE)
-
 /* Nonzero if TYPE represents a vector floating-point type.  */
 
 #define VECTOR_FLOAT_TYPE_P(TYPE)	\
