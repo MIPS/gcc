6ec549db9f0c29f985bd6f7bd9f2e47d94574390
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-06 19:24:26 +0000

    Redo branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@161881 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-meissner/gcc/testsuite/gcc.dg/mtune.c
+++
@@ -1,6 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-mtune=foo" } */
-/* { dg-error "mtune" "" { target *-*-* } 0 } */
-/* { dg-bogus "march" "" { target *-*-* } 0 } */
-/* { dg-bogus "mcpu" "" { target *-*-* } 0 } */
-int i;
--- power7-meissner/gcc/testsuite/gcc.target/i386/avx-cmpsd-1.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do run } */
-/* { dg-require-effective-target avx } */
-/* { dg-options "-O2 -mavx -std=c99" } */
-
-#define CHECK_H "avx-check.h"
-#define TEST avx_test
-
-#include "sse2-cmpsd-1.c"
