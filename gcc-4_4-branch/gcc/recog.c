170cd68d9116b1f5618927e429debfabc64f4b68
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-13 20:48:52 +0000

    svn merge -r162141:162142 svn+ssh://gcc.gnu.org/svn/gcc/trunk
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/redhat@162152 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_4-branch/gcc/recog.c
+++ gcc-4_4-branch/gcc/recog.c
@@ -2689,7 +2689,7 @@ constrain_operands (int strict)
 			case PRE_MODIFY:
 			case POST_MODIFY:
 			  if (strchr (recog_data.constraints[opno], '<') == NULL
-			      || strchr (recog_data.constraints[opno], '>')
+			      && strchr (recog_data.constraints[opno], '>')
 				 == NULL)
 			    return 0;
 			  break;
