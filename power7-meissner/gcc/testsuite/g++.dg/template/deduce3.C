e3cdd330313937d9945ac843b1953bbfd3ee7bc7
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-28 19:15:49 +0000

    Delete branch to recreate it
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@162654 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-meissner/gcc/testsuite/g++.dg/template/deduce3.C
+++
@@ -1,9 +0,0 @@
-template <typename T>
-void f(int, T (*)() = 0);	// { dg-message "candidate" }
-
-void g() {
-  typedef int A[2];
-  f<A>(0); // { dg-error "" }
-  typedef void F();
-  f<F>(0); // { dg-error "" }
-}
