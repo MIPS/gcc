3a088bf3c1f60bc70856b9afb2eb887f7b04799c
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-22 16:07:37 +0000

    Delete power7-fpconv branch to recreate it
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@165843 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-fpconv/gcc/testsuite/objc.dg/property/property-neg-2.m
+++
@@ -1,9 +0,0 @@
-/* This program checks for proper declaration of property. */
-/* { dg-do compile } */
-
-@interface Bar
-@end
-
-@implementation Bar
-@property int foo; /* { dg-error "no declaration of property 'foo' found in the interface" } */
-@end
