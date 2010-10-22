f7bb2e68c8588dbb8d69f86e69eabea437e8a0bf
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-10-22 15:35:24 +0000

    Delete power7-target branch to recreate it
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@165839 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-target/gcc/testsuite/g++.dg/overload/unknown1.C
+++
@@ -1,9 +0,0 @@
-// { dg-do compile }
-
-void foo(void);
-int foo(int);
-template <typename T> void bar(T f); // { dg-message "candidate" }
-
-void baz() {
-  bar(foo); // { dg-error "<unresolved overloaded function type>" }
-}
