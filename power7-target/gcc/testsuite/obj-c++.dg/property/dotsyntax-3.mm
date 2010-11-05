e7c63245b9d67169eb684a1afd5ff51dc99a7e4c
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-05 15:33:38 +0000

    Merge up 166360.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@166361 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-target/gcc/testsuite/obj-c++.dg/property/dotsyntax-3.mm
+++ power7-target/gcc/testsuite/obj-c++.dg/property/dotsyntax-3.mm
@@ -3,44 +3,46 @@
 /* { dg-xfail-run-if "Needs OBJC2 ABI" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime" } { "" } } */
 
 /* Test the 'dot syntax' without a declarated property.  This tests the case where
-   only the setter (or only the getter) exists.  */
+   the object is a Class.  */
+
 
 #include <stdlib.h>
 #include <objc/objc.h>
 #include <objc/runtime.h>
 
+static int a;
+static id b;
+
 @interface MyRootClass
 {
   Class isa;
-  int a;
-  id b;
 }
 + (id) initialize;
 + (id) alloc;
 - (id) init;
-- (int) a;
-- (void) setCount: (int)value;
-- (id) b;
-- (void) setNext: (id)value;
++ (int) count;
++ (void) setCount: (int)value;
++ (id) next;
++ (void) setNext: (id)value;
 @end
 
 @implementation MyRootClass
 + (id) initialize { return self; }
 + (id) alloc { return class_createInstance (self, 0); }
 - (id) init { return self; }
-- (int) a
++ (int) count
 {
   return a;
 }
-- (void) setCount: (int)value
++ (void) setCount: (int)value
 {
   a = value;
 }
-- (id) b
++ (id) next
 {
   return b;
 }
-- (void) setNext: (id)value
++ (void) setNext: (id)value
 {
   b = value;
 }
@@ -50,23 +52,13 @@ int main (void)
 {
   MyRootClass *object = [[MyRootClass alloc] init];
 
-  /* This should work because -setCount: exists (even if -count does
-     not).  */
-  object.count = 40;
-
-  /* This should work because -a exists (even if -setA: does not).  */
-  if (object.a != 40)
+  MyRootClass.count = 40;
+  if (MyRootClass.count != 40)
     abort ();
 
-  /* This should work because -setNext: exists (even if -next does
-     not).  */
-  object.next = object;
-
-  /* This should work because -b exists (even if -setB: does not).  */
-  if (object.b != object)
+  MyRootClass.next = object;
+  if (MyRootClass.next != object)
     abort ();
 
   return 0;
 }
-
-
