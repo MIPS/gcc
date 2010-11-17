f4a3db2c0f0b368c27ee085391a8d75da409ff55
Thomas Schwinge <thomas@schwinge.name>
2010-11-17 13:02:29 +0100

    Remove special /usr/include vs. /include handling for GNU/Hurd.

--- gcc/config/gnu.h
+++ gcc/config/gnu.h
@@ -28,10 +28,6 @@ along with GCC.  If not, see <http://www.gnu.org/licenses/>.
 #undef LIB_SPEC
 #define LIB_SPEC "%{pthread:-lpthread} %{bsd:-lbsd-compat} %{pg|p|profile:-lc_p;:-lc}"
 
-/* Standard include directory.  In GNU, "/usr" is a four-letter word.  */
-#undef STANDARD_INCLUDE_DIR
-#define STANDARD_INCLUDE_DIR "/include"
-
 #undef LINUX_TARGET_OS_CPP_BUILTINS
 #define LINUX_TARGET_OS_CPP_BUILTINS()		\
     do {					\
