0e6a11a04c75660612bd37131dcf6de304068886
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-01-11 23:40:50 +0000

    delete branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@168686 138bc75d-0d04-0410-961f-82ee72b054a4

--- power7-fpconv/gcc/testsuite/g++.dg/cpp0x/constexpr-ctor3.C
+++
@@ -1,9 +0,0 @@
-// { dg-options -std=c++0x }
-
-struct A
-{
-  int arr[1];
-
-  constexpr A()
-  : arr() { }
-};
--- power7-fpconv/gcc/testsuite/obj-c++.dg/property/property-neg-5.mm
+++
@@ -1,5 +0,0 @@
-/* { dg-do compile } */
-
-@interface Foo
-@property ( readonly, getter = HELLO, setter = THERE : ) int value; /* { dg-warning ".readonly. attribute conflicts with .setter. attribute" } */
-@end
