644ceb9e2b56568df4bd61c4282136589a816446
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-14 21:07:05 +0000

    Backport from mainline 	2011-02-02  Peter Bergner  <bergner@vnet.ibm.com>
    	PR rtl-optimization/47525
    	* df-scan.c: Update copyright years.
    	(df_get_call_refs): Do not mark global registers as DF_REF_REG_USE
    	and non-clobber DF_REF_REG_DEF for calls to const and pure functions.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@170153 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_5-branch/gcc/df-scan.c
+++ gcc-4_5-branch/gcc/df-scan.c
@@ -1,6 +1,6 @@
 /* Scanning of rtl for dataflow analysis.
    Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-   2008, 2009, 2010 Free Software Foundation, Inc.
+   2008, 2009, 2010, 2011 Free Software Foundation, Inc.
    Originally contributed by Michael P. Hayes
              (m.hayes@elec.canterbury.ac.nz, mhayes@redhat.com)
    Major rewrite contributed by Danny Berlin (dberlin@dberlin.org)
@@ -3492,18 +3492,21 @@ df_get_call_refs (struct df_collection_rec * collection_rec,
 		 DF_REF_CALL_STACK_USAGE | flags,
 		 -1, -1, VOIDmode);
 
-  /* Calls may also reference any of the global registers,
-     so they are recorded as used.  */
-  for (i = 0; i < FIRST_PSEUDO_REGISTER; i++)
-    if (global_regs[i])
-      {
-	df_ref_record (DF_REF_BASE, collection_rec, regno_reg_rtx[i],
-		       NULL, bb, insn_info, DF_REF_REG_USE, flags, -1, -1,
-		       VOIDmode);
-	df_ref_record (DF_REF_BASE, collection_rec, regno_reg_rtx[i],
-		       NULL, bb, insn_info, DF_REF_REG_DEF, flags, -1, -1,
-		       VOIDmode);
-      }
+  /* Calls to const functions cannot access any global registers and calls to
+     pure functions cannot set them.  All other calls may reference any of the
+     global registers, so they are recorded as used.  */
+  if (!RTL_CONST_CALL_P (insn_info->insn))
+    for (i = 0; i < FIRST_PSEUDO_REGISTER; i++)
+      if (global_regs[i])
+	{
+	  df_ref_record (DF_REF_BASE, collection_rec, regno_reg_rtx[i],
+			 NULL, bb, insn_info, DF_REF_REG_USE, flags, -1, -1,
+			 VOIDmode);
+	  if (!RTL_PURE_CALL_P (insn_info->insn))
+	    df_ref_record (DF_REF_BASE, collection_rec, regno_reg_rtx[i],
+			   NULL, bb, insn_info, DF_REF_REG_DEF, flags, -1, -1,
+			   VOIDmode);
+	}
 
   is_sibling_call = SIBLING_CALL_P (insn_info->insn);
   EXECUTE_IF_SET_IN_BITMAP (regs_invalidated_by_call_regset, 0, ui, bi)
