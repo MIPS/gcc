c771960951038eca4ed9dff45e973a3e80b7f64e
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-09 15:38:14 +0000

    Merge up to subversion id 170804
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@170820 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_5-branch/gcc/cp/pt.c
+++ gcc-4_5-branch/gcc/cp/pt.c
@@ -4951,7 +4951,8 @@ convert_nontype_argument (tree type, tree expr)
 
   /* Add the ADDR_EXPR now for the benefit of
      value_dependent_expression_p.  */
-  if (TYPE_PTROBV_P (type))
+  if (TYPE_PTROBV_P (type)
+      && TREE_CODE (TREE_TYPE (expr)) == ARRAY_TYPE)
     expr = decay_conversion (expr);
 
   /* If we are in a template, EXPR may be non-dependent, but still
@@ -6043,7 +6044,7 @@ coerce_template_parms (tree parms,
 		    sorry ("cannot expand %<%T%> into a fixed-length "
 			   "argument list", arg);
 		}
-	      return error_mark_node;
+	      ++lost;
             }
         }
       else if (require_all_args)
@@ -6071,7 +6072,7 @@ coerce_template_parms (tree parms,
            reported) that we are trying to recover from, e.g., a class
            template with a parameter list such as
            template<typename..., typename>.  */
-        return error_mark_node;
+	++lost;
       else
 	arg = convert_template_argument (TREE_VALUE (parm),
 					 arg, new_args, complain, 
