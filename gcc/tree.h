a657166498d93a1a9069d6e95a574ec85f2a91f5
lcwu <lcwu@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-25 21:54:48 +0000

    Incorporated Diego's review comments.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/annotalysis@171529 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree.h
+++ gcc/tree.h
@@ -5363,6 +5363,7 @@ extern tree decl_attributes (tree *, tree, int);
 /* Return true if the given identifier tree is the name of a lock attribute
    that takes arguments.  */
 extern bool is_lock_attribute_with_args (const_tree);
+
 /* Extract and return all lock attributes from the given attribute list.  */
 extern tree extract_lock_attributes (tree);
 
