ce383b3ffc9a88124c2a8ceb9cb10012ad5e1661
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-01-27 16:56:07 +0000

    update copyright
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@183636 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_6-meissner/gcc/config/rs6000/rs6000-protos.h
+++ gcc-4_6-meissner/gcc/config/rs6000/rs6000-protos.h
@@ -1,6 +1,6 @@
 /* Definitions of target machine for GNU compiler, for IBM RS/6000.
    Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-   2010, 2011
+   2010, 2011, 2012
    Free Software Foundation, Inc.
    Contributed by Richard Kenner (kenner@vlsi1.ultra.nyu.edu)
 
