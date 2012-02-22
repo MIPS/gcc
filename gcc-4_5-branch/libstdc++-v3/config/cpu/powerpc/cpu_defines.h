ba89f025a1d82a3b91a4c2b65b3ddfc1f7f84005
bergner <bergner@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-22 23:14:46 +0000

    * configure.host: Set OPTIMIZE_CXXFLAGS to -DIN_LIBSTDCXX for 	powerpc*-linux. 	* config/cpu/powerpc/cpu_defines.h 	(__rs6000_atomic_fetch_and_add_acquire_32): New define. 	(__rs6000_atomic_fetch_and_add_release_32): Likewise. 	* include/ext/atomicity.h (__exchange_and_add): Use them. 	(__atomic_add): Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@184495 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_5-branch/libstdc++-v3/config/cpu/powerpc/cpu_defines.h
+++ gcc-4_5-branch/libstdc++-v3/config/cpu/powerpc/cpu_defines.h
@@ -33,4 +33,68 @@
 // Integer divide instructions don't trap on PowerPC.
 #define __glibcxx_integral_traps false
 
+#ifdef IN_LIBSTDCXX
+#ifdef __PPC64__
+#define __rs6000_atomic_fetch_and_add_acquire_32(mem, constant) \
+({							\
+  __typeof (*mem) __tmpval, __tmp0, __tmp1;		\
+  __asm __volatile (					\
+    "1: lwarx %2,0,%4\n"				\
+    "   addi %1,%2,%5\n"				\
+    "   stwcx. %1,0,%4\n"				\
+    "   bne- 1b\n"					\
+    "   extsw %0,%2\n"					\
+    "   isync\n"					\
+    : "=&r" (__tmpval), "=&r" (__tmp0), "=&b" (__tmp1), "=m" (*mem) \
+    : "b" (mem), "i" (constant), "m" (*mem)		\
+    : "cr0", "memory");					\
+  __tmpval;						\
+})
+#define __rs6000_atomic_fetch_and_add_release_32(mem, constant) \
+({							\
+  __typeof (*mem) __tmpval, __tmp0, __tmp1;		\
+  __asm __volatile (					\
+    "   lwsync\n"					\
+    "1: lwarx %2,0,%4\n"				\
+    "   addi %1,%2,%5\n"				\
+    "   stwcx. %1,0,%4\n"				\
+    "   bne- 1b\n"					\
+    "   extsw %0,%2\n"					\
+    : "=&r" (__tmpval), "=&r" (__tmp0), "=&b" (__tmp1), "=m" (*mem) \
+    : "b" (mem), "i" (constant), "m" (*mem)		\
+    : "cr0", "memory");					\
+  __tmpval;						\
+})
+#else
+#define __rs6000_atomic_fetch_and_add_acquire_32(mem, constant) \
+({							\
+  __typeof (*mem) __tmpval, __tmp0;			\
+  __asm __volatile (					\
+    "1: lwarx %0,0,%3\n"				\
+    "   addi %1,%0,%4\n"				\
+    "   stwcx. %1,0,%3\n"				\
+    "   bne- 1b\n"					\
+    "   isync\n"					\
+    : "=&b" (__tmpval), "=&r" (__tmp0), "=m" (*mem)	\
+    : "b" (mem), "i" (constant), "m" (*mem)		\
+    : "cr0", "memory");					\
+  __tmpval;						\
+})
+#define __rs6000_atomic_fetch_and_add_release_32(mem, constant) \
+({							\
+  __typeof (*mem) __tmpval, __tmp0;			\
+  __asm __volatile (					\
+    "   lwsync\n"					\
+    "1: lwarx %0,0,%3\n"				\
+    "   addi %1,%0,%4\n"				\
+    "   stwcx. %1,0,%3\n"				\
+    "   bne- 1b\n"					\
+    : "=&b" (__tmpval), "=&r" (__tmp0), "=m" (*mem)	\
+    : "b" (mem), "i" (constant), "m" (*mem)		\
+    : "cr0", "memory");					\
+  __tmpval;						\
+})
+#endif /* __PPC64__ */
+#endif /* IN_LIBSTDCXX */
+
 #endif
