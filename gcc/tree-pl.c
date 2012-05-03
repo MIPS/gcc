cb64dca67a6fbb19aff56a0f0cbbbae3ace5d829
ienkovic <ilya.enkovich@intel.com>
2012-05-03 16:57:28 +0400

    NOP_EXPR support.

--- gcc/tree-pl.c
+++ gcc/tree-pl.c
@@ -632,6 +632,7 @@ pl_compute_bounds_for_assignment (tree node, gimple assign)
     case SSA_NAME:
     case ADDR_EXPR:
     case POINTER_PLUS_EXPR:
+    case NOP_EXPR:
       iter = gsi_for_stmt (assign);
       bounds = pl_find_bounds (rhs1, iter);
       break;
