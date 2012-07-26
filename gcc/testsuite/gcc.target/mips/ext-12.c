68ab5d26697caa3c5a3efc9f335b4b9a825276d1
Andrew Pinski <apinski@cavium.com>
2012-07-26 16:14:53 -0700

    2012-07-26  Andrew Pinski  <apinski@cavium.com>             Adam Nemet  <anemet@caviumnetworks.com>
    	* testsuite/gcc.target/mips/ext-12.c: New test.
    	* testsuite/gcc.target/mips/ext-13.c: New test.

---
+++ gcc/testsuite/gcc.target/mips/ext-12.c
@@ -0,0 +1,30 @@
+/* { dg-options "-O2 -mgp64 isa_rev>=2" } */
+/* { dg-final { scan-assembler-times "ext\\t" 5 } } */
+/* { dg-final { scan-assembler-times "dsll" 2 } } */
+/* { dg-final { scan-assembler-times "\\tsll" 1 } } */
+/* { dg-final { scan-assembler-not "and" } } */
+
+unsigned long long f (unsigned long long a)
+{
+  return a & 0x300000000ull;
+}
+
+unsigned long long g (unsigned long long a)
+{
+  return a & 0x100000000ull;
+}
+
+unsigned long long h (unsigned long long a)
+{
+  return a & 0xfffffffffull;
+}
+
+unsigned long long ff (unsigned long long a)
+{
+  return (a & 0xfff0000000000ull) >> 40;
+}
+
+unsigned hh (unsigned a)
+{
+  return a & 0xffff000u;
+}
---
+++ gcc/testsuite/gcc.target/mips/ext-13.c
@@ -0,0 +1,43 @@
+/* { dg-options "-O2 -mgp64 isa_rev>=2" } */
+/* { dg-final { scan-assembler-times "\\tdext" 2 } } */
+/* { dg-final { scan-assembler-times "\\tdins" 2 } } */
+
+unsigned long long f (unsigned long long a)
+{
+  /* li */
+  return a & 0x3000ull;
+}
+
+unsigned long long g (unsigned long long a)
+{
+  /* dext/dsll */
+  return a & 0x700000ull;
+}
+
+unsigned long long ff (unsigned long long a)
+{
+  /* dins */
+  return a & 0xffffffffff000000ull;
+}
+
+unsigned long long gg (unsigned long long a)
+{
+  /* dins */
+  return a & 0xfffffffffffff000ull;
+}
+
+unsigned long long hh (unsigned long long a)
+{
+  return a & 0x10300000000ull;
+}
+
+unsigned fff (unsigned a)
+{
+  /* dext/sll */
+  return a & 0xff00000u;
+}
+
+unsigned ggg (unsigned a)
+{
+  return a & 0xffu;
+}
