c3a7c24fd1101e87b1d6ff61770e681955e65933
Andrew Pinski <apinski@cavium.com>
2012-07-26 18:09:34 -0700

    2012-07-26  Andrew Pinski  <apinski@cavium.com>
    	Bug #3261
    	* testsuite/gcc.target/mips/movcc-4.c: New testcase.

---
+++ gcc/testsuite/gcc.target/mips/movcc-4.c
@@ -0,0 +1,13 @@
+/* { dg-options " isa>=4" } */
+/* { dg-final { scan-assembler-times "movz\t|movn\t" 1 } } */
+/* { dg-final { scan-assembler-not "bbit0\t|bbit1\t" } } */
+/* { dg-final { scan-assembler-not "xori\t|xor\t" } } */
+
+NOMIPS16 int f(int a, int b, int c)
+{
+  int d = a&0x1;
+  if (d==1)
+    return b;
+  return c;
+}
+
