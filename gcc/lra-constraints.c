6f69bae21c5e4d6af41c175858c4788ac7ea20f9
vmakarov <vmakarov@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-23 20:11:36 +0000

    2012-10-23  Vladimir Makarov  <vmakarov@redhat.com>
    	* lra-constraints.c (update_ebb_live_info): Process empty blocks.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/lra@192744 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lra-constraints.c
+++ gcc/lra-constraints.c
@@ -4290,8 +4290,6 @@ update_ebb_live_info (rtx head, rtx tail)
        curr_insn = prev_insn)
     {
       prev_insn = PREV_INSN (curr_insn);
-      if (! INSN_P (curr_insn))
-	continue;
       curr_bb = BLOCK_FOR_INSN (curr_insn);
       if (curr_bb != prev_bb)
 	{
@@ -4326,7 +4324,7 @@ update_ebb_live_info (rtx head, rtx tail)
 	  prev_bb = curr_bb;
 	  bitmap_and (&live_regs, &check_only_regs, df_get_live_out (curr_bb));
 	}
-      if (DEBUG_INSN_P (curr_insn))
+      if (! NONDEBUG_INSN_P (curr_insn))
 	continue;
       curr_id = lra_get_insn_recog_data (curr_insn);
       remove_p = false;
