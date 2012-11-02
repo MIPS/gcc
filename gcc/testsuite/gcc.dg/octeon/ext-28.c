9e59d17292e4e2fae97c82e4980e16499978d6cf
Andrew Pinski <apinski@cavium.com>
2012-11-02 13:58:52 -0700

    2012-11-02  Andrew Pinski  <apinski@cavium.com>
    	* testsuite/gcc.dg/octeon/ext-26.c: Check for uld only on the elf targets.
    	Check for ldr/ldl on the linux targets.
    	* testsuite/gcc.dg/octeon/ext-28.c: Likewise.

--- gcc/testsuite/gcc.dg/octeon/ext-28.c
+++ gcc/testsuite/gcc.dg/octeon/ext-28.c
@@ -1,6 +1,8 @@
 /* { dg-do compile { target *-octeon-* } } */
 /* { dg-options "-O2" } */
-/* { dg-final { scan-assembler "uld\t" } } */
+/* { dg-final { scan-assembler "uld\t" { target mips*-*-elf } } } */
+/* { dg-final { scan-assembler "ldr\t" { target mips*-*-linux-gnu } } } */
+/* { dg-final { scan-assembler "ldl\t" { target mips*-*-linux-gnu } } } */
 
 struct TRIAL
 {
