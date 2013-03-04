dcb1b7dab7a2311980bd724ebf8481ec809ad835
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-04 11:52:39 +0000

    svn merge -r196172:196429 svn+ssh://gcc.gnu.org/svn/gcc/trunk
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/redhat@196430 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_8-branch/gcc/ipa-cp.c
+++ gcc-4_8-branch/gcc/ipa-cp.c
@@ -1654,7 +1654,7 @@ good_cloning_opportunity_p (struct cgraph_node *node, int time_benefit,
 		 ") -> evaluation: " HOST_WIDEST_INT_PRINT_DEC
 		 ", threshold: %i\n",
 		 time_benefit, size_cost, (HOST_WIDE_INT) count_sum,
-		 evaluation, 500);
+		 evaluation, PARAM_VALUE (PARAM_IPA_CP_EVAL_THRESHOLD));
 
       return evaluation >= PARAM_VALUE (PARAM_IPA_CP_EVAL_THRESHOLD);
     }
@@ -1668,7 +1668,7 @@ good_cloning_opportunity_p (struct cgraph_node *node, int time_benefit,
 		 "size: %i, freq_sum: %i) -> evaluation: "
 		 HOST_WIDEST_INT_PRINT_DEC ", threshold: %i\n",
 		 time_benefit, size_cost, freq_sum, evaluation,
-		 CGRAPH_FREQ_BASE /2);
+		 PARAM_VALUE (PARAM_IPA_CP_EVAL_THRESHOLD));
 
       return evaluation >= PARAM_VALUE (PARAM_IPA_CP_EVAL_THRESHOLD);
     }
@@ -2807,12 +2807,15 @@ intersect_with_plats (struct ipcp_param_lattices *plats,
    vector result while subtracting OFFSET from the individual value offsets.  */
 
 static vec<ipa_agg_jf_item_t>
-agg_replacements_to_vector (struct cgraph_node *node, HOST_WIDE_INT offset)
+agg_replacements_to_vector (struct cgraph_node *node, int index,
+			    HOST_WIDE_INT offset)
 {
   struct ipa_agg_replacement_value *av;
   vec<ipa_agg_jf_item_t> res = vNULL;
 
   for (av = ipa_get_agg_replacements_for_node (node); av; av = av->next)
+    if (av->index == index
+	&& (av->offset - offset) >= 0)
     {
       struct ipa_agg_jf_item item;
       gcc_checking_assert (av->value);
@@ -2892,7 +2895,7 @@ intersect_aggregates_with_edge (struct cgraph_edge *cs, int index,
 	  if (agg_pass_through_permissible_p (orig_plats, jfunc))
 	    {
 	      if (!inter.exists ())
-		inter = agg_replacements_to_vector (cs->caller, 0);
+		inter = agg_replacements_to_vector (cs->caller, src_idx, 0);
 	      else
 		intersect_with_agg_replacements (cs->caller, src_idx,
 						 &inter, 0);
@@ -2925,9 +2928,9 @@ intersect_aggregates_with_edge (struct cgraph_edge *cs, int index,
       if (caller_info->ipcp_orig_node)
 	{
 	  if (!inter.exists ())
-	    inter = agg_replacements_to_vector (cs->caller, delta);
+	    inter = agg_replacements_to_vector (cs->caller, src_idx, delta);
 	  else
-	    intersect_with_agg_replacements (cs->caller, index, &inter,
+	    intersect_with_agg_replacements (cs->caller, src_idx, &inter,
 					     delta);
 	}
       else
@@ -3403,6 +3406,9 @@ decide_whether_version_node (struct cgraph_node *node)
       info = IPA_NODE_REF (node);
       info->do_clone_for_all_contexts = false;
       IPA_NODE_REF (clone)->is_all_contexts_clone = true;
+      for (i = 0; i < count ; i++)
+	vec_free (known_aggs[i].items);
+      known_aggs.release ();
       ret = true;
     }
   else
