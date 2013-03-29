15b926a1cc21ba3a03794793643d62189a4c2457
gdr <gdr@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-03-29 16:52:59 +0000

    * gcc/c-family/c-common.c (cxx_dialect): Default to C++1y.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/var-template@197242 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -240,7 +240,7 @@ int flag_use_repository;
 
 /* The C++ dialect being used. C++11 is the default.  */
 
-enum cxx_dialect cxx_dialect = cxx11;
+enum cxx_dialect cxx_dialect = cxx1y;
 
 /* Maximum template instantiation depth.  This limit exists to limit the
    time it takes to notice excessively recursive template instantiations.
