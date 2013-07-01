073c234f34c1b12044235770a0b42a55abc5a65d
Igor Zamyatin <igor.zamyatin@intel.com>
2013-07-01 15:14:44 +0400

    Added DWARF register numbers for bound registers both for 32 and 64 bits.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -2203,7 +2203,7 @@ int const dbx_register_map[FIRST_PSEUDO_REGISTER] =
   29, 30, 31, 32, 33, 34, 35, 36,       /* MMX */
   -1, -1, -1, -1, -1, -1, -1, -1,	/* extended integer registers */
   -1, -1, -1, -1, -1, -1, -1, -1,	/* extended SSE registers */
-  -1, -1, -1, -1,                       /* bound registers */
+  101, 102, 103, 104,			/* bound registers */
 };
 
 /* The "default" register map used in 64bit mode.  */
@@ -2217,7 +2217,7 @@ int const dbx64_register_map[FIRST_PSEUDO_REGISTER] =
   41, 42, 43, 44, 45, 46, 47, 48,       /* MMX */
   8,9,10,11,12,13,14,15,		/* extended integer registers */
   25, 26, 27, 28, 29, 30, 31, 32,	/* extended SSE registers */
-  -1, -1, -1, -1,                       /* bound registers */
+  126, 127, 128, 129,			/* bound registers */
 };
 
 /* Define the register numbers to be used in Dwarf debugging information.
