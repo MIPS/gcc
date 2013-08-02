31bb560a41ddfd5dfe524b556811d48549c50b8b
mfortune <matthew.fortune@imgtec.com>
2013-08-02 11:12:03 +0100

    Add reg_equiv_profitable_p target hook
    This new hook allows a target to decide if a register equivalence
    is actually worth using. For mips16 this allows a memory equivalence
    for an argument can be discarded if the argument save area will
    end up too far from the stack pointer for a 16bit lw/sw
    
    Conflicts:
    	gcc/config/mips/mips.c

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -71,6 +71,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "opts.h"
 #include "tree-pass.h"
 #include "context.h"
+#include "ira.h"
 
 /* Definitions used in ready queue reordering for first scheduling pass.  */
 
@@ -21959,6 +21960,19 @@ mips_lra_p (void)
 {
   return mips_lra_flag;
 }
+
+static bool
+mips_reg_equiv_profitable_p (struct ira_reg_equiv_s * equiv)
+{
+  if (TARGET_MIPS16
+      && optimize_size
+      && equiv->argument_p
+      && abs(frame_offset) > 1000)
+    return false;
+
+  return true;
+}
+
 
 /* Initialize the GCC target structure.  */
 #undef TARGET_ASM_ALIGNED_HI_OP
@@ -22229,6 +22243,9 @@ mips_lra_p (void)
 
 #undef TARGET_SCHED_SET_SCHED_FLAGS
 #define TARGET_SCHED_SET_SCHED_FLAGS mips_set_sched_flags
+
+#undef TARGET_REG_EQUIV_PROFITABLE_P
+#define TARGET_REG_EQUIV_PROFITABLE_P mips_reg_equiv_profitable_p
 
 struct gcc_target targetm = TARGET_INITIALIZER;
 
