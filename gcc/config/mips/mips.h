6f5abb2591b3e85434388fc8adff02edecf51d14
Graham Stott <grahams@imgtec.com>
2013-09-03 13:47:42 +0100

    Don't splt MSA moves.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -2252,14 +2252,14 @@ enum reg_class
 #define MSA_ARG_LAST (MSA_ARG_FIRST + MAX_ARGS_IN_MSA_REGISTERS - 1)
 
 /* True if MODE is vector and supported in a MSA vector register.  */
-#define MSA_SUPPORTED_VECTOR_MODE_P(mode)	\
-  (GET_MODE_SIZE (mode) == UNITS_PER_MSA_REG	\
-   && (GET_MODE_CLASS (mode) == MODE_VECTOR_INT	\
-       || GET_MODE_CLASS (mode) == MODE_VECTOR_FLOAT))
+#define MSA_SUPPORTED_VECTOR_MODE_P(MODE)		\
+  (GET_MODE_SIZE (MODE) == UNITS_PER_MSA_REG		\
+   && (GET_MODE_CLASS (MODE) == MODE_VECTOR_INT		\
+       || GET_MODE_CLASS (MODE) == MODE_VECTOR_FLOAT))
 
 /* True if MODE is supported in a MSA vector register.  */
-#define MSA_SUPPORTED_MODE_P(mode)	\
-  (mode == TImode || MSA_SUPPORTED_VECTOR_MODE_P (mode))
+#define MSA_SUPPORTED_MODE_P(MODE)	\
+  ((MODE) == TImode || MSA_SUPPORTED_VECTOR_MODE_P (MODE))
 
 /* 1 if N is a possible register number for function argument passing.
    We have no FP argument registers when soft-float.  When FP registers
