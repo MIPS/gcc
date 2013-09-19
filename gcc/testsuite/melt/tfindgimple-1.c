262278b6f3f63aee980d14448d9b5374178a81b8
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-09-19 14:22:55 +0000

    2013-09-19  Basile Starynkevitch  <basile@starynkevitch.net>
    	* melt/xtramelt-ana-simple.melt (findgimple_docmd): More, and
    	special cases for wild-cards.
    
    	* testsuite/melt/tfindgimple-1.c: New test case.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@202746 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/melt/tfindgimple-1.c
@@ -0,0 +1,26 @@
+// file tfindgimple-1.c
+/* run in build dir to check the findgimple mode
+ ln -sv $GCCMELT_SOURCE/gcc/testsuite/melt/tfindgimple-1.c
+ ./cc1 -O @meltbuild-common.args -fmelt-init=@melt-default-modules.quicklybuilt \
+    -fmelt-mode=findgimple -fmelt-arg="?_" \
+    -fmelt-debug tfindgimple-1.c \
+    -o /dev/null
+*/
+#include <stdio.h>
+
+int sum(int x, int y)
+{
+  return x+y;
+}
+
+int main (int argc, char** argv)
+{
+  int xx = (argc>1)?atoi(argv[1]):2;
+  int yy = (argc>2)?atoi(argv[2]):3;
+  printf ("xx=%d y=%d sum=%d\n", xx, yy, sum(xx,yy));
+  printf ("2*xx=%d sum(2*xx,1)=%d\n", xx, sum(xx,1));
+  printf("sum(3,4)=%d\n", sum(3,4));
+  return 0;
+}
+
+// eof tfindgimple-1.c
