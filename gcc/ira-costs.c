ab163470e0fbfa724b327cc4ae7d19c97368fc7d
mfortune <matthew.fortune@imgtec.com>
2013-09-27 15:20:39 +0100

    Disable the use of alt_class logic for mips16

--- gcc/ira-costs.c
+++ gcc/ira-costs.c
@@ -1811,7 +1811,8 @@ find_costs_and_classes (FILE *dump_file)
 		}
 	      else if (i_costs[k] == best_cost)
 		best = ira_reg_class_subunion[best][rclass];
-	      if (pass == flag_expensive_optimizations
+	      if (targetm.ira_use_alt_class_p ()
+		  && pass == flag_expensive_optimizations
 		  /* We still prefer registers to memory even at this
 		     stage if their costs are the same.  We will make
 		     a final decision during assigning hard registers
