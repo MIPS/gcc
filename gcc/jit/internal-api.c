43c92a5c930e9561aebc40dbf1c87069a3a0380c
David Malcolm <dmalcolm@redhat.com>
2013-10-09 17:04:06 -0400

    Fixes to GCC_JIT_BOOL_OPTION_KEEP_INTERMEDIATES.
    gcc/jit/
    	* internal-api.c (gcc::jit::context::~context): Fix indentation;
    	clean up memory allocations when using
    	GCC_JIT_BOOL_OPTION_KEEP_INTERMEDIATES.

--- gcc/jit/internal-api.c
+++ gcc/jit/internal-api.c
@@ -24,7 +24,7 @@ gcc::jit::context::
 ~context ()
 {
   if (m_bool_options[GCC_JIT_BOOL_OPTION_KEEP_INTERMEDIATES])
-      fprintf (stderr, "intermediate files written to %s\n", m_path_tempdir);
+    fprintf (stderr, "intermediate files written to %s\n", m_path_tempdir);
   else
     {
       /* Clean up .s/.so and tempdir. */
@@ -34,15 +34,15 @@ gcc::jit::context::
         unlink (m_path_so_file);
       if (m_path_tempdir)
         rmdir (m_path_tempdir);
-
-      free (m_path_template);
-      /* m_path_tempdir aliases m_path_template, or is NULL, so don't
-         attempt to free it .  */
-      free (m_path_c_file);
-      free (m_path_s_file);
-      free (m_path_so_file);
-      m_functions.release ();
     }
+
+  free (m_path_template);
+  /* m_path_tempdir aliases m_path_template, or is NULL, so don't
+     attempt to free it .  */
+  free (m_path_c_file);
+  free (m_path_s_file);
+  free (m_path_so_file);
+  m_functions.release ();
 }
 
 void
