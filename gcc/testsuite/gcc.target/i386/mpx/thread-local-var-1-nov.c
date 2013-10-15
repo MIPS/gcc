b04a3b92ecfebf1301c89967c0d839d458a95b90
Ilya Enkovich <enkovich.gnu@gmail.com>
2013-10-15 16:32:27 +0400

    Add tests for va_arg_pack and TLS.

---
+++ gcc/testsuite/gcc.target/i386/mpx/thread-local-var-1-nov.c
@@ -0,0 +1,31 @@
+/* { dg-do run } */
+/* { dg-options "-fcheck-pointers -mmpx" } */
+/* { dg-skip-if "" { *-*-* } { "-flto" } { "" } } */
+
+#include "mpx-check.h"
+#include "pthread.h"
+
+__thread int prebuf[100];
+__thread int buf[100];
+__thread int postbuf[100];
+
+int rd (int *p, int i)
+{
+  int res = p[i];
+  printf("%d\n", res);
+  return res;
+}
+
+void *thred_func (void *ptr)
+{
+  rd (buf, 0);
+  rd (buf, 99);
+}
+
+int mpx_test (int argc, char **argv)
+{
+  pthread_t thread;
+  pthread_create (&thread, NULL, thred_func, 0);
+  pthread_join (thread, NULL);
+  return 0;
+}
