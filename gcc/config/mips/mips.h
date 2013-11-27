90930231ecf338c5320dd1471a9b60ff80a58d8a
mfortune <matthew.fortune@imgtec.com>
2013-11-27 13:44:29 +0000

    Add maddf

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -967,6 +967,10 @@ struct mips_cpu_info {
 #define ISA_HAS_FP_MADD4_MSUB4  (ISA_HAS_FP4				\
 				 || (ISA_MIPS32R2 && !TARGET_MIPS16))
 
+/* ISA has floating-point maddf and msubf instructions 'd = d [+-] a * b  */
+#define ISA_HAS_FP_MADDF_MSUBF  (ISA_MIPS32R6				\
+				 || ISA_MIPS64R6)
+
 /* ISA has floating-point madd and msub instructions 'c = a * b [+-] c'.  */
 #define ISA_HAS_FP_MADD3_MSUB3  TARGET_LOONGSON_2EF
 
