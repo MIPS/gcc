7ffeb1077730a85fcd8d38686136f5ead22fd48d
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-07 13:16:20 +0000

    2014-01-07  Basile Starynkevitch  <basile@starynkevitch.net>
    	* melt-runtime.h (MELT_VERSION_STRING): Update to 1.1-pre!
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@206392 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-runtime.h
+++ gcc/melt-runtime.h
@@ -117,7 +117,7 @@ MELT_EXTERN int melt_count_runtime_extensions;
 /* The version string of MELT; this is parsed by make, so spaces are
    important!  That version string is extracted by scripts or
    makefiles... */
-#define MELT_VERSION_STRING "1.0.1+post"
+#define MELT_VERSION_STRING "1.1-pre"
 
 /* return a read only version string */
 extern const char* melt_version_str(void);
