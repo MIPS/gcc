72536ebf7392cb08e8fa5b630c8619418cb6a177
iusmanov <iusmanov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-09 08:12:33 +0000

    09-01-2014 Ilmir Usmanov <i.usmanov@samsung.com>         Remove auto-generated tests, fix cp build
            * gcc/testsuite/gfortran.dg/gacc/^[a-zA-Z].*f95: Remove
            * Makefile.in: remove libmudflap
            * gcc/cp/parser.h: Fix typo
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/openacc-1_0-branch@206459 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/parser.h
+++ gcc/cp/parser.h
@@ -300,7 +300,7 @@ typedef struct GTY(()) cp_parser {
 #define IN_OMP_BLOCK		4
 #define IN_OMP_FOR		8
 #define IN_IF_STMT             16
-#define IN_CILK_SIMD_FOR       3
+#define IN_CILK_SIMD_FOR       32
 #define IN_ACC_LOOP            64
 #define IN_ACC_BLOCK           128
   unsigned char in_statement;
