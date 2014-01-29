06d1451589279eb2049f0b60da7b6c57054b7b47
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-01-29 15:22:11 +0000

    2014-01-29  Basile Starynkevitch  <basile@starynkevitch.net>
    	* melt-runtime.cc (pragma_lex)
    	(c_register_pragma_with_expansion_and_data)
    	(c_register_pragma_with_data): Remove weak redeclaration.
    
    	* melt/warmelt-hooks.melt: Add new cheader with above weak
    	redeclarations and melt_register_pragmas_callback.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@207245 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-runtime.cc
+++ gcc/melt-runtime.cc
@@ -8782,25 +8782,6 @@ melt_predefined_index_by_name (const char* pname)
 
 
 
-/*** 
-     We declare weak functions because they cannot be linked when we
-     use lto (it loses langage specific informations).  If you use one
-     of those functions you must check them to be not NULL.
-***/
-// Function pragma_lex is declared in c-family/c-pragma.h
-extern enum cpp_ttype pragma_lex (tree *) MELT_WEAK_ON_HOST;
-// Function c_register_pragma_with_expansion_and_data from c-family/c-pragma.h
-extern void
-c_register_pragma_with_expansion_and_data (const char *space,
-					   const char *name,
-					   pragma_handler_2arg handler,
-					   void *data) MELT_WEAK_ON_HOST;
-// Function c_register_pragma_with_data from c-family/c-pragma.h
-extern void
-c_register_pragma_with_data (const char *space, const char *name,
-			     pragma_handler_2arg handler,
-			     void *data) MELT_WEAK_ON_HOST;
-
 
 
 
