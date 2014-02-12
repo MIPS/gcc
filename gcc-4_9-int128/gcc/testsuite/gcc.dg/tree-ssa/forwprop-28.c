d9db7b72cb9e3d0f452559218d18e5b0c46d81ef
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-02-12 19:23:29 +0000

    clone branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@207737 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc-4_9-int128/gcc/testsuite/gcc.dg/tree-ssa/forwprop-28.c
@@ -0,0 +1,82 @@
+/* { dg-do compile { target { ! "m68k*-*-* mmix*-*-* mep*-*-* bfin*-*-* v850*-*-* picochip*-*-* moxie*-*-* cris*-*-* m32c*-*-* fr30*-*-* mcore*-*-* powerpc*-*-* xtensa*-*-* arc*-*-* hppa*-*-* mips*-*-*"} } } */
+/* { dg-options "-O2 -fdump-tree-forwprop1" } */
+/* Skip on ARM Cortex-M, where LOGICAL_OP_NON_SHORT_CIRCUIT is set to false,
+   leading to two conditional jumps when evaluating an && condition.  Forwprop1
+   is not able to optimize this.  */
+/* { dg-skip-if "" { arm_cortex_m } } */
+
+extern char *frob (void);
+extern _Bool testit (void);
+
+test (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (temp == 0 && !rotate)
+    oof ();
+}
+
+test_2 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (!rotate && temp == 0)
+    oof ();
+}
+
+
+test_3 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (!rotate || temp == 0)
+    oof ();
+}
+
+
+test_4 (int code)
+{
+  char *temp = frob ();
+  int rotate = (code == 22);
+  if (temp == 0 || !rotate)
+    oof ();
+}
+
+
+test_5 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (temp == 0 && !rotate)
+    oof ();
+}
+
+test_6 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (!rotate && temp == 0)
+    oof ();
+}
+
+
+test_7 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (!rotate || temp == 0)
+    oof ();
+}
+
+
+test_8 (int code)
+{
+  _Bool temp = testit ();
+  _Bool rotate = (code == 22);
+  if (temp == 0 || !rotate)
+    oof ();
+}
+
+/* { dg-final { scan-tree-dump-times "Replaced" 8 "forwprop1"} } */
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
+
