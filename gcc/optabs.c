45d9dc400706aa0629c882ce64787ae1b9f97a95
Andrew Pinski <apinski@cavium.com>
2014-02-12 15:24:02 -0800

    2014-02-12  Andrew Pinski  <apinski@cavium.com>
    	* optabs.c (emit_conditional_move): Fix bug with the
    	previous change, need to check both modes of the
    	compare and not just the original mode.

--- gcc/optabs.c
+++ gcc/optabs.c
@@ -4617,7 +4617,8 @@ emit_conditional_move (rtx target, enum rtx_code code, rtx op0, rtx op1,
 	 try using those operands for op2 and op3 so
 	 same constants that were forced to a register
 	 use the same register.  */
-      if (GET_CODE (comparison) == code && orig_cmode == mode)
+      if (GET_CODE (comparison) == code
+	  && orig_cmode == mode && cmode == mode)
 	{
 	  if (rtx_equal_p (op0, op2))
 	    op2 = XEXP (comparison, 0);
