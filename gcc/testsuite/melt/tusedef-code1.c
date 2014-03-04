7067c2841d7faebdc59be73f6144ff7de6109a5d
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-03-04 13:24:00 +0000

    2014-03-04  Basile Starynkevitch  <basile@starynkevitch.net>
    	* testsuite/melt/tusedef-ana1.melt: Improve. Add gate function.
    
    	* testsuite/melt/tusedef-code1.c: Improve. Add main function.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@208317 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/melt/tusedef-code1.c
+++ gcc/testsuite/melt/tusedef-code1.c
@@ -1,17 +1,18 @@
 // file tusedef1.c
 #include <string.h>
+#include <stdio.h>
 
 #define MAX_HASH 0x3fffffff
 
 unsigned
 hash_cstring (const char *s, int l)
 {
+  int oldl = l;
+  unsigned h1 = 17, h2 = 0, h = 0;
   if (l < 0 && s)
     l = strlen (s);
   if (!s || l <= 0)
     return 11;
-  int oldl = l;
-  unsigned h1 = 17, h2 = 0;
   while (l > 8)
     {
       h1 = (13 * h2 + l) ^ (s[0] * 17 + s[1] * 31);
@@ -57,8 +58,28 @@ hash_cstring (const char *s, int l)
     goto end;
   l--;
 end:
-  unsigned h = ((h1 * 1907) + (h2 * 1459)) & MAX_HASH;
+  h = ((h1 * 1907) + (h2 * 1459)) & MAX_HASH;
   if (h == 0)
     return (oldl + 3) & MAX_HASH;
   return h;
 }
+
+
+int main(int argc, char**argv)
+{
+  int i;
+  if (argc>1) {
+    for (i=1; i<argc; i++) 
+      printf("argv[%d]=%s hash=%ux\n", i, argv[i],
+	     hash_cstring(argv[i], -1));
+  }
+  else {
+    static const char sample1[]="To be or not to be, that is the question.";
+    static const char sample2[]="To not be or to be, that is not the question!";
+    printf("argv0 %s: hash=%ux\n", argv[0], hash_cstring(argv[0], -1));
+    printf("sample1 %s: hash=%ux\n", sample1, hash_cstring(sample1, -1));
+    printf("sample2 %s: hash=%ux\n", sample2, hash_cstring(sample2, -1));
+  }
+  return 0;
+}
+
