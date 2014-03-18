311f92f661a6191919227db620976bc7df603545
Praveen Kumar Jonnalagadda <praveen.jonnalagadda@caviumnetworks.com>
2014-03-18 10:10:28 +0530

    2012-07-23  Andrew Pinski  <apinski@caviumnetworks.com>
            * testsuite/gcc.dg/tree-ssa/phi-opt-pass-order.c: New testcase.

---
+++ gcc/testsuite/gcc.dg/tree-ssa/phi-opt-pass-order.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-optimized" } */
+
+struct arc
+{
+  int ident;
+};
+int bea_is_dual_infeasible( struct arc *arc, int red_cost )
+{
+    return( (red_cost < 0 && arc->ident == 1)
+            || (red_cost > 0 && arc->ident == 2) );
+}
+
+/* There should be two == phiopt changes */
+/* { dg-final { scan-tree-dump-times "D.\[0-9\]+_. == " 2 "optimized" } } */
+/* { dg-final { scan-tree-dump-times "PHI" 1 "optimized" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
+
