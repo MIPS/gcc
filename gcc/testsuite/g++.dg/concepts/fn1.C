c52500d9381e87af820590456f1796020837abd5
asutton <asutton@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-02 15:39:10 +0000

    2014-03-10  Andrew Sutton  <andrew.n.sutton@gmail.com> 	* gcc/testuite/g++.dg/concepts/fn1.C: New test. 	* gcc/testuite/g++.dg/concepts/fn2.C: New test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-concepts@209024 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/concepts/fn1.C
@@ -0,0 +1,24 @@
+// { dg-options "-std=c++1y" }
+
+template<typename T>
+  concept bool C() { return __is_class(T); }
+
+struct S { } s;
+
+template<typename T>
+  requires C<T>()
+    void f(T x) { }
+
+// Calls to constrained functions are valid.
+template<typename T>
+  void g(T x) { f(x); }
+
+template<typename T>
+  void h(T x) {
+    f(s);
+  }
+
+int main() {
+  f(s);
+  g(s);
+}
---
+++ gcc/testsuite/g++.dg/concepts/fn2.C
@@ -0,0 +1,18 @@
+// { dg-options "-std=c++1y" }
+
+template<typename T>
+  concept bool C() { return __is_class(T); }
+
+template<typename T>
+  requires C<T>()
+    void f(T x) { }
+
+// Check call sites in templates when the args are non-dependent.
+template<typename T>
+  void h(T x) {
+    f(0); // { dg-error "matching" }
+  }
+
+int main() {
+  f(0); // { dg-error "matching" }
+}
