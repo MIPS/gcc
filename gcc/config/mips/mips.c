4d70b10a41cee5ea5b6055675c34871cb0cadb55
Matthew Fortune <matthew.fortune@imgtec.com>
2014-06-23 11:32:00 +0100

    R6 code review fixes

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -162,12 +162,10 @@ along with GCC; see the file COPYING3.  If not see
 #define MIPS_LUI(DEST, VALUE) \
   ((0xf << 26) | ((DEST) << 16) | (VALUE))
 
-/* Return the opcode to jump to register DEST.  */
+/* Return the opcode to jump to register DEST.  When the JR opcode is not
+   available use JALR $0, DEST.  */
 #define MIPS_JR(DEST) \
-  (((DEST) << 21) | 0x8)
-
-#define MIPS_JALR0(DEST) \
-  (((DEST) << 21) | 0x9)
+  (((DEST) << 21) | (ISA_HAS_JR ? 0x8 : 0x9))
 
 /* Return the opcode for:
 
@@ -2614,14 +2612,14 @@ umips_12bit_offset_address_p (rtx x, enum machine_mode mode)
    MODE is the mode of the value being accessed.  */
 
 bool
-mipsr6_9bit_offset_address_p (rtx x, enum machine_mode mode)
+mips_9bit_offset_address_p (rtx x, enum machine_mode mode)
 {
   struct mips_address_info addr;
 
   return (mips_classify_address (&addr, x, mode, false)
 	  && addr.type == ADDRESS_REG
 	  && CONST_INT_P (addr.offset)
-	  && MIPSR6_9BIT_OFFSET_P (INTVAL (addr.offset)));
+	  && MIPS_9BIT_OFFSET_P (INTVAL (addr.offset)));
 }
 
 /* Return the number of instructions needed to load constant X,
@@ -4207,7 +4205,12 @@ mips_rtx_costs (rtx x, int code, int outer_code, int opno ATTRIBUTE_UNUSED,
       *total = mips_zero_extend_cost (mode, XEXP (x, 0));
       return false;
     case TRUNCATE:
-      /* Costings for highpart multiplies.  */
+      /* Costings for highpart multiplies.  Matching patterns of the form:
+
+	 (lshiftrt:DI (mult:DI (sign_extend:DI (...)
+			       (sign_extend:DI (...))
+		      (const_int 32)
+      */
       if (ISA_HAS_R6MUL
 	  && (GET_CODE (XEXP (x, 0)) == ASHIFTRT
 	      || GET_CODE (XEXP (x, 0)) == LSHIFTRT)
@@ -4233,16 +4236,17 @@ mips_rtx_costs (rtx x, int code, int outer_code, int opno ATTRIBUTE_UNUSED,
 
 	  /* Sign extension is free, zero extension costs for DImode when
 	     on a 64bit core / when DMUL is present.  */
-	  if (ISA_HAS_R6DMUL && GET_MODE (XEXP (x, 0)) == DImode)
+	  for (int i = 0; i < 2; ++i)
 	    {
-	      if (GET_CODE (XEXP (XEXP (XEXP (x, 0), 0), 0)) == ZERO_EXTEND)
-		*total += rtx_cost (XEXP (XEXP (XEXP (x, 0), 0), 0),
-				    ZERO_EXTEND, 0, speed);
-
-	      if (GET_CODE (XEXP (XEXP (XEXP (x, 0), 0), 1)) == ZERO_EXTEND)
-		*total += rtx_cost (XEXP (XEXP (XEXP (x, 0), 0), 1),
-				    ZERO_EXTEND, 0, speed);
+	      rtx op = XEXP (XEXP (XEXP (x, 0), 0), i);
+	      if (ISA_HAS_R6DMUL
+		  && GET_CODE (op) == ZERO_EXTEND
+		  && GET_MODE (op) == DImode)
+		*total += rtx_cost (op, MULT, i, speed);
+	      else
+		*total += rtx_cost (XEXP (op, 0), GET_CODE (op), 0, speed);
 	    }
+
 	  return true;
 	}
       return false;
@@ -5030,8 +5034,8 @@ mips_emit_compare (enum rtx_code *code, rtx *op0, rtx *op1, bool need_eq_ne_p)
       enum rtx_code cmp_code;
 
       /* Floating-point tests use a separate C.cond.fmt or CMP.cond.fmt
-	 comparison to set a condition code register.  The branch or
-	 conditional move will then compare that register against zero.
+	 comparison to set a register.  The branch or conditional move will
+	 then compare that register against zero.
 
 	 Set CMP_CODE to the code of the comparison instruction and
 	 *CODE to the code that the branch or move should use.  */
@@ -5208,9 +5212,9 @@ mips_expand_conditional_trap (rtx comparison)
 
   mode = GET_MODE (XEXP (comparison, 0));
   op0 = force_reg (mode, op0);
-  if (!arith_operand (op1, mode)
-      || (!ISA_HAS_COND_TRAPI
-	  && !register_operand (op1, mode)))
+  if (!(ISA_HAS_COND_TRAPI
+	? arith_operand (op1, mode)
+	: reg_or_0_operand (op1, mode)))
     op1 = force_reg (mode, op1);
 
   emit_insn (gen_rtx_TRAP_IF (VOIDmode,
@@ -9920,7 +9924,7 @@ mips_must_initialize_gp_p (void)
 static bool
 mips_interrupt_extra_call_saved_reg_p (unsigned int regno)
 {
-  if ((ISA_HAS_MULT || TARGET_DSP)
+  if ((ISA_HAS_HILO || TARGET_DSP)
       && MD_REG_P (regno))
     return true;
 
@@ -11872,8 +11876,7 @@ mips_hard_regno_mode_ok_p (unsigned int regno, enum machine_mode mode)
 	    && (regno - ST_REG_FIRST) % 4 == 0);
 
   if (mode == CCFmode)
-    return (ISA_HAS_CCF
-	    && FP_REG_P (regno));
+    return (ISA_HAS_CCF && FP_REG_P (regno));
 
   if (mode == CCmode)
     return ISA_HAS_8CC ? ST_REG_P (regno) : regno == FPSW_REGNUM;
@@ -12048,8 +12051,6 @@ mips_mode_ok_for_mov_fmt_p (enum machine_mode mode)
   switch (mode)
     {
     case CCFmode:
-      return TARGET_HARD_FLOAT;
-
     case SFmode:
       return TARGET_HARD_FLOAT;
 
@@ -12307,7 +12308,8 @@ mips_secondary_reload_class (enum reg_class rclass,
 
   if (reg_class_subset_p (rclass, FP_REGS))
     {
-      if (MEM_P (x)
+      /* We don't need a reload if the pseudo is in memory.  */
+      if ((MEM_P (x) || regno == -1)
 	  && (GET_MODE_SIZE (mode) == 4 || GET_MODE_SIZE (mode) == 8))
 	/* In this case we can use lwc1, swc1, ldc1 or sdc1.  We'll use
 	   pairs of lwc1s and swc1s if ldc1 and sdc1 are not supported.  */
@@ -12325,10 +12327,6 @@ mips_secondary_reload_class (enum reg_class rclass,
 
       if (FP_REG_P (regno) && mips_mode_ok_for_mov_fmt_p (mode))
 	/* In this case we can use mov.fmt.  */
-	return NO_REGS;
-
-      /* We don't need a reload if the pseudo is in memory in CCF mode.  */
-      if (mode == CCFmode && regno == -1)
 	return NO_REGS;
 
       /* Otherwise, we need to reload through an integer register.  */
@@ -15930,8 +15928,10 @@ mips_mult_zero_zero_cost (struct mips_sim *state, bool setting)
 static void
 mips_set_fast_mult_zero_zero_p (struct mips_sim *state)
 {
-  if (TARGET_MIPS16 || !ISA_HAS_MULT)
-    /* No MTLO or MTHI available.  */
+  if (TARGET_MIPS16 || !ISA_HAS_HILO)
+    /* No MTLO or MTHI available for MIPS16. Also, when there are no HI or LO
+       registers then there is no reason to zero them, arbitrarily choose to
+       say that "MULT $0,$0" would be faster.  */
     mips_tuning_info.fast_mult_zero_zero_p = true;
   else
     {
@@ -17178,7 +17178,8 @@ mips_option_override (void)
   if ((target_flags_explicit & MASK_FLOAT64) != 0)
     {
       if (mips_isa_rev >= 6 && !TARGET_FLOAT64)
-	error ("unsupported combination: %s", "-mips[32|64]r6 -mfp32");
+	error ("the %qs architecture does not support %<-mfp32%>",
+	       mips_arch_info->name);
       else if (TARGET_SINGLE_FLOAT && TARGET_FLOAT64)
 	error ("unsupported combination: %s", "-mfp64 -msingle-float");
       else if (TARGET_64BIT && TARGET_DOUBLE_FLOAT && !TARGET_FLOAT64)
@@ -17195,10 +17196,10 @@ mips_option_override (void)
     }
   else
     {
-      /* -msingle-float selects 32-bit float registers.  Otherwise the
-	 float registers should be the same size as the integer ones.
-	 MIPS R6 has 64-bit float registers regardless of the size of
-	 the integer ones.  */
+      /* -msingle-float selects 32-bit float registers.  On r6 and later,
+         -mdouble-float selects 64-bit float registers, since the old paired
+	 register model is not supported.  In other cases the float registers
+	 should be the same size as the integer ones.  */
       if (mips_isa_rev >= 6 && TARGET_DOUBLE_FLOAT)
 	target_flags |= MASK_FLOAT64;
       else if (TARGET_64BIT && TARGET_DOUBLE_FLOAT)
@@ -17358,23 +17359,25 @@ mips_option_override (void)
     }
 
   /* Set NaN and ABS defaults.  */
-  if (mips_nan == MIPS_IEEE_754_DEFAULT && !ISA_HAS_NANLEGACY)
+  if (mips_nan == MIPS_IEEE_754_DEFAULT && !ISA_HAS_IEEE_754_LEGACY)
     mips_nan = MIPS_IEEE_754_2008;
-  if (mips_abs == MIPS_IEEE_754_DEFAULT && !ISA_HAS_NANLEGACY)
+  if (mips_abs == MIPS_IEEE_754_DEFAULT && !ISA_HAS_IEEE_754_LEGACY)
     mips_abs = MIPS_IEEE_754_2008;
 
-  /* Check for NaN encoding support.  */
+  /* Check for IEEE 754 legacy/2008 support.  */
   if ((mips_nan == MIPS_IEEE_754_LEGACY
        || mips_abs == MIPS_IEEE_754_LEGACY)
-      && !ISA_HAS_NANLEGACY)
-    warning (0, "the %qs architecture does not support the NaN legacy"
-	     " encoding", mips_arch_info->name);
+      && !ISA_HAS_IEEE_754_LEGACY)
+    warning (0, "the %qs architecture does not support %<-m%s=legacy%>",
+	     mips_arch_info->name,
+	     mips_nan == MIPS_IEEE_754_LEGACY ? "nan" : "abs");
 
   if ((mips_nan == MIPS_IEEE_754_2008
        || mips_abs == MIPS_IEEE_754_2008)
-      && !ISA_HAS_NAN2008)
-    warning (0, "the %qs architecture does not support the NaN 2008"
-	     " encoding", mips_arch_info->name);
+      && !ISA_HAS_IEEE_754_2008)
+    warning (0, "the %qs architecture does not support %<-m%s=2008%>",
+	     mips_arch_info->name,
+	     mips_nan == MIPS_IEEE_754_2008 ? "nan" : "abs");
 
   /* Pre-IEEE 754-2008 MIPS hardware has a quirky almost-IEEE format
      for all its floating point.  */
@@ -17431,8 +17434,12 @@ mips_option_override (void)
     TARGET_DSP = true;
 
   if (TARGET_DSP && mips_isa_rev >= 6)
-    error ("the %qs architecture does not support DSP instructions",
-	   mips_arch_info->name);
+    {
+      error ("the %qs architecture does not support DSP instructions",
+	     mips_arch_info->name);
+      TARGET_DSP = false;
+      TARGET_DSPR2 = false;
+    }
 
   /* .eh_frame addresses should be the same width as a C pointer.
      Most MIPS ABIs support only one pointer size, so the assembler
@@ -17614,7 +17621,7 @@ mips_conditional_register_usage (void)
     AND_COMPL_HARD_REG_SET (accessible_reg_set,
 			    reg_class_contents[(int) DSP_ACC_REGS]);
 
-  if (!ISA_HAS_MULT && !ISA_HAS_DMULT)
+  if (!ISA_HAS_HILO)
     AND_COMPL_HARD_REG_SET (accessible_reg_set,
 			    reg_class_contents[(int) MD_REGS]);
 
@@ -18186,10 +18193,7 @@ mips_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
       trampoline[i++] = OP (MIPS_LOAD_PTR (STATIC_CHAIN_REGNUM,
 					   static_chain_offset,
 					   PIC_FUNCTION_ADDR_REGNUM));
-      if (ISA_HAS_JR)
-	trampoline[i++] = OP (MIPS_JR (AT_REGNUM));
-      else
-	trampoline[i++] = OP (MIPS_JALR0 (AT_REGNUM));
+      trampoline[i++] = OP (MIPS_JR (AT_REGNUM));
       trampoline[i++] = OP (MIPS_MOVE (PIC_FUNCTION_ADDR_REGNUM, AT_REGNUM));
     }
   else if (ptr_mode == DImode)
@@ -18215,10 +18219,7 @@ mips_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
       trampoline[i++] = OP (MIPS_LOAD_PTR (STATIC_CHAIN_REGNUM,
 					   static_chain_offset - 12,
 					   RETURN_ADDR_REGNUM));
-      if (ISA_HAS_JR)
-	trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
-      else
-	trampoline[i++] = OP (MIPS_JALR0 (PIC_FUNCTION_ADDR_REGNUM));
+      trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
       trampoline[i++] = OP (MIPS_MOVE (RETURN_ADDR_REGNUM, AT_REGNUM));
     }
   else
@@ -18260,12 +18261,7 @@ mips_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
 
       /* Emit the JR here, if we can.  */
       if (!ISA_HAS_LOAD_DELAY)
-	{
-	  if (ISA_HAS_JR)
-	    trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
-	  else
-	    trampoline[i++] = OP (MIPS_JALR0 (PIC_FUNCTION_ADDR_REGNUM));
-	}
+	trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
 
       /* Emit the load of the static chain register.  */
       opcode = OP (MIPS_LOAD_PTR (STATIC_CHAIN_REGNUM,
@@ -18277,10 +18273,7 @@ mips_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
       /* Emit the JR, if we couldn't above.  */
       if (ISA_HAS_LOAD_DELAY)
 	{
-	  if (ISA_HAS_JR)
-	    trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
-	  else
-	    trampoline[i++] = OP (MIPS_JALR0 (PIC_FUNCTION_ADDR_REGNUM));
+	  trampoline[i++] = OP (MIPS_JR (PIC_FUNCTION_ADDR_REGNUM));
 	  trampoline[i++] = OP (MIPS_NOP);
 	}
     }
--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -838,15 +838,17 @@ struct mips_cpu_info {
 				 && TARGET_OCTEON			\
 				 && !TARGET_MIPS16)
 
+/* ISA has HI and LO registers.  */
+#define ISA_HAS_HILO		(mips_isa_rev <= 5)
+
+
 /* ISA supports instructions DMULT and DMULTU. */
 #define ISA_HAS_DMULT		(TARGET_64BIT				\
 				 && !TARGET_MIPS5900			\
-				 && mips_isa_rev <= 5)
+				 && ISA_HAS_HILO)
 
-/* ISA supports instructions MULT and MULTU.
-   This is always true, but the macro is needed for ISA_HAS_<D>MULT
-   in mips.md.  */
-#define ISA_HAS_MULT		(mips_isa_rev <= 5)
+/* ISA supports instructions MULT and MULTU.  */
+#define ISA_HAS_MULT		ISA_HAS_HILO
 
 #define ISA_HAS_R6MUL		(mips_isa_rev >= 6)
 #define ISA_HAS_R6DMUL		(TARGET_64BIT && mips_isa_rev >= 6)
@@ -968,11 +970,11 @@ struct mips_cpu_info {
 				      && (MODE) == V2SFmode))		\
 				 && !TARGET_MIPS16)
 
-#define ISA_HAS_LWL_LWR		(mips_isa_rev <= 5)
+#define ISA_HAS_LWL_LWR		(mips_isa_rev <= 5 && !TARGET_MIPS16)
 
-#define ISA_HAS_NANLEGACY	(mips_isa_rev <= 5)
+#define ISA_HAS_IEEE_754_LEGACY	(mips_isa_rev <= 5)
 
-#define ISA_HAS_NAN2008		(mips_isa_rev >= 2)
+#define ISA_HAS_IEEE_754_2008	(mips_isa_rev >= 2)
 
 /* ISA has count leading zeroes/ones instruction (not implemented).  */
 #define ISA_HAS_CLZ_CLO		(mips_isa_rev >= 1 && !TARGET_MIPS16)
@@ -1069,14 +1071,10 @@ struct mips_cpu_info {
 				 && TARGET_64BIT)
 
 /* The DSP ASE is available.  */
-#define ISA_HAS_DSP		(TARGET_DSP				\
-				 && mips_isa_rev <= 5			\
-				 && !TARGET_MIPS16)
+#define ISA_HAS_DSP		(TARGET_DSP && !TARGET_MIPS16)
 
 /* Revision 2 of the DSP ASE is available.  */
-#define ISA_HAS_DSPR2		(TARGET_DSPR2				\
-				 && mips_isa_rev <= 5			\
-				 && !TARGET_MIPS16)
+#define ISA_HAS_DSPR2		(TARGET_DSPR2 && !TARGET_MIPS16)
 
 /* True if the result of a load is not available to the next instruction.
    A nop will then be needed between instructions like "lw $4,..."
@@ -2121,7 +2119,7 @@ enum reg_class
 #define SMALL_INT_UNSIGNED(X) SMALL_OPERAND_UNSIGNED (INTVAL (X))
 #define LUI_INT(X) LUI_OPERAND (INTVAL (X))
 #define UMIPS_12BIT_OFFSET_P(OFFSET) (IN_RANGE (OFFSET, -2048, 2047))
-#define MIPSR6_9BIT_OFFSET_P(OFFSET) (IN_RANGE (OFFSET, -256, 255))
+#define MIPS_9BIT_OFFSET_P(OFFSET) (IN_RANGE (OFFSET, -256, 255))
 
 /* The HI and LO registers can only be reloaded via the general
    registers.  Condition code registers can only be loaded to the
