4d85ba8ea42b14be6daa19c7a6a4c88640688932
Matthew Fortune <matthew.fortune@imgtec.com>
2014-07-13 10:29:14 +0100

    P5600: Load store bonding patch
    Author: Sameera Deshpande

--- gcc/config/mips/mips-protos.h
+++ gcc/config/mips/mips-protos.h
@@ -354,6 +354,7 @@ extern bool mips_epilogue_uses (unsigned int);
 extern void mips_final_prescan_insn (rtx, rtx *, int);
 extern int mips_trampoline_code_size (void);
 extern void mips_function_profiler (FILE *);
+extern bool mips_load_store_bonding_p (rtx *, enum machine_mode, bool);
 
 typedef rtx (*mulsidi3_gen_fn) (rtx, rtx, rtx);
 #ifdef RTX_CODE
