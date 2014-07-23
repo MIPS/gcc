bb7899135bb8f338b34b0664b9260934308eb428
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-07-23 14:08:25 +0000

    2014-07-23  Basile Starynkevitch  <basile@starynkevitch.net>
    	{{caveat: the JSON output for JSONobjects with string values is wrong.}}
    	* melt/warmelt-base.melt (json_canonical_name): Return the string
    	if it is not a name.
    
    	* melt/warmelt-hooks.melt (do_blocking_jsonrpc2_call): Generate an
    	id. Should probably test it in the answer.
    
    	* testsuite/melt/tcalljsonrpc-1.melt: New file.
    	* testsuite/melt/tservjsonrpc-1.cc: New file.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@212939 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/melt/tservjsonrpc-1.cc
@@ -0,0 +1,62 @@
+// file tservjsonrpc-1.cc
+// in the public domain, by Basile Starynkevitch
+// inspired by some example from http://www.tntnet.org/cxxtools.html
+#include <unistd.h>
+#include <iostream>
+#include <cxxtools/arg.h>
+#include <cxxtools/log.h>
+#include <cxxtools/json/rpcserver.h>
+#include <cxxtools/eventloop.h>
+
+long add(long a1, long a2)
+{
+  long r = a1+a2;
+  std::clog << "add a1=" << a1 << " a2=" << a2 << " giving r=" << r << std::endl;
+  return r;
+}
+
+int main(int argc, char* argv[])
+{
+  try {
+    // option -i <ip-address> defines the ip address of the interface, where the
+    // server waits for connections. Default is empty, which tells the server to
+    // listen on all local interfaces
+    cxxtools::Arg<std::string> ip(argc, argv, 'i');
+
+    // option -p <number> specifies the port, where jsonrpc requests are expected.
+    // The default port is 8088 here.
+    cxxtools::Arg<unsigned short> port(argc, argv, 'p', 8088);
+    // create an event loop
+    cxxtools::EventLoop loop;
+
+    ////////////////////////////////////////////////////////////////////////
+    // Json rpc
+
+    // for the json rpc server we define a json server
+    cxxtools::json::RpcServer jsonServer(loop, ip, port);
+
+    // and register the functions in the server
+    jsonServer.registerFunction("add", add);
+
+    ////////////////////////////////////////////////////////////////////////
+    // Run
+
+    std::clog << __FILE__ << ":" << __LINE__ << " running pid #" << (int)getpid() << std::endl;
+    
+    // now start the servers by running the event loop
+    loop.run();
+
+
+  } catch (const std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+    }
+  std::clog << __FILE__ << " end" << std::endl;
+  return 0;
+}
+
+/**** for emacs
+  ++ Local Variables: ++
+  ++ compile-command: "g++ -v -Wall -O -g tservjsonrpc-1.cc $(cxxtools-config  --cxxflags --libs) -lcxxtools-json -o ./tservjsonrpc-1" ++
+  ++ End: ++
+****/
