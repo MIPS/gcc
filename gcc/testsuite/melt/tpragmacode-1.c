b4d6dfc7bd86d0d63adfc5cf60833774adf1c16d
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-05 08:05:07 +0000

    2014-08-05  Basile Starynkevitch  <basile@starynkevitch.net>
    	{{tpragma-1 test is ok}}
    	* testsuite/melt/tpragma-1.melt: Improve command in comment.
    
    	* testsuite/melt/tpragmacode-1.c: Invoke the right pragma!
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@213620 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/melt/tpragmacode-1.c
+++ gcc/testsuite/melt/tpragmacode-1.c
@@ -1,7 +1,7 @@
 // file tpragmacode-1.c for tpragma-1.melt
 // in the public domain, see tpragma-1.melt for details
-#pragma TESTGCCMELT TEST this 
+#pragma TESTGCCMELT TEST1 this 
 int zerofun (void) { 
-#pragma TESTGCCMELT TEST that
+#pragma TESTGCCMELT TEST1 that
   return 0; 
 }
