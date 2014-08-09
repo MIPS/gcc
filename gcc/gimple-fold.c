6366d58ad731ff64730c795e3e7885ea2915eea6
Andrew Pinski <apinski@cavium.com>
2014-08-09 15:53:44 -0700

    2014-08-09  Andrew Pinski  <apinski@cavium.com>
    	* gimple-fold.c: Include tm_p.h.

--- gcc/gimple-fold.c
+++ gcc/gimple-fold.c
@@ -22,6 +22,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "system.h"
 #include "coretypes.h"
 #include "tm.h"
+#include "tm_p.h"
 #include "tree.h"
 #include "stringpool.h"
 #include "expr.h"
