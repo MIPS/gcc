07c7ac3a61d5fee9e62d584137cd444c1f33c86a
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-12 11:27:56 +0000

    2014-08-12  Prathamesh Kulkarni  <bilbotheelffriend@gmail.com>
    	* match.pd: Add new pattern.
    
    	testsuite/
    	* match.c: New test-case.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/match-and-simplify@213857 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/tree-ssa/match.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O -fdump-tree-forwprop1-details" }  */
+
+int abs(int);
+
+int match_1 (int x)
+{
+  int t1 = abs (x);
+  int match_1_val = abs (t1);
+  return match_1_val;
+}  
+/* { dg-final { scan-tree-dump "gimple_simplified to match_1_val_\\d\+ = ABS_EXPR <x_\\d\+\\(D\\)" "forwprop1" } } */
+
+/* { dg-final { cleanup-tree-dump "forwprop1" } } */
