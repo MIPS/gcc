37e54d64a61180eca11b0c4f8e50bc9e6e7fe38c
Tom de Vries <tom@codesourcery.com>
2014-09-04 15:27:43 +0200

    Add pass_ch_oacc_kernels to pass_oacc_kernels

--- gcc/omp-low.h
+++ gcc/omp-low.h
@@ -28,6 +28,8 @@ extern void free_omp_regions (void);
 extern tree omp_reduction_init (tree, tree);
 extern bool make_gimple_omp_edges (basic_block, struct omp_region **, int *);
 extern void omp_finish_file (void);
+extern bool loop_in_oacc_kernels_region_p (struct loop *, basic_block *,
+					   basic_block *);
 
 extern GTY(()) vec<tree, va_gc> *offload_funcs;
 extern GTY(()) vec<tree, va_gc> *offload_vars;
