2ad622b99dce52d867f4e80f406a133a9982a929
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-11 21:59:42 +0000

    delete old branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@217375 138bc75d-0d04-0410-961f-82ee72b054a4

--- addr-patch/gcc/testsuite/g++.dg/other/error34.C
+++
@@ -1,7 +0,0 @@
-// PR c++/46538
-// { dg-do compile }
-// { dg-options "" }
-
-S () : str(__PRETTY_FUNCTION__) {}	// { dg-error "forbids declaration" "decl" }
-// { dg-error "only constructors" "constructor" { target *-*-* } 5 }
-// { dg-prune-output "__PRETTY_FUNCTION__" }
