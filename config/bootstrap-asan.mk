742206fc43d4fcb2461075e296636148ba45ed1b
H.J. Lu <hjl.tools@gmail.com>
2014-11-16 07:16:08 -0800

    Export "detect_leaks=0 detect_odr_violation=0"
    2014-11-15  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR bootstrap/63888
    	* bootstrap-asan.mk (ASAN_OPTIONS): Export "detect_leaks=0
    	detect_odr_violation=0"

--- config/bootstrap-asan.mk
+++ config/bootstrap-asan.mk
@@ -1,5 +1,8 @@
 # This option enables -fsanitize=address for stage2 and stage3.
 
+# Suppress LeakSanitizer and odr-violation in bootstrap.
+export ASAN_OPTIONS="detect_leaks=0 detect_odr_violation=0"
+
 STAGE2_CFLAGS += -fsanitize=address
 STAGE3_CFLAGS += -fsanitize=address
 POSTSTAGE1_LDFLAGS += -fsanitize=address -static-libasan \
