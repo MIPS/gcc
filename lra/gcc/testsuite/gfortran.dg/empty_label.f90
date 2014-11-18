b57582a2b87ace1612587ff32f49a7fe72759ae4
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-11-18 19:32:47 +0000

    clone branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@217734 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ lra/gcc/testsuite/gfortran.dg/empty_label.f90
@@ -0,0 +1,6 @@
+! { dg-do compile }
+! { dg-options "-Werror -fmax-errors=1" }
+100   ! { dg-warning "empty statement" }
+end
+! { dg-error "count reached limit" "" { target *-*-* } 0 }
+! { dg-excess-errors "compilation terminated" }
