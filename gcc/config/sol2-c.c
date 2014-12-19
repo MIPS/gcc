4560d75afeaadae4e927f215bb7b27f331c9031b
amacleod <amacleod@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-19 16:40:51 +0000

    2014-12-19  Andrew MacLeod <amacleod@redhat.com>
    	* builtins.c (fold_builtin_atomic_always_lock_free): Don't use ttype as
    	a variable name.
    	* config/sol2-c.c (solaris_pragma_align, solaris_pragma_init,
    	solaris_pragma_fini): Likewise.
    	* cp/decl.c (layout_var_decl): Likewise.
    	* cp/error.c (dump_expr): Likewise.
    	* except.c (output_ttype): Rename to output_tt.
    	(output_one_function_exception_table): call output_tt.
    	* lto-streamer-in.c (input_eh_regions): Don't use ttype as a variable
    	name.  
    	* lto-streamer-out.c (output_eh_regions): Likewise.
    	* tree.c (build_type_attribute_qual_variant): Likewise.
    	(build_type_attribute_variant): Likewise.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ttype@218954 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sol2-c.c
+++ gcc/config/sol2-c.c
@@ -85,7 +85,7 @@ static void
 solaris_pragma_align (cpp_reader *pfile ATTRIBUTE_UNUSED)
 {
   tree t, x;
-  enum cpp_ttype ttype;
+  enum cpp_tok_type tok_type;
   unsigned HOST_WIDE_INT low;
 
   if (pragma_lex (&x) != CPP_NUMBER
@@ -104,8 +104,8 @@ solaris_pragma_align (cpp_reader *pfile ATTRIBUTE_UNUSED)
       return;
     }
 
-  ttype = pragma_lex (&t);
-  if (ttype != CPP_NAME)
+  tok_type = pragma_lex (&t);
+  if (tok_type != CPP_NAME)
     {
       warning (0, "malformed %<#pragma align%>, ignoring");
       return;
@@ -121,17 +121,17 @@ solaris_pragma_align (cpp_reader *pfile ATTRIBUTE_UNUSED)
 	solaris_pending_aligns = tree_cons (t, build_tree_list (NULL, x),
 					    solaris_pending_aligns);
 
-      ttype = pragma_lex (&t);
-      if (ttype == CPP_COMMA)
+      tok_type = pragma_lex (&t);
+      if (tok_type == CPP_COMMA)
 	{
-	  ttype = pragma_lex (&t);
-	  if (ttype != CPP_NAME)
+	  tok_type = pragma_lex (&t);
+	  if (tok_type != CPP_NAME)
 	    {
 	      warning (0, "malformed %<#pragma align%>");
 	      return;
 	    }
 	}
-      else if (ttype == CPP_CLOSE_PAREN)
+      else if (tok_type == CPP_CLOSE_PAREN)
 	{
 	  if (pragma_lex (&t) != CPP_EOF)
 	    warning (0, "junk at end of %<#pragma align%>");
@@ -151,7 +151,7 @@ static void
 solaris_pragma_init (cpp_reader *pfile ATTRIBUTE_UNUSED)
 {
   tree t;
-  enum cpp_ttype ttype;
+  enum cpp_tok_type tok_type;
 
   if (pragma_lex (&t) != CPP_OPEN_PAREN)
     {
@@ -159,8 +159,8 @@ solaris_pragma_init (cpp_reader *pfile ATTRIBUTE_UNUSED)
       return;
     }
 
-  ttype = pragma_lex (&t);
-  if (ttype != CPP_NAME)
+  tok_type = pragma_lex (&t);
+  if (tok_type != CPP_NAME)
     {
       warning (0, "malformed %<#pragma init%>, ignoring");
       return;
@@ -180,17 +180,17 @@ solaris_pragma_init (cpp_reader *pfile ATTRIBUTE_UNUSED)
       else
 	solaris_pending_inits = tree_cons (t, NULL, solaris_pending_inits);
 
-      ttype = pragma_lex (&t);
-      if (ttype == CPP_COMMA)
+      tok_type = pragma_lex (&t);
+      if (tok_type == CPP_COMMA)
 	{
-	  ttype = pragma_lex (&t);
-	  if (ttype != CPP_NAME)
+	  tok_type = pragma_lex (&t);
+	  if (tok_type != CPP_NAME)
 	    {
 	      warning (0, "malformed %<#pragma init%>");
 	      return;
 	    }
 	}
-      else if (ttype == CPP_CLOSE_PAREN)
+      else if (tok_type == CPP_CLOSE_PAREN)
 	{
 	  if (pragma_lex (&t) != CPP_EOF)
 	    warning (0, "junk at end of %<#pragma init%>");
@@ -210,7 +210,7 @@ static void
 solaris_pragma_fini (cpp_reader *pfile ATTRIBUTE_UNUSED)
 {
   tree t;
-  enum cpp_ttype ttype;
+  enum cpp_tok_type tok_type;
 
   if (pragma_lex (&t) != CPP_OPEN_PAREN)
     {
@@ -218,8 +218,8 @@ solaris_pragma_fini (cpp_reader *pfile ATTRIBUTE_UNUSED)
       return;
     }
 
-  ttype = pragma_lex (&t);
-  if (ttype != CPP_NAME)
+  tok_type = pragma_lex (&t);
+  if (tok_type != CPP_NAME)
     {
       warning (0, "malformed %<#pragma fini%>, ignoring");
       return;
@@ -239,17 +239,17 @@ solaris_pragma_fini (cpp_reader *pfile ATTRIBUTE_UNUSED)
       else
 	solaris_pending_finis = tree_cons (t, NULL, solaris_pending_finis);
 
-      ttype = pragma_lex (&t);
-      if (ttype == CPP_COMMA)
+      tok_type = pragma_lex (&t);
+      if (tok_type == CPP_COMMA)
 	{
-	  ttype = pragma_lex (&t);
-	  if (ttype != CPP_NAME)
+	  tok_type = pragma_lex (&t);
+	  if (tok_type != CPP_NAME)
 	    {
 	      warning (0, "malformed %<#pragma fini%>");
 	      return;
 	    }
 	}
-      else if (ttype == CPP_CLOSE_PAREN)
+      else if (tok_type == CPP_CLOSE_PAREN)
 	{
 	  if (pragma_lex (&t) != CPP_EOF)
 	    warning (0, "junk at end of %<#pragma fini%>");
