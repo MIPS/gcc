2d28047a7d898425f2ac0e6cce97a9fdc3b22e8b
Matthew Fortune <matthew.fortune@imgtec.com>
2014-12-19 16:42:39 +0000

    Fix mips64-dsp-lsa.c (now mips64-lsa.c)

--- gcc/testsuite/gcc.target/mips/mips64-lsa.c
+++ gcc/testsuite/gcc.target/mips/mips64-lsa.c
@@ -1,9 +1,9 @@
 /* Test MIPS64 DSP instructions - should use LSA instead of LHX */
 /* { dg-do compile } */
-/* { dg-options "-mgp64 -mdsp (HAS_LSA)" } */
+/* { dg-options "-mabi=64 (HAS_LSA)" } */
 /* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
 
-/* { dg-final { scan-assembler "\tlsa\t" } } */
+/* { dg-final { scan-assembler "\tdlsa\t" } } */
 
 NOMIPS16 signed long long test (signed long long *a, int index)
 {
