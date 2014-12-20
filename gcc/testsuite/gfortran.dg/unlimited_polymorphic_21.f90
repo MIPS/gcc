77ce6b25ef01a91fe20998aa9b0ee356276d5696
Andre Vehreschild <vehre@gmx.de>
2014-12-20 16:24:47 +0100

    Extended testcase to also cover kind=4 char arrays.

--- gcc/testsuite/gfortran.dg/unlimited_polymorphic_21.f90
+++ gcc/testsuite/gfortran.dg/unlimited_polymorphic_21.f90
@@ -26,5 +26,31 @@ program test
     end select
 
     deallocate(P)
+
+    ! Now for kind=4 chars.
+
+    allocate(character(len=20,kind=4)::P)
+
+    select type(P)
+        type is (character(len=*,kind=4))
+            P ="some test string"
+            if (P .ne. 4_"some test string") then
+                call abort ()
+            end if
+            if (len(P) .ne. 20) then
+                call abort ()
+            end if
+            if (len(P) .eq. len("some test string")) then
+                call abort ()
+            end if
+        type is (character(len=*,kind=1))
+            call abort ()
+        class default
+            call abort ()
+    end select
+
+    deallocate(P)
+
+
 end program test
 
