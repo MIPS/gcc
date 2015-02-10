9040a25fded89747709436c29afcff96a5ffc930
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-02-10 19:48:10 +0000

    delete branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@220592 138bc75d-0d04-0410-961f-82ee72b054a4

--- addr/gcc/testsuite/gfortran.dg/line_length_5.f90
+++
@@ -1,6 +0,0 @@
-! { dg-do compile }
-! { dg-options "-Wline-truncation" }
-print *, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-end 
-! { dg-warning "Line truncated" " " { target *-*-* } 3 }
-! { dg-error "Unterminated character constant" " " { target *-*-* } 3 }
