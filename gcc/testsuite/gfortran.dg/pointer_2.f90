06fd5e1e98b73b2852b9fd7e03cfa020c5423cd9
Andre Vehreschild <vehre@gmx.de>
2015-03-12 16:18:52 +0100

    Added testcase.

---
+++ gcc/testsuite/gfortran.dg/pointer_2.f90
@@ -0,0 +1,20 @@
+! { dg-do compile }
+! Check that the compiler reports the errors, but does not segfault.
+! Contributed by: Andre Vehreschild  <vehre@gcc.gnu.org>
+!
+program test
+    implicit none
+    class(*), pointer :: P
+    class(*), allocatable :: P2
+
+    allocate(P2, source=convertType(P))
+
+contains
+
+  function convertType(in)
+    class(*), intent(in) :: in ! { dg-error "must be dummy, allocatable or pointer"}
+    class(*) :: convertType
+
+    convertType => in ! { dg-error "Non-POINTER in pointer association context \\(pointer assignment\\) at" }
+  end function
+end program test
