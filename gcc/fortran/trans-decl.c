0f6f7418836c533a49a5464828a0461aec437ebc
Aldy Hernandez <aldyh@redhat.com>
2015-03-24 09:23:50 -0700

    Emit early debug from gfc_emit_parameter_debug_info.

--- gcc/fortran/trans-decl.c
+++ gcc/fortran/trans-decl.c
@@ -4776,6 +4776,7 @@ gfc_emit_parameter_debug_info (gfc_symbol *sym)
 					      TREE_TYPE (decl),
 					      sym->attr.dimension,
 					      false, false);
+  debug_hooks->early_global_decl (decl);
 }
 
 
