0f4b797af9eb03f7845017d70a71d1e3c59d1683
Aldy Hernandez <aldyh@redhat.com>
2015-04-14 08:58:59 -0700

    Add FIXME note and Fortran test for an ICE.

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -18777,18 +18777,20 @@ gen_subprogram_die (tree decl, dw_die_ref context_die)
       expanded_location s = expand_location (DECL_SOURCE_LOCATION (decl));
       struct dwarf_file_data * file_index = lookup_filename (s.file);
       if ((is_cu_die (old_die->die_parent)
-	   || context_die == NULL
-	   /* For class scoped static functions, the dumped early
-	      version was the declaration, whereas the next time
-	      around with a different context should be the
-	      specification.  In this case, avoid reusing the DIE, but
-	      generate a specification below. E.g.:
+	   /* FIXME: Jason doesn't like this condition, but it fixes
+	      the inconsistency/ICE with the followin Fortran test:
 
-	      class C {
-	      public:
-	        static void moo () {}
-	      };  */
-	   || !is_cu_die (context_die))
+		 module some_m
+		 contains
+		    logical function funky (FLAG)
+		      funky = .true.
+		   end function
+		 end module
+
+	      Another alternative was !is_cu_die (context_die).
+	   */
+	   || old_die->die_parent->die_tag == DW_TAG_module
+	   || context_die == NULL)
 	   && (DECL_ARTIFICIAL (decl)
 	       || (get_AT_file (old_die, DW_AT_decl_file) == file_index
 		   && (get_AT_unsigned (old_die, DW_AT_decl_line)
