a9c01d139fc9dafd0f0f496e072fe69c17f4bd3d
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-29 03:41:54 +0000

    Remove debug code
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@222558 138bc75d-0d04-0410-961f-82ee72b054a4

--- ieee/gcc/testsuite/gcc.target/powerpc/float128-1.c
+++ ieee/gcc/testsuite/gcc.target/powerpc/float128-1.c
@@ -92,24 +92,6 @@ do_test (__float128 expected, __float128 got, const char *name)
 }
 
 
-#if defined(__FLOAT128__) && defined(_ARCH_PPC) && defined(WRAP_SUBKF3)
-extern __float128 __real___subkf3 (__float128, __float128);
-extern __float128 __wrap___subkf3 (__float128, __float128);
-
-__float128
-__wrap___subkf3 (__float128 a, __float128 b)
-{
-  __float128 r;
-
-  do_test (2.25q,  a, "*a*");
-  do_test (5.0q,   b, "*b*");
-  r = __real___subkf3 (a, b);
-  do_test (-2.75q, r, "*r*");
-
-  return r;
-}
-#endif
-
 int
 main (void)
 {
