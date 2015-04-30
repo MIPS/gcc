13c7502853dd658fd008d11688508a4237dc8aa2
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-04-30 14:30:04 +0000

    For C++14 the implicit cast of bool to int * needs to be an error. Question is here C++11, as for this we might want to do the same as done for c++98 (and lower), or we want same behavior as for c++14?? I spoke with Jonathan and so I will handle the c++ 11 and 14 cases being error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@222629 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/null3.C
+++ gcc/testsuite/g++.old-deja/g++.other/null3.C
@@ -3,5 +3,5 @@
 void x()
 {
  int* p = 1==0;	// { dg-warning "converting 'false' to pointer type" "" { xfail { c++11 } } }
- // { dg-warning "cannot convert 'bool' to" "" 5 { xfail { ! c++11 } }  }
+ // { dg-error "cannot convert" "convert" { xfail { ! c++11 } } 5 }
 }
