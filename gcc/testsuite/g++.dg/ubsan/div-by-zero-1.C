b7dda36848a34297939a5faf13304544415d14a9
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-24 07:37:38 +0000

    Fix line-number of error, and adjust check because an expression containing (1 / 0) isn't a constant.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@224876 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/ubsan/div-by-zero-1.C
+++ gcc/testsuite/g++.dg/ubsan/div-by-zero-1.C
@@ -6,5 +6,5 @@ foo (int i)
 {
   switch (i)
   case 0 * (1 / 0): /* { dg-warning "division by zero" } */
-    ;  /* { dg-error "division by zero" "" { target *-*-* } 10 } */
+    ;  /* { dg-error "is not a constant expression" "" { target *-*-* } 8 } */
 }
