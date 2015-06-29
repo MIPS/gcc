66d0714a61ae971153c53d3d3f3b3895b9071580
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-06-29 22:34:49 +0000

    don't use void*' within dg-rule
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@225152 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/warn/overflow-warn-4.C
+++ gcc/testsuite/g++.dg/warn/overflow-warn-4.C
@@ -59,7 +59,7 @@ void *p = 0 * (INT_MAX + 1); /* { dg-warning "integer overflow in expression" } 
 /* { dg-error "invalid conversion from 'int' to 'void" "null" { target *-*-* } 58 } */
 
 void *q = 0 * (1 / 0); /* { dg-warning "division by zero" } */
-/* { dg-error "invalid conversion from 'int' to 'void*'" "null" { target *-*-* } 61 } */
+/* { dg-error "invalid conversion from 'int' to 'void" "null" { target *-*-* } 61 } */
 void *r = (1 ? 0 : INT_MAX+1); /* { dg-bogus "integer overflow in expression" "" { xfail *-*-* } } */
 
 void
