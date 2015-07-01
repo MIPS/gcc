a6b4f994110f635941bca310c2d82588a424723d
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-01 15:54:14 +0000

    Fix compilation libgomp.oacc-c-c++-common/kernels-loop.c -g
    2015-07-01  Tom de Vries  <tom@codesourcery.com>
    
    	PR tree-optimization/66716
    	* tree-cfg.c (gimple_split_block_before_cond_jump): Split before
    	cond_jump, instead of split after last nondebug insn before cond_jump.
    
    	* c-c++-common/goacc/kernels-loop-g.c: New test.
    
    	* testsuite/libgomp.oacc-c-c++-common/kernels-loop-g.c: New test.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@225259 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/c-c++-common/goacc/kernels-loop-g.c
@@ -0,0 +1,19 @@
+/* { dg-additional-options "-O2" } */
+/* { dg-additional-options "-g" } */
+/* { dg-additional-options "-ftree-parallelize-loops=32" } */
+/* { dg-additional-options "-fdump-tree-parloops_oacc_kernels-all" } */
+/* { dg-additional-options "-fdump-tree-optimized" } */
+
+#include "kernels-loop.c"
+
+/* Check that only one loop is analyzed, and that it can be parallelized.  */
+/* { dg-final { scan-tree-dump-times "SUCCESS: may be parallelized" 1 "parloops_oacc_kernels" } } */
+/* { dg-final { scan-tree-dump-not "FAILED:" "parloops_oacc_kernels" } } */
+
+/* Check that the loop has been split off into a function.  */
+/* { dg-final { scan-tree-dump-times "(?n);; Function .*main._omp_fn.0" 1 "optimized" } } */
+
+/* { dg-final { scan-tree-dump-times "(?n)pragma omp target oacc_parallel.*num_gangs\\(32\\)" 1 "parloops_oacc_kernels" } } */
+
+/* { dg-final { cleanup-tree-dump "parloops_oacc_kernels" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
