64f1600e5ed33bcb131381e2aa5ab1eeb50a1bee
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-06 20:22:54 +0000

    Use target instead of xfail for testing warn/error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@225477 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/null3.C
+++ gcc/testsuite/g++.old-deja/g++.other/null3.C
@@ -2,6 +2,6 @@
 
 void x()
 {
- int* p = 1==0;	// { dg-warning "converting 'false' to pointer type" "" { xfail { c++11 } } }
- // { dg-error "cannot convert" "convert" { xfail { ! c++11 } } 5 }
+ int* p = 1==0;	// { dg-warning "converting 'false' to pointer type" "" { target { !c++11 } } }
+ // { dg-error "cannot convert" "convert" { target { c++11 } } 5 }
 }
