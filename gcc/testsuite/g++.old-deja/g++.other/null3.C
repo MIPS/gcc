c03b16a81cef904352e538eb09ef35b118818299
ktietz <ktietz@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-10 22:13:30 +0000

    Fix condition of test.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@225700 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.old-deja/g++.other/null3.C
+++ gcc/testsuite/g++.old-deja/g++.other/null3.C
@@ -2,6 +2,6 @@
 
 void x()
 {
- int* p = 1==0;	// { dg-warning "converting 'false' to pointer type" "" { target { !c++11 } } }
+ int* p = 1==0;	// { dg-warning "converting 'false' to pointer type" "" { target { ! c++11 } } }
  // { dg-error "cannot convert" "convert" { target { c++11 } } 5 }
 }
