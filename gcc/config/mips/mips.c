ee638aad59003a4f02afaf1bc73fbc9ef57ffbf1
Simon Dardis <simon.dardis@imgtec.com>
2015-07-16 15:12:33 +0100

    Scheduler changes for testing.
    This branch contains scheduler changes for the 24k, 74k and an initial
    scheduler for the m6200. This change specifically affects the bypass
    definitions for sequences such as alu->store result.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -14551,7 +14551,7 @@ mips_store_data_bypass_p (rtx out_insn, rtx in_insn)
   if (GET_CODE (PATTERN (in_insn)) == UNSPEC_VOLATILE)
     return false;
 
-  return !store_data_bypass_p (out_insn, in_insn);
+  return store_data_bypass_p (out_insn, in_insn);
 }
 
 
