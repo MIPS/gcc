be5ec016c6e0e3981c609851a40fc1645e6b5d36
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-07-22 14:24:47 +0000

    libgomp testsuite: Remove some explicit acc_device_nvidia usage.
    	libgomp/
    	* testsuite/libgomp.oacc-c-c++-common/lib-1.c: Remove explicit
    	acc_device_nvidia usage.
    	* testsuite/libgomp.oacc-c-c++-common/lib-10.c: Likewise.
    	* testsuite/libgomp.oacc-c-c++-common/lib-2.c: Likewise.
    	* testsuite/libgomp.oacc-c-c++-common/lib-9.c: Likewise.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@226072 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.oacc-c-c++-common/lib-1.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/lib-1.c
@@ -5,18 +5,8 @@
 int
 main (int argc, char **argv)
 {
-  acc_device_t devtype = acc_device_host;
-
-#if ACC_DEVICE_TYPE_nvidia
-  devtype = acc_device_nvidia;
-
-  if (acc_get_num_devices (devtype) == 0)
-    return 0;
-#endif
-
-  acc_init (devtype);
-
-  acc_init (devtype);
+  acc_init (acc_device_default);
+  acc_init (acc_device_default);
 
   return 0;
 }
