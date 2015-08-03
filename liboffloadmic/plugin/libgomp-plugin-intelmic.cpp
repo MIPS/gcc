MIPS/gcc

commit 243e584f140e9262efb18f7bf94af5602c78b27e

include/ 	* gomp-constants.h (GOMP_VERSION_INTEL_MIC): New.
	libgomp/
	* plugin/plugin-nvptx.c (GOMP_OFFLOAD_load_image_ver): Rename to ...
	(GOMP_OFFLOAD_load_image): ... here.  Swap version and ord args.
	Adjust version check and mesage.
	(GOMP_OFFLOAD_unload_image_ver): Rename to ...
	(GOMP_OFFLOAD_unload_image): ... here.  Swap version and ord args.
	* oacc-host.c: Include gomp-constants.h.
	(host_version): New.
	(host_load_image, host_unload_image): Add version arg.
	(host_dispatch): Adjust.
	* target.c (gomp_load_image_to_device): Swap version and devicep
	args. Adjust plugin load calling.  Adjust all callers.
	* target.c (gomp_unload_image_from_device): Swap version and devicep
	args. Adjust plugin unload calling. Adjust all callers.
	(load_plugin_for_device): Always look for version function and
	check it.  Adjust load and unload call lookup.
	* libgomp.h: Adjustt load and unload plugin fields.

	liboffloadmic/
	* plugin/libgomp-plugin-intelmic.cpp (GOMP_OFFLOAD_version): New.
	(GOMP_OFFLOAD_load_image): Add version arg and check it.
	(GOMP_OFFLOAD_unload_image): Likewise.


git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@226469 138bc75d-0d04-0410-961f-82ee72b054a4

liboffloadmic/plugin/libgomp-plugin-intelmic.cpp
@@ -327,11 +327,25 @@ offload_image (const void *target_image)
   free (image);
 }
 
+/* Return the libgomp version number we're compatible with.  There is
+   no requirement for cross-version compatibility.  */
+
+extern "C" unsigned
+GOMP_OFFLOAD_version (void)
+{
+  return GOMP_VERSION;
+}
+
 extern "C" int
-GOMP_OFFLOAD_load_image (int device, const void *target_image,
-			 addr_pair **result)
+GOMP_OFFLOAD_load_image (int device, const unsigned version,
+			 void *target_image, addr_pair **result)
 {
   TRACE ("(device = %d, target_image = %p)", device, target_image);
+
+  if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
+    GOMP_PLUGIN_fatal ("Offload data incompatible with intelmic plugin"
+		       " (expected %u, received %u)",
+		       GOMP_VERSION_INTEL_MIC, GOMP_VERSION_DEV (version));
 
   /* If target_image is already present in address_table, then there is no need
      to offload it.  */
@@ -353,8 +367,12 @@ GOMP_OFFLOAD_load_image (int device, const void *target_image,
 }
 
 extern "C" void
-GOMP_OFFLOAD_unload_image (int device, const void *target_image)
+GOMP_OFFLOAD_unload_image (int device, unsigned version,
+			   const void *target_image)
 {
+  if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
+    return;
+
   TRACE ("(device = %d, target_image = %p)", device, target_image);
 
   /* TODO: Currently liboffloadmic doesn't support __offload_unregister_image

gcc/config/nvptx/nvptx.c
@@ -2980,14 +2980,36 @@ nvptx_reorg (void)
     if (REG_N_SETS (i) == 0 && REG_N_REFS (i) == 0)
       regno_reg_rtx[i] = const0_rtx;
 
-  parallel *pars = nvptx_discover_pars (&bb_insn_map);
+  /* Determine launch dimensions of the function.  If it is not an
+     offloaded function  (i.e. this is a regular compiler), the
+     function has no neutering.  */
+  tree attr = get_oacc_fn_attrib (current_function_decl);
+  if (attr)
+    {
+      unsigned mask = 0;
+      tree dims = TREE_VALUE (attr);
+      unsigned ix;
 
-  nvptx_process_pars (pars);
-  nvptx_neuter_pars (pars, (GOMP_DIM_MASK (GOMP_DIM_VECTOR)
-			    | GOMP_DIM_MASK (GOMP_DIM_WORKER)), 0);
+      gcc_assert (dims);
+      for (ix = 0; ix != GOMP_DIM_MAX; ix++, dims = TREE_CHAIN (dims))
+	{
+	  tree cst = TREE_VALUE (dims);
 
-  delete pars;
+	  unsigned HOST_WIDE_INT dim = TREE_INT_CST_LOW (cst);
+	  if (dim != 1)
+	    mask |= GOMP_DIM_MASK (ix);
+	}
+      /* If there is worker neutering, there must be vector
+	 neutering.  Otherwise the hardware will fail.  */
+      gcc_assert ((mask & GOMP_DIM_MASK (GOMP_DIM_VECTOR))
+		  || !(mask & GOMP_DIM_MASK (GOMP_DIM_WORKER)));
 
+      parallel *pars = nvptx_discover_pars (&bb_insn_map);
+      nvptx_process_pars (pars);
+      nvptx_neuter_pars (pars, mask, 0);
+      delete pars;
+    }
+  
   nvptx_reorg_subreg ();
   
   regstat_free_n_sets_and_refs ();
@@ -3073,29 +3095,21 @@ nvptx_record_offload_symbol (tree decl)
     case FUNCTION_DECL:
       {
 	tree attr = get_oacc_fn_attrib (decl);
-	tree dims = NULL_TREE;
+	tree dims = TREE_VALUE (attr);
 	unsigned ix;
 	
-	if (attr)
-	  dims = TREE_VALUE (attr);
 	fprintf (asm_out_file, "//:FUNC_MAP \"%s\"",
 		 IDENTIFIER_POINTER (DECL_ASSEMBLER_NAME (decl)));
 
-	for (ix = 0; ix != GOMP_DIM_MAX; ix++)
+	for (ix = 0; ix != GOMP_DIM_MAX; ix++, dims = TREE_CHAIN (dims))
 	  {
-	    unsigned HOST_WIDE_INT dim = 0;
-	    if (dims)
-	      {
-		tree cst = TREE_VALUE (dims);
+	    tree cst = TREE_VALUE (dims);
 
-		/* When device_type support is added an ealier pass
-		   should have massaged the attribute to be
-		   ptx-specific.  */
-		gcc_assert (TREE_CODE (cst) == INTEGER_CST);
-
-		dim = TREE_INT_CST_LOW (cst);
-		dims = TREE_CHAIN (dims);
-	      }
+	    /* When device_type support is added an ealier pass
+	       should have massaged the attribute to be
+	       ptx-specific.  */
+	    gcc_assert (TREE_CODE (cst) == INTEGER_CST);
+	    unsigned HOST_WIDE_INT dim = TREE_INT_CST_LOW (cst);
 	    fprintf (asm_out_file, ", " HOST_WIDE_INT_PRINT_HEX, dim);
 	  }
 	

libgomp/libgomp.h
@@ -750,22 +750,9 @@ struct gomp_device_descr
   int (*get_num_devices_func) (void);
   void (*init_device_func) (int);
   void (*fini_device_func) (int);
-
   unsigned (*version_func) (void);
-  
-  /* When all plugins updated, we can remove these unions and just
-     have the versioned entry points.  */
-  union 
-  {
-    int (*unver_func) (int, const void *, struct addr_pair **);
-    int (*ver_func) (unsigned, int, const void *, struct addr_pair **);
-  } load_image;
-  union
-  {
-    void (*unver_func) (int, const void *);
-    void (*ver_func) (unsigned, int, const void *);
-  } unload_image;
-  
+  int (*load_image_func) (int, unsigned, const void *, struct addr_pair **);
+  void (*unload_image_func) (int, unsigned, const void *);
   void *(*alloc_func) (int, size_t);
   void (*free_func) (int, void *);
   void *(*dev2host_func) (int, void *, const void *, size_t);

libgomp/oacc-host.c
@@ -28,6 +28,7 @@
 
 #include "libgomp.h"
 #include "oacc-int.h"
+#include "gomp-constants.h"
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -69,8 +70,15 @@ host_fini_device (int n __attribute__ ((unused)))
 {
 }
 
+static unsigned
+host_version (void)
+{
+  return GOMP_VERSION;
+}
+
 static int
 host_load_image (int n __attribute__ ((unused)),
+		 unsigned v __attribute__ ((unused)),
 		 const void *t __attribute__ ((unused)),
 		 struct addr_pair **r __attribute__ ((unused)))
 {
@@ -79,6 +87,7 @@ host_load_image (int n __attribute__ ((unused)),
 
 static void
 host_unload_image (int n __attribute__ ((unused)),
+		   unsigned v __attribute__ ((unused)),
 		   const void *t __attribute__ ((unused)))
 {
 }
@@ -203,9 +212,9 @@ static struct gomp_device_descr host_dispatch =
     .get_num_devices_func = host_get_num_devices,
     .init_device_func = host_init_device,
     .fini_device_func = host_fini_device,
-    .version_func = NULL,
-    .load_image = {.unver_func = host_load_image},
-    .unload_image = {.unver_func = host_unload_image},
+    .version_func = host_version,
+    .load_image_func = host_load_image,
+    .unload_image_func = host_unload_image,
     .alloc_func = host_alloc,
     .free_func = host_free,
     .dev2host_func = host_dev2host,

libgomp/plugin/plugin-nvptx.c
@@ -1650,9 +1650,9 @@ GOMP_OFFLOAD_version (void)
    number ORD.  Allocate and return TARGET_TABLE.  */
 
 int
-GOMP_OFFLOAD_load_image_ver (unsigned version, int ord,
-			     const void *target_data,
-			     struct addr_pair **target_table)
+GOMP_OFFLOAD_load_image (int ord, unsigned version,
+			 const void *target_data,
+			 struct addr_pair **target_table)
 {
   CUmodule module;
   const char *const *var_names;
@@ -1665,9 +1665,9 @@ GOMP_OFFLOAD_load_image_ver (unsigned version, int ord,
   struct ptx_image_data *new_image;
   struct ptx_device *dev;
 
-  if (GOMP_VERSION_DEV (version) != GOMP_VERSION_NVIDIA_PTX)
+  if (GOMP_VERSION_DEV (version) > GOMP_VERSION_NVIDIA_PTX)
     GOMP_PLUGIN_fatal ("Offload data incompatible with PTX plugin"
-		       " (version %u ! %u)",
+		       " (expected %u, received %u)",
 		       GOMP_VERSION_NVIDIA_PTX, GOMP_VERSION_DEV (version));
   
   GOMP_OFFLOAD_init_device (ord);
@@ -1739,13 +1739,12 @@ GOMP_OFFLOAD_load_image_ver (unsigned version, int ord,
    function descriptors allocated by G_O_load_image.  */
 
 void
-GOMP_OFFLOAD_unload_image_ver (unsigned version, int ord,
-			       const void *target_data)
+GOMP_OFFLOAD_unload_image (int ord, unsigned version, const void *target_data)
 {
   struct ptx_image_data *image, **prev_p;
   struct ptx_device *dev = ptx_devices[ord];
 
-  if (GOMP_VERSION_DEV (version) != GOMP_VERSION_NVIDIA_PTX)
+  if (GOMP_VERSION_DEV (version) > GOMP_VERSION_NVIDIA_PTX)
     return;
   
   pthread_mutex_lock (&dev->image_lock);

libgomp/target.c
@@ -652,8 +652,7 @@ gomp_update (struct gomp_device_descr *devicep, size_t mapnum, void **hostaddrs,
    emitting variable and functions in the same order.  */
 
 static void
-gomp_load_image_to_device (unsigned version,
-			   struct gomp_device_descr *devicep,
+gomp_load_image_to_device (struct gomp_device_descr *devicep, unsigned version,
 			   const void *host_table, const void *target_data,
 			   bool is_register_lock)
 {
@@ -671,17 +670,9 @@ gomp_load_image_to_device (unsigned version,
   struct addr_pair *target_table = NULL;
   int i, num_target_entries;
 
-  if (devicep->version_func)
-    num_target_entries
-      = devicep->load_image.ver_func (version, devicep->target_id,
-					target_data, &target_table);
-  else if (GOMP_VERSION_DEV (version))
-    gomp_fatal ("Plugin too old for offload data (0 < %u)",
-		GOMP_VERSION_DEV (version));
-  else
-    num_target_entries
-      = devicep->load_image.unver_func (devicep->target_id,
-					target_data, &target_table);
+  num_target_entries
+    = devicep->load_image_func (devicep->target_id, version,
+				target_data, &target_table);
 
   if (num_target_entries != num_funcs + num_vars)
     {
@@ -763,8 +754,8 @@ gomp_load_image_to_device (unsigned version,
    The device must be locked.   */
 
 static void
-gomp_unload_image_from_device (unsigned version,
-			       struct gomp_device_descr *devicep,
+gomp_unload_image_from_device (struct gomp_device_descr *devicep,
+			       unsigned version,
 			       const void *host_table, const void *target_data)
 {
   void **host_func_table = ((void ***) host_table)[0];
@@ -790,11 +781,7 @@ gomp_unload_image_from_device (unsigned version,
       node = splay_tree_lookup (&devicep->mem_map, &k);
     }
 
-  if (devicep->version_func)
-    devicep->unload_image.ver_func (version,
-				    devicep->target_id, target_data);
-  else
-    devicep->unload_image.unver_func (devicep->target_id, target_data);
+  devicep->unload_image_func (devicep->target_id, version, target_data);
 
   /* Remove mappings from splay tree.  */
   for (j = 0; j < num_funcs; j++)
@@ -840,7 +827,7 @@ GOMP_offload_register_ver (unsigned version, const void *host_table,
       struct gomp_device_descr *devicep = &devices[i];
       gomp_mutex_lock (&devicep->lock);
       if (devicep->type == target_type && devicep->is_initialized)
-	gomp_load_image_to_device (version, devicep,
+	gomp_load_image_to_device (devicep, version,
 				   host_table, target_data, true);
       gomp_mutex_unlock (&devicep->lock);
     }
@@ -884,7 +871,7 @@ GOMP_offload_unregister_ver (unsigned version, const void *host_table,
       struct gomp_device_descr *devicep = &devices[i];
       gomp_mutex_lock (&devicep->lock);
       if (devicep->type == target_type && devicep->is_initialized)
-	gomp_unload_image_from_device (version, devicep,
+	gomp_unload_image_from_device (devicep, version,
 				       host_table, target_data);
       gomp_mutex_unlock (&devicep->lock);
     }
@@ -921,7 +908,7 @@ gomp_init_device (struct gomp_device_descr *devicep)
     {
       struct offload_image_descr *image = &offload_images[i];
       if (image->type == devicep->type)
-	gomp_load_image_to_device (image->version, devicep,
+	gomp_load_image_to_device (devicep, image->version,
 				   image->host_table, image->target_data,
 				   false);
     }
@@ -941,7 +928,7 @@ gomp_unload_device (struct gomp_device_descr *devicep)
 	{
 	  struct offload_image_descr *image = &offload_images[i];
 	  if (image->type == devicep->type)
-	    gomp_unload_image_from_device (image->version, devicep,
+	    gomp_unload_image_from_device (devicep, image->version,
 					   image->host_table,
 					   image->target_data);
 	}
@@ -1147,23 +1134,11 @@ gomp_load_plugin_for_device (struct gomp_device_descr *device,
   ((device->f##_func = dlsym (plugin_handle, "GOMP_OFFLOAD_" #n))	\
    || (last_missing = #n, 0))
 
-  if (DLSYM_OPT (version, version))
+  DLSYM (version);
+  if (device->version_func () != GOMP_VERSION)
     {
-      unsigned v = device->version_func ();
-      if (v != GOMP_VERSION)
-	{
-	  err = "plugin version mismatch";
-	  goto fail;
-	}
-      if (!DLSYM_OPT (load_image.ver, load_image_ver)
-	  || !DLSYM_OPT (unload_image.ver, unload_image_ver))
-	goto  dl_fail;
-    }
-  else
-    {
-      if (!DLSYM_OPT (load_image.unver, load_image)
-	  || !DLSYM_OPT (unload_image.unver, unload_image))
-	goto dl_fail;
+      err = "plugin version mismatch";
+      goto fail;
     }
 
   DLSYM (get_name);
@@ -1172,6 +1147,8 @@ gomp_load_plugin_for_device (struct gomp_device_descr *device,
   DLSYM (get_num_devices);
   DLSYM (init_device);
   DLSYM (fini_device);
+  DLSYM (load_image);
+  DLSYM (unload_image);
   DLSYM (alloc);
   DLSYM (free);
   DLSYM (dev2host);

commit 4e0158f41a00d6c4d09ca8a48eb63832abdd2f84

Fix intelmic libgomp plugin build
... which got broken in r226469:

    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp: In function 'unsigned int GOMP_OFFLOAD_version()':
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp:336:10: error: 'GOMP_VERSION' was not declared in this scope
       return GOMP_VERSION;
              ^
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp: In function 'int GOMP_OFFLOAD_load_image(int, unsigned int, void*, addr_pair**)':
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp:345:32: error: 'GOMP_VERSION_DEV' was not declared in this scope
       if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
                                    ^
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp:345:36: error: 'GOMP_VERSION_INTEL_MIC' was not declared in this scope
       if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
                                        ^
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp: In function 'void GOMP_OFFLOAD_unload_image(int, unsigned int, const void*)':
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp:373:32: error: 'GOMP_VERSION_DEV' was not declared in this scope
       if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
                                    ^
    [...]/source-gcc/liboffloadmic/plugin/libgomp-plugin-intelmic.cpp:373:36: error: 'GOMP_VERSION_INTEL_MIC' was not declared in this scope
       if (GOMP_VERSION_DEV (version) > GOMP_VERSION_INTEL_MIC)
                                        ^
    make[6]: *** [libgomp_plugin_intelmic_la-libgomp-plugin-intelmic.lo] Error 1

	liboffloadmic/
	* plugin/Makefile.am (include_src_dir): Set.
	[PLUGIN_HOST] (libgomp_plugin_intelmic_la_CPPFLAGS): Use it.
	* plugin/Makefile.in: Regenerate.
	* plugin/libgomp-plugin-intelmic.cpp: Include "gomp-constants.h".

liboffloadmic/plugin/Makefile.am
@@ -36,6 +36,7 @@ build_dir = $(top_builddir)
 source_dir = $(top_srcdir)
 coi_inc_dir = $(top_srcdir)/../include/coi
 myo_inc_dir = $(top_srcdir)/../include/myo
+include_src_dir = $(top_srcdir)/../../include
 libgomp_src_dir = $(top_srcdir)/../../libgomp
 libgomp_dir = $(build_dir)/../../libgomp
 liboffload_src_dir = $(top_srcdir)/../runtime
@@ -52,7 +53,7 @@ target_install_dir = $(accel_search_dir)/lib/gcc/$(accel_target)/$(gcc_version)$
 if PLUGIN_HOST
   toolexeclib_LTLIBRARIES = libgomp-plugin-intelmic.la
   libgomp_plugin_intelmic_la_SOURCES = libgomp-plugin-intelmic.cpp
-  libgomp_plugin_intelmic_la_CPPFLAGS = $(CPPFLAGS) -DLINUX -DCOI_LIBRARY_VERSION=2 -DMYO_SUPPORT -DOFFLOAD_DEBUG=1 -DSEP_SUPPORT -DTIMING_SUPPORT -DHOST_LIBRARY=1 -I$(coi_inc_dir) -I$(myo_inc_dir) -I$(liboffload_src_dir) -I$(libgomp_src_dir) -I$(libgomp_dir) -I$(target_prefix_dir)/include -I$(target_build_dir) -I$(target_install_dir)/include
+  libgomp_plugin_intelmic_la_CPPFLAGS = $(CPPFLAGS) -DLINUX -DCOI_LIBRARY_VERSION=2 -DMYO_SUPPORT -DOFFLOAD_DEBUG=1 -DSEP_SUPPORT -DTIMING_SUPPORT -DHOST_LIBRARY=1 -I$(coi_inc_dir) -I$(myo_inc_dir) -I$(liboffload_src_dir) -I$(libgomp_src_dir) -I$(libgomp_dir) -I$(include_src_dir) -I$(target_prefix_dir)/include -I$(target_build_dir) -I$(target_install_dir)/include
   libgomp_plugin_intelmic_la_LDFLAGS = -L$(liboffload_dir)/.libs -loffloadmic_host -version-info 1:0:0
 else # PLUGIN_TARGET
   plugin_includedir = $(libsubincludedir)

liboffloadmic/plugin/Makefile.in
@@ -306,6 +306,7 @@ build_dir = $(top_builddir)
 source_dir = $(top_srcdir)
 coi_inc_dir = $(top_srcdir)/../include/coi
 myo_inc_dir = $(top_srcdir)/../include/myo
+include_src_dir = $(top_srcdir)/../../include
 libgomp_src_dir = $(top_srcdir)/../../libgomp
 libgomp_dir = $(build_dir)/../../libgomp
 liboffload_src_dir = $(top_srcdir)/../runtime
@@ -320,7 +321,7 @@ target_build_dir = $(accel_search_dir)/$(accel_target)$(MULTISUBDIR)/liboffloadm
 target_install_dir = $(accel_search_dir)/lib/gcc/$(accel_target)/$(gcc_version)$(MULTISUBDIR)
 @PLUGIN_HOST_TRUE@toolexeclib_LTLIBRARIES = libgomp-plugin-intelmic.la
 @PLUGIN_HOST_TRUE@libgomp_plugin_intelmic_la_SOURCES = libgomp-plugin-intelmic.cpp
-@PLUGIN_HOST_TRUE@libgomp_plugin_intelmic_la_CPPFLAGS = $(CPPFLAGS) -DLINUX -DCOI_LIBRARY_VERSION=2 -DMYO_SUPPORT -DOFFLOAD_DEBUG=1 -DSEP_SUPPORT -DTIMING_SUPPORT -DHOST_LIBRARY=1 -I$(coi_inc_dir) -I$(myo_inc_dir) -I$(liboffload_src_dir) -I$(libgomp_src_dir) -I$(libgomp_dir) -I$(target_prefix_dir)/include -I$(target_build_dir) -I$(target_install_dir)/include
+@PLUGIN_HOST_TRUE@libgomp_plugin_intelmic_la_CPPFLAGS = $(CPPFLAGS) -DLINUX -DCOI_LIBRARY_VERSION=2 -DMYO_SUPPORT -DOFFLOAD_DEBUG=1 -DSEP_SUPPORT -DTIMING_SUPPORT -DHOST_LIBRARY=1 -I$(coi_inc_dir) -I$(myo_inc_dir) -I$(liboffload_src_dir) -I$(libgomp_src_dir) -I$(libgomp_dir) -I$(include_src_dir) -I$(target_prefix_dir)/include -I$(target_build_dir) -I$(target_install_dir)/include
 @PLUGIN_HOST_TRUE@libgomp_plugin_intelmic_la_LDFLAGS = -L$(liboffload_dir)/.libs -loffloadmic_host -version-info 1:0:0
 @PLUGIN_HOST_FALSE@plugin_includedir = $(libsubincludedir)
 @PLUGIN_HOST_FALSE@plugin_include_HEADERS = main_target_image.h

liboffloadmic/plugin/libgomp-plugin-intelmic.cpp
@@ -1,6 +1,6 @@
 /* Plugin for offload execution on Intel MIC devices.
 
-   Copyright (C) 2014 Free Software Foundation, Inc.
+   Copyright (C) 2014-2015 Free Software Foundation, Inc.
 
    Contributed by Ilya Verbin <ilya.verbin@intel.com>.
 
@@ -38,6 +38,7 @@
 #include "libgomp-plugin.h"
 #include "compiler_if_host.h"
 #include "main_target_image.h"
+#include "gomp-constants.h"
 
 #define LD_LIBRARY_PATH_ENV	"LD_LIBRARY_PATH"
 #define MIC_LD_LIBRARY_PATH_ENV	"MIC_LD_LIBRARY_PATH"