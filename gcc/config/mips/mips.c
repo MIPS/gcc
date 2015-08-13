dcf2e111c58aafcc6ee960dad010f82fcbff6d8f
Robert Suchanek <robert.suchanek@imgtec.com>
2015-08-13 10:53:46 +0100

    [MIPS] Fix register renaming in the interrupt handlers.
    gcc/
    	* config/mips/mips-protos.h (mips_hard_regno_rename_ok): New prototype.
    	* config/mips/mips.c (mips_hard_regno_rename_ok): New function.
    	(mips_hard_regno_scratch_ok): Likewise.
    	(TARGET_HARD_REGNO_SCRATCH_OK): Define macro.
    	* config/mips/mips.h (HARD_REGNO_RENAME_OK): New.
    
    gcc/testsuite/
    	* gcc.target/mips/interrupt_handler-bug-1.c: New test.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -12959,6 +12959,33 @@ mips_hard_regno_mode_ok_p (unsigned int regno, machine_mode mode)
   return false;
 }
 
+/* Return nonzero if register OLD_REG can be renamed to register NEW_REG.  */
+
+bool
+mips_hard_regno_rename_ok (unsigned int old_reg ATTRIBUTE_UNUSED,
+			   unsigned int new_reg)
+{
+  /* Interrupt functions can only use registers that have already been
+     saved by the prologue, even if they would normally be call-clobbered.  */
+  if (cfun->machine->interrupt_handler_p && !df_regs_ever_live_p (new_reg))
+    return false;
+
+  return true;
+}
+
+/* Return nonzero if register REGNO can be used as a scratch register
+   in peephole2.  */
+
+bool
+mips_hard_regno_scratch_ok (unsigned int regno)
+{
+  /* See mips_hard_regno_rename_ok.  */
+  if (cfun->machine->interrupt_handler_p && !df_regs_ever_live_p (regno))
+    return false;
+
+  return true;
+}
+
 /* Implement HARD_REGNO_NREGS.  */
 
 unsigned int
@@ -22427,6 +22454,9 @@ mips_lra_p (void)
 
 #undef TARGET_SCHED_SET_SCHED_FLAGS
 #define TARGET_SCHED_SET_SCHED_FLAGS mips_set_sched_flags
+
+#undef TARGET_HARD_REGNO_SCRATCH_OK
+#define TARGET_HARD_REGNO_SCRATCH_OK mips_hard_regno_scratch_ok
 
 struct gcc_target targetm = TARGET_INITIALIZER;
 
