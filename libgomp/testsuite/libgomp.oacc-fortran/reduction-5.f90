5942d12154a0420ec8516d16c99e56a26d96ea33
cesar <cesar@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-08-27 01:17:22 +0000

    libgomp/ 	* testsuite/libgomp.oacc-c-c++-common/reduction.h: New file. 	* testsuite/libgomp.oacc-c-c++-common/reduction-1.c: Update to check 	worker and vector reductions. 	* testsuite/libgomp.oacc-c-c++-common/reduction-2.c: Likewise. 	* testsuite/libgomp.oacc-c-c++-common/reduction-3.c: Likewise. 	* testsuite/libgomp.oacc-c-c++-common/reduction-4.c: Likewise. 	* testsuite/libgomp.oacc-c-c++-common/reduction-5.c: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-1.f90: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-2.f90: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-3.f90: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-4.f90: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-5.f90: Likewise. 	* testsuite/libgomp.oacc-fortran/reduction-6.f90: Likewise.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@227249 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgomp/testsuite/libgomp.oacc-fortran/reduction-5.f90
+++ libgomp/testsuite/libgomp.oacc-fortran/reduction-5.f90
@@ -4,9 +4,12 @@
 
 program reduction
   integer, parameter    :: n = 40, c = 10
-  integer               :: i, vsum, sum
+  integer               :: i, vsum, gs, ws, vs, cs
 
-  call redsub (sum, n, c)
+  call redsub_gang (gs, n, c)
+  call redsub_worker (gs, n, c)
+  call redsub_vector (vs, n, c)
+  call redsub_combined (cs, n, c)
 
   vsum = 0
 
@@ -15,10 +18,11 @@ program reduction
      vsum = vsum + c
   end do
 
-  if (sum.ne.vsum) call abort ()
+  if (gs .ne. vsum) call abort ()
+  if (vs .ne. vsum) call abort ()
 end program reduction
 
-subroutine redsub(sum, n, c)
+subroutine redsub_gang(sum, n, c)
   integer :: sum, n, c
 
   sum = 0
@@ -29,4 +33,43 @@ subroutine redsub(sum, n, c)
      sum = sum + c
   end do
   !$acc end parallel
-end subroutine redsub
+end subroutine redsub_gang
+
+subroutine redsub_worker(sum, n, c)
+  integer :: sum, n, c
+
+  sum = 0
+
+  !$acc parallel copyin (n, c) num_workers(4) vector_length (32) copy(sum)
+  !$acc loop reduction(+:sum) worker
+  do i = 1, n
+     sum = sum + c
+  end do
+  !$acc end parallel
+end subroutine redsub_worker
+
+subroutine redsub_vector(sum, n, c)
+  integer :: sum, n, c
+
+  sum = 0
+
+  !$acc parallel copyin (n, c) vector_length(32) copy(sum)
+  !$acc loop reduction(+:sum) vector
+  do i = 1, n
+     sum = sum + c
+  end do
+  !$acc end parallel
+end subroutine redsub_vector
+
+subroutine redsub_combined(sum, n, c)
+  integer :: sum, n, c
+
+  sum = 0
+
+  !$acc parallel num_gangs (8) num_workers (4) vector_length(32) copy(sum)
+  !$acc loop reduction(+:sum) gang worker vector
+  do i = 1, n
+     sum = sum + c
+  end do
+  !$acc end parallel
+end subroutine redsub_combined
