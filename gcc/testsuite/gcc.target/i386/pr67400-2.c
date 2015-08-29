5c716aa6fb4c4ff80d49aaf2092e24d3f97a878d
H.J. Lu <hjl.tools@gmail.com>
2015-08-28 19:14:49 -0700

    Load external function address via GOT slot
    For -fno-plt, we load the external function address via the GOT slot
    so that linker won't create an PLT entry for extern function address.
    
    gcc/
    
    	PR target/pr67400
    	* config/i386/i386-protos.h (ix86_force_load_from_GOT_p): New.
    	* config/i386/i386.c (ix86_force_load_from_GOT_p): New function.
    	(ix86_legitimate_address_p): Allow UNSPEC_GOTPCREL if
    	ix86_force_load_from_GOT_p returns true.
    	(ix86_print_operand_address): Support UNSPEC_GOTPCREL if
    	ix86_force_load_from_GOT_p returns true.
    	(ix86_expand_move): Load the external function address via the
    	GOT slot if ix86_force_load_from_GOT_p returns true.
    	* config/i386/predicates.md (x86_64_immediate_operand): Return
    	false if ix86_force_load_from_GOT_p returns true.
    
    gcc/testsuite/
    
    	PR target/pr67400
    	* gcc.target/i386/pr67400-1.c: New test.
    	* gcc.target/i386/pr67400-2.c: Likewise.
    	* gcc.target/i386/pr67400-3.c: Likewise.
    	* gcc.target/i386/pr67400-4.c: Likewise.

---
+++ gcc/testsuite/gcc.target/i386/pr67400-2.c
@@ -0,0 +1,14 @@
+/* { dg-do compile { target *-*-linux* } } */
+/* { dg-options "-O2 -fno-pic -fno-plt" } */
+
+extern void bar (void);
+extern void *p;
+
+void
+foo (void)
+{
+  p = &bar;
+}
+
+/* { dg-final { scan-assembler "mov\(l|q\)\[ \t\]*bar@GOTPCREL" { target { ! ia32 } } } } */
+/* { dg-final { scan-assembler-not "mov\(l|q\)\[ \t\]*\\\$bar," { target { ! ia32 } } } } */
