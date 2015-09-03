3428b712848f1e595a4ef17422968e13037bf251
Matthew Fortune <matthew.fortune@imgtec.com>
2015-09-03 12:07:43 +0100

    Release branch only commit: o32 and 64-bit arch support
    Update all multilib specs to enable -mabi=32 -march=<64-bit arch> to work
    
    Using the o32 ABI with a 64-bit architecture should naturally map to the
    o32 support libraries for the corresponding 32-bit architecture.
    
    Removing MULTILIB_DEFAULTS is necessary to ensure that all the library
    variants are considered as the logic around MULTILIB_DEFAULTS does not
    match what we want. It appears to be designed to eliminate axes of a
    multilib specification depending on how a compiler is configured. I.e.
    it removes the o32 component if the ABI is o32 by default and n32 if
    n32 is default. It also does not account for more specialised versions
    of a library on top of the simple default variant. At the very least
    this causes no obvious harm and achieves the desired behaviour.

--- gcc/config/mips/mti-elf.h
+++ gcc/config/mips/mti-elf.h
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
+#undef MULTILIB_DEFAULTS
+
 #undef DRIVER_SELF_SPECS
 #define DRIVER_SELF_SPECS						\
   /* Make sure a -mips option is present.  This helps us to pick	\
