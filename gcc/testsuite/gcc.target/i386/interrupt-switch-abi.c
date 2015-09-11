31e4f4c1c1cb78e64cdb52446df1551fa456b602
H.J. Lu <hjl.tools@gmail.com>
2015-09-11 08:34:08 -0700

    Remove railing whitespaces

--- gcc/testsuite/gcc.target/i386/interrupt-switch-abi.c
+++ gcc/testsuite/gcc.target/i386/interrupt-switch-abi.c
@@ -3,10 +3,10 @@
 extern void bar(int);
 
 void f1(){  bar(1); }
-__attribute__((interrupt)) 
+__attribute__((interrupt))
 void f2(){  bar(2); }
 void f3(){  bar(3); }
-__attribute__((exception)) 
+__attribute__((exception))
 void f4(){  bar(4); }
 void f5(){  bar(5); }
 
