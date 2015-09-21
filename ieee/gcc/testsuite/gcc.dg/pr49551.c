beffc02ecf491e7ee46bb4eb733d1faf8cbb8eb8
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-09-21 18:03:08 +0000

    delete branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@227979 138bc75d-0d04-0410-961f-82ee72b054a4

--- ieee/gcc/testsuite/gcc.dg/pr49551.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-O -fdata-sections" } */
-/* { dg-skip-if "-fdata-sections not supported" { nvptx-*-* } { "*" } { "" } } */
-
-int x = 1;
-int x;
-
-/* { dg-final { scan-assembler-not {comm[\t ]+x} } } */
