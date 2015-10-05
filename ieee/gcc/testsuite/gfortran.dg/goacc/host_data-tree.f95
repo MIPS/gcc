49d1a552af1d089c242928d8bfea19ca881b7004
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-05 23:21:37 +0000

    clone branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@228506 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ ieee/gcc/testsuite/gfortran.dg/goacc/host_data-tree.f95
@@ -0,0 +1,12 @@
+! { dg-do compile } 
+! { dg-additional-options "-fdump-tree-original" } 
+
+program test
+  implicit none
+  integer :: i
+
+  !$acc host_data use_device(i)
+  !$acc end host_data
+end program test
+! { dg-prune-output "unimplemented" }
+! { dg-final { scan-tree-dump-times "pragma acc host_data use_device\\(i\\)" 1 "original" } } 
