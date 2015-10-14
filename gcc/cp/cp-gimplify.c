3b06878777b45f4cc3012c825276b02b9348e206
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-14 08:24:01 +0000

    Fix folding of Cilk+ for condition.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/c++-delayed-folding@228801 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/cp-gimplify.c
+++ gcc/cp/cp-gimplify.c
@@ -945,7 +945,8 @@ cp_fold_r (tree *stmt_p, int *walk_subtrees, void *data)
   *stmt_p = stmt = cp_fold (*stmt_p, wtd->fold_hash);
 
   code = TREE_CODE (stmt);
-  if (code == OMP_FOR || code == OMP_SIMD || code == OMP_DISTRIBUTE)
+  if (code == OMP_FOR || code == OMP_SIMD || code == OMP_DISTRIBUTE
+      || code == CILK_FOR || code == CILK_SIMD)
     {
       tree x;
       int i, n;
