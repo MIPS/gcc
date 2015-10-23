cd0e7e4167a8360ba61095e2fca99c3574d95faf
tungchieh.tsai@mediatek.com <tungchieh.tsai@mediatek.com>
2015-10-23 15:36:09 +0800

    Add -minline-intermix
    This option allows inlining even if the functions mismatch on compression
    attributes

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -1631,7 +1631,8 @@ mips_merge_decl_attributes (tree olddecl, tree newdecl)
 static bool
 mips_can_inline_p (tree caller, tree callee)
 {
-  if (mips_get_compress_mode (callee) != mips_get_compress_mode (caller))
+  if (!TARGET_INLINE_INTERMIX
+      && mips_get_compress_mode (callee) != mips_get_compress_mode (caller))
     return false;
   return default_target_can_inline_p (caller, callee);
 }
