8233c9945687d953df30460a23d93c01e501e42f
Matthew Fortune <matthew.fortune@imgtec.com>
2015-11-04 07:56:53 +0000

    Fix ICE for constant pool data in GP area for MIPS16

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -5274,6 +5274,15 @@ mips_split_move_insn (rtx dest, rtx src, rtx insn)
 /* Return the appropriate instructions to move SRC into DEST.  Assume
    that SRC is operand 1 and DEST is operand 0.  */
 
+bool
+mips_constant_pool_symbol_in_sdata (rtx x, enum mips_symbol_context context)
+{
+  enum mips_symbol_type symbol_type;
+  return (mips_symbolic_constant_p (x, context, &symbol_type)
+	  && symbol_type == SYMBOL_GP_RELATIVE
+	  && CONSTANT_POOL_ADDRESS_P (x));
+}
+
 const char *
 mips_output_move (rtx dest, rtx src)
 {
@@ -5428,7 +5437,12 @@ mips_output_move (rtx dest, rtx src)
 	}
 
       if (src_code == HIGH)
-	return TARGET_MIPS16 ? "#" : "lui\t%0,%h1";
+	{
+	  if (mips_constant_pool_symbol_in_sdata (XEXP (src, 0), SYMBOL_CONTEXT_MEM))
+	    return "move\t%0,$28";
+
+	  return TARGET_MIPS16 ? "#" : "lui\t%0,%h1";
+	}
 
       if (CONST_GP_P (src))
 	return "move\t%0,%1";
