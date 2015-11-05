bb6471ea2c8abb1c47ee0879839aa4e813931c29
vries <vries@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-05 14:41:57 +0000

    Remove misc oacc kernels functions
    2015-11-05  Tom de Vries  <tom@codesourcery.com>
    
    	* omp-low.c (get_bbs_in_oacc_kernels_region):
    	(loop_get_oacc_kernels_region_entry, get_oacc_kernels_region_exit)
    	(oacc_kernels_region_entry_p): Remove.
    	* omp-low.h: Same.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@229813 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/omp-low.h
+++ gcc/omp-low.h
@@ -30,11 +30,6 @@ extern tree omp_reduction_init (tree, tree);
 extern bool make_gimple_omp_edges (basic_block, struct omp_region **, int *);
 extern void omp_finish_file (void);
 extern tree omp_member_access_dummy_var (tree);
-extern bool oacc_kernels_region_entry_p (basic_block, gomp_target **);
-extern basic_block get_oacc_kernels_region_exit (basic_block);
-extern basic_block loop_get_oacc_kernels_region_entry (struct loop *);
-extern vec<basic_block> get_bbs_in_oacc_kernels_region (basic_block,
-							basic_block);
 extern void replace_oacc_fn_attrib (tree, tree);
 extern tree build_oacc_routine_dims (tree);
 extern tree get_oacc_fn_attrib (tree);
