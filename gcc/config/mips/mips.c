bc7d7a5e5d90028d916fa1acb60c504ef7cfb125
Matthew Fortune <matthew.fortune@imgtec.com>
2015-11-10 15:34:20 +0000

    Add support for a limit for inlining memcpy
    Expose it with an option: -mblockmov-limit. A memcpy strictly less
    than this value will be considered for inlining.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -8237,16 +8237,19 @@ mips_expand_block_move (rtx dest, rtx src, rtx length)
 
   if (CONST_INT_P (length))
     {
-      if (INTVAL (length) <= MIPS_MAX_MOVE_BYTES_STRAIGHT)
+      if (mips_movmem_limit == -1 || INTVAL (length) < mips_movmem_limit)
 	{
-	  mips_block_move_straight (dest, src, INTVAL (length));
-	  return true;
-	}
-      else if (optimize)
-	{
-	  mips_block_move_loop (dest, src, INTVAL (length),
-				MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER);
-	  return true;
+	  if (INTVAL (length) <= MIPS_MAX_MOVE_BYTES_STRAIGHT)
+	    {
+	      mips_block_move_straight (dest, src, INTVAL (length));
+	      return true;
+	    }
+	  else if (optimize)
+	    {
+	      mips_block_move_loop (dest, src, INTVAL (length),
+				    MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER);
+	      return true;
+	    }
 	}
     }
   return false;
