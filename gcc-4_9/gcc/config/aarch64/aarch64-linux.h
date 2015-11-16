16fa641cab30906d06d248669810026878c17057
brooks <brooks@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-11-16 22:54:46 +0000

    Merged revisions 223046 via svnmerge from svn+ssh://brooks@gcc.gnu.org/svn/gcc/branches/gcc-4_9-branch
    ........
      r223046 | yroux | 2015-05-12 01:55:54 -0700 (Tue, 12 May 2015) | 18 lines
      
      2015-05-12  Yvan Roux  <yvan.roux@linaro.org>
      
            Backport from mainline.
            2015-05-05  Yvan Roux  <yvan.roux@linaro.org>
      
            * config/aarch64/aarch64-elf-raw.h (CA53_ERR_843419_SPEC): Define.
            (LINK_SPEC): Include CA53_ERR_843419_SPEC.
            * config/aarch64/aarch64-linux.h (CA53_ERR_843419_SPEC): Define.
            (LINK_SPEC): Include CA53_ERR_843419_SPEC.
            * config/aarch64/aarch64.opt (mfix-cortex-a53-843419): New option.
            * configure: Regenerate.
            * configure.ac: Add --enable-fix-cortex-a53-843419 option.
            * doc/install.texi (aarch64*-*-*): Document new
            --enable-fix-cortex-a53-843419 option.
            * doc/invoke.texi (AArch64 Options): Document -mfix-cortex-a53-843419
            and -mno-fix-cortex-a53-843419 options.
    ........
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@230441 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/config/aarch64/aarch64-linux.h
+++ gcc-4_9/gcc/config/aarch64/aarch64-linux.h
@@ -30,7 +30,7 @@
 
 #define CPP_SPEC "%{pthread:-D_REENTRANT}"
 
-#define LINUX_TARGET_LINK_SPEC0  "%{h*}		\
+#define LINUX_TARGET_LINK_SPEC  "%{h*}		\
    %{static:-Bstatic}				\
    %{shared:-shared}				\
    %{symbolic:-Bsymbolic}			\
@@ -48,7 +48,17 @@
   " %{mfix-cortex-a53-835769:--fix-cortex-a53-835769}"
 #endif
 
-#define LINUX_TARGET_LINK_SPEC LINUX_TARGET_LINK_SPEC0 CA53_ERR_835769_SPEC
+#ifdef TARGET_FIX_ERR_A53_843419_DEFAULT
+#define CA53_ERR_843419_SPEC \
+  " %{!mno-fix-cortex-a53-843419:--fix-cortex-a53-843419}"
+#else
+#define CA53_ERR_843419_SPEC \
+  " %{mfix-cortex-a53-843419:--fix-cortex-a53-843419}"
+#endif
+
+#define LINK_SPEC LINUX_TARGET_LINK_SPEC \
+                  CA53_ERR_835769_SPEC \
+                  CA53_ERR_843419_SPEC
 
 #define LINK_SPEC LINUX_TARGET_LINK_SPEC
 
