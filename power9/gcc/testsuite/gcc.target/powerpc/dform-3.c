3d1b2c311f8716ef165c54c09050268f7f289398
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-01 23:39:43 +0000

    add test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@231134 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ power9/gcc/testsuite/gcc.target/powerpc/dform-3.c
@@ -0,0 +1,18 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_p9vector_ok } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power9" } } */
+/* { dg-options "-mcpu=power9 -mpower9-dform -O2" } */
+
+void foo (vector double *p) { p[1] = p[2] + p[3]; }
+static vector double x[4];
+vector double *p_x = x;
+void bar (void) { x[1] = x[2] + x[3]; }
+
+/* { dg-final { scan-assembler     "lvx "     } } */
+/* { dg-final { scan-assembler     "stvx "    } } */
+/* { dg-final { scan-assembler-not "lxvd2x "  } } */
+/* { dg-final { scan-assembler-not "lxvw4x "  } } */
+/* { dg-final { scan-assembler-not "stxvd2x " } } */
+/* { dg-final { scan-assembler-not "stxvw4x " } } */
+/* { dg-final { scan-assembler-not "mfvsrd "  } } */
+/* { dg-final { scan-assembler-not "mtvsrd "  } } */
