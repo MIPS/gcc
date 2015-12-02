1248534c4faef82d01aa90a60b9cc515ec36adc4
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-02 00:11:59 +0000

    fix test
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@231137 138bc75d-0d04-0410-961f-82ee72b054a4

--- power9/gcc/testsuite/gcc.target/powerpc/dform-3.c
+++ power9/gcc/testsuite/gcc.target/powerpc/dform-3.c
@@ -8,8 +8,8 @@ static vector double x[4];
 vector double *p_x = x;
 void bar (void) { x[1] = x[2] + x[3]; }
 
-/* { dg-final { scan-assembler     "lvx "     } } */
-/* { dg-final { scan-assembler     "stvx "    } } */
+/* { dg-final { scan-assembler     "lxv "     } } */
+/* { dg-final { scan-assembler     "stxv "    } } */
 /* { dg-final { scan-assembler-not "lxvd2x "  } } */
 /* { dg-final { scan-assembler-not "lxvw4x "  } } */
 /* { dg-final { scan-assembler-not "stxvd2x " } } */
