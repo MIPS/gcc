30024c6274b84c13f43e18110c6f5f2f3ca24701
Andrew Bennett <andrew.bennett@imgtec.com>
2015-12-14 10:43:19 +0000

    MIPS: Add support for the new MIPS SAVE/RESTORE instructions and the new LUI/ORI MIPS16 instructions.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -215,7 +215,9 @@ struct mips_cpu_info {
 /* Generate mips16e code. Default 16bit ASE for mips32* and mips64* */
 #define GENERATE_MIPS16E	(TARGET_MIPS16 && mips_isa >= 32)
 /* Generate mips16e register save/restore sequences.  */
-#define GENERATE_MIPS16E_SAVE_RESTORE (GENERATE_MIPS16E && mips_abi == ABI_32)
+#define GENERATE_MIPS16E_SAVE_RESTORE ((GENERATE_MIPS16E \
+					|| (TARGET_USE_SAVE_RESTORE && TARGET_SOFT_FLOAT)) \
+				       && mips_abi == ABI_32)
 
 /* True if we're generating a form of MIPS16 code in which general
    text loads are allowed.  */
@@ -1331,6 +1333,7 @@ struct mips_cpu_info {
 %{mhard-float} %{msoft-float} \
 %{msingle-float} %{mdouble-float} \
 %{mforbidden-slots} \
+%{mmips16e2} \
 %(subtarget_asm_spec)"
 
 /* Extra switches sometimes passed to the linker.  */
