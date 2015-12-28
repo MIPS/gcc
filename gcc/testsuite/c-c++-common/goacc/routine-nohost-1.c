2a1324c2968062751b8785140a2a1fbba469148a
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-12-28 21:02:39 +0000

    * c-c++-common/goacc/routine-nohost-1.c: Adjust expected dump output.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@231979 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/c-c++-common/goacc/routine-nohost-1.c
+++ gcc/testsuite/c-c++-common/goacc/routine-nohost-1.c
@@ -9,18 +9,12 @@ int THREE(void)
   return 3;
 }
 
-/* { dg-final { scan-tree-dump "Discarding function .THREE. with .nohost. clause" "oaccdevlow" } } */
-
-
 #pragma acc routine nohost
 extern void NOTHING(void);
 
 void NOTHING(void)
 {
 }
-
-/* { dg-final { scan-tree-dump "Discarding function .NOTHING. with .nohost. clause" "oaccdevlow" } } */
-
 
 extern float ADD(float, float);
 
@@ -31,4 +25,4 @@ float ADD(float x, float y)
   return x + y;
 }
 
-/* { dg-final { scan-tree-dump "Discarding function .ADD. with .nohost. clause" "oaccdevlow" } } */
+/* { dg-final { scan-tree-dump-times "Discarding function" 3 "oaccdevlow" } } */
