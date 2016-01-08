f13cee19985bd4eabaa2b4c64a059e945e1a7cde
cesar <cesar@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-08 22:33:58 +0000

    gcc/ 	* lto-cgraph.c (input_varpool_node): Reduce the assert for 	missing variables to an error.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@232181 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/lto-cgraph.c
+++ gcc/lto-cgraph.c
@@ -1422,9 +1422,11 @@ input_varpool_node (struct lto_file_decl_data *file_data,
     node->set_section_for_node (section);
   node->resolution = streamer_read_enum (ib, ld_plugin_symbol_resolution,
 					        LDPR_NUM_KNOWN);
-  gcc_assert (flag_ltrans
-	      || (!node->in_other_partition
-		  && !node->used_from_other_partition));
+
+  int success = flag_ltrans || (!node->in_other_partition
+				&& !node->used_from_other_partition);
+  if (!success)
+    error ("Missing %<%s%>", node->name ());
 
   return node;
 }
