a11fc047f0277bde60a8bc053f635f374f873e67
nathan <nathan@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-01-12 14:05:28 +0000

    * gcc.dg/goacc/kern-1.c: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@232271 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/goacc/kern-1.c
@@ -0,0 +1,23 @@
+/* { dg-additional-options "-fopenacc -O2 -ftree-parallelize-loops=32" } */
+
+/* The reduction on sum could cause an ICE with a non-simple latch loop.   */
+
+int printf (char const *, ...);
+
+int
+main ()
+{
+  int i;
+  double a[1000], sum = 0;
+
+  
+#pragma acc kernels pcopyin(a[0:1000])
+#pragma acc loop reduction(+:sum)
+  for(int i=0; i<1000; i++) {
+    sum += a[i];
+  }
+
+  printf ("%lf\n", sum);
+
+  return 0;
+}
