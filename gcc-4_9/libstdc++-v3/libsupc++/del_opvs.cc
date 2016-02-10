5dc99e43d43dc33afb67a8db92ce23f1584f549c
brooks <brooks@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-02-10 23:15:28 +0000

    Backport libstdc++ portions of r218755 to Google 4.9 branch.
    This supersedes the existing del_opsz.cc implementation of non-vector sized delete, so that file is removed. 
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@233310 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/libstdc++-v3/libsupc++/del_opvs.cc
+++ gcc-4_9/libstdc++-v3/libsupc++/del_opvs.cc
@@ -1,7 +1,6 @@
 // Boilerplate support routines for -*- C++ -*- dynamic memory management.
 
-// Copyright (C) 2012
-// Free Software Foundation
+// Copyright (C) 1997-2014 Free Software Foundation, Inc.
 //
 // This file is part of GCC.
 //
@@ -25,26 +24,10 @@
 // <http://www.gnu.org/licenses/>.
 
 #include <bits/c++config.h>
-
-#if !_GLIBCXX_HOSTED
-// A freestanding C runtime may not provide "free" -- but there is no
-// other reasonable way to implement "operator delete".
-namespace std
-{
-_GLIBCXX_BEGIN_NAMESPACE_VERSION
-  extern "C" void free(void*);
-_GLIBCXX_END_NAMESPACE_VERSION
-} // namespace
-#else
-# include <cstdlib>
-#endif
-
 #include "new"
 
 _GLIBCXX_WEAK_DEFINITION void
-operator delete(void* ptr,
-                std::size_t bytes __attribute__((__unused__))) throw ()
+operator delete[] (void *ptr, std::size_t) _GLIBCXX_USE_NOEXCEPT
 {
-  if (ptr)
-    std::free(ptr);
+  ::operator delete[] (ptr);
 }
