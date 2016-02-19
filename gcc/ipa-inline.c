e74135614edb7752574e8696452c007425416d47
Robert Suchanek <robert.suchanek@imgtec.com>
2016-02-19 15:33:10 +0000

    Possible inlining improvements with -Os.
    --param early-inlining-insns-cold=NUMBER
    --param max-inline-insns-small-and-cold=NUMBER
    
    Analysis show that the main difference between -O2 and -Os goes down to
    inlining of cold or unlikely functions. The new parameters (defaulted to 0)
    mean to disable this limitations with -Os. NUMBER could be set to something
    like 4-32 to see the impact.
    
    The main reason that smaller functions are treated as cold or unlikely
    is the function cgraph_maybe_hot_edge_p () always returning FALSE for -Os.

--- gcc/ipa-inline.c
+++ gcc/ipa-inline.c
@@ -626,7 +626,7 @@ want_early_inline_function_p (struct cgraph_edge *e)
       if (growth <= 0)
 	;
       else if (!e->maybe_hot_p ()
-	       && growth > 0)
+	       && growth > PARAM_VALUE (PARAM_EARLY_INLINING_INSNS_COLD))
 	{
 	  if (dump_file)
 	    fprintf (dump_file, "  will not early inline: %s->%s, "
@@ -831,6 +831,7 @@ want_inline_small_function_p (struct cgraph_edge *e, bool report)
 	}
       /* If call is cold, do not inline when function body would grow. */
       else if (!e->maybe_hot_p ()
+	       && growth > PARAM_VALUE (PARAM_MAX_INLINE_INSNS_SMALL_AND_COLD)
 	       && (growth >= MAX_INLINE_INSNS_SINGLE
 		   || growth_likely_positive (callee, growth)))
 	{
