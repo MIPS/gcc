6019a32679e92f57a86f5d3e14cc4a6b4cbd7d97
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-03-11 18:29:22 +0000

    Just use direct move to sign/zero extend SImode
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@234144 138bc75d-0d04-0410-961f-82ee72b054a4

--- power9/gcc/testsuite/gcc.target/powerpc/ppc-round2.c
+++ power9/gcc/testsuite/gcc.target/powerpc/ppc-round2.c
@@ -3,15 +3,13 @@
 /* { dg-require-effective-target powerpc_p8vector_ok } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
 /* { dg-options "-O2 -mcpu=power8" } */
-/* { dg-final { scan-assembler-times "fcfid "      1 } } */
-/* { dg-final { scan-assembler-times "fcfids "     1 } } */
+/* { dg-final { scan-assembler-times "fcfid "      2 } } */
+/* { dg-final { scan-assembler-times "fcfids "     2 } } */
 /* { dg-final { scan-assembler-times "fctiwuz "    2 } } */
-/* { dg-final { scan-assembler-times "mfvsrd "     2 } } */
+/* { dg-final { scan-assembler-times "fctiwz "     2 } } */
+/* { dg-final { scan-assembler-times "mfvsrd "     4 } } */
+/* { dg-final { scan-assembler-times "mtvsrwa "    2 } } */
 /* { dg-final { scan-assembler-times "mtvsrwz "    2 } } */
-/* { dg-final { scan-assembler-times "vupkhsw "    2 } } */
-/* { dg-final { scan-assembler-times "xscvdpsxws " 2 } } */
-/* { dg-final { scan-assembler-times "xscvsxddp "  2 } } */
-/* { dg-final { scan-assembler-times "xxpermdi "   2 } } */
 /* { dg-final { scan-assembler-not   "lwz"           } } */
 /* { dg-final { scan-assembler-not   "lfiwax "       } } */
 /* { dg-final { scan-assembler-not   "lfiwzx "       } } */
