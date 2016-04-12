0796beded9dbf6be7ec2795ae622b0458cfb9bf2
Robert Suchanek <robert.suchanek@imgtec.com>
2016-04-12 11:09:19 +0100

    MIPS: Add support for the new MIPS SAVE/RESTORE instructions
    Check to see if it is safe to use the SAVE/RESTORE instruction in a function.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -218,7 +218,9 @@ struct mips_cpu_info {
 /* Generate mips16e code. Default 16bit ASE for mips32* and mips64* */
 #define GENERATE_MIPS16E	(TARGET_MIPS16 && mips_isa >= 32)
 /* Generate mips16e register save/restore sequences.  */
-#define GENERATE_MIPS16E_SAVE_RESTORE (GENERATE_MIPS16E && mips_abi == ABI_32)
+#define GENERATE_MIPS16E_SAVE_RESTORE ((GENERATE_MIPS16E \
+					|| (TARGET_USE_SAVE_RESTORE && TARGET_SOFT_FLOAT)) \
+				       && mips_abi == ABI_32)
 
 /* True if we're generating a form of MIPS16 code in which general
    text loads are allowed.  */
