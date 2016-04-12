32eb0a65db1735d4b332fe965912d1a02898b740
Robert Suchanek <robert.suchanek@imgtec.com>
2016-04-12 11:07:06 +0100

    Add MIPS16E2 support.

--- gcc/config/mips/mips-protos.h
+++ gcc/config/mips/mips-protos.h
@@ -376,4 +376,7 @@ extern void mips_register_frame_header_opt (void);
 
 extern const char * mips_output_epi_jump (void);
 
+extern bool mips_bit_clear_p (enum machine_mode, unsigned HOST_WIDE_INT);
+extern void mips_bit_clear_info (enum machine_mode, unsigned HOST_WIDE_INT,
+				 int *, int *);
 #endif /* ! GCC_MIPS_PROTOS_H */
