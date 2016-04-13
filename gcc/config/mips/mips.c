a5d4d49bfdbe6ca6089d930e03ca02c3e6edde54
Robert Suchanek <robert.suchanek@imgtec.com>
2016-04-13 11:56:50 +0100

    Disable madd/msub when -mno-imadd is used with -mdsp.
    gcc/
    	* config/mips/mips.c (mips_option_override): Move DSP ASE checks
    	earlier in the function.  Enable madd/msub patterns by default
    	for DSP ASE.
    	* config/mips/mips.md (<u>msubsidi4): Remove override for DSP.
    	(<u>maddsidi4): Ditto.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -19761,13 +19761,27 @@ mips_option_override (void)
     warning (0, "the %qs architecture does not support branch-likely"
 	     " instructions", mips_arch_info->name);
 
+  /* If TARGET_DSPR2, enable TARGET_DSP.  */
+  if (TARGET_DSPR2)
+    TARGET_DSP = true;
+
+  if (TARGET_DSP && mips_isa_rev >= 6)
+    {
+      error ("the %qs architecture does not support DSP instructions",
+	     mips_arch_info->name);
+      TARGET_DSP = false;
+      TARGET_DSPR2 = false;
+    }
+
   /* If the user hasn't specified -mimadd or -mno-imadd set
      MASK_IMADD based on the target architecture and tuning
      flags.  */
   if ((target_flags_explicit & MASK_IMADD) == 0)
     {
-      if (ISA_HAS_MADD_MSUB &&
-          (mips_tune_info->tune_flags & PTF_AVOID_IMADD) == 0)
+      if ((ISA_HAS_MADD_MSUB &&
+	   (mips_tune_info->tune_flags & PTF_AVOID_IMADD) == 0)
+	  /* Enable for DSP by default */
+	  || TARGET_DSP)
 	target_flags |= MASK_IMADD;
       else
 	target_flags &= ~MASK_IMADD;
@@ -19956,18 +19970,6 @@ mips_option_override (void)
       error ("%qs requires a target that provides the %qs instruction",
 	     "-mr10k-cache-barrier", "cache");
       mips_r10k_cache_barrier = R10K_CACHE_BARRIER_NONE;
-    }
-
-  /* If TARGET_DSPR2, enable TARGET_DSP.  */
-  if (TARGET_DSPR2)
-    TARGET_DSP = true;
-
-  if (TARGET_DSP && mips_isa_rev >= 6)
-    {
-      error ("the %qs architecture does not support DSP instructions",
-	     mips_arch_info->name);
-      TARGET_DSP = false;
-      TARGET_DSPR2 = false;
     }
 
   /* .eh_frame addresses should be the same width as a C pointer.
