9a07f84f6ee47422e428349f5da7a8ddd6584b3c
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-04-19 17:36:55 +0000

    gcc/ChangeLog:
    2016-04-18  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	* doc/extend.texi: Add documentation for the vec_adu(),
    	vec_adub(), vec_aduh(), and vec_aduw() built-ins.
    	* config/rs6000/rs6000.h (MASK_P9_VECTOR): New macro definition.
    	This change is redundant with a previously merged patch.
    	(RS6000_BTM_P9_VECTOR): Correct error in previous definition of
    	this macro.  This change is redundant with a previously merged patch.
    	* config/rs6000/rs6000-c.c (altivec_overloaded_builtins): Added
    	three P9V_BUILTIN_VEC_VADU entries for three overloaded variants
    	of the builtin_vec_vadu() function and P9V_BUILTIN_VEC_VADUB,
    	P9V_BUILTIN_VEC_VADUH, and P9V_BUILTIN_VEC_VADUW entries for the
    	builtin_vec_vadub(), builtin_vec_vaduh(), and builtin_vec_vaduw()
    	functions.
    	* config/rs6000/rs6000-builtin.def: Add macro expansions for
    	__builtin_altivec_vadub, __builtin_altivec_vaduh, and
    	__builtin_altivec_vaduw, and for overloaded functions
    	__builtin_vec_vadu, __builtin_vec_vadub, __builtin_vec_vaduh, and
    	__builtin_vec_vaduw.
    	* config/rs6000/altivec.md (UNSPEC_VADU): New unspec constant.
    	("vadu<mode>3"): New expand pattern.
    	("*p9_vadu<mode>3"): New insn pattern.
    	* config/rs6000/altivec.h (vec_adu): New macro definition.
    	(vec_adub): New macro definition.
    	(vec_aduh): New macro definition.
    	(vec_aduw): new macro definition.
    
    gcc/testsuite/ChangeLog:
    
    2016-04-18  Kelvin Nilsen  <kelvin@gcc.gnu.org>
    
    	* gcc.target/powerpc/vadsdu-0.c: New test.
    	* gcc.target/powerpc/vadsdu-1.c: New test.
    	* gcc.target/powerpc/vadsdu-2.c: New test.
    	* gcc.target/powerpc/vadsdu-3.c: New test.
    	* gcc.target/powerpc/vadsdu-4.c: New test.
    	* gcc.target/powerpc/vadsdu-5.c: New test.
    	* gcc.target/powerpc/vadsdub-1.c: New test.
    	* gcc.target/powerpc/vadsdub-2.c: New test.
    	* gcc.target/powerpc/vadsduh-1.c: New test.
    	* gcc.target/powerpc/vadsduh-2.c: New test.
    	* gcc.target/powerpc/vadsduw-1.c: New test.
    	* gcc.target/powerpc/vadsduw-2.c: New test.
    
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@235212 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ pre-gcc7/gcc/testsuite/gcc.target/powerpc/vadsdu-2.c
@@ -0,0 +1,21 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-options "-mcpu=power9" } */
+
+/* This test should succeed on both 32- and 64-bit configurations.  */
+#include <altivec.h>
+
+__vector unsigned short
+doAbsoluteDifferenceUnsignedShort (__vector unsigned short *p, 
+				   __vector unsigned short *q)
+{
+  __vector unsigned short source_1, source_2;
+  __vector unsigned short result;
+
+  source_1 = *p;
+  source_2 = *q;
+
+  result = __builtin_vec_vadu (source_1, source_2);
+  return result;
+}
+
+/* { dg-final { scan-assembler	   "vabsduh" } } */
