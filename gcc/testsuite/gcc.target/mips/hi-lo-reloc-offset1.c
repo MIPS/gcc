c39108ae2b7f582cf330752d06782464a0b181d5
Andrew Bennett <andrew.bennett@imgtec.com>
2016-05-05 16:12:54 +0100

    Merge packed attribute tests into one testcase

--- gcc/testsuite/gcc.target/mips/hi-lo-reloc-offset1.c
+++ gcc/testsuite/gcc.target/mips/hi-lo-reloc-offset1.c
@@ -1,14 +1,35 @@
 /* { dg-options "-mcode-readable=no -mno-gpopt" } */
-/* { dg-final { scan-assembler-not "%lo\\(h\\+6\\)" } } */
+/* { dg-final { scan-assembler-not "%lo\\(h\\+\[1-9\]\[0-9\]?)" } } */
+/* { dg-final { scan-assembler-not "%lo\\(j\\+\[1-9\]\[0-9\]?)" } } */
+/* { dg-final { scan-assembler-not "%lo\\(k\\+\[1-9\]\[0-9\]?)" } } */
 
 struct __attribute__((packed))
 {
+  char c;
   short s;
-  unsigned long long l;
-} h;
+  int i;
+} h __attribute__((aligned(1)));
+
+struct __attribute__((packed))
+{
+  char c;
+  long long l;
+} j __attribute__((aligned(1)));
+
+struct __attribute__((packed))
+{
+  char c;
+  float f;
+  double d;
+} k __attribute__((aligned(1)));
 
 void
 foo (void)
 {
-  h.l = 0;
+  h.c = 0;
+  h.s = 0;
+  h.i = 0;
+  j.l = 0;
+  k.f = 0;
+  k.d = 0;
 }
