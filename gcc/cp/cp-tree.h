9057691d2f7af9b9c1b4e7adb3756b3662de6e0c
cesar <cesar@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-05-09 20:23:31 +0000

    Backport trunk r235290: 	2016-04-20  Ilya Verbin  <ilya.verbin@intel.com>
    	gcc/c-family/
    	PR c++/69363
    	* c-cilkplus.c (c_finish_cilk_clauses): Remove function.
    	* c-common.h (c_finish_cilk_clauses): Remove declaration.
    
    	gcc/c/
    	PR c++/69363
    	* c-parser.c (c_parser_cilk_all_clauses): Use c_finish_omp_clauses
    	instead of c_finish_cilk_clauses.
    	* c-tree.h (c_finish_omp_clauses): Add new default argument.
    	* c-typeck.c (c_finish_omp_clauses): Add new argument.  Allow
    	floating-point variables in the linear clause for Cilk Plus.
    
    	gcc/cp/
    	PR c++/69363
    	* cp-tree.h (finish_omp_clauses): Add new default argument.
    	* parser.c (cp_parser_cilk_simd_all_clauses): Use finish_omp_clauses
    	instead of c_finish_cilk_clauses.
    	* semantics.c (finish_omp_clauses): Add new argument.  Allow
    	floating-point variables in the linear clause for Cilk Plus.
    
    	gcc/testsuite/
    	PR c++/69363
    	* c-c++-common/cilk-plus/PS/clauses3.c: Adjust dg-error string.
    	* c-c++-common/cilk-plus/PS/clauses4.c: New test.
    	* c-c++-common/cilk-plus/PS/pr69363.c: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@236047 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -6397,7 +6397,8 @@ extern tree omp_reduction_id			(enum tree_code, tree, tree);
 extern tree cp_remove_omp_priv_cleanup_stmt	(tree *, int *, void *);
 extern void cp_check_omp_declare_reduction	(tree);
 extern void finish_omp_declare_simd_methods	(tree);
-extern tree finish_omp_clauses			(tree, bool, bool, bool = false);
+extern tree finish_omp_clauses			(tree, bool, bool, bool = false,
+						 bool = false);
 extern tree push_omp_privatization_clauses	(bool);
 extern void pop_omp_privatization_clauses	(tree);
 extern void save_omp_privatization_clauses	(vec<tree> &);
