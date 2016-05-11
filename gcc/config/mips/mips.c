8f29f511f073877d8bb30717bd8808ac24b87d93
Matthew Fortune <matthew.fortune@imgtec.com>
2016-05-11 21:52:36 +0100

    Support -mblockmov-limit with MIPS16 COPYW instructions
    -mblockmov-limit is not just a restrictive limit in this context but
    also serves to raise the limit for aligned copy.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -9426,10 +9426,12 @@ mips16_expand_copy (rtx dest, rtx src, rtx length, rtx alignment)
 
   byte_count = INTVAL (length);
 
-  if (byte_count > MIPS_MAX_MOVE_BYTES_STRAIGHT)
+  if (byte_count > (mips_movmem_limit == -1
+		    ? MIPS_MAX_MOVE_BYTES_STRAIGHT
+		    : mips_movmem_limit))
     return false;
 
-  if (byte_count == MIPS_MAX_MOVE_BYTES_STRAIGHT
+  if (byte_count >= MIPS_MAX_MOVE_BYTES_STRAIGHT
       && align < 4)
     return false;
 
