5bf409e38fb86cc8ad64a122e1f5a1fe2c205900
Andrew Bennett <andrew.bennett@imgtec.com>
2016-05-13 11:32:50 +0100

    Prevent adjust_address_1 from generating a new lo_sum where the offset is not a modulo of the mode's alignment
        PR target/71043
        gcc/
    	* emit-rtl.c (adjust_address_1): Do not generate a new lo_sum
    	where the offset on the symbol is greater than the symbol's
    	alignment.
    
        testsuite/
    	* gcc.target/mips/hi-lo-reloc-packed-struct.c: New test.

--- gcc/emit-rtl.c
+++ gcc/emit-rtl.c
@@ -2240,8 +2240,14 @@ adjust_address_1 (rtx memref, machine_mode mode, HOST_WIDE_INT offset,
 	 object, we can merge it into the LO_SUM.  */
       if (GET_MODE (memref) != BLKmode && GET_CODE (addr) == LO_SUM
 	  && offset >= 0
-	  && (unsigned HOST_WIDE_INT) offset
+	  && ((unsigned HOST_WIDE_INT) offset
 	      < GET_MODE_ALIGNMENT (GET_MODE (memref)) / BITS_PER_UNIT)
+	  && (GET_MODE_ALIGNMENT (mode) / BITS_PER_UNIT
+	      <= GET_MODE_ALIGNMENT (GET_MODE (memref)) / BITS_PER_UNIT)
+	  && (offset == 0
+	      || mode == BLKmode
+	      || (((unsigned HOST_WIDE_INT) offset
+		  % (GET_MODE_ALIGNMENT (mode) / BITS_PER_UNIT)) == 0)))
 	addr = gen_rtx_LO_SUM (address_mode, XEXP (addr, 0),
 			       plus_constant (address_mode,
 					      XEXP (addr, 1), offset));
---
+++ gcc/testsuite/gcc.target/mips/hi-lo-reloc-packed-struct.c
@@ -0,0 +1,20 @@
+/* { dg-options "-mno-gpopt" } */
+/* { dg-final { scan-assembler-not "%lo\\(n\\+\[4-9\]\\)" } } */
+/* { dg-final { scan-assembler-not "%lo\\(n\\+1\[0-9\]\\)" } } */
+
+struct __attribute__((packed))
+{
+  char c;
+  short s;
+  int i;
+  long long l;
+} n;
+
+void
+foo (void)
+{
+  n.c = 0;
+  n.s = 0;
+  n.i = 0;
+  n.l = 0;
+}
