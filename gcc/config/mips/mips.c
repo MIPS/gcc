153f4ec8c9a9369e8730cb847cbf867a6efe5bf4
Matthew Fortune <matthew.fortune@imgtec.com>
2016-05-20 09:21:05 +0100

    Update move_by_pieces_p overrides for copyw
    The choice of whether to copy by pieces is used for multiple purposes
    not all of which are asking the same question. When the question is
    asking how to move data that is already in memory to another piece
    of memory then the logic we had was good. When the question is
    speculative to see if it is worth placing data in memory and then
    copying it then the response from move_by_pieces_p is taken to mean
    it is always more expensive to do the constant creation and storing
    inline than in memory so we get data pushed to memory. In many cases
    this extra data is a performance degradation as we now have loads
    and stores instead of a series of ALU ops with stores interspersed.
    
    The straight line constant/store code is longer and can be bloated
    for no good reason but will usually be faster.
    
    However, leave an option to use COPYW as the performance data is
    not clear cut and enable by default.
    
    Optimising the inline sequence would be useful as the following code
    could be simplified:
    
    struct mys
    {
      char a;
      char b;
      int c;
    };
    
    const struct mys ref = {-1,-1,-1};
    
    void foo(struct mys a[])
    {
      a[0] = ref;
      a[1] = ref;
      a[2] = ref;
      a[3] = ref;
    }
    
    This yields the following:
    
            li      $2,1
            li      $3,1
            neg     $2,$2
            neg     $3,$3
            sb      $2,0($4)
            sb      $2,1($4)
            sw      $3,4($4)
            sb      $3,8($4)
            sb      $2,9($4)
            sw      $3,12($4)
            sb      $2,16($4)
            sb      $2,17($4)
            sw      $3,20($4)
            sb      $2,24($4)
            sb      $2,25($4)
            sw      $3,28($4)
    
    But could produce 4 less stores and 2 less setup instructions.
    
            li      $3,1
            neg     $2,$2
            sw      $2,0($4)
            sw      $2,4($4)
            sw      $2,8($4)
            sw      $2,12($4)
            sw      $2,16($4)
            sw      $2,20($4)
            sw      $2,24($4)
            sw      $2,28($4)

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -8808,12 +8808,9 @@ mips_use_by_pieces_infrastructure_p (unsigned HOST_WIDE_INT size,
 	return false;
       if (align < BITS_PER_WORD)
 	return size < UNITS_PER_WORD;
-      /* It is more profitable to use COPYW for at least 2 words.  */
-      if (ISA_HAS_COPY
+      /* It can be more profitable to use COPYW for at least 2 words.  */
+      if (TARGET_USE_COPYW_BY_PIECES && ISA_HAS_COPY
 	  && align >= BITS_PER_WORD && size >= 2 * UNITS_PER_WORD)
-	return false;
-      /* It is more profitable to use UCOPYW for at least 1 word.  */
-      if (ISA_HAS_COPY && align < BITS_PER_WORD && size >= UNITS_PER_WORD)
 	return false;
       return size <= MIPS_MAX_MOVE_BYTES_STRAIGHT;
     }
