2c2b0d18222bd916dd6df81109ba2280311a236d
Robert Suchanek <robert.suchanek@imgtec.com>
2016-06-08 23:13:36 +0100

    Remove LWM/SWM and refactor existing LWM/SWM code

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -246,6 +246,7 @@ struct mips_cpu_info {
 #define GENERATE_MIPS16E_SAVE_RESTORE ((GENERATE_MIPS16E \
 					|| (TARGET_USE_SAVE_RESTORE && TARGET_SOFT_FLOAT)) \
 				       && mips_abi == ABI_32)
+#define ISA_HAS_LWM_SWM		(TARGET_MICROMIPS && mips_isa_rev < 7)
 
 /* True if we're generating a form of MIPS16 code in which general
    text loads are allowed.  */
