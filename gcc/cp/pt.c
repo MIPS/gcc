38f39fe8cb908026fe67e5acf5e8f45167e292ff
Jason Merrill <jason@redhat.com>
2016-06-20 09:41:37 +0300

    constraint_memo

--- gcc/cp/pt.c
+++ gcc/cp/pt.c
@@ -24695,7 +24695,7 @@ struct concept_spec_hasher : ggc_ptr_hash<concept_spec_entry>
   }
 };
 
-static GTY (()) hash_table<constraint_sat_hasher> *constraint_memos;
+//static GTY (()) hash_table<constraint_sat_hasher> *constraint_memos;
 static GTY (()) hash_table<concept_spec_hasher> *concept_memos;
 
 /* Search for a memoized satisfaction result. Returns one of the
@@ -24705,7 +24705,7 @@ tree
 lookup_constraint_satisfaction (tree, tree)
 {
   return NULL_TREE;
-  /*
+  /* FIXME
   constraint_sat_entry elt = { ci, args, NULL_TREE };
   constraint_sat_entry* found = constraint_memos->find (&elt);
   if (found)
@@ -24878,7 +24878,7 @@ void
 init_constraint_processing (void)
 {
   decl_constraints = hash_table<constr_hasher>::create_ggc(37);
-  constraint_memos = hash_table<constraint_sat_hasher>::create_ggc(37);
+  //  constraint_memos = hash_table<constraint_sat_hasher>::create_ggc(37);
   concept_memos = hash_table<concept_spec_hasher>::create_ggc(37);
   concept_expansions = hash_table<concept_spec_hasher>::create_ggc(37);
   subsumption_table = hash_table<subsumption_hasher>::create_ggc(37);
