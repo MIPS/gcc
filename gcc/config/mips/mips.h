e81b18edff767a075e00ee4a250826711d77223d
Matthew Fortune <matthew.fortune@imgtec.com>
2016-07-06 15:17:29 +0100

    Infer all micromipsr7 options from -mips32r7
    All micromipsr7 options that make it compliant with the published
    spec are enabled automatically with just -mips32r7.
    
    This is a driver specs change so be careful with incremental rebuilds
    as g++ will not get updated unless xg++ is deleted and specifically
    rebuilt.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -883,6 +883,9 @@ struct mips_cpu_info {
 /* A spec that infers the -mdsp setting from an -march argument.  */
 #define BASE_DRIVER_SELF_SPECS \
   MIPS_ISA_NAN2008_SPEC,       \
+  "%{mips32r7: %{!mcode-readable*: -mcode-readable=no} \
+	       %{!mno-grow-frame-downwards: -mgrow-frame-downwards} \
+	       %{!mcheck-zero-division: -mno-check-zero-division}}" \
   "%{!mno-dsp: \
      %{march=24ke*|march=34kc*|march=34kf*|march=34kx*|march=1004k* \
        |march=interaptiv*: -mdsp} \
