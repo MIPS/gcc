850c20328adb6358c487c88f149b6ee3535392a7
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-07-20 19:15:24 +0000

    checkpoint
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@238542 138bc75d-0d04-0410-961f-82ee72b054a4

--- stage-vector/gcc/testsuite/gcc.target/powerpc/vec-extract-2.c
+++ stage-vector/gcc/testsuite/gcc.target/powerpc/vec-extract-2.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
 /* { dg-skip-if "" { powerpc*-*-darwin* } { "*" } { "" } } */
 /* { dg-require-effective-target powerpc_vsx_ok } */
+/* { dg-options "-O2 -mvsx" } */
 
 #include <altivec.h>
 
