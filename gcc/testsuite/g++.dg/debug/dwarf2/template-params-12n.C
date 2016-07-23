f5e8ef2b48137b10fffc1fa32f151e0b8d08826b
Alexandre Oliva <aoliva@redhat.com>
2016-07-23 06:39:00 -0300

    Refresh of debug-tp-pmf-loc.patch

--- gcc/testsuite/g++.dg/debug/dwarf2/template-params-12n.C
+++ gcc/testsuite/g++.dg/debug/dwarf2/template-params-12n.C
@@ -1,6 +1,6 @@
 // { dg-options "-gdwarf-2 -dA" }
 // { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param" 1 } }
-// { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param\[^\n\]*\n\[^#\n\]*# DW_AT_name\n\[^#\n\]*# DW_AT_type\n\[^#\n\]*# DW_AT_const_value" 1 } }
+// { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param\[^\n\]*\n\[^\n\]* DW_AT_name\n\[^\n\]* DW_AT_type\n\[^\n\]* DW_AT_const_value" 1 } }
 #include "template-params-12.H"
 #if __cplusplus > 199711L
 template void t<static_cast<void (S::*)()>(0)>();
