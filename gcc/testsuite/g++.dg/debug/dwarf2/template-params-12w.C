1f7887d536ae98e7aa1f7a95552969f926779b14
Alexandre Oliva <aoliva@redhat.com>
2016-07-23 06:04:35 -0300

    emit loc exprs for C++ non-virtual pmf template value parms

---
+++ gcc/testsuite/g++.dg/debug/dwarf2/template-params-12w.C
@@ -0,0 +1,5 @@
+// { dg-options "-gdwarf-2 -dA" }
+// { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param" 1 } }
+// { dg-final { scan-assembler-times "DIE \\(\[^\n\]*\\) DW_TAG_template_value_param\[^\n\]*\n\[^#\n\]*# DW_AT_name\n\[^#\n\]*# DW_AT_type\n\[^#\n\]*# DW_AT_const_value" 1 } }
+#include "template-params-12.H"
+template void t<&S::v>();
