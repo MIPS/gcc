afb3fb7519ac7ca8a16467b70f7b7dad4b5bbc3c
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-10 23:18:29 +0000

    checkpoint
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@239345 138bc75d-0d04-0410-961f-82ee72b054a4

--- stage-vector/gcc/testsuite/gcc.target/powerpc/vec-init-1.c
+++ stage-vector/gcc/testsuite/gcc.target/powerpc/vec-init-1.c
@@ -6,8 +6,14 @@
 #include <stddef.h>
 #include <altivec.h>
 
-extern void check (vector int a)                    __attribute__((__noinline__));
-extern vector int pack (int a, int b, int c, int d) __attribute__((__noinline__));
+extern void check (vector int a)
+  __attribute__((__noinline__));
+
+extern vector int pack (int a, int b, int c, int d)
+  __attribute__((__noinline__));
+
+extern void pack2 (vector int *p, int a, int b, int c, int d)
+  __attribute__((__noinline__));
 
 void
 check (vector int a)
@@ -26,11 +32,20 @@ pack (int a, int b, int c, int d)
   return (vector int) { a, b, c, d };
 }
 
+void
+pack2 (vector int *p, int a, int b, int c, int d)
+{
+  *p = (vector int) { a, b, c, d };
+}
+
 vector int sv = (vector int) { -1, 2, 0, -3 };
+vector int sv2;
 
 int main (void)
 {
   check (sv);
   check (pack (-1, 2, 0, -3));
+  pack2 (&sv2, -1, 2, 0, -3);
+  check (sv2);
   return 0;
 }
