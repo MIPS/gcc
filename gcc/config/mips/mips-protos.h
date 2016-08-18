0e32073dee77b76abf08ceb9ed85d958c694b3cf
Robert Suchanek <robert.suchanek@imgtec.com>
2016-08-18 23:11:06 +0100

    Optimization: add -mopt-refs.
    Use free saved regs for multiple references to symbols.

--- gcc/config/mips/mips-protos.h
+++ gcc/config/mips/mips-protos.h
@@ -192,6 +192,7 @@ enum mips_split_type {
 
 extern int mips_reg_parm_stack_space (tree, bool);
 extern void mips_adjust_reg_alloc_order (void);
+extern rtl_opt_pass * make_pass_optimize_multi_refs (gcc::context *);
 extern bool mips_string_constant_p (rtx);
 extern bool mips_symbolic_constant_p (rtx, enum mips_symbol_context,
 				      enum mips_symbol_type *);
