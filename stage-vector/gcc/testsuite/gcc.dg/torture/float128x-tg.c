e2781c19dbac4eb52dcbfb124f97e3eaf5a4b845
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-08-23 20:51:37 +0000

    delete branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@239717 138bc75d-0d04-0410-961f-82ee72b054a4

--- stage-vector/gcc/testsuite/gcc.dg/torture/float128x-tg.c
+++
@@ -1,9 +0,0 @@
-/* Test _Float128x type-generic built-in functions.  */
-/* { dg-do run } */
-/* { dg-options "" } */
-/* { dg-add-options float128x } */
-/* { dg-require-effective-target float128x_runtime } */
-
-#define WIDTH 128
-#define EXT 1
-#include "floatn-tg.h"
