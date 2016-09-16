9b3f35db57211dce14a9366a01b6bf9c28e59be3
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-16 23:03:48 +0000

    fix VMRGEW on little endian
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@240208 138bc75d-0d04-0410-961f-82ee72b054a4

--- meissner-slp/gcc/config/rs6000/rs6000.c
+++ meissner-slp/gcc/config/rs6000/rs6000.c
@@ -6859,15 +6859,30 @@ rs6000_expand_vector_init (rtx target, rtx vals)
 	  rtx op2 = force_reg (SFmode, XVECEXP (vals, 0, 2));
 	  rtx op3 = force_reg (SFmode, XVECEXP (vals, 0, 3));
 
-	  emit_insn (gen_vsx_concat_v2sf (dbl_even, op0, op1));
-	  emit_insn (gen_vsx_concat_v2sf (dbl_odd, op2, op3));
-	  emit_insn (gen_vsx_xvcvdpsp (flt_even, dbl_even));
-	  emit_insn (gen_vsx_xvcvdpsp (flt_odd, dbl_odd));
-
-	  if (TARGET_P8_VECTOR)
-	    emit_insn (gen_p8_vmrgew_v4sf_direct (target, flt_even, flt_odd));
+	  if (TARGET_P8_VECTOR && !BYTES_BIG_ENDIAN)
+	    {
+	      emit_insn (gen_vsx_concat_v2sf (dbl_even, op0, op2));
+	      emit_insn (gen_vsx_concat_v2sf (dbl_odd, op1, op3));
+	      emit_insn (gen_vsx_xvcvdpsp (flt_even, dbl_even));
+	      emit_insn (gen_vsx_xvcvdpsp (flt_odd, dbl_odd));
+	      emit_insn (gen_p8_vmrgew_v4sf_direct (target, flt_odd, flt_even));
+	      fprintf (stderr, "vmrgew\n");
+	    }
 	  else
-	    rs6000_expand_extract_even (target, flt_even, flt_odd);
+	    {
+	      emit_insn (gen_vsx_concat_v2sf (dbl_even, op0, op1));
+	      emit_insn (gen_vsx_concat_v2sf (dbl_odd, op2, op3));
+	      emit_insn (gen_vsx_xvcvdpsp (flt_even, dbl_even));
+	      emit_insn (gen_vsx_xvcvdpsp (flt_odd, dbl_odd));
+
+	      if (TARGET_P8_VECTOR)
+		{
+		  emit_insn (gen_p8_vmrgew_v4sf_direct (target, flt_even, flt_odd));
+		  fprintf (stderr, "vmrgew\n");
+		}
+	      else
+		rs6000_expand_extract_even (target, flt_even, flt_odd);
+	    }
 	}
       return;
     }
