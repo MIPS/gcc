8b73e0525ce91eeca284c41ee59ee65a0c8317aa
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-20 18:01:46 +0000

    clone branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@240283 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ minmax/gcc/testsuite/g++.dg/expr/lval4.C
@@ -0,0 +1,10 @@
+// ++i is always an lvalue
+void
+f()
+{
+  bool i = 0;
+  ++i = 3;
+  // { dg-warning "deprecated" "" { target *-*-* } 6 }
+}
+
+
---
+++ minmax/gcc/testsuite/gcc.target/arc/mmac-24.c
@@ -0,0 +1,8 @@
+/* { dg-options "-mmac-24" } */
+/* { dg-do assemble } */
+
+int f (int i)
+{
+  __asm__("mult %1, %1, %1" : "=r"(i) : "r"(i));
+  return i;
+}
