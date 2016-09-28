ae7fbc31ca5e5283a1ffbd1e2cb0bc455f07ffdf
Richard Sandiford <richard.sandiford@linaro.org>
2016-09-28 11:15:55 +0100

    poly_int: legitimate address offsets in ivopts
    This patch makes ivopts record the minimum and maximum offsets
    as poly_int64s rather than HOST_WIDE_INTs.  A later patch will
    make it probe for polynomial offset ranges; at the moment they're
    constants stored as polynomials.

--- gcc/tree-ssa-loop-ivopts.c
+++ gcc/tree-ssa-loop-ivopts.c
@@ -4235,7 +4235,7 @@ struct ainc_cost_data
 };
 
 static comp_cost
-get_address_cost_ainc (HOST_WIDE_INT ainc_step, poly_int64 ainc_offset,
+get_address_cost_ainc (poly_int64 ainc_step, poly_int64 ainc_offset,
 		       machine_mode addr_mode, machine_mode mem_mode,
 		       addr_space_t as, bool speed)
 {
@@ -4308,14 +4308,14 @@ get_address_cost_ainc (HOST_WIDE_INT ainc_step, poly_int64 ainc_offset,
       ainc_cost_data_list[idx] = data;
     }
 
-  HOST_WIDE_INT msize = GET_MODE_SIZE (mem_mode);
-  if (must_eq (ainc_offset, 0) && msize == ainc_step)
+  poly_int64 msize = GET_MODE_SIZE (mem_mode);
+  if (must_eq (ainc_offset, 0) && must_eq (msize, ainc_step))
     return comp_cost (data->costs[AINC_POST_INC], 0);
-  if (must_eq (ainc_offset, 0) && msize == -ainc_step)
+  if (must_eq (ainc_offset, 0) && must_eq (msize, -ainc_step))
     return comp_cost (data->costs[AINC_POST_DEC], 0);
-  if (must_eq (ainc_offset, msize) && msize == ainc_step)
+  if (must_eq (ainc_offset, msize) && must_eq (msize, ainc_step))
     return comp_cost (data->costs[AINC_PRE_INC], 0);
-  if (must_eq (ainc_offset, -msize) && msize == -ainc_step)
+  if (must_eq (ainc_offset, -msize) && must_eq (msize, -ainc_step))
     return comp_cost (data->costs[AINC_PRE_DEC], 0);
 
   return infinite_cost;
@@ -4392,9 +4392,11 @@ get_address_cost (struct ivopts_data *data, struct iv_use *use,
     }
   else
     {
-      if (can_autoinc && ratio == 1 && cst_and_fits_in_hwi (cand->iv->step))
+      poly_int64 ainc_step;
+      if (can_autoinc
+	  && ratio == 1
+	  && poly_tree_p (cand->iv->step, &ainc_step))
 	{
-	  HOST_WIDE_INT ainc_step = int_cst_value (cand->iv->step);
 	  poly_int64 ainc_offset = (aff_inv->offset).force_shwi ();
 
 	  if (stmt_after_increment (data->current_loop, cand, use->stmt))
