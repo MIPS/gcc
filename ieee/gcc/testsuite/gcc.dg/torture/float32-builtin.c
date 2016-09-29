fb5c65b7e15476a9cf0a7b9759d241f1c41c8049
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-09-29 18:54:47 +0000

    Delete branch.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@240630 138bc75d-0d04-0410-961f-82ee72b054a4

--- ieee/gcc/testsuite/gcc.dg/torture/float32-builtin.c
+++
@@ -1,9 +0,0 @@
-/* Test _Float32 built-in functions.  */
-/* { dg-do run } */
-/* { dg-options "" } */
-/* { dg-add-options float32 } */
-/* { dg-require-effective-target float32_runtime } */
-
-#define WIDTH 32
-#define EXT 0
-#include "floatn-builtin.h"
