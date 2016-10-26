5584ff3c2ba2c6ddf99f9cc98d9b304bc13c6b79
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-26 16:12:07 +0000

    Update {,zero_}extendsi<mode>2; Fixup vsx-simode tests.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@241574 138bc75d-0d04-0410-961f-82ee72b054a4

--- intvsx/gcc/testsuite/gcc.target/powerpc/vsx-simode.c
+++ intvsx/gcc/testsuite/gcc.target/powerpc/vsx-simode.c
@@ -7,14 +7,14 @@
 double load_asm_d_constraint (int *p)
 {
   double ret;
-  __asm__ ("xxlor %x0,%x1,%x1\t# load d constraint" : "=d" (ret) : "v" (*p));
+  __asm__ ("xxlor %x0,%x1,%x1\t# load d constraint" : "=d" (ret) : "d" (*p));
   return ret;
 }
 
 void store_asm_d_constraint (int *p, double x)
 {
   int i;
-  __asm__ ("xxlor %x0,%x1,%x1\t# store d constraint" : "=v" (i) : "d" (x));
+  __asm__ ("xxlor %x0,%x1,%x1\t# store d constraint" : "=d" (i) : "d" (x));
   *p = i;
 }
 
