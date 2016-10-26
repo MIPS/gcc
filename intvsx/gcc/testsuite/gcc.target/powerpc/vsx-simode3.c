8648784d9d15fbfcb023adea2156050697c45b41
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-10-26 19:43:26 +0000

    fix typo #2
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@241595 138bc75d-0d04-0410-961f-82ee72b054a4

--- intvsx/gcc/testsuite/gcc.target/powerpc/vsx-simode3.c
+++ intvsx/gcc/testsuite/gcc.target/powerpc/vsx-simode3.c
@@ -4,14 +4,14 @@
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power8" } } */
 /* { dg-options "-mcpu=power8 -O2 -mvsx-small-integer" } */
 
-double load_asm_d_constraint (int *p)
+double load_asm_v_constraint (int *p)
 {
   double ret;
   __asm__ ("xxlor %x0,%x1,%x1\t# load v constraint" : "=d" (ret) : "v" (*p));
   return ret;
 }
 
-void store_asm_d_constraint (int *p, double x)
+void store_asm_v_constraint (int *p, double x)
 {
   int i;
   __asm__ ("xxlor %x0,%x1,%x1\t# store v constraint" : "=v" (i) : "d" (x));
