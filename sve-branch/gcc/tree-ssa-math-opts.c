09b3e9984b480a4992be78a9b85136ac3f47793c
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-11 17:26:20 +0000

    poly_int: symbolic_number
    This patch changes symbol_number::bytepos from a HOST_WIDE_INT
    to a poly_int64.  perform_symbolic_merge can cope with symbolic
    offsets as long as the difference between the two offsets is
    constant.  (This could happen for a constant-sized field that
    occurs at a variable offset, for example.)
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@242244 138bc75d-0d04-0410-961f-82ee72b054a4

--- sve-branch/gcc/tree-ssa-math-opts.c
+++ sve-branch/gcc/tree-ssa-math-opts.c
@@ -1950,7 +1950,7 @@ struct symbolic_number {
   tree type;
   tree base_addr;
   tree offset;
-  HOST_WIDE_INT bytepos;
+  poly_int64 bytepos;
   tree alias_set;
   tree vuse;
   unsigned HOST_WIDE_INT range;
@@ -2177,7 +2177,7 @@ perform_symbolic_merge (gimple *source_stmt1, struct symbolic_number *n1,
   if (rhs1 != rhs2)
     {
       uint64_t inc;
-      HOST_WIDE_INT start_sub, end_sub, end1, end2, end;
+      HOST_WIDE_INT start1, start2, start_sub, end_sub, end1, end2, end;
       struct symbolic_number *toinc_n_ptr, *n_end;
 
       if (!n1->base_addr || !n2->base_addr
@@ -2188,23 +2188,27 @@ perform_symbolic_merge (gimple *source_stmt1, struct symbolic_number *n1,
 	  || (n1->offset && !operand_equal_p (n1->offset, n2->offset, 0)))
 	return NULL;
 
-      if (n1->bytepos < n2->bytepos)
+      start1 = 0;
+      if (!(n2->bytepos - n1->bytepos).is_constant (&start2))
+	return NULL;
+
+      if (start1 < start2)
 	{
 	  n_start = n1;
-	  start_sub = n2->bytepos - n1->bytepos;
+	  start_sub = start2 - start1;
 	  source_stmt = source_stmt1;
 	}
       else
 	{
 	  n_start = n2;
-	  start_sub = n1->bytepos - n2->bytepos;
+	  start_sub = start1 - start2;
 	  source_stmt = source_stmt2;
 	}
 
       /* Find the highest address at which a load is performed and
 	 compute related info.  */
-      end1 = n1->bytepos + (n1->range - 1);
-      end2 = n2->bytepos + (n2->range - 1);
+      end1 = start1 + (n1->range - 1);
+      end2 = start2 + (n2->range - 1);
       if (end1 < end2)
 	{
 	  end = end2;
@@ -2223,7 +2227,7 @@ perform_symbolic_merge (gimple *source_stmt1, struct symbolic_number *n1,
       else
 	toinc_n_ptr = (n_start == n1) ? n2 : n1;
 
-      n->range = end - n_start->bytepos + 1;
+      n->range = end - MIN (start1, start2) + 1;
 
       /* Check that the range of memory covered can be represented by
 	 a symbolic number.  */
