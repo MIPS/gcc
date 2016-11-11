98c5dd370059ef6dcb0b6ef11e989bbf8274afd9
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-11 17:11:27 +0000

    Widening optab cleanup
    widening_optab_handler had the comment:
    
          /* ??? Why does find_widening_optab_handler_and_mode attempt to
             widen things that can't be widened?  E.g. add_optab... */
          if (op > LAST_CONV_OPTAB)
            return CODE_FOR_nothing;
    
    I think it comes from expand_binop using
    find_widening_optab_handler_and_mode for two things: to test whether
    a "normal" optab like add_optab is supported for a standard binary
    operation and to test whether a "convert" optab is supported for a
    widening operation like umul_widen_optab.  In the former case from_mode
    and to_mode must be the same, in the latter from_mode must be narrower
    than to_mode.
    
    For the former case, find_widening_optab_handler_and_mode is only really
    testing the modes that are passed in.  permit_non_widening must be true
    here.
    
    For the latter case, find_widening_optab_handler_and_mode should only
    really consider new from_modes that are wider than the original
    from_mode and narrower than the original to_mode.  Logically
    permit_non_widening should be false, since widening optabs aren't
    supposed to take operands that are the same width as the destination.
    We get away with permit_non_widening being true because no target
    would/should define an .md pattern with matching modes.
    
    But really, it seems better for expand_binop to handle these two
    cases itself rather than pushing them down.  With that change,
    find_widening_optab_handler_and_mode is only ever called with
    permit_non_widening set to false and is only ever called with
    a "proper" convert optab.  We then no longer need widening_optab_handler,
    we can just use convert_optab_handler directly.
    
    The patch also passes the instruction code down to expand_binop_directly.
    This should be more efficient and removes an extra call to
    find_widening_optab_handler_and_mode.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@242165 138bc75d-0d04-0410-961f-82ee72b054a4

--- sve-branch/gcc/expr.c
+++ sve-branch/gcc/expr.c
@@ -8629,7 +8629,7 @@ expand_expr_real_2 (sepops ops, rtx target, machine_mode tmode,
 	{
 	  machine_mode innermode = TYPE_MODE (TREE_TYPE (treeop0));
 	  this_optab = usmul_widen_optab;
-	  if (find_widening_optab_handler (this_optab, mode, innermode, 0)
+	  if (find_widening_optab_handler (this_optab, mode, innermode)
 		!= CODE_FOR_nothing)
 	    {
 	      if (TYPE_UNSIGNED (TREE_TYPE (treeop0)))
@@ -8664,7 +8664,7 @@ expand_expr_real_2 (sepops ops, rtx target, machine_mode tmode,
 
 	  if (TREE_CODE (treeop0) != INTEGER_CST)
 	    {
-	      if (find_widening_optab_handler (this_optab, mode, innermode, 0)
+	      if (find_widening_optab_handler (this_optab, mode, innermode)
 		    != CODE_FOR_nothing)
 		{
 		  expand_operands (treeop0, treeop1, NULL_RTX, &op0, &op1,
@@ -8686,7 +8686,7 @@ expand_expr_real_2 (sepops ops, rtx target, machine_mode tmode,
 					       unsignedp, this_optab);
 		  return REDUCE_BIT_FIELD (temp);
 		}
-	      if (find_widening_optab_handler (other_optab, mode, innermode, 0)
+	      if (find_widening_optab_handler (other_optab, mode, innermode)
 		    != CODE_FOR_nothing
 		  && innermode == word_mode)
 		{
