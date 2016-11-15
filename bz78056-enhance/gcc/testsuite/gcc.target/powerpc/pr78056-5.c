b7e078e75447020c4a25c6eadb53570806a1319c
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-15 15:22:42 +0000

    improve pr78056-5.c
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@242427 138bc75d-0d04-0410-961f-82ee72b054a4

--- bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-5.c
+++ bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-5.c
@@ -1,11 +1,15 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
-/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power6" } } */
+/* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power5" } } */
 /* powerpc_vsx_ok represents power7 */
 /* { dg-require-effective-target powerpc_vsx_ok } */
 /* { dg-skip-if "" { powerpc*-*-aix* } } */
-/* { dg-options "-mcpu=power6" } */
+/* There is an "incompatibility" between power6 flags and power7 which
+   is not resolved with the __attribute__ directive below, so we'll
+   test the __attribute__ directive using a -mcpu=power5 command
+   line.  */
+/* { dg-options "-mcpu=power5" } */
 
-/* Though the command line specifies power7 target, this function is
+/* Though the command line specifies power5 target, this function is
    to support power7.  */
 __attribute__((target("cpu=power7")))
 int
