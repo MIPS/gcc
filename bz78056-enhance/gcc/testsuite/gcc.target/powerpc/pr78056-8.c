07d70776037f86687b6ba4375bf768b2ded401d5
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-11-30 18:32:22 +0000

    another small test refinement
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@243043 138bc75d-0d04-0410-961f-82ee72b054a4

--- bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-8.c
+++ bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-8.c
@@ -1,12 +1,16 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power5" } } */
 /* Is desirable to require-effective-target to be p5_ok, but there's
-   no such effective target.  */
+   no such effective target, so we're just assuming that the effective
+   target is at minimum power5 ok.  */
 /* dfp_hw represents power 6 */
 /* { dg-skip-if "" { dfp_hw } } */
 /* { dg-skip-if "" { powerpc*-*-aix* } } */
-/* { dg-options "-mcpu=power5 -mabi=altivec" } */
+/* { dg-options "-mcpu=power5" } */
 
+/* This test will fail if the effective target is not power 5 ok, but
+   there is no dg directive to "unsupport" this test on such a
+   platform.  */
 /* This test should succeed on both 32- and 64-bit configurations.  */
 /* Though the command line specifies power5 target, this function is
    to support power6. Expect an error message here because this target
