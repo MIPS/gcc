5727612afec4b39095b8d5011673945b08b1803e
kelvin <kelvin@138bc75d-0d04-0410-961f-82ee72b054a4>
2016-12-01 15:44:34 +0000

    tinkering with pr78056-8.c
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ibm@243117 138bc75d-0d04-0410-961f-82ee72b054a4

--- bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-8.c
+++ bz78056-enhance/gcc/testsuite/gcc.target/powerpc/pr78056-8.c
@@ -2,15 +2,20 @@
 /* { dg-skip-if "do not override -mcpu" { powerpc*-*-* } { "-mcpu=*" } { "-mcpu=power5" } } */
 /* Is desirable to require-effective-target to be p5_ok, but there's
    no such effective target, so we're just assuming that the effective
-   target is at minimum power5 ok.  */
+   target is at minimum power5 ok.  This test will fail if the
+   effective target is not power 5 ok, but there is no dg directive to
+   "unsupport" this test on such a platform.  */
 /* dfp_hw represents power 6 */
+
+/* kelvin is just tinkering here.  dejagnu seems to be ignoring my
+ * Power-specific dfp_hw selector.  Maybe it will do better with this
+ * target-independent selector, which appears to be "equivalent",
+ */
+/* { dg-skip-if "" { hard-dfp } } */
 /* { dg-skip-if "" { dfp_hw } } */
 /* { dg-skip-if "" { powerpc*-*-aix* } } */
 /* { dg-options "-mcpu=power5" } */
 
-/* This test will fail if the effective target is not power 5 ok, but
-   there is no dg directive to "unsupport" this test on such a
-   platform.  */
 /* This test should succeed on both 32- and 64-bit configurations.  */
 /* Though the command line specifies power5 target, this function is
    to support power6. Expect an error message here because this target
