138af10045f46cb3f8c9fadaadc380e53c1fef90
Matthew Fortune <matthew.fortune@imgtec.com>
2016-12-22 12:32:27 +0000

    Testsuite: PIC test fixes.

--- gcc/testsuite/gcc.dg/pic-1.c
+++ gcc/testsuite/gcc.dg/pic-1.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { ! { *-*-darwin* hppa*-*-* } } } } */
 /* { dg-require-effective-target fpic } */
 /* { dg-options "-fpic" } */
+/* { dg-options "-fpic -mabicalls -mno-gpopt" { target mips*-*-* } } */
 
 #if __PIC__ != 1
 # error __PIC__ is not 1!
