9effac4d1dd3c4afdfcf16cc946ad2ed59f4253e
Matthew Fortune <matthew.fortune@imgtec.com>
2016-12-22 12:32:27 +0000

    Testsuite: PIC test fixes

--- gcc/testsuite/gcc.dg/pie-4.c
+++ gcc/testsuite/gcc.dg/pie-4.c
@@ -1,5 +1,6 @@
 /* { dg-do compile { target { ! { *-*-darwin* hppa*64*-*-* } } } } */
 /* { dg-options "-fno-PIE" } */
+/* { dg-options "-fno-PIE -mno-abicalls" { target mips*-*-* } } */
 
 #ifdef __PIC__
 # error __PIC__ is defined!
