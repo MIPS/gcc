508fc0e378ce33d337e1f5030a232181a2c792f6
Robert Suchanek <robert.suchanek@imgtec.com>
2017-01-04 16:14:20 +0000

    FIXME [Dejagnu][mips.exp]: tests modified to pass but need revising

--- gcc/testsuite/gcc.target/mips/const-anchor-2.c
+++ gcc/testsuite/gcc.target/mips/const-anchor-2.c
@@ -1,4 +1,5 @@
 /* Derive a constant (0x30001) from another constant.  */
+/* { dg-skip-if "R7: needs revising" { *-*-* } { "-mips32r7 -mmicromips" } { "" } } */
 /* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
 /* { dg-final { scan-assembler-not "0x300000|196608" } } */
 /* { dg-final { scan-assembler "\td?addiu\t\\\$4,\\\$\[0-9\]*,32763" } } */
