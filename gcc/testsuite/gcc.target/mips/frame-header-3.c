9255516c302c4701a6754c2922c849681f70562c
Robert Suchanek <robert.suchanek@imgtec.com>
2017-01-04 16:14:20 +0000

    Dejagnu: tests modified to pass but some may need revising
    Comments to the tests:
    
    gcc/testsuite/gcc.dg/tree-ssa/ivopts-lt-2.c
    
      This looks correct. Addition of branch compare with immediate changed
      the dump slightly.
    
    gcc/testsuite/gcc.target/mips/const-anchor-1.c
    gcc/testsuite/gcc.target/mips/const-anchor-2.c
    gcc/testsuite/gcc.target/mips/const-anchor-3.c
    gcc/testsuite/gcc.target/mips/const-anchor-4.c
    
      Changed to isa_rev<=6. With LI48 at least some tests may not be
      relevant.
    
    gcc/testsuite/gcc.target/mips/ext-7.c
    
      Changed the immediate to fit 12-bit. It should work as before.
    
    gcc/testsuite/gcc.target/mips/frame-header-1.c
    gcc/testsuite/gcc.target/mips/frame-header-2.c
    gcc/testsuite/gcc.target/mips/frame-header-3.c
    
      Updated tests to include SAVE instruction when adjusting the stack
      pointer.
    
    gcc/testsuite/gcc.target/mips/get-fcsr-1.c
    
      Do not run with -O0 as the result is not always placed directly in the
      return register. This will neeed further adjustment for R7 with ABI
      changes.
    
    gcc/testsuite/gcc.target/mips/lsa.c
    gcc/testsuite/gcc.target/mips/mips64-lsa.c
    
      Only for isa_rev<=6. Now indexed scaled load is expected.
    
    gcc/testsuite/gcc.target/mips/mulsize-6.c
    
      Likewise. LSA has now range 0 to 3, hence, it will fail.
    
    gcc/testsuite/gcc.target/mips/r10k-cache-barrier-3.c
    
      Addition of SAVE instruction causes not replacing $sp with
      virtual_incoming_args_rtx.  Not sure what's best: isa_rev<=6 or forbid
      the option (-mr10k-cache-barrier) like for MIPS16.
    
    gcc/testsuite/gcc.target/mips/seq-1.c
    gcc/testsuite/gcc.target/mips/seq-r7-1.c
    
      Genuine code gen change because of `seqi'. Add a new test.
    
    gcc/testsuite/gcc.target/mips/timode-2.c
    
      Fails to assemble: incompatible ABI (o64), unsupported instructions.
      Changed to isa_rev<=6.

--- gcc/testsuite/gcc.target/mips/frame-header-3.c
+++ gcc/testsuite/gcc.target/mips/frame-header-3.c
@@ -7,7 +7,7 @@
 /* { dg-do compile } */
 /* { dg-options "-mframe-header-opt -mabi=32 -mno-abicalls" } */
 /* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
-/* { dg-final { scan-assembler "\taddiu\t\\\$sp,\\\$sp,-24" } } */
+/* { dg-final { scan-assembler "\taddiu\t\\\$sp,\\\$sp,-24|\tsave\t24" } } */
 
 NOMIPS16 void __attribute__((noinline, weak))
 bar (int* a)
