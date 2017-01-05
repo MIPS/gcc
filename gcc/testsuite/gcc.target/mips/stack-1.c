6fb30848ace6ae0e32fd6722aea41ae85e412cf7
Matthew Fortune <matthew.fortune@imgtec.com>
2017-01-05 23:41:59 +0000

    Testsuite: Handle MIPS16e2 options and add tests
    Testsuite: Disable tests at -O0 due to introducing a frame
    
    SAVE/RESTORE end up introducing a frame owing to saving more data
    than strictly necessary
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/mips/iamr2.c: New test.
    	* gcc.target/mips/mips16e2-cmov.c: New test.
    	* gcc.target/mips/mips16e2.c: New test.
    	* gcc.target/mips/r10k-cache-barrier-9.c: Skip test for -O0.
    	* gcc.target/mips/stack-1.c: Likewise.
    	* gcc.target/mips/mips.exp (mips-dg-init): Handle recognize
    	mips16e2 target.
    	(mips-dg-options): Add dependencies for mips16e2.

--- gcc/testsuite/gcc.target/mips/stack-1.c
+++ gcc/testsuite/gcc.target/mips/stack-1.c
@@ -1,3 +1,4 @@
+/* { dg-skip-if "code quality test" { *-*-* } { "-O0" } { "" } } */
 /* { dg-final { scan-assembler "\td?addiu\t(\\\$sp,)?\\\$sp,\[1-9\]" } } */
 /* { dg-final { scan-assembler "\tlw\t" } } */
 /* { dg-final { scan-assembler-not "\td?addiu\t(\\\$sp,)?\\\$sp,\[1-9\].*\tlw\t" } } */
