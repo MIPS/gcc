840bd2c7b05a50bc3fc7bfedc1b9e4430136fb89
jakub <jakub@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-02-16 09:00:57 +0000

    svn merge -r244559:245502 svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-6-branch
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/redhat@245503 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-6-branch/gcc/config/rs6000/rs6000.c
+++ gcc-6-branch/gcc/config/rs6000/rs6000.c
@@ -38605,6 +38605,7 @@ rtx_is_swappable_p (rtx op, unsigned int *special)
 	  case UNSPEC_VSX_CVSPDPN:
 	    return 0;
 	  case UNSPEC_VSPLT_DIRECT:
+	  case UNSPEC_VSX_XXSPLTD:
 	    *special = SH_SPLAT;
 	    return 1;
 	  case UNSPEC_REDUC_PLUS:
