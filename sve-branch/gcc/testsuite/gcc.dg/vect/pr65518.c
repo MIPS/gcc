81b73c90af24710ccb89db19c65f13100e607b1b
rsandifo <rsandifo@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-03-17 18:09:56 +0000

    Allow single-element interleaving for non-power-of-2 strides
    This has two related effects:
    
    (1) It makes the handling of large power-of-2 strides equivalent
        to the handling of non-power-of-2-strides (requires both the
        vect_analyze_group_access_1 and get_load_store_type changes).
    
    (2) Allows LD3 to be used for isolated a[i * 3] accesses,
        in a similar way to the current a[i * 2] and a[i * 4]
        for LD2 and LD4 respectively.
    
    We can now vectorize pr65518.c using elementwise accesses,
    just as we would have done if the array size had been 17
    rather than 16.  Whether that's a good idea is up to the
    cost model (again like it was previously for non-power-of-2
    array sizes).
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@246238 138bc75d-0d04-0410-961f-82ee72b054a4

--- sve-branch/gcc/testsuite/gcc.dg/vect/pr65518.c
+++ sve-branch/gcc/testsuite/gcc.dg/vect/pr65518.c
@@ -26,18 +26,12 @@ int main ()
   for (i = 0; i < 8; ++i)
     for (j = 0; j < 16; ++j)
       {
-	g[i].g[j] = n++;
-	__asm__ volatile ("");
+        g[i].g[j] = n++;
+        __asm__ volatile ("");
       }
   if (addfst (g, 8) != 456)
     abort ();
   return 0;
 }
 
-/* We don't want to vectorize the single-element interleaving in the way
-   we currently do that (without ignoring not needed vectors in the
-   gap between gptr[0].g[0] and gptr[1].g[0]), because that's very
-   sub-optimal and causes memory explosion (even though the cost model
-   should reject that in the end).  */
-
-/* { dg-final { scan-tree-dump-times "vectorized 0 loops in function" 2 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops in function" 1 "vect" } } */
