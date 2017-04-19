5ad3498a70512431e47858d528cfcb6462cd1b4b
Matthew Fortune <matthew.fortune@imgtec.com>
2017-04-19 15:10:49 +0100

    Add support for -mcrc and -mginv options.
    	* config/mips/mips.h (ASM_SPEC): Pass through -mcrc, -mno-crc,
    	-mginv and -mno-ginv to the assembler.
    	* config/mips/mips.opt (-mcrc): New option.
    	(-mginv): Likewise.
    	* doc/invoke.text (-mcrc): Document.
    	(-mginv): Likewise.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -1356,6 +1356,8 @@ struct mips_cpu_info {
 %{meva} %{mno-eva} \
 %{mvirt} %{mno-virt} \
 %{mxpa} %{mno-xpa} \
+%{mcrc} %{mno-crc} \
+%{mginv} %{mno-ginv} \
 %{mmsa} %{mno-msa} \
 %{msmartmips} %{mno-smartmips} \
 %{mmt} %{mno-mt} \
