b483285e73819564fe96a48798eb81a2d773d906
Robert Suchanek <robert.suchanek@imgtec.com>
2017-04-24 13:20:54 +0100

    Add MUL[4X4].

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -1038,6 +1038,9 @@ struct mips_cpu_info {
 				      && mips_isa_rev <= 5))		\
 				 && !TARGET_MIPS16)
 
+/* ISA has a 16-bit two-operand multiplication instruction.  */
+#define ISA_HAS_MUL4X4		(TARGET_MICROMIPS_R7 && ISA_HAS_XLP)
+
 /* ISA has a three-operand multiplication instruction.  */
 #define ISA_HAS_DMUL3		(TARGET_64BIT				\
 				 && TARGET_OCTEON			\
