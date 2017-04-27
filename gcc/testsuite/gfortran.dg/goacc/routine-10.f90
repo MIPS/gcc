666279e043702abf61bb91508c4d544551e09eed
cesar <cesar@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-04-27 21:52:40 +0000

    Fix an ICE involving OpenACC routines inside broken fortran functions.
    	gcc/fortran/
    	* openmp.c (gfc_match_oacc_routine): Don't match OpenACC routines
    	when the current function failed to parse due to a syntax error.
    
    	gcc/testsuite/
    	* gfortran.dg/goacc/routine-10.f90: New test.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@247353 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/goacc/routine-10.f90
@@ -0,0 +1,8 @@
+! Ensure that GFortran doesn't ICE with incomplete function
+! definitions.
+
+! { dg-do compile }
+
+integer function f1 ! { dg-error "Expected formal argument list in function definition" }
+  !$acc routine ! { dg-error "Unclassifiable OpenACC directive" }
+end function f1 ! { dg-error "Expecting END PROGRAM statement" }
