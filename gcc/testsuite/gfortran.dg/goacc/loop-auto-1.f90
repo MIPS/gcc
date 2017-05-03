3734b96902f61c7459af4c3765dae0790d368808
cesar <cesar@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-03 21:35:19 +0000

    Don't mark OpenACC auto loops as independent inside acc parallel regions.
    	gcc/
    	* omp-low.c (lower_oacc_head_mark):
    
    	gcc/testsuite/
    	* c-c++-common/goacc/loop-auto-1.c: Adjust test case to conform to
    	the new behavior of the auto clause in OpenACC 2.5.
    	* c-c++-common/goacc/loop-auto-2.c: Likewise.
    	* gcc.dg/goacc/loop-processing-1.c: Likewise.
    	* c-c++-common/goacc/loop-auto-3.c: New test.
    	* gfortran.dg/goacc/loop-auto-1.f90: New test.
    
    	libgomp/
    	* testsuite/libgomp.oacc-c-c++-common/loop-auto-1.c: Adjust test case
    	to conform to the new behavior of the auto clause in OpenACC 2.5.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@247569 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gfortran.dg/goacc/loop-auto-1.f90
@@ -0,0 +1,91 @@
+! Ensure that the auto clause falls back to seq parallelism when the
+!  OpenACC loop is not explicitly independent.
+
+! { dg-compile }
+! { dg-additional-options "-fopt-info-note-omp" }
+
+program test
+  implicit none
+  integer, parameter :: n = 100
+  integer i, j, k, l
+  
+  !$acc parallel loop auto ! { dg-message "Detected parallelism <acc loop seq>" }
+  do i = 1, n
+     !$acc loop auto independent ! { dg-message "Detected parallelism <acc loop gang>" }
+     do j = 1, n
+        !$acc loop worker vector ! { dg-message "Detected parallelism <acc loop worker vector>" }
+        do k = 1, n
+        end do
+     end do
+  end do
+ 
+  !$acc parallel loop auto independent ! { dg-message "Detected parallelism <acc loop gang worker>" }
+  do i = 1, n
+     !$acc loop auto ! { dg-message "Detected parallelism <acc loop seq>" }
+     do j = 1, n
+        !$acc loop auto ! { dg-message "Detected parallelism <acc loop seq>" }
+        do k = 1, n
+           !$acc loop auto independent ! { dg-message "Detected parallelism <acc loop vector>" }
+           do l = 1, n
+           end do
+        end do
+     end do
+  end do
+
+  !$acc parallel loop gang ! { dg-message "Detected parallelism <acc loop gang>" }
+  do i = 1, n
+     !$acc loop worker ! { dg-message "Detected parallelism <acc loop worker>" }
+     do j = 1, n
+        !$acc loop vector ! { dg-message "Detected parallelism <acc loop vector>" }
+        do k = 1, n
+           !$acc loop auto independent ! { dg-message "Detected parallelism <acc loop seq>" }
+           do l = 1, n
+           end do
+           !$acc loop auto ! { dg-message "Detected parallelism <acc loop seq>" }
+           do l = 1, n
+           end do
+        end do
+     end do
+  end do
+  
+
+  !$acc parallel loop ! { dg-message "Detected parallelism <acc loop seq>" }
+  do i = 1, n
+     !$acc loop gang worker ! { dg-message "Detected parallelism <acc loop gang worker>" }
+     do j = 1, n
+        !$acc loop auto ! { dg-message "Detected parallelism <acc loop seq>" }
+	do k = 1, n
+          !$acc loop vector ! { dg-message "Detected parallelism <acc loop vector>" }
+          do l = 1, n
+          end do
+       end do
+       !$acc loop auto independent ! { dg-message "Detected parallelism <acc loop vector>" }
+       do l = 1, n
+       end do
+    end do
+    !$acc loop worker ! { dg-message "Detected parallelism <acc loop worker>" }
+    do j = 1, n
+       !$acc loop vector ! { dg-message "Detected parallelism <acc loop vector>" }
+       do k = 1, n
+       end do
+    end do
+  end do
+
+  !$acc parallel loop ! { dg-message "Detected parallelism <acc loop gang>" }
+  do i = 1, n
+     !$acc loop ! { dg-message "Detected parallelism <acc loop worker>" }
+     do j = 1, n
+        !$acc loop ! { dg-message "Detected parallelism <acc loop seq>" }
+        do k = 1, n
+           !$acc loop ! { dg-message "Detected parallelism <acc loop vector>" }
+           do l = 1, n
+           end do
+        end do
+     end do
+  end do
+end program test
+
+! { dg-warning "insufficient partitioning available to parallelize loop" "" { target *-*-* } 41 }
+! { dg-warning "insufficient partitioning available to parallelize loop" "" { target *-*-* } 52 }
+! { dg-warning "insufficient partitioning available to parallelize loop" "" { target *-*-* } 78 }
+
