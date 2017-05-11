8a571f4acf96e46269821174aad4493a97d133cf
Richard Sandiford <richard.sandiford@linaro.org>
2017-05-11 13:52:02 +0100

    poly_int: vect_no_alias_p

--- gcc/tree-vect-data-refs.c
+++ gcc/tree-vect-data-refs.c
@@ -3043,46 +3043,33 @@ vect_vfa_segment_size (struct data_reference *dr, tree length_factor)
    and SEGMENT_LENGTH_B are the memory lengths accessed by A and B
    respectively.  */
 
-static bool
-vect_no_alias_p (struct data_reference *a, struct data_reference *b,
-                 tree segment_length_a, tree segment_length_b)
+static int
+vect_compile_time_alias (struct data_reference *a, struct data_reference *b,
+			 poly_uint64 segment_length_a,
+			 poly_uint64 segment_length_b)
 {
-  gcc_assert (TREE_CODE (DR_INIT (a)) == INTEGER_CST
-	      && TREE_CODE (DR_INIT (b)) == INTEGER_CST);
-  if (tree_int_cst_equal (DR_INIT (a), DR_INIT (b)))
-    return false;
+  poly_offset_int offset_a, offset_b;
+  if (!poly_tree_p (DR_INIT (a), &offset_a)
+      || !poly_tree_p (DR_INIT (b), &offset_b))
+    return -1;
 
-  tree seg_a_min = DR_INIT (a);
-  tree seg_a_max = fold_build2 (PLUS_EXPR, TREE_TYPE (seg_a_min),
-				seg_a_min, segment_length_a);
   /* For negative step, we need to adjust address range by TYPE_SIZE_UNIT
      bytes, e.g., int a[3] -> a[1] range is [a+4, a+16) instead of
      [a, a+12) */
   if (tree_int_cst_compare (DR_STEP (a), size_zero_node) < 0)
-    {
-      tree unit_size = TYPE_SIZE_UNIT (TREE_TYPE (DR_REF (a)));
-      seg_a_min = fold_build2 (PLUS_EXPR, TREE_TYPE (seg_a_max),
-			       seg_a_max, unit_size);
-      seg_a_max = fold_build2 (PLUS_EXPR, TREE_TYPE (DR_INIT (a)),
-			       DR_INIT (a), unit_size);
-    }
-  tree seg_b_min = DR_INIT (b);
-  tree seg_b_max = fold_build2 (PLUS_EXPR, TREE_TYPE (seg_b_min),
-				seg_b_min, segment_length_b);
+    offset_a += vect_get_dr_size (a);
   if (tree_int_cst_compare (DR_STEP (b), size_zero_node) < 0)
-    {
-      tree unit_size = TYPE_SIZE_UNIT (TREE_TYPE (DR_REF (b)));
-      seg_b_min = fold_build2 (PLUS_EXPR, TREE_TYPE (seg_b_max),
-			       seg_b_max, unit_size);
-      seg_b_max = fold_build2 (PLUS_EXPR, TREE_TYPE (DR_INIT (b)),
-			       DR_INIT (b), unit_size);
-    }
+    offset_b += vect_get_dr_size (b);
 
-  if (tree_int_cst_le (seg_a_max, seg_b_min)
-      || tree_int_cst_le (seg_b_max, seg_a_min))
-    return true;
+  if (ranges_must_overlap_p (offset_a, segment_length_a,
+			     offset_b, segment_length_b))
+    return 1;
 
-  return false;
+  if (!ranges_may_overlap_p (offset_a, segment_length_a,
+			     offset_b, segment_length_b))
+    return 0;
+
+  return -1;
 }
 
 /* Function vect_prune_runtime_alias_test_list.
@@ -3183,20 +3170,26 @@ vect_prune_runtime_alias_test_list (loop_vec_info loop_vinfo)
 	comp_res = compare_tree (DR_OFFSET (dr_a), DR_OFFSET (dr_b));
 
       /* Alias is known at compilation time.  */
+      poly_uint64 const_segment_length_a, const_segment_length_b;
       if (comp_res == 0
 	  && TREE_CODE (DR_STEP (dr_a)) == INTEGER_CST
 	  && TREE_CODE (DR_STEP (dr_b)) == INTEGER_CST
-	  && TREE_CODE (segment_length_a) == INTEGER_CST
-	  && TREE_CODE (segment_length_b) == INTEGER_CST)
+	  && poly_tree_p (segment_length_a, &const_segment_length_a)
+	  && poly_tree_p (segment_length_b, &const_segment_length_b))
 	{
-	  if (vect_no_alias_p (dr_a, dr_b, segment_length_a, segment_length_b))
+	  int res = vect_compile_time_alias (dr_a, dr_b,
+					     const_segment_length_a,
+					     const_segment_length_b);
+	  if (res == 0)
 	    continue;
 
-	  if (dump_enabled_p ())
-	    dump_printf_loc (MSG_NOTE, vect_location,
-			     "not vectorized: compilation time alias.\n");
-
-	  return false;
+	  if (res == 1)
+	    {
+	      if (dump_enabled_p ())
+		dump_printf_loc (MSG_NOTE, vect_location,
+				 "not vectorized: compilation time alias.\n");
+	      return false;
+	    }
 	}
 
       dr_with_seg_len_pair_t dr_with_seg_len_pair
