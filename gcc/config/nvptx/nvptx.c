ecf20445ebd75ec462ff92500a61e1226634036a
Thomas Schwinge <thomas@codesourcery.com>
2017-05-12 09:20:35 +0000

    [PR middle-end/69921] Use "oacc kernels parallelized" attribute for parallelized OpenACC kernels
    	gcc/
    	PR middle-end/69921
    	* tree-parloops.c (create_parallel_loop): Set "oacc kernels
    	parallelized" attribute for parallelized OpenACC kernels.
    	* omp-offload.c (execute_oacc_device_lower): Use it.
    	* config/nvptx/nvptx.c (nvptx_goacc_validate_dims): Likewise.
    	gcc/testsuite/
    	* c-c++-common/goacc/classify-kernels-unparallelized.c: Adjust.
    	* c-c++-common/goacc/classify-kernels.c: Likewise.
    	* c-c++-common/goacc/kernels-acc-loop-reduction.c: Likewise.
    	* c-c++-common/goacc/kernels-acc-loop-smaller-equal.c: Likewise.
    	* c-c++-common/goacc/kernels-counter-vars-function-scope.c:
    	Likewise.
    	* c-c++-common/goacc/kernels-double-reduction-n.c: Likewise.
    	* c-c++-common/goacc/kernels-double-reduction.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-2-acc-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-2.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-3-acc-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-3.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-acc-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-data-2.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-data-enter-exit-2.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-data-enter-exit.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-data-update.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-data.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-g.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-mod-not-zero.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-n-acc-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-n.c: Likewise.
    	* c-c++-common/goacc/kernels-loop-nest.c: Likewise.
    	* c-c++-common/goacc/kernels-loop.c: Likewise.
    	* c-c++-common/goacc/kernels-one-counter-var.c: Likewise.
    	* c-c++-common/goacc/kernels-parallel-loop-data-enter-exit.c:
    	Likewise.
    	* c-c++-common/goacc/kernels-reduction.c: Likewise.
    	* gfortran.dg/goacc/classify-kernels-unparallelized.f95: Likewise.
    	* gfortran.dg/goacc/classify-kernels.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit-2.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-enter-exit.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data-update.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-data.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop-n.f95: Likewise.
    	* gfortran.dg/goacc/kernels-loop.f95: Likewise.
    	* gfortran.dg/goacc/kernels-parallel-loop-data-enter-exit.f95:
    	Likewise.
    
    trunk r247957
    
    (cherry picked from gomp-4_0-branch r247958)

--- gcc/config/nvptx/nvptx.c
+++ gcc/config/nvptx/nvptx.c
@@ -4800,28 +4800,17 @@ nvptx_goacc_validate_dims (tree decl, int dims[], int fn_level)
 	 higher, as otherwise we're not expecting any parallelization to
 	 happen.  */
       if (optimize >= 2
-	  && lookup_attribute ("oacc kernels", DECL_ATTRIBUTES (decl)))
+	  && lookup_attribute ("oacc kernels", DECL_ATTRIBUTES (decl))
+	  && !lookup_attribute ("oacc kernels parallelized",
+				DECL_ATTRIBUTES (decl)))
 	{
-	  bool avoid_offloading_p = true;
-	  for (unsigned ix = 0; ix != GOMP_DIM_MAX; ix++)
-	    {
-	      if (dims[ix] == 0 || dims[ix] > 1)
-		{
-		  avoid_offloading_p = false;
-		  break;
-		}
-	    }
-	  if (avoid_offloading_p)
-	    {
-	      warning_at (DECL_SOURCE_LOCATION (decl), 0,
-			  "OpenACC kernels construct will be executed"
-			  " sequentially; will by default avoid offloading to"
-			  " prevent data copy penalty");
-	      DECL_ATTRIBUTES (decl)
-		= tree_cons (get_identifier ("omp avoid offloading"),
-			     NULL_TREE, DECL_ATTRIBUTES (decl));
-
-	    }
+	  warning_at (DECL_SOURCE_LOCATION (decl), 0,
+		      "OpenACC kernels construct will be executed"
+		      " sequentially; will by default avoid offloading to"
+		      " prevent data copy penalty");
+	  DECL_ATTRIBUTES (decl)
+	    = tree_cons (get_identifier ("omp avoid offloading"),
+			 NULL_TREE, DECL_ATTRIBUTES (decl));
 	}
     }
 
