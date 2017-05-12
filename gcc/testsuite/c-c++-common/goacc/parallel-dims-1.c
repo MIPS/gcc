1c87e0644c7e9f84f62c4b1a4f5bca35fba80b36
tschwinge <tschwinge@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-05-12 09:34:37 +0000

    OpenACC C front end maintenance: c_parser_oacc_single_int_clause
    	gcc/c/
    	* c-parser.c (c_parser_omp_clause_num_gangs)
    	(c_parser_omp_clause_num_workers)
    	(c_parser_omp_clause_vector_length): Merge functions into...
    	(c_parser_oacc_single_int_clause): ... this new function.  Adjust
    	all users.
    	gcc/testsuite/
    	* c-c++-common/goacc/parallel-dims-1.c: New file.
    	* c-c++-common/goacc/parallel-dims-2.c: Likewise.
    
    trunk r247960
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gomp-4_0-branch@247961 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/c-c++-common/goacc/parallel-dims-1.c
@@ -0,0 +1,9 @@
+/* Valid use of OpenACC parallelism dimensions clauses: num_gangs, num_workers,
+   vector_length.  */
+
+void f(int i)
+{
+#pragma acc parallel /* { dg-bogus "region is (gang|worker|vector) partitioned" "" { xfail *-*-* } } */ \
+  num_gangs(i) num_workers(i) vector_length(i)
+  ;
+}
