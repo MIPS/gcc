96e17000dd3aa833ce94d599e788069ba5e0569a
Richard Sandiford <richard.sandiford@linaro.org>
2017-05-14 11:57:54 +0100

    vect_align_stack_vars

--- gcc/testsuite/gcc.dg/vect/vect-93.c
+++ gcc/testsuite/gcc.dg/vect/vect-93.c
@@ -86,5 +86,5 @@ int main (void)
 
 /* in main: */
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target { vect_no_align && { ! vect_hw_misalign } } } } } */
-/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" { xfail { ! vect_unaligned_possible } } } } */
+/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 1 "vect" { xfail { ! { vect_unaligned_possible && vect_align_stack_vars } } } } } */
 
