fee323623af2a0e157c353b43e00d3f86a66e3c4
Robert Suchanek <robert.suchanek@imgtec.com>
2017-06-02 14:59:37 +0100

    Tests: require `fpic' effective target

--- gcc/testsuite/gcc.dg/pic-2.c
+++ gcc/testsuite/gcc.dg/pic-2.c
@@ -1,4 +1,4 @@
-/* { dg-do compile { target { ! { mips*-*-* } } } } */
+/* { dg-do compile { target { { ! { mips*-*-* } }  && { ! { nanomips*-*-* } } } } } */
 /* { dg-options "-fPIC" } */
 
 #if __PIC__ != 2
