662d928ce0f117edc40c7e08e71a6a15cf447e05
Matthew Fortune <matthew.fortune@imgtec.com>
2017-06-14 17:20:25 +0100

    Release branch only commit: Hazard barrier return support
    This patch is a slightly modified version of a patch posted to the GCC
    mailing list. It is not a backport of the final patch.
    
    2017-04-25  Prachi Godbole  <prachi.godbole@imgtec.com>
    
    gcc/
    	* config/mips/mips.h (machine_function): New variable
    	use_hazard_barrier_return_p.
    	* config/mips/mips.md (UNSPEC_JRHB): New unspec.
    	(mips_hb_return_internal): New insn pattern.
    	* config/mips/mips.c (mips_attribute_table): Add attribute
    	use_hazard_barrier_return.
    	(mips_use_hazard_barrier_return_p): New static function.
    	(mips_function_attr_inlinable_p): Likewise.
    	(mips_compute_frame_info): Set use_hazard_barrier_return_p.  Emit error
    	for unsupported architecture choice.
    	(mips_function_ok_for_sibcall, mips_can_use_return_insn): Return false
    	for use_hazard_barrier_return.
    	(mips_expand_epilogue): Emit hazard barrier return.
    	* doc/extend.texi: Document use_hazard_barrier_return.
    
    gcc/testsuite/
    	* gcc.target/mips/hazard-barrier-return-attribute.c: New test.

--- gcc/config/mips/mips.h
+++ gcc/config/mips/mips.h
@@ -3476,6 +3476,9 @@ struct GTY(())  machine_function {
   /* True if GCC stored callee saved registers in the frame header.  */
   bool use_frame_header_for_callee_saved_regs;
 
+  /* True if the function should generate hazard barrier return.  */
+  bool use_hazard_barrier_return_p;
+
   /* True if attribute common_epilogue is defined for this function.  */
   bool use_common_epilogue_p;
 
