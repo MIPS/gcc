a80b92ae03f5fa7e3b9ef51c814bffba9f553eb0
Robert Suchanek <robert.suchanek@imgtec.com>
2017-06-21 10:33:25 +0100

    Add a new genopt-nanomips.sh script to generate nanomips-cpus.def.
    The original script was deriving new (mostly shorter) names.  This
    conflicts with nanoMIPS as we do not want to create e.g. `r32r6' for
    -march=32r6.
    
    Revert the initial changes as they are no longer needed.

--- gcc/config/mips/genopt-nanomips.sh
+++ gcc/config/mips/genopt-nanomips.sh
@@ -20,9 +20,9 @@
 
 cat <<EOF
 ; -*- buffer-read-only: t -*-
-; Generated automatically by genopt.sh from mips-cpus.def.
+; Generated automatically by genopt-nanomips.sh from nanomips-cpus.def.
 
-; Copyright (C) 2011-2016 Free Software Foundation, Inc.
+; Copyright (C) 2017 Free Software Foundation, Inc.
 ;
 ; This file is part of GCC.
 ;
@@ -46,10 +46,6 @@ Known MIPS CPUs (for use with the -march= and -mtune= options):
 
 Enum
 Name(mips_mips_opt_value) Type(int)
-Known MIPS ISA levels (for use with the -mips option):
-
-EnumValue
-Enum(mips_arch_opt_value) String(from-abi) Value(MIPS_ARCH_OPTION_FROM_ABI)
 
 EnumValue
 Enum(mips_arch_opt_value) String(native) Value(MIPS_ARCH_OPTION_NATIVE) DriverOnly
@@ -63,61 +59,16 @@ BEGIN {
 
 # Write an entry for a single string accepted as a -march= argument.
 
-function write_one_arch_value(name, value, flags)
+function write_one_arch_value(name, value)
 {
     print "EnumValue"
-    print "Enum(mips_arch_opt_value) String(" name ") Value(" value ")" flags
+    print "Enum(mips_arch_opt_value) String(" name ") Value(" value ")"
     print ""
-    if (name ~ "^mips") {
-	sub("^mips", "", name)
-	print "EnumValue"
-	print "Enum(mips_mips_opt_value) String(" name ") Value(" value ")"
-	print ""
-    }
-}
-
-# The logic for matching CPU name variants should be the same as in GAS.
-
-# Write an entry for a single string accepted as a -march= argument,
-# plus any variant with a final "000" replaced by "k".
-
-function write_arch_value_maybe_k(name, value, flags)
-{
-    write_one_arch_value(name, value, flags)
-    if (name ~ "000$") {
-	sub("000$", "k", name)
-	write_one_arch_value(name, value, "")
-    }
-}
-
-# Write all the entries for a -march= argument.  In addition to
-# replacement of a final "000" with "k", an argument starting with
-# "vr", "rm" or "r" followed by a number, or just a plain number,
-# matches a plain number or "r" followed by a plain number.
-
-function write_all_arch_values(name, value)
-{
-    write_arch_value_maybe_k(name, value, " Canonical")
-    cname = name
-    if (cname ~ "^vr") {
-	sub("^vr", "", cname)
-    } else if (cname ~ "^rm") {
-	sub("^rm", "", cname)
-    } else if (cname ~ "^r") {
-	sub("^r", "", cname)
-    }
-    if (cname ~ "^[0-9]") {
-	if (cname != name)
-	    write_arch_value_maybe_k(cname, value, "")
-	rname = "r" cname
-	if (rname != name)
-	    write_arch_value_maybe_k(rname, value, "")
-    }
 }
 
 /^MIPS_CPU/ {
     name = $2
     gsub("\"", "", name)
-    write_all_arch_values(name, value)
+    write_one_arch_value(name, value)
     value++
-}' $1/mips-cpus.def
+}' $1/nanomips-cpus.def
