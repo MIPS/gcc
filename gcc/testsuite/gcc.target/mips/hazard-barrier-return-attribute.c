e7b234ec255d277e75c19acfdb532ab193630b4a
Matthew Fortune <matthew.fortune@imgtec.com>
2017-07-05 11:09:01 +0100

    Update broken hazard barrier return test
    gcc/testsuite/
    	* mips/hazard-barrier-return-attribute.c: Remove duplicate
    	test code.

--- gcc/testsuite/gcc.target/mips/hazard-barrier-return-attribute.c
+++ gcc/testsuite/gcc.target/mips/hazard-barrier-return-attribute.c
@@ -9,18 +9,4 @@ foo ()
 {
   return bar ();
 }
-/* { dg-final { scan-assembler "\tjr.hb\t$31\n" } } */
-/* { dg-final { scan-assembler "\tnop\n" } } */
-/* Test attribute for clearing hazards while returning.  */
-/* { dg-do compile } */
-/* { dg-options "isa_rev>=2 -mno-mips16" } */
-
-extern int bar ();
-
-int __attribute__ ((use_hazard_barrier_return))
-foo ()
-{
-  return bar ();
-}
-/* { dg-final { scan-assembler "\tjr.hb\t$31\n" } } */
-/* { dg-final { scan-assembler "\tnop\n" } } */
+/* { dg-final { scan-assembler "(\tjr.hb\t\\\$31\n\tnop\\n|\tjrc.hb\t)" } } */
