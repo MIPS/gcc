RTEMS/gnu-mirror-gcc

Use "oacc kernels" attribute for OpenACC kernels
	gcc/
	* omp-low.c (expand_omp_target) <GF_OMP_TARGET_KIND_OACC_KERNELS>:
	Set "oacc kernels" attribute.
	(set_oacc_fn_attrib): Remove is_kernel formal parameter.  Adjust
	all users.
	(oacc_fn_attrib_kernels_p): Remove function.
	(oacc_validate_dims, execute_oacc_device_lower): Look for "oacc
	kernels" attribute instead of calling oacc_fn_attrib_kernels_p.
	* tree-ssa-loop.c (gate_oacc_kernels): Likewise.
	* config/nvptx/nvptx.c (nvptx_goacc_validate_dims): Likewise.
	* tree-parloops.c (create_parallel_loop): If oacc_kernels_p,
	assert "oacc kernels" attribute is set.
	gcc/testsuite/
	* c-c++-common/goacc/oaccdevlow-kernels.c: Adjust.
	* c-c++-common/goacc/oaccdevlow-parallel.c: Likewise.
	* c-c++-common/goacc/oaccdevlow-routine.c: Likewise.
	* gfortran.dg/goacc/oaccdevlow-kernels.f95: Likewise.
	* gfortran.dg/goacc/oaccdevlow-parallel.f95: Likewise.
	* gfortran.dg/goacc/oaccdevlow-routine.f95: Likewise.

trunk r247955

From-SVN: r247956

gcc/tree-parloops.c
@@ -2041,10 +2041,13 @@ create_parallel_loop (struct loop *loop, tree loop_fn, tree data,
   /* Prepare the GIMPLE_OMP_PARALLEL statement.  */
   if (oacc_kernels_p)
     {
+      gcc_checking_assert (lookup_attribute ("oacc kernels",
+					     DECL_ATTRIBUTES (cfun->decl)));
+
       tree clause = build_omp_clause (loc, OMP_CLAUSE_NUM_GANGS);
       OMP_CLAUSE_NUM_GANGS_EXPR (clause)
 	= build_int_cst (integer_type_node, n_threads);
-      set_oacc_fn_attrib (cfun->decl, clause, true, NULL);
+      set_oacc_fn_attrib (cfun->decl, clause, NULL);
     }
   else
     {

MIPS/gcc

	* omp-expand.c (expand_omp_target) <GF_OMP_TARGET_KIND_OACC_KERNELS>:
	* omp-general.c (oacc_set_fn_attrib): Remove is_kernel formal
	parameter.  Adjust all users.
	* omp-offload.c (execute_oacc_device_lower): Look for "oacc
(cherry picked from gomp-4_0-branch r247956)

gcc/tree-parloops.c
@@ -2043,10 +2043,13 @@ create_parallel_loop (struct loop *loop, tree loop_fn, tree data,
   /* Prepare the GIMPLE_OMP_PARALLEL statement.  */
   if (oacc_kernels_p)
     {
+      gcc_checking_assert (lookup_attribute ("oacc kernels",
+					     DECL_ATTRIBUTES (cfun->decl)));
+
       tree clause = build_omp_clause (loc, OMP_CLAUSE_NUM_GANGS);
       OMP_CLAUSE_NUM_GANGS_EXPR (clause)
 	= build_int_cst (integer_type_node, n_threads);
-      oacc_set_fn_attrib (cfun->decl, clause, true, NULL);
+      oacc_set_fn_attrib (cfun->decl, clause, NULL);
     }
   else
     {