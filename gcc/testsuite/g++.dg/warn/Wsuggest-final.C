d23d4fa88f2fd8818bd0767afe13a3994d6ebe93
David Malcolm <dmalcolm@redhat.com>
2017-07-19 10:37:18 -0400

    FIXME: DEFER: changes to gcc/testsuite/g++.dg/warn/Wsuggest-final.C (doesn't seem to be finished, presumably would need blt)

--- gcc/testsuite/g++.dg/warn/Wsuggest-final.C
+++ gcc/testsuite/g++.dg/warn/Wsuggest-final.C
@@ -1,9 +1,23 @@
 // { dg-do compile }
-// { dg-options "-O2 -Wsuggest-final-types -Wsuggest-final-methods" }
+// { dg-options "-O2 -Wsuggest-final-types -Wsuggest-final-methods -fdiagnostics-show-caret" }
 int c;
 struct A { // { dg-warning "final would enable devirtualization of 4 calls" }
+/* { dg-begin-multiline-output "" }
+ struct A {
+        ^
+   { dg-end-multiline-output "" } */
+
 virtual void a() {} // { dg-warning "final would enable devirtualization of 2 calls" }
+/* { dg-begin-multiline-output "" }
+ virtual void a() {}
+              ^
+   { dg-end-multiline-output "" } */
+
  virtual void b() {c++;} // { dg-warning "final would enable devirtualization of 2 calls"  }
+/* { dg-begin-multiline-output "" }
+  virtual void b() {c++;}
+               ^
+   { dg-end-multiline-output "" } */
 };
 void
 t(struct A *a)
