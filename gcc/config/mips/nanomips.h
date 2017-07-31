4d773455038e10bb4e62a6821dd63464fbe7ee1f
Jaydeep Patil <jaydeep.patil@imgtec.com>
2017-07-31 00:19:58 -0700

    configure script changes required for nonomips-img-linux

--- gcc/config/mips/nanomips.h
+++ gcc/config/mips/nanomips.h
@@ -17,8 +17,6 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#define NANOMIPS_SUPPORT
-
 #undef MIPS_SUPPORT_PS_3D
 #undef MIPS_SUPPORT_MSA
 #undef MIPS_SUPPORT_LOONGSON
