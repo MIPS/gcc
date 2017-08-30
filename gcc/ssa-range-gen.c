e0494f04763d080c8e6938fae340848aa286b804
amacleod <amacleod@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-08-30 14:21:14 +0000

    Initial cut of class path_ranger
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/range-gen2@251534 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/ssa-range-gen.c
+++ gcc/ssa-range-gen.c
@@ -46,325 +46,6 @@ along with GCC; see the file COPYING3.  If not see
 #include "ssa-range-gen.h"
 #include "ssa-range-stmt.h"
 
-
-gori::gori () : range_generator (def_chain)
-{
-  gori_map.create (0);
-  gori_map.safe_grow_cleared (last_basic_block_for_fn (cfun));
-}
-
-/* Is the last stmt in a block interesting to look at for range info.  */
-
-gimple *
-gori::last_stmt_gori (basic_block bb)
-{
-  gimple *stmt;
-
-  stmt = last_stmt (bb);
-  if (stmt && gimple_code (stmt) == GIMPLE_COND)
-    return stmt;
-  return NULL;
-}
-
-void
-gori::build (basic_block bb)
-{
-  gimple *stmt;
-
-  /* Non-NULL means its already been processed.  */
-  if (gori_map[bb->index])
-    return;
-
-  gori_map[bb->index] = BITMAP_ALLOC (NULL);
-
-  /* Look for a branch of some sort at the end of the block, and build the
-     GORI map and definition dependcies for it.  */
-  stmt = last_stmt_gori (bb);
-  if (stmt)
-    {
-      range_stmt rn (stmt);
-      if (rn.valid())
-	{
-	  tree ssa1,ssa2;
-	  if (rn.ssa_required (&ssa1, &ssa2))
-	    {
-	      if (ssa1)
-	        {
-		  bitmap_copy (gori_map[bb->index],
-			       def_chain[SSA_NAME_VERSION (ssa1)]);
-		  bitmap_set_bit (gori_map[bb->index], SSA_NAME_VERSION (ssa1));
-		}
-	      if (ssa2)
-	        {
-		  bitmap_ior_into (gori_map[bb->index],
-				   def_chain[SSA_NAME_VERSION (ssa2)]);
-		  bitmap_set_bit (gori_map[bb->index], SSA_NAME_VERSION (ssa2));
-		}
-	      return;
-	    }
-	}
-    }
-}
-
-
-void
-gori::build ()
-{
-  basic_block bb;
-
-  FOR_EACH_BB_FN (bb, cfun)
-    {
-      build (bb);
-    }
-}
-
-void
-gori::dump (FILE *f)
-{
-  basic_block bb;
-  bitmap_iterator bi;
-  unsigned y;
-
-  if (!f)
-    return;
-
-  fprintf (f, "\nDUMPING DEF_CHAIN\n");
-  def_chain.dump (f);
-
-  fprintf (f, "\nDUMPING GORI MAP\n");
-  FOR_EACH_BB_FN (bb, cfun)
-    {
-      if (gori_map[bb->index])
-        {
-          fprintf (f, "basic block %3d  : ", bb->index);
-	  EXECUTE_IF_SET_IN_BITMAP (gori_map[bb->index], 0, y, bi)
-	    {
-	      print_generic_expr (f, ssa_name (y), TDF_SLIM);
-	      fprintf (f, "  ");
-	    }
-	  fprintf (f, "\n");
-	}
-    }
-  fprintf (f, "\n");
-
-}
-
-bool 
-gori::remove_from_gori_map (basic_block bb, tree name)
-{
-  bitmap_clear_bit (gori_map[bb->index], SSA_NAME_VERSION (name));
-  return false;
-}
-
-
-/* Name is not directly mentioned in the gori map, but if one of the
-   compnents it is built from are, we can derive the value from that. ie
-	 a_3 = c_2 * 2
-	 b_6 = a_3 + 5
-	 if (a_3 > 10)
-   The GORI map will only have c_2 and a_3 since they comprise the
-   calculation of a_3.   b_6 will not be there as the only way to know
-   that b_6 can be calculated from a_3 would be to visit all the uses of
-   a_3 and see whether it is used to help define b_6.
-   Far easier now that b_6 is requested would be to simply check now
-   if a_3 is used to construct b_6. If it is get the expression for a_3
-   and adjust it to b_6.  */
-   
-bool
-gori::get_derived_range_stmt (range_stmt& stmt, tree name, basic_block bb)
-{
-  gimple *s;
-  tree n1,n2;
-  unsigned name_v = SSA_NAME_VERSION (name);
-
-  s = last_stmt_gori (bb);
-  gcc_assert (s);
-
-  stmt = s;
-  if (!stmt.valid ())
-    return false;
-
-  if (!stmt.ssa_required (&n1, &n2))
-    return false;
-
-  if (n1 && !bitmap_bit_p (def_chain[name_v], SSA_NAME_VERSION (n1)))
-    n1 = NULL_TREE;
-    
-  if (n2 && !bitmap_bit_p (def_chain[name_v], SSA_NAME_VERSION (n2)))
-    n2 = NULL_TREE;
-  
-  /* Non-null n1 or n2 indicates it is used in the calculating name.  */
-
-  /* Used on both sides too complicated.  */
-  if (n1 && n2)
-    return false;
-
-  /* Well we aren't actually DOING it yet... :-)  */
-  return false;
-}
-
-
-
-bool
-gori::range_p (basic_block bb, tree name)
-{
-  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
-
-  if (!gori_map[bb->index])
-    build (bb);
-
-  return bitmap_bit_p (gori_map[bb->index], SSA_NAME_VERSION (name));
-}
-
-
-/* Known range on an edge.  */
-bool
-gori::range_on_edge (irange& r, tree name, edge e)
-{
-  gimple *stmt;
-  basic_block bb = e->src;
-
-  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
-
-  if (!INTEGRAL_TYPE_P (TREE_TYPE (name)) && !POINTER_TYPE_P (TREE_TYPE (name)))
-    return false;
-
-  if (!range_p (bb, name))
-    return false;
-
-  stmt = last_stmt_gori (bb);
-  gcc_assert (stmt);
-
-  if (e->flags & EDGE_TRUE_VALUE)
-    {
-      irange bool_true (boolean_type_node,  1 , 1);
-      return range_generator.get_range_from_stmt (stmt, r, name, bool_true);
-    }
-
-  if (e->flags & EDGE_FALSE_VALUE)
-    {
-      irange bool_false (boolean_type_node,  0 , 0);
-      return range_generator.get_range_from_stmt (stmt, r, name, bool_false);
-    }
-
-  return false;
-}
-
-
-
-
-void
-gori::exercise (FILE *output)
-{
-
-  basic_block bb;
-  irange range;
-  
-  if (output)
-    dump (output);
-
-  FOR_EACH_BB_FN (bb, cfun)
-    {
-      edge_iterator ei;
-      edge e;
-      bool printed = false;
-      FOR_EACH_EDGE (e, ei, bb->succs)
-        {
-	  unsigned x;
-	  for (x = 1; x < num_ssa_names; x++)
-	    {
-	      tree name = ssa_name (x);
-	      if (name && range_p (bb, name))
-		{
-		  if (range_on_edge (range, name, e))
-		    {
-		      if (output)
-			{
-			  printed = true;
-			  fprintf (output, "BB%3d: ", bb->index);
-			  if (e->flags & EDGE_TRUE_VALUE)
-			    fprintf (output, " T: ");
-			  else if (e->flags & EDGE_FALSE_VALUE)
-			    fprintf (output, " F: ");
-			  print_generic_expr (output, name, TDF_SLIM);
-			  fprintf(output, "  \t");
-			  range.dump(output);
-			  fprintf(output, "\n");
-			}
-		    }
-		}
-	    }
-	}
-      if (printed)
-        fprintf (output, "\n");
-
-    }
-    
-}
-
-
-#if ANDREW_SAID_WRONG_CLASS
-
-/* Known Range at the beginning of a statment.  */
-bool
-gori::get_range (irange& r, tree name, gimple *s)
-{
-  gimple *def_stmt = SSA_NAME_DEF_STMT (name);
-  /* If def is in the same block as stmt, then only the def provides range.  */
-  if (def_stmt && gimple_bb (def_stmt) == gimple_bb (s))
-    {
-      range_stmt rn (def_stmt);
-      /* IF it isnt a definition  that is understood, we know nothing.  */
-      if (!rn.valid())
-        return false;
-      
-      /* We wont try to find dynamic ranges for anything in the def as yet.
-         A mechanism is neded to prevent infinite recursion when a cycle
-	 of uisage occurs.  */
-      return rn.fold (r);
-    }
-  /* The defintion is not in this block, so whatever range is on entry.  */
-  return get_range (r, name, gimple_bb (s));
-}
-
-
-
-/* Known range on entry to a basic block.  */
-bool
-gori::get_range (irange& r, tree name, basic_block bb)
-{
-  edge_iterator ei;
-  edge e;
-  irange er;
-
-  r.clear ();
-  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
-  /* Union all the ranges on the incoming edges.  */
-  FOR_EACH_EDGE (e, ei, bb->preds)
-    {
-      /* If we can't get a range on an edge, it defaults range_for_type
-         and union of that with anything will be range_for_type. 
-	 Simply report we can't get a range.  */
-      if (!get_range (er, name, e))
-        return false;
-      r.Union (er);
-    }
-
-  /* If range isn't supported, don't bother.  */
-  er.set_range_for_type (TREE_TYPE (name));
-
-  /* If the result is not valid or is range_for_type, report no range.  */
-   if (r == er)
-     return false;
-
-  return true;
-}
-
-#endif
-
-/*  ---------------------------------------------------------------------  */
-
-
 bool
 ranger::combine_range (range_stmt& stmt, irange& r, tree name,
 		       const irange& lhs, bool op1_in_chain, bool op2_in_chain)
@@ -624,3 +305,369 @@ ranger::get_range_from_stmt (gimple *stmt, irange& r, tree name,
 
   return get_range (rn, r, name, lhs);
 }
+
+/*  ---------------------------------------------------------------------  */
+
+
+gori::gori () : range_generator (def_chain)
+{
+  gori_map.create (0);
+  gori_map.safe_grow_cleared (last_basic_block_for_fn (cfun));
+}
+
+/* Is the last stmt in a block interesting to look at for range info.  */
+
+gimple *
+gori::last_stmt_gori (basic_block bb)
+{
+  gimple *stmt;
+
+  stmt = last_stmt (bb);
+  if (stmt && gimple_code (stmt) == GIMPLE_COND)
+    return stmt;
+  return NULL;
+}
+
+void
+gori::build (basic_block bb)
+{
+  gimple *stmt;
+
+  /* Non-NULL means its already been processed.  */
+  if (gori_map[bb->index])
+    return;
+
+  gori_map[bb->index] = BITMAP_ALLOC (NULL);
+
+  /* Look for a branch of some sort at the end of the block, and build the
+     GORI map and definition dependcies for it.  */
+  stmt = last_stmt_gori (bb);
+  if (stmt)
+    {
+      range_stmt rn (stmt);
+      if (rn.valid())
+	{
+	  tree ssa1,ssa2;
+	  if (rn.ssa_required (&ssa1, &ssa2))
+	    {
+	      if (ssa1)
+	        {
+		  bitmap_copy (gori_map[bb->index],
+			       def_chain[SSA_NAME_VERSION (ssa1)]);
+		  bitmap_set_bit (gori_map[bb->index], SSA_NAME_VERSION (ssa1));
+		}
+	      if (ssa2)
+	        {
+		  bitmap_ior_into (gori_map[bb->index],
+				   def_chain[SSA_NAME_VERSION (ssa2)]);
+		  bitmap_set_bit (gori_map[bb->index], SSA_NAME_VERSION (ssa2));
+		}
+	      return;
+	    }
+	}
+    }
+}
+
+
+void
+gori::build ()
+{
+  basic_block bb;
+
+  FOR_EACH_BB_FN (bb, cfun)
+    {
+      build (bb);
+    }
+}
+
+void
+gori::dump (FILE *f)
+{
+  basic_block bb;
+  bitmap_iterator bi;
+  unsigned y;
+
+  if (!f)
+    return;
+
+  fprintf (f, "\nDUMPING DEF_CHAIN\n");
+  def_chain.dump (f);
+
+  fprintf (f, "\nDUMPING GORI MAP\n");
+  FOR_EACH_BB_FN (bb, cfun)
+    {
+      if (gori_map[bb->index])
+        {
+          fprintf (f, "basic block %3d  : ", bb->index);
+	  EXECUTE_IF_SET_IN_BITMAP (gori_map[bb->index], 0, y, bi)
+	    {
+	      print_generic_expr (f, ssa_name (y), TDF_SLIM);
+	      fprintf (f, "  ");
+	    }
+	  fprintf (f, "\n");
+	}
+    }
+  fprintf (f, "\n");
+
+}
+
+bool 
+gori::remove_from_gori_map (basic_block bb, tree name)
+{
+  bitmap_clear_bit (gori_map[bb->index], SSA_NAME_VERSION (name));
+  return false;
+}
+
+
+/* Name is not directly mentioned in the gori map, but if one of the
+   compnents it is built from are, we can derive the value from that. ie
+	 a_3 = c_2 * 2
+	 b_6 = a_3 + 5
+	 if (a_3 > 10)
+   The GORI map will only have c_2 and a_3 since they comprise the
+   calculation of a_3.   b_6 will not be there as the only way to know
+   that b_6 can be calculated from a_3 would be to visit all the uses of
+   a_3 and see whether it is used to help define b_6.
+   Far easier now that b_6 is requested would be to simply check now
+   if a_3 is used to construct b_6. If it is get the expression for a_3
+   and adjust it to b_6.  */
+   
+bool
+gori::get_derived_range_stmt (range_stmt& stmt, tree name, basic_block bb)
+{
+  gimple *s;
+  tree n1,n2;
+  unsigned name_v = SSA_NAME_VERSION (name);
+
+  s = last_stmt_gori (bb);
+  gcc_assert (s);
+
+  stmt = s;
+  if (!stmt.valid ())
+    return false;
+
+  if (!stmt.ssa_required (&n1, &n2))
+    return false;
+
+  if (n1 && !bitmap_bit_p (def_chain[name_v], SSA_NAME_VERSION (n1)))
+    n1 = NULL_TREE;
+    
+  if (n2 && !bitmap_bit_p (def_chain[name_v], SSA_NAME_VERSION (n2)))
+    n2 = NULL_TREE;
+  
+  /* Non-null n1 or n2 indicates it is used in the calculating name.  */
+
+  /* Used on both sides too complicated.  */
+  if (n1 && n2)
+    return false;
+
+  /* Well we aren't actually DOING it yet... :-)  */
+  return false;
+}
+
+
+
+bool
+gori::range_p (basic_block bb, tree name)
+{
+  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
+
+  if (!gori_map[bb->index])
+    build (bb);
+
+  return bitmap_bit_p (gori_map[bb->index], SSA_NAME_VERSION (name));
+}
+
+
+/* Known range on an edge.  */
+bool
+gori::range_on_edge (irange& r, tree name, edge e)
+{
+  gimple *stmt;
+  basic_block bb = e->src;
+
+  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
+
+  if (!INTEGRAL_TYPE_P (TREE_TYPE (name)) && !POINTER_TYPE_P (TREE_TYPE (name)))
+    return false;
+
+  if (!range_p (bb, name))
+    return false;
+
+  stmt = last_stmt_gori (bb);
+  gcc_assert (stmt);
+
+  if (e->flags & EDGE_TRUE_VALUE)
+    {
+      irange bool_true (boolean_type_node,  1 , 1);
+      return range_generator.get_range_from_stmt (stmt, r, name, bool_true);
+    }
+
+  if (e->flags & EDGE_FALSE_VALUE)
+    {
+      irange bool_false (boolean_type_node,  0 , 0);
+      return range_generator.get_range_from_stmt (stmt, r, name, bool_false);
+    }
+
+  return false;
+}
+
+
+
+
+void
+gori::exercise (FILE *output)
+{
+
+  basic_block bb;
+  irange range;
+  
+  if (output)
+    dump (output);
+
+  FOR_EACH_BB_FN (bb, cfun)
+    {
+      edge_iterator ei;
+      edge e;
+      bool printed = false;
+      FOR_EACH_EDGE (e, ei, bb->succs)
+        {
+	  unsigned x;
+	  for (x = 1; x < num_ssa_names; x++)
+	    {
+	      tree name = ssa_name (x);
+	      if (name && range_p (bb, name))
+		{
+		  if (range_on_edge (range, name, e))
+		    {
+		      if (output)
+			{
+			  printed = true;
+			  fprintf (output, "BB%3d: ", bb->index);
+			  if (e->flags & EDGE_TRUE_VALUE)
+			    fprintf (output, " T: ");
+			  else if (e->flags & EDGE_FALSE_VALUE)
+			    fprintf (output, " F: ");
+			  print_generic_expr (output, name, TDF_SLIM);
+			  fprintf(output, "  \t");
+			  range.dump(output);
+			  fprintf(output, "\n");
+			}
+		    }
+		}
+	    }
+	}
+      if (printed)
+        fprintf (output, "\n");
+
+    }
+    
+}
+
+
+
+bool
+gori::range_on_stmt (irange& r, tree name, gimple *g)
+{
+  range_stmt rn (g);
+  irange lhs;
+
+  /* If we don't understand the stmt... */
+  if (!rn.valid())
+    return false;
+
+  /* If both operands are the same, we know nothing.  */
+  if (rn.operand1 () == rn.operand2 ())
+    return false;
+
+  /* If neither operand is what we are looking for, then return nothing.  */
+  if (rn.operand1 () != name && rn.operand2 () != name)
+    return false;
+
+  /* So far only understand LHS if its an assignment.  */
+  if (gimple_code (g) != GIMPLE_ASSIGN)
+    return false;
+
+  if (range_generator.get_operand_range (lhs, gimple_get_lhs (g)))
+    return range_generator.get_range (rn, r, name, lhs);
+
+  return false;
+}
+
+bool
+gori::range_of_def (irange& r, gimple *g)
+{
+  range_stmt rn (g);
+
+  /* If we don't understand the stmt... */
+  if (!rn.valid())
+    return false;
+  
+  return rn.fold (r);
+}
+
+
+/*  ---------------------------------------------------------------------  */
+
+
+
+path_ranger::path_ranger () 
+{
+}
+
+/* Known range on an edge on the path back to the DEF of name.  */
+bool
+path_ranger::path_range_edge (irange& r, tree name, edge e)
+{
+  /* Until the walker is implelemted... just call the local routine.  */
+  return range_on_edge (r, name, e);
+}
+
+bool
+path_ranger::path_range_entry (irange& r, tree name, basic_block bb)
+{
+  edge_iterator ei;
+  edge e;
+  irange er;
+
+  r.clear ();
+  gcc_checking_assert (TREE_CODE (name) == SSA_NAME);
+  /* Union all the ranges on the incoming edges.  */
+  FOR_EACH_EDGE (e, ei, bb->preds)
+    {
+      /* If we can't get a range on an edge, it defaults range_for_type
+         and union of that with anything will be range_for_type. 
+	 Simply report we can't get a range.  */
+      if (!range_on_edge (er, name, e))
+        return false;
+      r.union_ (er);
+    }
+
+  er.set_range_for_type (TREE_TYPE (name));
+  /* If the result is range_for_type, report no range.  */
+   if (r == er)
+     return false;
+
+  return true;
+}
+
+bool
+path_ranger::path_range_stmt (irange& r, tree name, gimple *g)
+{
+  return range_on_stmt (r, name, g);
+}
+
+void
+path_ranger::dump(FILE *f)
+{
+  gori::dump (f);
+}
+
+
+void
+path_ranger::exercise (FILE *f)
+{
+  gori::exercise (f);
+}
+
+
--- gcc/ssa-range-gen.h
+++ gcc/ssa-range-gen.h
@@ -32,10 +32,10 @@ class ranger
   ssa_define_chain& def_chain;
   bool combine_range (range_stmt& stmt, irange& r, tree name, const irange& lhs,
 		      bool op1_in_chain, bool op2_in_chain);
-  bool get_operand_range (irange& r, tree op);
-  bool get_range (range_stmt& stmt, irange& r, tree name, const irange& lhs);
 public:
   ranger (ssa_define_chain & dc);
+  bool get_operand_range (irange& r, tree op);
+  bool get_range (range_stmt& stmt, irange& r, tree name, const irange& lhs);
   bool get_range_from_stmt (gimple *stmt, irange& r, tree name,
 			    const irange& lhs);
 };
@@ -55,19 +55,41 @@ class gori
   bool get_derived_range_stmt (range_stmt& stmt, tree name, basic_block bb);
   gimple *last_stmt_gori (basic_block bb);
 
-public:
-  gori ();
   void build ();
   void build (basic_block bb);
+
+public:
+  gori ();
 
   /* True if NAME Generates range info on one or more outgoing edges of BB.  */
   bool range_p (basic_block bb, tree name);
   /* What is the static calculated range of NAME on outgoing edge E.  */
   bool range_on_edge (irange& r, tree name, edge e);
+  /* What infomation does stmt g provide about name.  */
+  bool range_on_stmt (irange& r, tree name, gimple *g);
+  /* What infomation does stmt g provide about the defintion.  */
+  bool range_of_def (irange& r, gimple *g);
 
   void dump (FILE *f);
   void exercise (FILE *f);   /* do a full mapping pass, dump if provided.  */
 };
 
+
+
+
+/* This class utilizes the basic block GORI map and is used to query the range
+   of SSA_NAMEs across multiple basic blocks and edges.  */
+class path_ranger : public gori
+{
+  public:
+  path_ranger ();
+  /* What is the known range of name from its DEF point to edge E.  */
+  bool path_range_edge (irange& r, tree name, edge e);
+  bool path_range_entry (irange& r, tree name, basic_block bb);
+  bool path_range_stmt (irange& r, tree name, gimple *g);
+
+  void dump (FILE *f);
+  void exercise (FILE *f);   /* do a full mapping pass, dump if provided.  */
+};
 
 #endif /* GCC_SSA_RANGE_GEN_H */
