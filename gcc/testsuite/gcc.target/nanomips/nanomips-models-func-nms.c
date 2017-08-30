021b3966b0e11ba7b804b4378e48a16cf34b5053
Toma Tabacu <toma.tabacu@imgtec.com>
2017-08-30 12:01:14 +0100

    Add tests for code and data models.

---
+++ gcc/testsuite/gcc.target/nanomips/nanomips-models-func-nms.c
@@ -0,0 +1,140 @@
+/* { dg-do compile } */
+/* { dg-options "-m32 -march=32r6s -fpic" } */
+/* { dg-skip-if "" { *-*-* } { "-O1" "-O2" "-O3" "-Os" } { "" } } */
+
+/* { dg-final { scan-assembler "balc\ta_sta_func" } } */
+
+/* { dg-final { scan-assembler "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(a_longcall\\)" } } */
+/* { dg-final { scan-assembler "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(a_longcall\\)" } } */
+/* jalrc reg # 1 */
+
+/* { dg-final { scan-assembler "lw\t\\\$\[ast0-9\]+,%got_call\\(a_ext_func\\)\\(\\\$gp\\)" } } */
+/* { dg-final { scan-assembler "\\\.reloc\t1f,R_NANOMIPS_JALR,a_ext_func" } } */
+/* jalrc reg # 2 */
+
+/* { dg-final { scan-assembler "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(a_sta_func\\)" } } */
+/* { dg-final { scan-assembler "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(a_sta_func\\)" } } */
+/* balc a_sta_fptr # 1*/
+
+/* { dg-final { scan-assembler "lw\t\\\$\[ast0-9\]+,%got_disp\\(a_ext_func\\)\\(\\\$gp\\)" } } */
+/* balc a_sta_fptr # 2*/
+
+/* { dg-final { scan-assembler-times "balc\ta_sta_fptr" 2 } } */
+
+
+/* { dg-final { scan-assembler "balc\tm_sta_func" } } */
+
+/* { dg-final { scan-assembler "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(m_longcall\\)" } } */
+/* { dg-final { scan-assembler "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(m_longcall\\)" } } */
+/* jalrc reg # 3 */
+
+/* { dg-final { scan-assembler "lw\t\\\$\[ast0-9\]+,%got_disp\\(m_ext_func\\)\\(\\\$gp\\)" } } */
+/* jalrc reg # 4 */
+
+/* { dg-final { scan-assembler "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(m_sta_func\\)" } } */
+/* { dg-final { scan-assembler "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(m_sta_func\\)" } } */
+/* balc m_sta_fptr # 1*/
+
+/* { dg-final { scan-assembler "lw\t\\\$\[ast0-9\]+,%got_disp\\(m_ext_func\\)\\(\\\$gp\\)" } } */
+/* balc m_sta_fptr # 2*/
+
+/* { dg-final { scan-assembler-times "balc\tm_sta_fptr" 2 } } */
+
+
+/* aluipc reg, %pcrel_hi(l_sta_func) # 1 */
+/* addiu reg, reg, %lo(l_sta_func) # 1 */
+/* jalrc reg # 5 */
+
+/* aluipc reg, %pcrel_hi(l_longcall) */
+/* addiu reg, reg, %lo(l_longcall) */
+/* jalrc reg # 6 */
+
+/* aluipc reg, %got_pcrel_hi(l_ext_func) # 1 */
+/* lw reg, %got_lo(l_ext_func)(reg) # 1 */
+/* jalrc reg # 7 */
+
+/* aluipc reg, %pcrel_hi(l_sta_func) # 2 */
+/* addiu reg, reg, %lo(l_sta_func) # 2 */
+/* aluipc reg, %pcrel_hi(l_sta_fptr) # 1 */
+/* addiu reg, reg, %lo(l_sta_fptr) # 1 */
+/* jalrc reg # 8 */
+
+/* aluipc reg, %got_pcrel_hi(l_ext_func) # 2 */
+/* lw reg, %got_lo(l_ext_func)(reg) # 2 */
+/* aluipc reg, %pcrel_hi(l_sta_fptr) # 2 */
+/* addiu reg, reg, %lo(l_sta_fptr) # 2 */
+/* jalrc reg # 9 */
+
+/* { dg-final { scan-assembler "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(l_longcall\\)" } } */
+/* { dg-final { scan-assembler "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(l_longcall\\)" } } */
+
+/* { dg-final { scan-assembler-times "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(l_sta_func\\)" 2 } } */
+/* { dg-final { scan-assembler-times "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(l_sta_func\\)" 2 } } */
+
+/* { dg-final { scan-assembler-times "aluipc\t\\\$\[ast0-9\]+,%pcrel_hi\\(l_sta_fptr\\)" 2 } } */
+/* { dg-final { scan-assembler-times "addiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%lo\\(l_sta_fptr\\)" 2 } } */
+
+/* { dg-final { scan-assembler-times "aluipc\t\\\$\[ast0-9\]+,%got_pcrel_hi\\(l_ext_func\\)" 2 } } */
+/* { dg-final { scan-assembler-times "lw\t\\\$\[ast0-9\]+,%got_lo\\(l_ext_func\\)\\(\\\$\[ast0-9\]+\\)" 2 } } */
+
+/* { dg-final { scan-assembler-times "jalrc\t\\\$\[ast0-9\]+" 9 } } */
+/* { dg-final { scan-assembler "aluipc\t\\\$gp,%pcrel_hi\\(_gp\\)" } } */
+
+__attribute__ ((model("auto"), noclone ,noinline))
+static int a_sta_fptr(int t()) { asm (""); return 1; }
+
+__attribute__ ((model("auto"), noclone ,noinline))
+static int a_sta_func(void) { asm (""); return 1; }
+
+__attribute__ ((model("auto"), noclone ,noinline, long_call))
+static int a_longcall(void) { asm (""); return 1; }
+
+__attribute__ ((model("auto")))
+extern int a_ext_func();
+
+__attribute__ ((model("medium"), noclone ,noinline))
+static int m_sta_fptr(int t()) { asm (""); return 1; }
+
+__attribute__ ((model("medium"), noclone ,noinline))
+static int m_sta_func(void) { asm (""); return 1; }
+
+__attribute__ ((model("medium"), noclone ,noinline, long_call))
+static int m_longcall(void) { asm (""); return 1; }
+
+__attribute__ ((model("medium")))
+extern int m_ext_func();
+
+__attribute__ ((model("large"), noclone ,noinline))
+static int l_sta_fptr(int t()) { asm (""); return 1; }
+
+__attribute__ ((model("large"), noclone ,noinline))
+static int l_sta_func(void) { asm (""); return 1; }
+
+__attribute__ ((model("large"), noclone ,noinline, long_call))
+static int l_longcall(void) { asm (""); return 1; }
+
+__attribute__ ((model("large")))
+extern int l_ext_func();
+
+int
+foo ()
+{
+  a_sta_func();
+  a_longcall();
+  a_ext_func();
+  a_sta_fptr(&a_sta_func);
+  a_sta_fptr(&a_ext_func);
+
+  m_sta_func();
+  m_longcall();
+  m_ext_func();
+  m_sta_fptr(&m_sta_func);
+  m_sta_fptr(&m_ext_func);
+
+  l_sta_func();
+  l_longcall();
+  l_ext_func();
+  l_sta_fptr(&l_sta_func);
+  l_sta_fptr(&l_ext_func);
+  return 0;
+}
