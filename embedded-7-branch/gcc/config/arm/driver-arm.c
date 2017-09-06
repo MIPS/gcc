610b40efc3f87a2cae618d793a0496cca8bf6777
thopre01 <thopre01@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-06 14:56:01 +0000

    [ARM] Add MIDR info for ARM Cortex-R7 and Cortex-R8
    2017-09-04  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	Backport from mainline
    	2017-07-04  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	gcc/
    	* config/arm/driver-arm.c (arm_cpu_table): Add entry for ARM
    	Cortex-R7 and Cortex-R8 processors.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@251802 138bc75d-0d04-0410-961f-82ee72b054a4

--- embedded-7-branch/gcc/config/arm/driver-arm.c
+++ embedded-7-branch/gcc/config/arm/driver-arm.c
@@ -54,6 +54,8 @@ static struct vendor_cpu arm_cpu_table[] = {
     {"0xd09", "armv8-a+crc", "cortex-a73"},
     {"0xc14", "armv7-r", "cortex-r4"},
     {"0xc15", "armv7-r", "cortex-r5"},
+    {"0xc17", "armv7-r", "cortex-r7"},
+    {"0xc18", "armv7-r", "cortex-r8"},
     {"0xc20", "armv6-m", "cortex-m0"},
     {"0xc21", "armv6-m", "cortex-m1"},
     {"0xc23", "armv7-m", "cortex-m3"},
