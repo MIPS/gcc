ea198015860a84c8b75adc72dea5b6a2e1dc652e
aldyh <aldyh@138bc75d-0d04-0410-961f-82ee72b054a4>
2017-09-13 17:04:19 +0000

    2017-08-17  Richard Biener  <rguenther@suse.de>
    	* tree-vrp.c (vrp_int_const_binop): Do not set *overflow_p
    	to true when overflow is undefined and we saturated the
    	result.
    
    	* gcc.dg/tree-ssa/vrp117.c: New testcase.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/range-gen2@252456 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-vrp.c
+++ gcc/tree-vrp.c
@@ -1679,6 +1679,8 @@ vrp_int_const_binop (enum tree_code code, tree val1, tree val2,
   signop sign = TYPE_SIGN (TREE_TYPE (val1));
   wide_int res;
 
+  *overflow_p = false;
+
   switch (code)
     {
     case RSHIFT_EXPR:
@@ -1750,8 +1752,6 @@ vrp_int_const_binop (enum tree_code code, tree val1, tree val2,
       gcc_unreachable ();
     }
 
-  *overflow_p = overflow;
-
   if (overflow
       && TYPE_OVERFLOW_UNDEFINED (TREE_TYPE (val1)))
     {
@@ -1794,6 +1794,8 @@ vrp_int_const_binop (enum tree_code code, tree val1, tree val2,
 	return wi::min_value (TYPE_PRECISION (TREE_TYPE (val1)),
 			      TYPE_SIGN (TREE_TYPE (val1)));
     }
+
+  *overflow_p = overflow;
 
   return res;
 }
