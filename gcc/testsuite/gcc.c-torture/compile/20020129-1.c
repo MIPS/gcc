4511762444e23112edce0d42f130825a99ed67fd
Matthew Fortune <matthew.fortune@imgtec.com>
2017-09-14 14:56:14 +0100

    Testsuite: Skip tests that make function calls to variables for some MIPS ISAs
    The compressed MIPS ISAs (microMIPS and MIPS16) require the LSB of
    an address to indicate which ISA to execute.  The non-conformant
    patterns used in these tests cannot set the ISA mode bit and may
    attempt to direct call the variable which triggers an error from
    the assembler about calling a different ISA mode.
    
    gcc/testsuite/
    	* gcc.c-torture/compile/20020129-1.c: Skip for MIPS16/microMIPS.
    	* gcc.c-torture/compile/pr37433-1.c: Likewise.
    	* gcc.c-torture/compile/pr37433.c: Likewise.
    	* lib/target-supports.exp
    	(check_effective_target_mips_compressed): New function.

--- gcc/testsuite/gcc.c-torture/compile/20020129-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20020129-1.c
@@ -1,5 +1,10 @@
 /* Test call to static variable.  */
 
+/* MIPS compressed ISAs require the LSB of an address to indicate which
+   ISA mode to use.  This test cannot do that and raises an assembler
+   warning (binutils 2.29 onwards) of a branch to a different ISA.  */
+/* { dg-skip-if "" { mips_compressed } } */
+
 typedef struct
 {
   long long a[10];
