21121f0c5e4eb0692d7c8cf194476d6a8a0dc9f9
Robert Suchanek <robert.suchanek@imgtec.com>
2017-09-15 09:38:20 +0100

    Update jump-table-1.c to scan for gprel rather gp_rel.

--- gcc/testsuite/gcc.target/nanomips/jump-table-1.c
+++ gcc/testsuite/gcc.target/nanomips/jump-table-1.c
@@ -37,4 +37,4 @@ foo (int i)
 /* { dg-final { scan-assembler "\tlw\t" } } */
 /* { dg-final { scan-assembler "\t\\.rdata" } } */
 /* { dg-final { scan-assembler-times "\t\\.word\t\\\$L" 12 } } */
-/* { dg-final { scan-assembler-times "lw\t\\\$a0,%gp_rel\\(x\[0-9\]\{1,2\}\\)\\(\\\$gp\\)" 12 } } */
+/* { dg-final { scan-assembler-times "lw\t\\\$a0,%gprel\\(x\[0-9\]\{1,2\}\\)\\(\\\$gp\\)" 12 } } */
