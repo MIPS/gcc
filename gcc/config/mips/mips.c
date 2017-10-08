4ea3a82b5e8e23591d79a9ca63018acceb53c2a5
Matthew Fortune <matthew.fortune@imgtec.com>
2017-10-08 22:09:21 +0100

    Lightweight fix for shrink-wrapping being inhibited by -mgpopt
    This should be solved using the various PIC related macros such as
    PIC_OFFSET_TABLE_REGNUM and pic_offset_table_rtx but changing these
    is too dangerous without investigation.  The lightweight fix just
    clears the global pointer from being related to the prologue and
    only affects shrink wrapping.

--- gcc/config/mips/mips.c
+++ gcc/config/mips/mips.c
@@ -24879,6 +24879,15 @@ mips_starting_frame_offset (void)
   return crtl->outgoing_args_size + MIPS_GP_SAVE_AREA_SIZE;
 }
 
+/* Implement TARGET_SET_UP_BY_PROLOGUE.  */
+
+static void
+mips_set_up_by_prologue (hard_reg_set_container *regs)
+{
+  if (!TARGET_USE_GOT && TARGET_GPOPT)
+    CLEAR_HARD_REG_BIT (regs->set, GLOBAL_POINTER_REGNUM);
+}
+
 void
 mips_bit_clear_info (unsigned HOST_WIDE_INT m, int *start_pos, int *size)
 {
@@ -25015,6 +25024,9 @@ mips_bit_clear_p (enum machine_mode mode, unsigned HOST_WIDE_INT m)
 
 #undef TARGET_IN_SMALL_DATA_P
 #define TARGET_IN_SMALL_DATA_P mips_in_small_data_p
+
+#undef TARGET_SET_UP_BY_PROLOGUE
+#define TARGET_SET_UP_BY_PROLOGUE mips_set_up_by_prologue
 
 #undef TARGET_MACHINE_DEPENDENT_REORG
 #define TARGET_MACHINE_DEPENDENT_REORG mips_reorg
