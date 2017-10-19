b291215dd3ec0f3585881d99dcfef46ee5417849
Matthew Fortune <matthew.fortune@imgtec.com>
2017-10-19 14:10:26 +0100

    Add -muse-copyw-ucopyw option (hidden from help).
    gcc/
    	* config/mips/mips.c (mips_option_override): Set default for
    	TARGET_USE_COPYW_UCOPYW.
    	* config/mips/mips.h (ISA_HAS_COPY): Update to reference
    	TARGET_USE_COPYW_UCOPYW.
    	* config/mips/mips.opt (-muse-copyw-ucopyw): New hidden optionn.
    
    gcc/testsuite/
    	* gcc.target/mips/memcpy-3.c: New test.
    	* gcc.target/mips/memcpy-4.c: Likewise.
    	* gcc.target/mips/mips.exp: Accept -muse-copyw-ucopyw and
    	isa=interaptiv-mr2.

---
+++ gcc/testsuite/gcc.target/mips/memcpy-3.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "isa=interaptiv-mr2 -mno-memcpy (-mips16)" } */
+/* { dg-skip-if "code quality test" { *-*-* } { "" } { "" } } */
+
+char * ref = "123456789012";
+
+__attribute__((mips16))
+void
+f1 (int *p)
+{
+  __builtin_memcpy (p, ref, 12);
+}
+
+/* { dg-final { scan-assembler "\tucopyw\t" } } */
