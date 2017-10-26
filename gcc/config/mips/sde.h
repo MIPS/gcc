8b6625210ef79b783a1fc469609c3933fd1831df
Jack Romo <jack.romo@imgtec.com>
2017-10-26 11:15:43 +0100

    Add Catherine Moore's compactEH implementation

--- gcc/config/mips/sde.h
+++ gcc/config/mips/sde.h
@@ -18,6 +18,11 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
+#ifdef HAVE_GAS_EH_FRAME_ENTRY
+#undef MIPS_COMPACT_EH_PCREL
+#define MIPS_COMPACT_EH_PCREL 1
+#endif
+
 #undef DRIVER_SELF_SPECS
 #define DRIVER_SELF_SPECS						\
   /* Set the ISA for the default multilib.  */				\
