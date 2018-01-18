1cb54cc1b22f8adf9fda5da604df4e2a07a65546
Faraz Shahbazker <faraz.shahbazker@mips.com>
2018-01-18 07:48:20 -0800

    | GT-104: Add PC-relative address calculation for jump table optimization
    gcc/gcc/
    	* config/mips/mips-protos.h (mips_classify_symbol): New.
    	* config/mips/mips.c (mips_classify_symbol): Make global.
    	* config/mips/mips.md (casesi_internal_nanomips_<mode>): Generate
    	label reference instruction according to address mode. Clean up
    	unwanted spaces in emitted assembly and correct indentation.
    
    gcc/gcc/testsuite/
    	* gcc.target/nanomips/jump-table-opt-large-nmf.c: New file.
    	* gcc.target/nanomips/jump-table-opt-large-nms.c: New file.
    	* gcc.target/nanomips/jump-table-opt-large-no-pcrel-nmf.c: New file.
    	* gcc.target/nanomips/jump-table-opt-large-no-pcrel-nms.c: New file.
    	* gcc.target/nanomips/jump-table-opt-nmf.c: New file.
    	* gcc.target/nanomips/jump-table-opt-nms.c: New file.
    	* gcc.target/nanomips/jump-table.c: New file.

---
+++ gcc/testsuite/gcc.target/nanomips/jump-table-opt-large-nms.c
@@ -0,0 +1,8 @@
+/* { dg-options "-march=32r6s -mcmodel=large" } */
+/* { dg-additional-options "-mjump-table-opt" } */
+/* { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
+#include "jump-table.c"
+/* { dg-final { scan-assembler "\taluipc\t.*%pcrel_hi" } } */
+/* { dg-final { scan-assembler "\tori\t.*%lo" } } */
+/* { dg-final { scan-assembler "\tlbx\t" } } */
+/* { dg-final { scan-assembler "\tbrsc\t" } } */
