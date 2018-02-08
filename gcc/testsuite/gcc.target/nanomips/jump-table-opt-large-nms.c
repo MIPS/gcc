34b5da348aac8ffd6fd3417ebb5ac9f96b721fa6
Faraz Shahbazker <faraz.shahbazker@mips.com>
2018-02-08 11:30:28 -0800

    GT-169: Branch mis-predict in jump table optimization
    Indicate in casesi expansion that jump index is more likely to be
    within total range, than out side it.
    
    gcc/
    	* config/mips/mips.md (casesi): Mark exit path for
    	out-of-range index as unlikely.
    
    gcc/testsuite/
    	* gcc.target/nanomips/jump-table-opt-large-nmf.c: Add reference output.
    	* gcc.target/nanomips/jump-table-opt-large-nms.c: Likewise.
    	* gcc.target/nanomips/jump-table-opt-large-no-pcrel-nmf.c: Likewise.
    	* gcc.target/nanomips/jump-table-opt-large-no-pcrel-nms.c: Likewise.
    	* gcc.target/nanomips/jump-table-opt-nmf.c: Likewise.
    	* gcc.target/nanomips/jump-table-opt-nms.c: Likewise.

--- gcc/testsuite/gcc.target/nanomips/jump-table-opt-large-nms.c
+++ gcc/testsuite/gcc.target/nanomips/jump-table-opt-large-nms.c
@@ -2,6 +2,7 @@
 /* { dg-additional-options "-mjump-table-opt" } */
 /* { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
 #include "jump-table.c"
+/* { dg-final { scan-assembler "\tbgeiuc\t" } } */
 /* { dg-final { scan-assembler "\taluipc\t.*%pcrel_hi" } } */
 /* { dg-final { scan-assembler "\tori\t.*%lo" } } */
 /* { dg-final { scan-assembler "\tlbx\t" } } */
--- gcc/testsuite/gcc.target/nanomips/jump-table-opt-large-no-pcrel-nmf.c
+++ gcc/testsuite/gcc.target/nanomips/jump-table-opt-large-no-pcrel-nmf.c
@@ -2,6 +2,7 @@
 /* { dg-additional-options "-mjump-table-opt" } */
 /* { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
 #include "jump-table.c"
+/* { dg-final { scan-assembler "\tbgeiuc\t" } } */
 /* { dg-final { scan-assembler "\tli\t.*,\\\$L" } } */
 /* { dg-final { scan-assembler "\tlbx\t" } } */
 /* { dg-final { scan-assembler "\tbrsc\t" } } */
