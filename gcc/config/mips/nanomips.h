01b2dac4a05080ce6e905d5750ba169dbeb8ae6c
Matthew Fortune <matthew.fortune@imgtec.com>
2018-03-19 21:53:10 +0000

    Fix trampoline

--- gcc/config/mips/nanomips.h
+++ gcc/config/mips/nanomips.h
@@ -218,6 +218,16 @@ FP_ASM_SPEC "\
    (GET_MODE (PATTERN (VEC_INSN)) == QImode ? \
      1 : exact_log2 (GET_MODE_SIZE (GET_MODE (PATTERN (VEC_INSN)))))
 
+/* Trampolines are a block of code followed by two pointers.  */
+
+#undef TRAMPOLINE_SIZE
+#define TRAMPOLINE_SIZE ((8 * 2) + GET_MODE_SIZE (ptr_mode) * 2)
+
+/* 32-bit alignment is sufficient for the nanoMIPS trampoline.  */
+
+#undef TRAMPOLINE_ALIGNMENT
+#define TRAMPOLINE_ALIGNMENT 32
+
 /* Describe how we implement __builtin_eh_return.  */
 
 /* At the moment, nothing appears to use more than 2 EH data registers.
