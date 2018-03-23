10f697dfcdaa77b842de6e9a62c68b33a49d3c16
Tom de Vries <tom@codesourcery.com>
2018-03-23 13:33:32 +0100

    [nvptx] Fix whitespace in nvptx_single
    2018-03-23  Tom de Vries  <tom@codesourcery.com>
    
    	* config/nvptx/nvptx.c (nvptx_single): Fix whitespace.

--- gcc/config/nvptx/nvptx.c
+++ gcc/config/nvptx/nvptx.c
@@ -4100,7 +4100,7 @@ nvptx_single (unsigned mask, basic_block from, basic_block to)
 	    pred = gen_reg_rtx (BImode);
 	    cfun->machine->axis_predicate[mode - GOMP_DIM_WORKER] = pred;
 	  }
-	
+
 	rtx br;
 	if (mode == GOMP_DIM_VECTOR)
 	  br = gen_br_true (pred, label);
