78ded3345d0f72e4a2629aad976de1a49fb0def7
Tom de Vries <tom@codesourcery.com>
2018-03-26 15:05:48 +0200

    [openacc] Add target hook TARGET_GOACC_ADJUST_PARALLELISM
    2018-03-26  Cesar Philippidis  <cesar@codesourcery.com>
    	    Tom de Vries  <tom@codesourcery.com>
    
    	* doc/tm.texi.in: Add placeholder for TARGET_GOACC_ADJUST_PARALLELISM.
    	* doc/tm.texi: Regenerate.
    	* omp-offload.c (oacc_loop_fixed_partitions): Use the adjust_parallelism
    	hook to modify this_mask.
    	(oacc_loop_auto_partitions): Use the adjust_parallelism hook to modify
    	this_mask and loop->mask.
    	(default_goacc_adjust_parallelism): New function.
    	* target.def (adjust_parallelism): New hook.
    	* targhooks.h (default_goacc_adjust_parallelism): Declare.

--- gcc/omp-offload.c
+++ gcc/omp-offload.c
@@ -1258,6 +1258,13 @@ oacc_loop_fixed_partitions (oacc_loop *loop, unsigned outer_mask)
 	}
     }
 
+  /* FIXME: Ideally, we should be coalescing parallelism here if the
+     hardware supports it.  E.g. Instead of partitioning a loop
+     across worker and vector axes, sometimes the hardware can
+     execute those loops together without resorting to placing
+     extra thread barriers.  */
+  this_mask = targetm.goacc.adjust_parallelism (this_mask, outer_mask);
+
   mask_all |= this_mask;
 
   if (loop->flags & OLF_TILE)
@@ -1349,6 +1356,7 @@ oacc_loop_auto_partitions (oacc_loop *loop, unsigned outer_mask,
 	  this_mask ^= loop->e_mask;
 	}
 
+      this_mask = targetm.goacc.adjust_parallelism (this_mask, outer_mask);
       loop->mask |= this_mask;
     }
 
@@ -1396,7 +1404,9 @@ oacc_loop_auto_partitions (oacc_loop *loop, unsigned outer_mask,
 			" to parallelize element loop");
 	}
 
+      loop->mask = targetm.goacc.adjust_parallelism (loop->mask, outer_mask);
       loop->mask |= this_mask;
+
       if (!loop->mask && noisy)
 	warning_at (loop->loc, 0,
 		    tiling
@@ -1772,6 +1782,15 @@ default_goacc_dim_limit (int ARG_UNUSED (axis))
 #else
   return 1;
 #endif
+}
+
+/* Default adjustment of loop parallelism is not required.  */
+
+unsigned
+default_goacc_adjust_parallelism (unsigned this_mask,
+				  unsigned ARG_UNUSED (outer_mask))
+{
+  return this_mask;
 }
 
 namespace {
--- gcc/targhooks.h
+++ gcc/targhooks.h
@@ -115,6 +115,7 @@ extern bool default_goacc_validate_dims (tree, int [], int);
 extern int default_goacc_dim_limit (int);
 extern bool default_goacc_fork_join (gcall *, const int [], bool);
 extern void default_goacc_reduction (gcall *);
+extern unsigned default_goacc_adjust_parallelism (unsigned, unsigned);
 
 /* These are here, and not in hooks.[ch], because not all users of
    hooks.h include tm.h, and thus we don't have CUMULATIVE_ARGS.  */
