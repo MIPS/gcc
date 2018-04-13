1ea20b5173dd07db2131a04ad9b06f152f3a4b48
Matthew Fortune <matthew.fortune@imgtec.com>
2018-04-13 16:08:20 +0100

    Pass mdsp/mno-dsp to the assembler for nanoMIPS
    gcc/
    	* config/mips/nanomips.h (ASM_SPEC): Add mdsp and mno-dsp.

--- gcc/config/mips/nanomips.h
+++ gcc/config/mips/nanomips.h
@@ -185,6 +185,7 @@ along with GCC; see the file COPYING3.  If not see
 %{.S:-mlegacyregs} \
 %{m32} %{m64} \
 %{march=*} \
+%{mdsp} %{mno-dsp} \
 %{mdspr3} %{mno-dspr3} \
 %{mmcu} %{mno-mcu} \
 %{meva} %{mno-eva} \
