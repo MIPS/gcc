0b45df1351b1146fb4d490ad5aeae2900cfbb30b
Tom de Vries <tom@codesourcery.com>
2018-04-16 13:24:30 +0200

    [libgomp, testsuite] Use dg-set-target-env-var instead of setenv
    2018-04-16  Tom de Vries  <tom@codesourcery.com>
    
    	* testsuite/libgomp.oacc-c-c++-common/loop-default-compile.c: Use
    	dg-set-target-env-var instead of setenv.
    	* testsuite/libgomp.oacc-c-c++-common/loop-default-runtime.c: Same.

--- libgomp/testsuite/libgomp.oacc-c-c++-common/loop-default-compile.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/loop-default-compile.c
@@ -2,14 +2,12 @@
 /* This code uses nvptx inline assembly guarded with acc_on_device, which is
    not optimized away at -O0, and then confuses the target assembler.
    { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
+/* { dg-set-target-env-var "GOMP_OPENACC_DIM" "8:8" } */
 
 #include "loop-default.h"
-#include <stdlib.h>
 
 int main ()
 {
   /* Environment should be ignored.  */
-  setenv ("GOMP_OPENACC_DIM", "8:8",  1);
-  
   return test_1 (16, 16, 32);
 }
--- libgomp/testsuite/libgomp.oacc-c-c++-common/loop-default-runtime.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/loop-default-runtime.c
@@ -2,13 +2,11 @@
 /* This code uses nvptx inline assembly guarded with acc_on_device, which is
    not optimized away at -O0, and then confuses the target assembler.
    { dg-skip-if "" { *-*-* } { "-O0" } { "" } } */
+/* { dg-set-target-env-var "GOMP_OPENACC_DIM" "8:16" } */
 
 #include "loop-default.h"
-#include <stdlib.h>
 
 int main ()
 {
-  setenv ("GOMP_OPENACC_DIM", "8:16", 1);
-
   return test_1 (8, 16, 32);
 }
