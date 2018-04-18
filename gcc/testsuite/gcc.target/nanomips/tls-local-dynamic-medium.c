e2879705492536d5dec48774985fb27f22ccbca1
Toma Tabacu <toma.tabacu@mips.com>
2018-04-18 14:50:51 +0100

    GT-299: Add traditional general dynamic TLS model to GCC (follow-up 2)
    gcc/testsuite/
    
      * gcc.target/nanomips/tls-global-dynamic-auto.c: Remove check for tab.
      * gcc.target/nanomips/tls-global-dynamic-large.c: Likewise.
      * gcc.target/nanomips/tls-local-dynamic-auto.c: Likewise.
      * gcc.target/nanomips/tls-local-dynamic-large.c: Likewise.
      * gcc.target/nanomips/tls-local-dynamic-medium.c: Likewise.

--- gcc/testsuite/gcc.target/nanomips/tls-local-dynamic-medium.c
+++ gcc/testsuite/gcc.target/nanomips/tls-local-dynamic-medium.c
@@ -7,7 +7,7 @@
 __thread int tls_var;
 
 /* { dg-final { scan-assembler "f_addr:.*\taddiu\\.w\t\\\$a0,\\\$gp,%tlsld\\(tls_var\\).*f_addr\n" } } */
-/* { dg-final { scan-assembler "f_addr:.*\t__tls_get_addr.*f_addr\n" } } */
+/* { dg-final { scan-assembler "f_addr:.*__tls_get_addr.*f_addr\n" } } */
 /* { dg-final { scan-assembler "f_addr:.*\taddiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%dtprel\\(tls_var\\).*f_addr\n" } } */
 int *
 f_addr ()
@@ -18,7 +18,7 @@ f_addr ()
 }
 
 /* { dg-final { scan-assembler "f_load:.*\taddiu\\.w\t\\\$a0,\\\$gp,%tlsld\\(tls_var\\).*f_load\n" } } */
-/* { dg-final { scan-assembler "f_load:.*\t__tls_get_addr.*f_load\n" } } */
+/* { dg-final { scan-assembler "f_load:.*__tls_get_addr.*f_load\n" } } */
 /* { dg-final { scan-assembler "f_load:.*\taddiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%dtprel\\(tls_var\\).*f_load\n" } } */
 /* { dg-final { scan-assembler "f_load:.*\tlw\t\\\$\[ast0-9\]+,0\\(\\\$\[ast0-9\]+\\).*f_load\n" } } */
 int
@@ -30,7 +30,7 @@ f_load ()
 }
 
 /* { dg-final { scan-assembler "f_store:.*\taddiu\\.w\t\\\$a0,\\\$gp,%tlsld\\(tls_var\\).*f_store\n" } } */
-/* { dg-final { scan-assembler "f_store:.*\t__tls_get_addr.*f_store\n" } } */
+/* { dg-final { scan-assembler "f_store:.*__tls_get_addr.*f_store\n" } } */
 /* { dg-final { scan-assembler "f_load:.*\taddiu\t\\\$\[ast0-9\]+,\\\$\[ast0-9\]+,%dtprel\\(tls_var\\).*f_load\n" } } */
 /* { dg-final { scan-assembler "f_store:.*\tsw\t\\\$\[ast0-9\]+,0\\(\\\$\[ast0-9\]+\\).*f_store\n" } } */
 int
