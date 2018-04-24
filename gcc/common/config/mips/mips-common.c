cce350a1c46faead32d17fc0a816a53570fe34e3
Matthew Fortune <matthew.fortune@imgtec.com>
2018-04-24 10:22:31 +0100

    | Enable PIC by default for nanomips-linux-musl
    The nanoMIPS ABI is designed to assume PIC code as standard which is
    then relaxed to position dependent if appropriate for a given link.
    
    /
    	* config/picflag.m4: Assume PIC is default for nanoMIPS.
    gcc/
    	* common/config/mips/mips-common.c
    	(mips_option_optimization_table): Enable PIC for all levels.
    	* configure: Regenerate.

--- gcc/common/config/mips/mips-common.c
+++ gcc/common/config/mips/mips-common.c
@@ -63,6 +63,7 @@ static const struct default_options mips_option_optimization_table[] =
 #ifdef NANOMIPS_SUPPORT
     { OPT_LEVELS_SIZE, OPT_freorder_blocks_edge_sort_, NULL, RB_EDGE_SORT_1 },
     { OPT_LEVELS_SIZE, OPT_mcheck_zero_division, NULL, 0 },
+    { OPT_LEVELS_ALL, OPT_fpic, NULL, 1 },
 #endif
     { OPT_LEVELS_NONE, 0, NULL, 0 }
   };
