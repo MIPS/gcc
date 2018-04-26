77ee271e7095ae245b2c048a3b7b74c138b27105
Toma Tabacu <toma.tabacu@mips.com>
2018-04-26 08:57:00 +0100

    | GT-260: Use %got_call instead of %got_disp for medium model PIC calls.
    gcc/
    
      * config/mips/mips.c (mips_classify_symbol_1): Update condition for %got_call
      and tweak handling of long calls.
      (mips_find_pic_call_symbol): Search for a call's symbol_ref in its
      REG_CALL_DECL note instead of relying on a previously emitted %got_call load.
      (mips_annotate_pic_calls): Remove check for the automatic model and simplify
      'if' condition.
    
    gcc/testsuite/
    
      * gcc.target/nanomips/nanomips-models-func-extern-medium-nmf.c: Check for
      %got_call instead of %got_disp.
      * gcc.target/nanomips/nanomips-models-func-extern-medium-nms.c: Likewise.

--- gcc/testsuite/gcc.target/nanomips/nanomips-models-func-extern-medium-nmf.c
+++ gcc/testsuite/gcc.target/nanomips/nanomips-models-func-extern-medium-nmf.c
@@ -2,18 +2,21 @@
 /* { dg-options "-m32 -march=32r6 -mcmodel=large -fpic -mpcrel" } */
 /* { dg-skip-if "" { *-*-* } { "-O1" "-O2" "-O3" "-Os" } { "" } } */
 
-/* lw reg, %got_disp(f_extern)($gp) # 1 */
+/* lw reg, %got_call(f_extern)($gp) */
+/* .reloc 1f, R_NANOMIPS_JALR16, f_extern */
 /* jalrc reg # 1 */
 
-/* lw reg, %got_disp(f_extern)($gp) # 2 */
+/* lw reg, %got_disp(f_extern)($gp) # 1 */
 /* lapc.b reg, f_takes_fptr */
 /* jalrc reg # 2 */
 
-/* lw reg, %got_disp(f_extern)($gp) # 3 */
+/* lw reg, %got_disp(f_extern)($gp) # 2 */
 /* lapc.b reg, f_long_takes_fptr */
 /* jalrc reg # 3 */
 
-/* { dg-final { scan-assembler-times "lw\t\\\$\[ast0-9\]+,%got_disp\\(f_extern\\)\\(\\\$gp\\)" 3 } } */
+/* { dg-final { scan-assembler "lw\t\\\$\[ast0-9\]+,%got_call\\(f_extern\\)\\(\\\$gp\\)" } } */
+/* { dg-final { scan-assembler "\\\.reloc\t1f,R_NANOMIPS_JALR16,f_extern" } } */
+/* { dg-final { scan-assembler-times "lw\t\\\$\[ast0-9\]+,%got_disp\\(f_extern\\)\\(\\\$gp\\)" 2 } } */
 /* { dg-final { scan-assembler "lapc.b\t\\\$\[ast0-9\]+,f_takes_fptr" } } */
 /* { dg-final { scan-assembler "lapc.b\t\\\$\[ast0-9\]+,f_long_takes_fptr" } } */
 
