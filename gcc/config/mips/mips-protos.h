0dbfeef67456c6af8aa0f1211389aaaf673aff12
Zoran Jovanovic <zoran.jovanovic@rt-rk.com>
2018-06-15 17:22:06 +0200

    gcc/     * config/mips/mips.c (mips_bit_clear_info): New argument mode added.     Number of loop iterations changed from 32 to mode number of bits.     (mips_bit_clear_info): Number of loop iteration changed from 32     to (UNITS_PER_WORD * BITS_PER_UNIT).

--- gcc/config/mips/mips-protos.h
+++ gcc/config/mips/mips-protos.h
@@ -395,5 +395,6 @@ extern void mips_register_frame_header_opt (void);
 extern void mips_expand_vec_cond_expr (machine_mode, machine_mode, rtx *);
 
 extern bool mips_bit_clear_p (enum machine_mode, unsigned HOST_WIDE_INT);
-extern void mips_bit_clear_info (unsigned HOST_WIDE_INT, int *, int *);
+extern void mips_bit_clear_info (enum machine_mode, unsigned HOST_WIDE_INT,
+				 int *, int *);
 #endif /* ! GCC_MIPS_PROTOS_H */
