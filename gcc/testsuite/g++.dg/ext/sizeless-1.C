d3758abd38d3b7bcfa8db15dee5521aa38f0f6bf
Richard Sandiford <richard.sandiford@arm.com>
2018-09-28 19:04:15 +0100

    sizeless C++: initial test
    This patch adds a new test for the (very small) number of sizeless
    type uses that work without modification.
    
    2019-04-04  Richard Sandiford  <richard.sandiford@arm.com>
    
    gcc/testsuite/
    	* g++.dg/ext/sizeless-1.C: New test.

---
+++ gcc/testsuite/g++.dg/ext/sizeless-1.C
@@ -0,0 +1,16 @@
+typedef __sizeless_1 ta;
+
+// Pointers to sizeless types.
+
+ta *global_ta_ptr;
+
+// Sizeless arguments and return values.
+
+ta ext_produce_ta ();
+
+// Main tests for statements and expressions.
+
+void
+statements (int n)
+{
+}
