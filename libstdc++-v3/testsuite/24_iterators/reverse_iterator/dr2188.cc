28477d96c28e9e6f644509b5a58ffc1aa0e3e68e
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2018-11-09 12:03:50 +0000

    Merge branches/gcc-6-branch rev 265890.
    Change-Id: I455aeaee1501d788ce5c2059ca82fb7a66ee2fca

--- libstdc++-v3/testsuite/24_iterators/reverse_iterator/dr2188.cc
+++ libstdc++-v3/testsuite/24_iterators/reverse_iterator/dr2188.cc
@@ -1,7 +1,4 @@
-// { dg-do compile }
-// { dg-options "-std=gnu++14" }
-
-// Copyright (C) 2014-2016 Free Software Foundation, Inc.
+// Copyright (C) 2018 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -18,14 +15,33 @@
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-#include <chrono>
+// { dg-do run { target c++11 } }
+
+// PR libstdc++/86734
+
+#include <iterator>
+#include <testsuite_hooks.h>
 
 void
 test01()
 {
-  using namespace std::literals::chrono_literals;
+  // LWG DR 2188
+  // Reverse iterator does not fully support targets that overload operator&
+  struct X {
+    int val;
+    int* operator&() { return &val; }
+    const int* operator&() const { return &val; }
+  };
 
-  // std::numeric_limits<int64_t>::max() == 9223372036854775807;
-  auto h = 9223372036854775808h;
-  // { dg-error "cannot be represented" "" { target *-*-* } 798 }
+  X x[2] = { {1}, {2} };
+  std::reverse_iterator<X*> rev(x+2);
+  VERIFY( rev->val == 2 );
+  ++rev;
+  VERIFY( rev->val == 1 );
+}
+
+int
+main()
+{
+  test01();
 }
