9bb505b0262d28bc7458605507667c195dbe8444
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-02-15 10:25:50 -0800

    Skip no-PIC default test for -fpic/fPIC runs
    gcc/testsuite/
    	* gcc.target/nanomips/nanomips-module-default-pic0.c: Skip
    	for -fpic/-fPIC runs.

--- gcc/testsuite/gcc.target/nanomips/nanomips-module-default-pic0.c
+++ gcc/testsuite/gcc.target/nanomips/nanomips-module-default-pic0.c
@@ -1,6 +1,7 @@
 /* Default for bare metal is no pic directive.  */
 /* { dg-do compile { target nanomips*-*-elf } }  */
 /* { dg-options "-march=32r6" }  */
+/* { dg-skip-if "" { *-*-* } { "-fpic" "-fPIC" } { "" } } */
 /* { dg-final { scan-assembler-not "\\\.module\tpic\[012\]" } }  */
 
 void foo (void) { }
