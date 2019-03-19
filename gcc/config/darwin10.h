a44357fc023c9a1901a43fc16ccd942dd8dfed06
Iain Sandoe <iain@sandoe.co.uk>
2019-03-19 08:27:37 +0000

    backport 263765/263766 use a shim crt for the Darwin10 Unwinder fix.
    gcc/
    
    	Backport from mainline
    	2018-08-22  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/darwin10.h (LINK_GCC_C_SEQUENCE_SPEC): Adjust to use the
    	Darwin10-specific unwinder-shim.
    	* config/darwin12.h (LINK_GCC_C_SEQUENCE_SPEC): Remove.
    	* config/rs6000/darwin.h (DARWIN_CRT1_SPEC, DARWIN_DYLIB1_SPEC):
    	New to cater for Darwin10 Rosetta.
    
    libgcc/
    
    	Backport from mainline
    	018-08-22  Iain Sandoe  <iain@sandoe.co.uk>
    
    	* config/unwind-dw2-fde-darwin.c
    	(_darwin10_Unwind_FindEnclosingFunction): move from here ...
    	* config/darwin10-unwind-find-enc-func.c: … to here.
    	* config/t-darwin: Build Darwin10 unwinder shim crt.
    	* libgcc/config.host: Add the Darwin10 unwinder shim.

--- gcc/config/darwin10.h
+++ gcc/config/darwin10.h
@@ -25,10 +25,12 @@ along with GCC; see the file COPYING3.  If not see
 #undef  LINK_GCC_C_SEQUENCE_SPEC
 #define LINK_GCC_C_SEQUENCE_SPEC \
 "%:version-compare(>= 10.6 mmacosx-version-min= -no_compact_unwind) \
+   %{!nostdlib:%:version-compare(>< 10.6 10.7 mmacosx-version-min= -ld10-uwfef.o)} \
    %{!static:%{!static-libgcc: \
       %:version-compare(>= 10.6 mmacosx-version-min= -lSystem) } } \
    %{fno-pic|fno-PIC|fno-pie|fno-PIE|fapple-kext|mkernel|static|mdynamic-no-pic: \
-      %:version-compare(>= 10.7 mmacosx-version-min= -no_pie) } %G %L"
+      %:version-compare(>= 10.7 mmacosx-version-min= -no_pie) } \
+   %G %L"
 
 #undef DEF_MIN_OSX_VERSION
 #define DEF_MIN_OSX_VERSION "10.6"
