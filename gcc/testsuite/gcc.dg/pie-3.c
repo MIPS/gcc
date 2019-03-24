5b99a5634c894c3651b15270e7fce2bb9c46383d
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-03-24 14:12:55 -0700

    Test update: XFAIL some pie tests for PIC-default multilibs
    These tests are written to fail if PIC is enabled even without
    an explicit option being passed to the compiler, so better to
    XFAIL them in that situation.
    
    gcc/testsuite/
    	* gcc.dg/pie-3.c: XFAIL for PIC mode.
    	* gcc.dg/pie-4.c: Likewise.

--- gcc/testsuite/gcc.dg/pie-3.c
+++ gcc/testsuite/gcc.dg/pie-3.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { ! { *-*-darwin* hppa*64*-*-* } } } } */
 /* { dg-options "-fno-pie" } */
 /* { dg-options "-fno-pie -mno-abicalls" { target mips*-*-* } } */
+/* { dg-xfail-if "FAIL if PIC is enabled by default" { ! nonpic } } */
 
 #ifdef __PIC__
 # error __PIC__ is defined!
--- gcc/testsuite/gcc.dg/pie-4.c
+++ gcc/testsuite/gcc.dg/pie-4.c
@@ -1,6 +1,7 @@
 /* { dg-do compile { target { ! { *-*-darwin* hppa*64*-*-* } } } } */
 /* { dg-options "-fno-PIE" } */
 /* { dg-options "-fno-PIE -mno-abicalls" { target mips*-*-* } } */
+/* { dg-xfail-if "FAIL if PIC is enabled by default" { ! nonpic } } */
 
 #ifdef __PIC__
 # error __PIC__ is defined!
