1329437116384b5ace63e78ca4a29339d451449b
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-03-26 07:44:09 -0700

    Testsuite update - mark some tests as incompatible for -mpid
    gcc/testsuite/
    	* gcc.target/nanomips/length-2.c: Skip for -mpid.
    	* gcc.target/nanomips/length-3.c: Likewise.
    	* gcc.target/nanomips/length-4.c: Likewise.

--- gcc/testsuite/gcc.target/nanomips/length-2.c
+++ gcc/testsuite/gcc.target/nanomips/length-2.c
@@ -1,5 +1,6 @@
 /* { dg-options "-dp -mno-gpopt -mcmodel=medium -march=nms1" } */
 /* { dg-final { scan-assembler "\[^\n\]*movsi_nanomips/7\[^\n\]*length = 4" } } */
+/* { dg-xfail-if "incompatible with PID mode" { *-*-* } { "-mpid" } { "" } } */
 
 int g;
 
--- gcc/testsuite/gcc.target/nanomips/length-4.c
+++ gcc/testsuite/gcc.target/nanomips/length-4.c
@@ -1,5 +1,6 @@
 /* { dg-options "-dp -mno-gpopt -mcmodel=medium -mno-pcrel -fno-pic" } */
 /* { dg-final { scan-assembler "\[^\n\]*movsi_nanomips/7\[^\n\]*length = 6" } } */
+/* { dg-xfail-if "incompatible with PID mode" { *-*-* } { "-mpid" } { "" } } */
 
 int g;
 
