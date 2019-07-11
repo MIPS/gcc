57e7b455a7a0de757ce72709d6074f2c7fb5acab
Jason Merrill <jason@redhat.com>
2019-07-11 14:34:36 -0400

    Fix g++.dg/cpp1y/auto-neg1.C.
        * typeck2.c (build_functional_cast): Don't rely on the location of
        'auto'.

--- gcc/cp/typeck2.c
+++ gcc/cp/typeck2.c
@@ -2220,8 +2220,7 @@ build_functional_cast (tree exp, tree parms, tsubst_flags_t complain)
       if (!CLASS_PLACEHOLDER_TEMPLATE (anode))
 	{
 	  if (complain & tf_error)
-	    error_at (DECL_SOURCE_LOCATION (TEMPLATE_TYPE_DECL (anode)),
-		      "invalid use of %qT", anode);
+	    error ("invalid use of %qT", anode);
 	  return error_mark_node;
 	}
       else if (!parms)
