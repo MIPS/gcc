2d2a326d8f578fd3fead5bf74e48ac639220662b
David Malcolm <dmalcolm@redhat.com>
2019-11-18 21:06:27 -0500

    [analyzer] Fix issues in diagnostic_manager::prune_path
    There was a bad upper limit in diagnostic_manager::prune_path's test for
    pruning redundant interprocedural pass information, leading to an ICE.
    
    Additional, the test for pruning
      [..., call, function-entry, return, ...]
    triples doesn't work for -fanalyzer-verbosity=0, which doesn't generate
    function-entry events, leading to cases where -fanalyzer-verbosity=0 could
    be more verbose than higher limits, e.g.:
    
      'test_3': events 1-4
        |
        |   NN |   free (ptr);
        |      |   ^~~~~~~~~~
        |      |   |
        |      |   (1) first 'free' here
        |   NN |   called_by_test_3 ();
        |      |   ~~~~~~~~~~~~~~~~~~~
        |      |   |
        |      |   (2) calling 'called_by_test_3' from 'test_3'
        |      |   (3) returning to 'test_3' from 'called_by_test_3'
        |   NN |   free (ptr); /* { dg-warning "double-'free' of 'ptr'" } */
        |      |   ~~~~~~~~~~
        |      |   |
        |      |   (4) second 'free' here; first 'free' was at (1)
        |
    
    where the call/return would be pruned at higher verbosities.
    
    This patch fixes the limits, and prunes the case of a [call, return] pair.
    
    gcc/ChangeLog:
    	* analyzer/diagnostic-manager.cc (diagnostic_manager::prune_path):
    	When pruning [..., call, function-entry, return, ...] triples,
    	fix the bounds test and move it inside the loop.  Also test for
    	[..., call, return, ...] pairs.
    
    gcc/testsuite/ChangeLog:
    	* gcc.dg/analyzer/analyzer-verbosity-0.c: Add coverage for pruning
    	a call/return to an empty function.
    	* gcc.dg/analyzer/analyzer-verbosity-1.c: Likewise.
    	* gcc.dg/analyzer/analyzer-verbosity-2.c: Likewise.

--- gcc/testsuite/gcc.dg/analyzer/analyzer-verbosity-2.c
+++ gcc/testsuite/gcc.dg/analyzer/analyzer-verbosity-2.c
@@ -189,3 +189,33 @@ void test_2 (void *ptr, int a, int b)
   { dg-end-multiline-output "" } */
 
 // TODO: range cases
+
+/* The call/return to this function shouldn't appear in the path.  */
+
+void called_by_test_3 (void)
+{
+}
+
+void test_3 (void *ptr)
+{
+  free (ptr);
+  called_by_test_3 ();
+  free (ptr); /* { dg-warning "double-'free' of 'ptr'" } */
+}
+
+/* { dg-begin-multiline-output "" }
+   NN |   free (ptr);
+      |   ^~~~~~~~~~
+  'test_3': events 1-2
+    |
+    |   NN |   free (ptr);
+    |      |   ^~~~~~~~~~
+    |      |   |
+    |      |   (1) first 'free' here
+    |   NN |   called_by_test_3 ();
+    |   NN |   free (ptr);
+    |      |   ~~~~~~~~~~
+    |      |   |
+    |      |   (2) second 'free' here; first 'free' was at (1)
+    |
+  { dg-end-multiline-output "" } */
