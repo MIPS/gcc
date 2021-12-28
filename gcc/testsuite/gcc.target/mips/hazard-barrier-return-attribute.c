3b88d9d2c01624e974babe367c87be54e0e6dad5
Chao-ying Fu <cfu@wavecomp.com>
2021-12-28 13:03:24 -0800

    Hazard barrier return support. Port from this commit 3b6d51cd99c1f9a4fbef0872c9d51cfa4ec594b3. This patch is a slightly modified version of a patch posted to the GCC mailing list. It is not a backport of the final patch.
    2017-04-25  Prachi Godbole  <prachi.godbole@imgtec.com>
    
    gcc/
    	* config/mips/mips.h (machine_function): New variable
    	use_hazard_barrier_return_p.
    	* config/mips/mips.md (UNSPEC_JRHB): New unspec.
    	(mips_hb_return_internal): New insn pattern.
    	* config/mips/mips.c (mips_attribute_table): Add attribute
    	use_hazard_barrier_return.
    	(mips_use_hazard_barrier_return_p): New static function.
    	(mips_function_attr_inlinable_p): Likewise.
    	(mips_compute_frame_info): Set use_hazard_barrier_return_p.  Emit error
    	for unsupported architecture choice.
    	(mips_function_ok_for_sibcall, mips_can_use_return_insn): Return false
    	for use_hazard_barrier_return.
    	(mips_expand_epilogue): Emit hazard barrier return.
    	* doc/extend.texi: Document use_hazard_barrier_return.
    
    gcc/testsuite/
    	* gcc.target/mips/hazard-barrier-return-attribute.c: New test.

---
+++ gcc/testsuite/gcc.target/mips/hazard-barrier-return-attribute.c
@@ -0,0 +1,13 @@
+/* Test attribute for clearing hazards while returning.  */
+/* { dg-do compile } */
+/* { dg-options "isa_rev>=2 -mno-mips16" } */
+
+extern int bar ();
+
+int __attribute__ ((use_hazard_barrier_return))
+foo ()
+{
+  return bar ();
+}
+/* { dg-final { scan-assembler "(\tjr.hb\t\\\$31\n\tnop\\n|\tjrc.hb\t)" } } */
+
