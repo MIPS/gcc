1d2b6bc09d7f9ab4935c9d886b5c02c92a94af32
Chao-ying Fu <cfu@wavecomp.com>
2024-07-05 15:14:09 -0700

    Add a test for ldp with a pointer chasing loop.

---
+++ gcc/testsuite/gcc.target/riscv/mipsloadpair.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-fpointer-chasing -march=rv64gc_xmipslsp -mtune=mips-p8700" { target { rv64 } } } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-Os" "-O1" "-Og" "-g" "-Oz" } } */
+
+typedef struct mylist
+{
+  struct mylist *next;
+  long value;
+} MYLIST;
+
+MYLIST *test1(MYLIST *start, long value)
+{
+  while (start && start->value != value)
+    start = start->next;
+ 
+  return start;
+}
+
+/* { dg-final { scan-assembler-times "ldp" 1 } } */
