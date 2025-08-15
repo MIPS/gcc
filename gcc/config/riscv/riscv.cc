5e43321ba8298aab513e11435c9147a9cd3851ae
Umesh Kalappa <ukalappa.mips@gmail.com>
2025-08-15 07:35:40 -0600

    RISC-V: MIPS prefetch extensions for MIPS RV64 P8700 and can be enabled with xmipscbop.
    Addressed the comments and tested "runtest --tool gcc --target_board='riscv-sim/-march=rv64gc_zba_zbb_zbc_zbs/-mabi=lp64/-mcmodel=medlow' riscv.exp" and 32 bit too
    
    lint warnings can be ignored for riscv-ext.opt.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-ext-mips.def (DEFINE_RISCV_EXT):
    	Added mips prefetch extension.
    	* config/riscv/riscv-ext.opt: Generated file.
    	* config/riscv/riscv.md (prefetch):
    	Added mips prefetch address operand constraint.
    	* config/riscv/constraints.md: Added mips specific constraint.
    	* config/riscv/predicates.md (prefetch_operand):
    	Updated for mips nine bits offset.
    	* config/riscv/riscv.cc (riscv_prefetch_offset_address_p):
    	Legitimate address with offset for prefetch check.
    	* config/riscv/riscv-protos.h: Likewise.
    	* config/riscv/riscv.h:
    	Macros to support for mips cached type.
    	* doc/riscv-ext.texi: Updated for mips prefetch.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/mipsprefetch.c: Test file for mips.pref.

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -14120,6 +14120,37 @@ bool need_shadow_stack_push_pop_p ()
   return is_zicfiss_p () && riscv_save_return_addr_reg_p ();
 }
 
+/*
+    HINT : argument specify the target cache
+
+    TODO : LOCALITY is unused.
+
+    Return the first operand of the associated PREF or PREFX insn.  */
+rtx
+riscv_prefetch_cookie (rtx hint, rtx locality)
+{
+  return (GEN_INT (INTVAL (hint)
+		   + CacheHint::DCACHE_HINT + INTVAL (locality) * 0));
+}
+
+/* Return true if X is a legitimate address with offset for prefetch.
+   MODE is the mode of the value being accessed.  */
+bool
+riscv_prefetch_offset_address_p (rtx x, machine_mode mode)
+{
+  struct riscv_address_info addr;
+
+  if (riscv_classify_address (&addr, x, mode, false)
+      && addr.type == ADDRESS_REG)
+    {
+      if (TARGET_XMIPSCBOP)
+	return (CONST_INT_P (addr.offset)
+		&& MIPS_RISCV_9BIT_OFFSET_P (INTVAL (addr.offset)));
+    }
+
+  return true;
+}
+
 /* Initialize the GCC target structure.  */
 #undef TARGET_ASM_ALIGNED_HI_OP
 #define TARGET_ASM_ALIGNED_HI_OP "\t.half\t"
